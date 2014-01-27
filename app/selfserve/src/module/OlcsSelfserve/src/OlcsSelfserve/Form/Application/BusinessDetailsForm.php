faf4d5a0c5cecd362ead251f1da88c5afcab80e6
Shaun Lizzio <shaun@lizzio.co.uk>
2014-01-27 12:27:29 +0000

    Added translations Removed form classes Added company name and number

--- app/selfserve/src/module/OlcsSelfserve/src/OlcsSelfserve/Form/Application/BusinessDetailsForm.php
+++ app/selfserve/src/module/OlcsSelfserve/src/OlcsSelfserve/Form/Application/BusinessDetailsForm.php
@@ -25,15 +25,74 @@ class BusinessDetailsForm extends OlcsForm
             'name' => 'business[entityTypes]',
             'required' => true,
             'attributes' =>  array(
-                'class' => 'form-control multiselect',
+                'class' => '',
                 'id' => 'business[entityTypes]'
-                )
+                ),
+            'options' => array(
+                'label' => $this->getResourceString('your-business-form-label-type-of-business'),
+            ),
         ));
         
         $entityTypesElem = $this->get('business[entityTypes]');
         $entityTypeGroup = $this->getSelectResourceStrings(self::$entityTypesArray);
         $entityTypesElem->setValueOptions($this->setSelect($entityTypeGroup, 'Select type'));
 
+        $this->add(array(
+            'type' => 'Zend\Form\Element\Text',
+            'name' => 'business[companyNumber]',
+            'required' => true,
+            'attributes' =>  array(
+                'class' => '',
+                'id' => 'business[companyNumber]'
+                ),
+            'options' => array(
+                'label' => $this->getResourceString('your-business-form-label-registered-company-number'),
+            ),
+        ));
+        
+        $this->add(array(
+            'type' => 'Zend\Form\Element\Text',
+            'name' => 'business[companyName]',
+            'required' => true,
+            'attributes' =>  array(
+                'class' => '',
+                'id' => 'business[companyName]'
+                ),
+            'options' => array(
+                'label' => $this->getResourceString('your-business-form-label-registered-company-name'),
+            ),
+        ));
+                
+        $this->add(array(
+            'name' => 'business[tradingNameId]',
+            'type' => 'Text',
+            'attributes' => array(
+                'class' => '',
+                'id' => 'business[tradingNameId]',
+            )
+        ));
+
+        $this->add(array(
+            'type' => 'Zend\Form\Element\Collection',
+            'name' => 'business[tradingNames]',
+            'options' => array(
+                'count' => 0,
+                'target_element' => array(
+                    'type' => 'Zend\Form\Element\Hidden',
+                    'id' => 'business[tradingNames]',
+                ),
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'tradingAddAnother',
+            'type' => 'button',
+            'attributes' => array(
+                'class' => 'btn btn-add disabled',
+                'id' => 'tradingAddAnother',
+                'value' => 'Search'
+            )
+        ));
     }
         
     public function isValid() {
