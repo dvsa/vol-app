959be87191304f218fdb0f4aa307f9bd0c46b07d
Shaun Lizzio <shaun@lizzio.co.uk>
2014-02-04 12:23:38 +0000

    Added LLP and fixed toggling between entity types

--- app/selfserve/src/public/js/selfserve/application/businessDetails.js
+++ app/selfserve/src/public/js/selfserve/application/businessDetails.js
@@ -8,38 +8,86 @@ function defaultView()
 
 function showRegisteredCompany()
 {
+
+    $('label[for="operatorName"]').text('Registered company name:');
+    $('#business-company-details > h2').text('Registered company details:');
+    $('div#operatorNameField div.instruction-text p').text('Instruction text goes here');
+
+    $('#business-company-details').show();
+    $('#business-trading').show();
+    $('#business-types').show();
+}
+
+function showSoleTrader()
+{
+    $('#business-company-details').hide();
+
+    $('label[for="operatorName"]').text('Operator name:');
+    $('#business-company-details > h2').text('Sole trader details:');
+    $('div#operatorNameField div.instruction-text p').text('Instruction text goes here');
+
+    $('#business-trading').show();
+    $('#business-types').show();
+
+}
+
+function showPartnership()
+{
+    $('#business-trading').show();
+    $('#business-types').show();
+    $('#companyNumberField').hide();
+
+    $('label[for="operatorName"]').text('Partnership name:');
+    $('div#operatorNameField div.instruction-text p').text('e.g. John Smith & Partners');
+    $('#business-company-details > h2').text('Partnership details:');
+
+    $('#business-company-details').show();
+    $('#operatorNameField').show();
+
+}
+
+function showLLP()
+{
+    $('#business-trading').hide();
+    $('#business-types').hide();
+    $('#operatorNameField').hide();
+
+    $('#business-company-details > h2').text('LLP details:');
+    $('label[for="companyNumId"]').text('LLP number:');
+    $('div#operatorNameField div.instruction-text p').text('Instruction text goes here');
+
+    $('#business-company-details').show();
+    $('#companyNumberField').show();
+
+}
+
+function showPublicAuthority()
+{
+    $('#companyNumberField').hide();
+
+    $('label[for="operatorName"]').text('Public authority name:');
+    $('#business-company-details > h2').text('Public authority details:');
+    $('div#operatorNameField div.instruction-text p').text('Instruction text goes here');
+    
+    $('#operatorNameField').show();
     $('#business-company-details').show();
     $('#business-trading').show();
     $('#business-types').show();
     
 }
 
-function showSoleTrader()
-{
-    $('#business-trading').hide();
-    $('#business-types').hide();
-}
-
-function showPartnership()
-{
-    $('#business-trading').hide();
-    $('#business-types').hide();
-}
-
-function showPublicAuthority()
-{
-    $('#business-trading').hide();
-    $('#business-types').hide();
-}
-
 function showOther()
 {
-    $('#business-trading').hide();
-    $('#business-types').hide();
-    $('label[for="operatorName"]').text('Operator name:');
-    $('#business-company-details').show();
+
     $('#companyNumberField').hide();
     
+    $('label[for="operatorName"]').text('Organisation name:');
+    $('div#operatorNameField div.instruction-text p').text('Instruction text goes here');
+    $('#business-company-details > h2').text('Organisation details:');
+
+    $('#business-company-details').show();
+    $('#business-trading').show();
+    $('#business-types').show();    
 }
 
 function typeSelect() {
@@ -50,6 +98,9 @@ function typeSelect() {
                 break;
             case 'Sole Trader':
                 showSoleTrader();
+                break;
+            case 'LLP':
+                showLLP();
                 break;
             case 'Partnership':
                 showPartnership();
