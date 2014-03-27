03174efea5d41ef457438c65f720f78e6c779bd2
Rob Caiger <rob@clocal.co.uk>
2014-03-27 14:27:02 +0000

    removed vendors

--- app/internal/module/Olcs/src/Controller/SearchController.php
+++ app/internal/module/Olcs/src/Controller/SearchController.php
@@ -10,29 +10,30 @@ namespace Olcs\Controller;
 
 use Common\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-use Common\Exception\ResourceNotFoundException;
-use Common\Exception\BadRequestException;
-use OlcsEntities\Entity\User;
-use Common\Exception\ResourceConflictException;
 
 class SearchController extends AbstractActionController
 {
-    
+
     public function indexAction()
     {
-        $form = $this->getForm('search');
-        $callback = function() {
-            $this->redirect()->toRoute('styleguide', array('action' => 'typography'));
-        };
-        $form = $this->formPost($form, $callback);
+        $form = $this->generateFormFromGet(
+            'search',
+            function() {
+                $this->redirect()->toRoute('styleguide', array('action' => 'typography'));
+            },
+            'Person',
+            1
+        );
+
         $view = new ViewModel(['form' => $form]);
         $view->setTemplate('form');
         return $view;
     }
-    
-    protected function redirectUser() {
+
+    protected function redirectUser()
+    {
         print 'Doing something like a redirect';
         $this->redirect()->toRoute('olcsHome');
     }
-    
+
 }
