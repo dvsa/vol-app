4890609f181b627cc7a59c0879eb34f3376a7494
Henry Hayes <henryhayesuk@gmail.com>
2014-03-28 11:58:24 +0000

    All public assets removed

--- app/internal/public/js/olcs/init.js
+++
@@ -1,9 +0,0 @@
-var olcs = {};
-function runOlcsJavascript(olcsAppType, controller, action) {
-    $(document).ready(function () {
-        olcs['common'].run();
-        olcs[olcsAppType].run();
-        olcs[olcsAppType][controller].run();
-        olcs[olcsAppType][controller][action].run();
-    });
-}
