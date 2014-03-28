a1e6be0d972677a64a4b0561b6b227e302c6ce57
adminmwc <adminmwc@192.168.56.1>
2014-03-28 11:53:24 +0000

    moved translator config to common

--- app/internal/module/Olcs/config/module.config.php
+++ app/internal/module/Olcs/config/module.config.php
@@ -109,16 +109,6 @@ return array(
             )
         )
     ),
-    'translator' => array(
-        'locale' => 'en_US',
-        'translation_file_patterns' => array(
-            array(
-                'type'     => 'gettext',
-                'base_dir' => __DIR__ . '/../language',
-                'pattern'  => '%s.mo',
-            ),
-        ),
-    ),
     'controllers' => array(
         'invokables' => array(
             'DefaultController' => 'Olcs\Olcs\Placeholder\Controller\DefaultController',
