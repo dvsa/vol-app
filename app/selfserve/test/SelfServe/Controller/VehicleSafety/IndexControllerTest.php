31619a3b1ee74241acf6a94f4fc1dfb2420c1898
Shaun Lizzio <shaun@lizzio.co.uk>
2014-04-15 16:20:21 +0100

    Added integration tests for vehicle-safety indexController

---
+++ app/selfserve/test/SelfServe/Controller/VehicleSafety/IndexControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SelfServe\test\BusinessType;
+
+
+use PHPUnit_Framework_TestCase;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+
+class IndexControllerTest extends AbstractHttpControllerTestCase
+{
+
+    protected function setUp()
+    {
+        $this->setApplicationConfig(
+            include __DIR__.'/../../../../config/application.config.php'
+        );
+
+        parent::setUp();
+
+    }
+    
+    /**
+     * Test access to main index action
+     */
+    public function testIndexActionAccess()
+    { 
+        $this->dispatch('/selfserve/1/vehicle-safety/index');
+        
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('selfserve');
+        $this->assertControllerName('selfserve\vehiclessafety\index');
+        $this->assertControllerClass('IndexController');
+        $this->assertMatchedRouteName('selfserve/vehicles-safety');   
+    }
+    
+    /**
+     * Test press Add vehicle button
+     */
+    public function testPostAddVehicleAccess()
+    { 
+        $postData = array(
+            'action'  => 'add',
+        );
+        $this->dispatch('/selfserve/1/vehicle-safety/index', 'POST', $postData);
+        $this->assertResponseStatusCode(302);
+        $this->assertRedirectTo('/selfserve/1/vehicle/add');
+        
+    }
+    
+
+}
