d5beef9b68f80e01c8be8e7919cd4b5b74d2b771
Rob Caiger <rob@clocal.co.uk>
2014-04-22 14:07:07 +0100

    Appeal form config, controller, actions. Persisting of data and unit tests

---
+++ app/internal/module/Olcs/src/Form/Forms/appeal.form.php
@@ -0,0 +1,98 @@
+<?php
+
+return [
+    'appeal' => [
+        'name' => 'appeal',
+        'attributes' => [
+            'method' => 'post',
+        ],
+        'fieldsets' => [
+            [
+                'name' => 'details',
+                'options' => [
+                    'label' => 'Appeal Details'
+                ],
+                'elements' => [
+                    'deadlineDate' => [
+                        'type' => 'dateSelectWithEmpty',
+                        'label' => 'Appeal deadline',
+                        'required' => false
+                    ],
+                    'appealDate' => [
+                        'type' => 'dateSelectWithEmpty',
+                        'label' => 'Date of appeal',
+                        'required' => true
+                    ],
+                    'appealNumber' => [
+                        'label' => 'Appeal number',
+                        'type' => 'text',
+                        'filters' => '\Common\Form\Elements\InputFilters\TextMax20'
+                    ],
+                    'reason' => [
+                        'label' => 'Reason',
+                        'type' => 'select',
+                        'value_options' => 'appeal_reasons',
+                        'required' => true
+                    ],
+                    'outlineGround' => [
+                        'type' => 'text',
+                        'filters' => '\Common\Form\Elements\InputFilters\TextMax4000Required',
+                        'label' => 'Outline ground',
+                        'class' => 'extra-long'
+                    ],
+                    'hearingDate' => [
+                        'type' => 'dateSelectWithEmpty',
+                        'label' => 'Date of appeal hearing',
+                        'required' => false
+                    ],
+                    'decisionDate' => [
+                        'type' => 'dateSelectWithEmpty',
+                        'label' => 'Date of decision',
+                        'required' => false
+                    ],
+                    'papersDue' => [
+                        'type' => 'dateSelectWithEmpty',
+                        'label' => 'Papers due at tribunal',
+                        'required' => false
+                    ],
+                    'papersSent' => [
+                        'type' => 'dateSelectWithEmpty',
+                        'label' => 'Papers sent date',
+                        'required' => false
+                    ],
+                    'outcome' => [
+                        'label' => 'Outcome',
+                        'type' => 'select',
+                        'value_options' => 'appeal_outcomes',
+                        'required' => true
+                    ],
+                    'comment' => [
+                        'type' => 'text',
+                        'filters' => '\Common\Form\Elements\InputFilters\TextMax4000Required',
+                        'label' => 'Comments',
+                        'class' => 'extra-long'
+                    ]
+                ]
+            ]
+        ],
+        'elements' => [
+            'case' => [
+                'type' => 'hidden'
+            ],
+            'id' => [
+                'type' => 'hidden'
+            ],
+            'version' => [
+                'type' => 'hidden'
+            ],
+            'crsf' => [
+                'type' => 'crsf',
+            ],
+            'submit' => [
+                'type' => 'submit',
+                'label' => 'Save',
+                'class' => 'action--primary large'
+            ]
+        ]
+    ]
+];
