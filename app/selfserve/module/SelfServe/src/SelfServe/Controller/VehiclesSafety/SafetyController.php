6394b37830fb6806fdbe157fa4aef85a5563030e
Rob Caiger <rob@clocal.co.uk>
2014-04-28 09:48:07 +0100

    Controller actions finished for Vehicle Safety (Safety), tweaks to the form config

--- app/selfserve/module/SelfServe/src/SelfServe/Controller/VehiclesSafety/SafetyController.php
+++ app/selfserve/module/SelfServe/src/SelfServe/Controller/VehiclesSafety/SafetyController.php
@@ -24,9 +24,36 @@ class SafetyController extends FormJourneyActionController
      */
     public function indexAction()
     {
-        $data = array();
+        $applicationId = $this->params()->fromRoute('applicationId');
 
-        $form = $this->generateFormWithData('vehicle-safety', 'processVehicleSafety', $data);
+        $bundle = array(
+            'properties' => array(
+                'version',
+                'safetyConfirmation'
+            ),
+            'children' => array(
+                'licence' => array(
+                    'properties' => array(
+                        'id',
+                        'version',
+                        'safetyInsVehicles',
+                        'safetyInsTrailers',
+                        'safetyInsVaries',
+                        'tachographIns',
+                        'tachographInsName'
+                    )
+                )
+            )
+        );
+
+        $data = $this->makeRestCall('Application', 'GET', array('id' => $applicationId), $bundle);
+
+        $form = $this->generateFormWithData(
+            'vehicle-safety',
+            'processVehicleSafety',
+            array('data' => $this->formatDataForForm($data)),
+            true
+        );
 
         $view = $this->getViewModel(['form' => $form]);
         $view->setTemplate('self-serve/vehicle-safety/safety');
@@ -35,16 +62,81 @@ class SafetyController extends FormJourneyActionController
     }
 
     /**
+     * Format data for the form
+     *
+     * @param array $data
+     * @return array
+     */
+    protected function formatDataForForm($data)
+    {
+        $data['id'] = $applicationId;
+
+        foreach ($data['licence'] as $key => $val) {
+            $data['licence.' . $key] = $val;
+        }
+
+        unset($data['licence']);
+
+        $data['safetyConfirmation'] = (
+            !isset($data['safetyConfirmation']) || $data['safetyConfirmation'] == 0
+            ? array()
+            : array('1')
+        );
+
+        $data['licence.safetyInsVehicles'] = 'inspection_interval_vehicle.' . $data['licence.safetyInsVehicles'];
+
+        $data['licence.safetyInsTrailers'] = 'inspection_interval_trailer.' . $data['licence.safetyInsTrailers'];
+
+        $data['licence.tachographIns'] = 'tachograph_analyser.' . $data['licence.tachographIns'];
+
+        return $data;
+    }
+
+    /**
      * Process the vehicle safety form
      *
      * @param array $data
      */
-    private function processVehicleSafety($data)
+    protected function processVehicleSafety($data)
     {
-        print '<pre>';
-        print_r($data);
-        print '</pre>';
-        exit;
+        $applicationId = $this->params()->fromRoute('applicationId');
+
+        $applicationData = array(
+            'id' => $applicationId,
+            'version' => $data['data']['version'],
+            'safetyConfirmation' => $data['data']['safetyConfirmation'][0]
+        );
+
+        $licenceData = array();
+
+        foreach ($data['data'] as $key => $value) {
+
+            if (strstr($key, 'licence.')) {
+
+                $licenceData[str_replace('licence.', '', $key)] = $value;
+            }
+        }
+
+        $licenceData['safetyInsVehicles'] = str_replace(
+            'inspection_interval_vehicle.',
+            '',
+            $licenceData['safetyInsVehicles']
+        );
+
+        $licenceData['safetyInsTrailers'] = str_replace(
+            'inspection_interval_trailer.',
+            '',
+            $licenceData['safetyInsTrailers']
+        );
+
+        $licenceData['tachographIns'] = str_replace('tachograph_analyser.', '', $licenceData['tachographIns']);
+
+        $this->makeRestCall('Application', 'PUT', $applicationData);
+
+        $this->makeRestCall('Licence', 'PUT', $licenceData);
+
+        // @todo Redirect to previous history when this page is created
+        return $this->redirect()->toRoute(null, array('applicationId' => $applicationId));
     }
 
     protected function completeAction()
--- app/selfserve/module/SelfServe/src/SelfServe/Form/Forms/vehicle-safety.form.php
+++ app/selfserve/module/SelfServe/src/SelfServe/Form/Forms/vehicle-safety.form.php
@@ -13,49 +13,58 @@ return array(
                 'name' => 'data',
                 'options' => array(0),
                 'elements' => array(
-                    'safety_ins_vehicles' => array(
+                    'licence.safetyInsVehicles' => array(
                         'type' => 'select',
                         'label' => $translationPrefix . 'vehicleInspectionInterval',
                         'value_options' => 'inspection_interval_vehicle',
                         'required' => true
                     ),
-                    'safety_ins_trailers' => array(
+                    'licence.safetyInsTrailers' => array(
                         'type' => 'select',
                         'label' => $translationPrefix . 'trailerInspectionInterval',
                         'value_options' => 'inspection_interval_trailer',
                         'required' => true
                     ),
-                    'safety_ins_varies' => array(
+                    'licence.safetyInsVaries' => array(
                         'type' => 'yesNoRadio',
                         'label' => $translationPrefix . 'moreFrequentInspections',
                         'required' => true
                     ),
-                    'tachograph_ins' => array(
+                    'licence.tachographIns' => array(
                         'type' => 'radio',
                         'label' => $translationPrefix . 'tachographAnalyser',
                         'value_options' => 'tachograph_analyser',
                         'required' => true
                     ),
-                    'tachograph_ins_name' => array(
+                    'licence.tachographInsName' => array(
                         'type' => 'text',
                         'filters' => '\Common\Form\Elements\InputFilters\VehicleSafetyTachographAnalyserContractor',
                         'label' => $translationPrefix . 'tachographAnalyserContractor'
                     ),
-                    'safety_confirmation' => array(
+                    'safetyConfirmation' => array(
                         // @todo See if we can make this a checkbox
                         'type' => 'multicheckbox',
                         'label' => 'Confirm',
                         'value_options' => array(
-                            $translationPrefix . 'safetyConfirmation'
+                            '1' => $translationPrefix . 'safetyConfirmation'
                         )
+                    ),
+                    'id' => array(
+                        'type' => 'hidden'
+                    ),
+                    'version' => array(
+                        'type' => 'hidden'
+                    ),
+                    'licence.version' => array(
+                        'type' => 'hidden'
+                    ),
+                    'licence.id' => array(
+                        'type' => 'hidden'
                     )
                 )
             )
         ),
         'elements' => array(
-            'version' => array(
-                'type' => 'hidden'
-            ),
             'submit' => array(
                 'type' => 'submit',
                 'label' => 'Continue',
