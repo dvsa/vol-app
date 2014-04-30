fb6cddbfb6c9de1d81efa5691f614d3253590bed
Ian.Lindsay <Ian.Lindsay@valtech.co.uk>
2014-04-30 18:18:29 +0100

    Penalty form

---
+++ app/internal/module/Olcs/src/Form/Forms/penalty-comment.form.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'penalty-comment' => [
+        'name' => 'penalty-comment',
+        'attributes' => [
+            'method' => 'post',
+        ],
+        'elements' => [
+            'notes' => [
+                        'type'  => 'textarea',
+                        'label' => 'Enter penalties',
+                        'class' => 'extra-long'
+                    ],
+            'id' => [
+                'type' => 'hidden'
+            ],
+            'version' => [
+                'type' => 'hidden'
+            ],
+            'crsf' => [
+                'type' => 'crsf',
+            ],
+            'submit' => [
+                'type' => 'submit',
+                'label' => 'Save',
+                'class' => 'action--primary large'
+            ]
+        ]
+    ]
+];
