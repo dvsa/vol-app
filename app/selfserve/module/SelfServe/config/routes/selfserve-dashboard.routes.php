a1adaa42e0031c042c2f1d88bcee6eac1b45e84b
Rob Caiger <rob@clocal.co.uk>
2014-05-20 13:11:37 +0100

    Merged and fixed dashboard routes

---
+++ app/selfserve/module/SelfServe/config/routes/selfserve-dashboard.routes.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'dashboard' => array(
+        'type' => 'segment',
+        'options' => array(
+            'route' => '/dashboard[/user/:userId]',
+            'defaults' => array(
+                'controller' => 'Selfserve\Dashboard\Index',
+                'action' => 'index'
+            )
+        ),
+    ),
+    'new-licence' => array(
+        'type' => 'literal',
+        'options' => array(
+            'route' => '/dashboard/application/create',
+            'defaults' => array(
+                'controller' => 'Selfserve\Dashboard\Index',
+                'action' => 'createApplication'
+            )
+        ),
+    )
+];
