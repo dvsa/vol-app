3dad11a02a319ce0a3505f5dd2f2cf533fc3d5f7
Nick Payne <nick@kurai.co.uk>
2014-06-04 15:37:20 +0100

    ensure correct target JS file is built

--- app/internal/Gruntfile.js
+++ app/internal/Gruntfile.js
@@ -1,1 +1,1 @@
-module.exports = require("./vendor/olcs/OlcsCommon/grunt-base")(["module/Olcs/assets/js/src/**/*.js"]);
+module.exports = require("./vendor/olcs/OlcsCommon/grunt-base")(["module/Olcs/assets/js/src/**/*.js"], "internal");
