646d412874cd3c38b0a79401fbb7529f2e082042
Jess Rowbottom <jess@wrenthorpe.net>
2014-07-29 15:45:43 +0100

    Added in Undertakings section.

--- app/selfserve/module/SelfServe/config/journeys/application.journey.php
+++ app/selfserve/module/SelfServe/config/journeys/application.journey.php
@@ -163,6 +163,11 @@ return array(
                         )
 
                     ),
+                    'Undertakings' => array(
+                        'restriction' => array(
+                            'psv'
+                        )
+                    ),
                     'Safety' => array(
 
                     )
