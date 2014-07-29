ce0c2b42cf77432aec65bb5a717f79dd892c3e84
Jess Rowbottom <jess@wrenthorpe.net>
2014-07-29 15:51:24 +0100

    Added vehicle safety undertakings for stub.

---
+++ app/selfserve/module/SelfServe/src/SelfServe/Form/Forms/application_vehicle-safety_undertakings.form.php
@@ -0,0 +1,17 @@
+<?php
+
+$translationPrefix = 'application_vehicle-safety_undertakings';
+
+return array(
+    $translationPrefix => array(
+        'name' => $translationPrefix,
+        'attributes' => array(
+            'method' => 'post',
+        ),
+        'fieldsets' =>  array(
+            array(
+                'type' => 'journey-buttons'
+            )
+        )
+    )
+);
