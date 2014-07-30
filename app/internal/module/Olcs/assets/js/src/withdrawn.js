cc0004fd7b4f730de15b7eac45fc0c691c9d1994
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-07-30 18:32:09 +0100

    withdrawn js

---
+++ app/internal/module/Olcs/assets/js/src/withdrawn.js
@@ -0,0 +1,20 @@
+function checkWithdrawn() {
+    if ($('#isWithdrawn').prop('checked')) {
+        toggleWithdrawnDate('show');
+    } else {
+        toggleWithdrawnDate('hide');
+    }
+}
+
+function toggleWithdrawnDate(action) {
+    if (action === 'show') {
+        $( "select[name*='[withdrawnDate]']" ).show();
+        $('label[for="withdrawnDate"]').show();
+    } else {
+        $( "select[name*='[withdrawnDate]']" ).hide();
+        $('label[for="withdrawnDate"]').hide();
+    }
+}
+
+
+
