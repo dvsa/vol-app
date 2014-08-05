161f63b844e35e760fa0ef90784a744a85a43316
Rob Caiger <rob@clocal.co.uk>
2014-08-05 09:13:05 +0100

    Removed entities from selfserve

--- app/selfserve/config/application.config.php
+++ app/selfserve/config/application.config.php
@@ -5,14 +5,12 @@ return array(
         'Common',
         'DoctrineModule',
         'DoctrineORMModule',
-        'OlcsEntities',
         'SelfServe'
     ),
     'module_listener_options' => array(
         'module_paths' => array(
             __DIR__ . '/../module',
             __DIR__ . '/../vendor',
-            __DIR__ . '/../vendor/olcs/olcs-entities',
             __DIR__ . '/../vendor/olcs/OlcsCommon'
         ),
         'config_glob_paths' => array(
