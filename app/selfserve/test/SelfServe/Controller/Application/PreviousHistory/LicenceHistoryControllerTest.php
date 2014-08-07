448a9a103ac32a514936fd4163d674b747fed34b
Alex P <alex.peshkov@valtech.co.uk>
2014-08-07 10:49:32 +0100

    olcs-2235 unit tests / temporary commit

--- app/selfserve/test/SelfServe/Controller/Application/PreviousHistory/LicenceHistoryControllerTest.php
+++ app/selfserve/test/SelfServe/Controller/Application/PreviousHistory/LicenceHistoryControllerTest.php
@@ -21,7 +21,7 @@ class LicenceHistoryControllerTest extends AbstractApplicationControllerTestCase
     protected $controllerName =  '\SelfServe\Controller\Application\PreviousHistory\LicenceHistoryController';
 
     protected $defaultRestResponse = array();
-
+    protected $call=1;
     /**
      * Test back button
      */
@@ -49,15 +49,67 @@ class LicenceHistoryControllerTest extends AbstractApplicationControllerTestCase
 
     /**
      * Test indexAction with submit
+     * @group acurrent
      */
     public function testIndexActionWithSubmit()
     {
-        $this->setUpAction('index', null, array('foo' => 'bar'));
-
+        $this->setUpAction(
+            'index',
+            null,
+            array(
+                'dataLicencesCurrent' => array(
+                    'id' => '',
+                    'version' => '1',
+                    'currentLicence' => 'Y'
+                ),
+                'table-licences-current' => array(
+                    'rows' => 1
+                ),
+                'dataLicencesApplied' => array(
+                    'appliedForLicence' => 'Y'
+                ),
+                'table-licences-applied' => array(
+                    'rows' => 1
+                ),
+                'dataLicencesRevoked' => array(
+                    'refusedLicence' => 'Y'
+                ),
+                'table-licences-revoked' => array(
+                    'rows' => 1
+                ),
+                'dataLicencesRefused' => array(
+                    'revokedLicence' => 'Y',
+                ),
+                'table-licences-refused' => array(
+                    'rows' => 1
+                ),
+                'dataLicencesDisqualified' => array(
+                    'disqualifiedLicence' => 'N',
+                ),
+                'table-licences-disqualified' => array(
+                    'rows' => 1
+                ),
+                'dataLicencesPublicInquiry' => array(
+                    'publicInquiryLicence' => 'Y'
+                ),
+                'table-licences-public-inquiry' => array(
+                    'rows' => 1
+                ),
+                'dataLicencesHeld' => array(
+                    'heldLicence' => 'N'
+                ),
+                'table-licences-held' => array(
+                    'rows' => 0
+                ),
+                'form-actions' => array(
+                    'submit' => ''
+                )
+            )
+        );
         $this->controller->setEnabledCsrf(false);
         $response = $this->controller->indexAction();
-
-        // Make sure we get a view not a response
+        
+        // Make sure we get a response not a view
         $this->assertInstanceOf('Zend\Http\Response', $response);
     }
 
@@ -71,6 +123,12 @@ class LicenceHistoryControllerTest extends AbstractApplicationControllerTestCase
      */
     protected function mockRestCalls($service, $method, $data = array(), $bundle = array())
     {
+        echo PHP_EOL . 'call number ' . $this->call++ . PHP_EOL;
+        echo 'service ' . $service . PHP_EOL;
+        echo 'method ' . $method . PHP_EOL;
+        echo 'bundle:' . PHP_EOL;
+        print_r($bundle);
+        
         if ($service == 'Application' && $method == 'GET' && $bundle == ApplicationController::$licenceDataBundle) {
 
             return array(
@@ -86,7 +144,7 @@ class LicenceHistoryControllerTest extends AbstractApplicationControllerTestCase
                 )
             );
         }
-
+        
         if ($service == 'ApplicationCompletion' && $method == 'GET') {
 
             return array(
@@ -124,6 +182,37 @@ class LicenceHistoryControllerTest extends AbstractApplicationControllerTestCase
                         'lastSection' => ''
                     )
                 )
+            );
+        }
+        $previousLicenceBundle = array(
+            'properties' => array(
+                'id',
+                'version',
+                'licNo',
+                'holderName',
+                'willSurrender',
+                'purchaseDate',
+                'disqualificationDate',
+                'disqualificationLength',
+                'previousLicenceType'
+             )   
+        );
+        if ($service == 'PreviousLicence' && $method == 'GET' && $bundle == $previousLicenceBundle) {
+            return array(
+                //'Count' => 1,
+                //'Results' => array(
+                //    array(
+                        'id' => 1,
+                        'version' => 1,
+                        'licNo' => 'ln',
+                        'holderName' => 'hn',
+                        'willSurrender' => true,
+                        'purchaseDate' => '',
+                        'disqualificationDate' => '',
+                        'disqualificationLength' => '1',
+                        'previousLicenceType' => 'CURRENT'
+                  //  )
+                //)    
             );
         }
     }
