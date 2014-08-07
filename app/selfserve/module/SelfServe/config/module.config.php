e35494532fa75753ef7814607434aa755b14628c
Henry Hayes <henryhayesuk@gmail.com>
2014-08-07 16:14:06 +0100

    Layouts moved to local

--- app/selfserve/module/SelfServe/config/module.config.php
+++ app/selfserve/module/SelfServe/config/module.config.php
@@ -33,6 +33,16 @@ return array(
         'default' => 'd-m-Y'
     ),
     'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/self-serve/global-layout/layout.phtml',
+            'error/404'               => __DIR__ . '/../view/self-serve/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/self-serve/error/index.phtml'
+        ),
         'template_path_stack' => array(
             __DIR__ . '/../view'
         )
