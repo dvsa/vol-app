a2b8c8f669da6ffc608384e733792bdcbc9e5a01
Nick Payne <nick@kurai.co.uk>
2014-08-08 10:36:39 +0100

    ensure form labels go through translation process

--- app/internal/module/Olcs/src/Form/Forms/tasks-home.form.php
+++ app/internal/module/Olcs/src/Form/Forms/tasks-home.form.php
@@ -12,27 +12,27 @@ return array(
                 'elements' => array(
                     'team' => array(
                         'type' => 'select',
-                        'label' => 'Team',
+                        'label' => 'tasks-home.data.team',
                     ),
                     'owner' => array(
                         'type' => 'select',
-                        'label' => 'Owner',
+                        'label' => 'tasks-home.data.owner',
                     ),
                     'category' => array(
                         'type' => 'select',
-                        'label' => 'Category',
+                        'label' => 'tasks-home.data.category',
                     ),
                     'sub_category' => array(
                         'type' => 'select',
-                        'label' => 'Sub category',
+                        'label' => 'tasks-home.data.sub_category',
                     ),
                     'date' => array(
                         'type' => 'select',
-                        'label' => 'Date',
+                        'label' => 'tasks-home.data.date',
                     ),
                     'urgent' => array(
                         'type' => 'checkbox',
-                        'label' => 'Urgent only',
+                        'label' => 'tasks-home.data.urgent',
                     )
                 )
             )
