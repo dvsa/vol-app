d6a9f75b5e042ae7dca6fcd8072fa32b097c21e7
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-08-08 16:13:00 +0100

    Add action for prohibition form

--- app/internal/module/Olcs/src/Controller/CaseProhibitionController.php
+++ app/internal/module/Olcs/src/Controller/CaseProhibitionController.php
@@ -25,6 +25,17 @@ class CaseProhibitionController extends CaseController
     {
         $caseId = $this->fromRoute('case');
         $licence = $this->fromRoute('licence');
+        $action = $this->fromPost('action');
+
+        if ($action) {
+            $action = strtolower($action);
+
+            if ($action == 'add') {
+                return $this->redirectToCrud($action, null);
+            } elseif ($id) {
+                return $this->redirectToCrud($action, $id);
+            }
+        }
 
         $prohibition = array();
 
@@ -33,6 +44,12 @@ class CaseProhibitionController extends CaseController
                 'case' => array(
                     'properties' => array(
                         'id'
+                    )
+                ),
+                'prohibitionType' => array(
+                    'properties' => array(
+                        'handle',
+                        'comment'
                     )
                 )
             )
@@ -43,11 +60,22 @@ class CaseProhibitionController extends CaseController
         if ($result['Count']) {
             $prohibition = $result['Results'][0];
             $prohibition['case'] = $prohibition['case']['id'];
+            $table = $this->buildTable('prohibition', [0 => $prohibition]);
         } else {
             $prohibition['case'] = $caseId;
+            $table = $this->buildTable('prohibition', []);
         }
 
-        $form = $this->generateProhibitionForm($prohibition);
+        $prohibitionNote = $this->makeRestCall('ProhibitionNote', 'GET', array('case' => $caseId));
+        $prohibitionNote['case'] = $caseId;
+
+        if ($prohibitionNote['Count']) {
+            $prohibitionNote = $prohibitionNote['Results'][0];
+        }
+
+        $prohibitionNote['case'] = $caseId;
+
+        $form = $this->generateProhibitionNoteForm($prohibitionNote);
 
         $this->setBreadcrumb(array('licence_case_list/pagination' => array('licence' => $licence)));
 
@@ -62,6 +90,7 @@ class CaseProhibitionController extends CaseController
                 'case' => $case,
                 'tabs' => $tabs,
                 'tab' => 'prohibitions',
+                'table' => $table,
                 'summary' => $summary,
                 'commentForm' => $form,
             ]
@@ -72,16 +101,66 @@ class CaseProhibitionController extends CaseController
     }
 
     /**
+     * Add action
+     *
+     * @return void|\Zend\View\Model\ViewModel
+     */
+    public function addAction()
+    {
+        $licenceId = $this->fromRoute('licence');
+        $caseId = $this->fromRoute('case');
+
+        $this->setBreadcrumb(
+            array(
+                'licence_case_list/pagination' => array('licence' => $licenceId),
+                'case_prohibition' => array('licence' => $licenceId, 'case' => $caseId)
+            )
+        );
+
+        $form = $this->generateFormWithData(
+            'prohibition',
+            'processAddProhibition',
+            array(
+                'case' => $caseId
+            )
+        );
+
+        $view = $this->getView(
+            [
+                'params' => [
+                    'pageTitle' => 'Add prohibition',
+                    'pageSubTitle' => ''
+                ],
+                'form' => $form,
+            ]
+        );
+
+        $view->setTemplate('prohibition/form');
+
+        return $view;
+    }
+
+    /**
+     * Edit action
+     *
+     * @return void|\Zend\View\Model\ViewModel
+     */
+    public function editAction()
+    {
+
+    }
+
+    /**
      * Creates and returns the prohibition form.
      *
      * @param array $prohibition
      * @return \Zend\Form\Form
      */
-    private function generateProhibitionForm($prohibition)
+    private function generateProhibitionNoteForm($prohibition)
     {
         $form = $this->generateForm(
             'prohibition-comment',
-            'saveProhibitionForm'
+            'saveProhibitionNoteForm'
         );
         $form->setData($prohibition);
 
@@ -89,22 +168,59 @@ class CaseProhibitionController extends CaseController
     }
 
     /**
-     * Saves the penalty form.
+     * Saves the prohibition notes form.
      *
      * @param array $data
+     * @return Redirect
      */
-    public function saveProhibitionForm($data)
+    public function saveProhibitionNoteForm($data)
     {
         unset($data['cancel']);
 
         if ($data['submit'] === '') {
             if (!empty($data['id'])) {
-                $this->processEdit($data, 'Prohibition');
+                $this->processEdit($data, 'ProhibitionNote');
             } else {
-                $this->processAdd($data, 'Prohibition');
+                $this->processAdd($data, 'ProhibitionNote');
             }
         }
 
         return $this->redirect()->toRoute('case_prohibition', array(), array(), true);
+    }
+
+    /**
+     * Processes the add prohibition form
+     *
+     * @param array $data
+     */
+    public function processAddProhibition ($data)
+    {
+        $result = $this->processAdd($data, 'Prohibition');
+
+        if (isset($result['id'])) {
+            return $this->redirectToAction();
+        }
+
+        return $this->redirectToAction('add');
+    }
+
+    /**
+     * Redirects to the add or edit action
+     *
+     * @param string $action
+     * @param int $id
+     * @return Redirect
+     */
+    private function redirectToCrud($action, $id = null)
+    {
+        return $this->redirect()->toRoute(
+            'case_prohibition',
+            array(
+                'action' => $action,
+                'id' => $id,
+            ),
+            array(),
+            true
+        );
     }
 }
