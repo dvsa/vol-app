abf1fa7670edc3742e42a1daa5705a3b8cd7bfc7
Shaun Lizzio <shaun@lizzio.co.uk>
2014-08-14 10:34:30 +0100

    Added defects to form

--- app/internal/module/Olcs/src/Form/Forms/prohibition.form.php
+++ app/internal/module/Olcs/src/Form/Forms/prohibition.form.php
@@ -40,6 +40,11 @@ return [
                         'label' => 'Location prohibition issued',
                         'filters' => '\Common\Form\Elements\InputFilters\TextMax255',
                     ],
+                    'defects' =>
+                        [
+                            'label' => 'Defects (optional)',
+                            'type' => 'defects'
+                        ],
                 ]
             ],
             array(
