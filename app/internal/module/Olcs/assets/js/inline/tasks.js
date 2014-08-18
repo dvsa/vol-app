e45d1acf32251db78acf1a58d7f1b73c5bfc1d7f
Nick Payne <nick@kurai.co.uk>
2014-08-18 13:57:47 +0100

    opt in to table ajax logic

--- app/internal/module/Olcs/assets/js/inline/tasks.js
+++ app/internal/module/Olcs/assets/js/inline/tasks.js
@@ -24,4 +24,31 @@ $(function() {
     dest: "#subCategory",
     url: "/tasks/sub-categories"
   });
+
+  OLCS.tableHandler({
+    table: ".table__form",
+    container: ".table__form",
+    filter: ".table__form"
+  });
+
+  /**
+   * Non component logic; bridge the table controls to the form
+   */
+  $(document).on(
+    "click",
+    ".table__form .sortable a, .table__form .results-settings a",
+    function(e) {
+      e.preventDefault();
+
+      queryParams = OLCS.queryString.parse(
+        $(this).attr("href")
+      );
+
+      $.each(["sort", "order", "limit"], function(k, v) {
+        if (queryParams[v]) {
+          $("#" + v).val(queryParams[v]);
+        }
+      });
+    }
+  );
 });
