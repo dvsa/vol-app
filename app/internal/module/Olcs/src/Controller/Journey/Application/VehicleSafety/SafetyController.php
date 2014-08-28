5daf14c1017482f9535746fed898f1efd66fb5b9
Rob Caiger <rob@clocal.co.uk>
2014-08-28 15:32:45 +0100

    Check section status before setting it

--- app/internal/module/Olcs/src/Controller/Journey/Application/VehicleSafety/SafetyController.php
+++ app/internal/module/Olcs/src/Controller/Journey/Application/VehicleSafety/SafetyController.php
@@ -27,6 +27,13 @@ class SafetyController extends ParentController
     protected $hideInternalFormElements = true;
 
     /**
+     * Whether or not the fields are required
+     *
+     * @var boolean
+     */
+    protected $requiredFields = false;
+
+    /**
      * Remove the trailer fields for PSV
      *
      * @param \Zend\Form\Fieldset $form
@@ -36,7 +43,9 @@ class SafetyController extends ParentController
     {
         $form = parent::alterForm($form);
 
-        $this->setFieldsAsNotRequired($form->getInputFilter());
+        if (!$this->requiredFields) {
+            $this->setFieldsAsNotRequired($form->getInputFilter());
+        }
 
         return $form;
     }
@@ -60,5 +69,90 @@ class SafetyController extends ParentController
         }
 
         return $inputs;
+    }
+
+    /**
+     * Update the section statuses
+     *  - Internally we remove the required field validation, so here we need to explicitly check if the section is
+     *      complete
+     *
+     * @param array $subSections
+     */
+    protected function updateSectionStatuses(array $subSections)
+    {
+        $completion = parent::updateSectionStatuses($subSections);
+        $sectionIndex = $this->formatSectionStatusIndex();
+
+        // Get a new form without the removed validation
+        $this->requiredFields = true;
+        $form = $this->getNewForm();
+        $form->remove('csrf');
+
+        $formData = $form->getData();
+
+        $flatFormData = $this->getFlatData($formData);
+
+        $started = $this->hasSectionBeenStarted($flatFormData);
+
+        $incompleteStatus = self::COMPLETION_STATUS_NOT_STARTED;
+
+        if ($started) {
+            $incompleteStatus = self::COMPLETION_STATUS_INCOMPLETE;
+        }
+
+        // Check if the form would be valid
+        $completion[$sectionIndex] = $form->isValid() ? self::COMPLETION_STATUS_COMPLETE : $incompleteStatus;
+
+        return $completion;
+    }
+
+    /**
+     * Check if the section has been started
+     *
+     * @param array $data
+     * @return boolean
+     */
+    protected function hasSectionBeenStarted($data)
+    {
+        // Loop through the data
+        foreach ($data as $key => $value) {
+            // Check if we have any non-default values
+            switch ($key) {
+                case 'rows':
+                    if ($value > 0) {
+                        return true;
+                    }
+                    break;
+                case 'safetyConfirmation':
+                    if ($value != 'N') {
+                        return true;
+                    }
+                    break;
+                default:
+                    if (!empty($value)) {
+                        return true;
+                    }
+                    break;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Flattern the data
+     *
+     * @param array $data
+     * @return array
+     */
+    protected function getFlatData($data)
+    {
+        $flatData = array_merge($data['licence'], $data['table'], $data['application']);
+        unset($flatData['id']);
+        unset($flatData['version']);
+        unset($flatData['table']);
+        unset($flatData['action']);
+
+        return $flatData;
     }
 }
