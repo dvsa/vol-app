97a1e721c5ec04c35a0482dad32517813e7d8fbd
Nick Payne <nick@kurai.co.uk>
2014-08-29 11:38:46 +0100

    update unit tests for closed task behaviour

--- app/internal/test/Olcs/src/Controller/TaskControllerTest.php
+++ app/internal/test/Olcs/src/Controller/TaskControllerTest.php
@@ -206,6 +206,147 @@ class TaskControllerTest extends AbstractHttpControllerTestCase
         ];
 
         $taskData = [
+            'urgent' => 'Y',
+            'category' => ['id' => 100],
+            'taskSubCategory' => ['id' => 1],
+            'assignedToTeam' => ['id' => 10],
+            'assignedToUser' => ['id' => 1],
+        ];
+
+        $this->controller->expects($this->at(2))
+            ->method('makeRestCall')
+            ->with('Task', 'GET', ['id' => 456])
+            ->will($this->returnValue($taskData));
+
+        $this->controller->expects($this->at(6))
+            ->method('makeRestCall')
+            ->with('Category', 'GET', $altListData)
+            ->will($this->returnValue($altResponse));
+
+        $this->controller->expects($this->at(7))
+            ->method('makeRestCall')
+            ->with('TaskSubCategory', 'GET', $extendedListData)
+            ->will($this->returnValue($response));
+
+        $this->controller->expects($this->at(8))
+            ->method('makeRestCall')
+            ->with('Team', 'GET', $standardListData)
+            ->will($this->returnValue($response));
+
+        $this->controller->expects($this->at(9))
+            ->method('makeRestCall')
+            ->with('User', 'GET', $extendedListData)
+            ->will($this->returnValue($response));
+
+        $fromRoute = $this->getMock('\stdClass', ['fromRoute']);
+        $fromRoute->expects($this->at(0))
+            ->method('fromRoute')
+            ->with('task')
+            ->will($this->returnValue(456));
+
+        $fromRoute->expects($this->at(1))
+            ->method('fromRoute')
+            ->with('licence')
+            ->will($this->returnValue(123));
+
+        $this->controller->expects($this->any())
+            ->method('params')
+            ->will($this->returnValue($fromRoute));
+
+        $toArray = $this->getMock('\stdClass', ['toArray']);
+        $toArray->expects($this->any())
+            ->method('toArray')
+            ->will($this->returnValue([]));
+
+        $this->request->expects($this->any())
+            ->method('getPost')
+            ->will($this->returnValue($toArray));
+
+        $view = $this->controller->editAction();
+
+        list($header, $content) = $view->getChildren();
+
+        $this->assertEquals('Edit task', $header->getVariable('pageTitle'));
+    }
+
+    public function testEditActionClosedTask()
+    {
+        $form = $this->getMock(
+            '\Zend\Form\Form',
+            [
+                'get', 'setValue', 'setValueOptions',
+                'remove', 'setData', 'setAttribute',
+                'getFieldsets', 'getElements'
+            ]
+        );
+
+        $form->expects($this->any())
+            ->method('get')
+            ->will($this->returnSelf());
+
+        $e1 = $this->getMock('\stdClass', ['setAttribute']);
+        $e1->expects($this->once())
+            ->method('setAttribute')
+            ->with('disabled', 'disabled');
+
+        $e2 = $this->getMock('\stdClass', ['setAttribute']);
+        $e2->expects($this->once())
+            ->method('setAttribute')
+            ->with('disabled', 'disabled');
+
+        $form->expects($this->any())
+            ->method('getFieldsets')
+            ->will($this->returnValue([$e1]));
+
+        $form->expects($this->any())
+            ->method('getElements')
+            ->will($this->returnValue([$e2]));
+
+        $form->expects($this->once())
+            ->method('setAttribute')
+            ->with('disabled', 'disabled');
+
+        $this->controller->expects($this->once())
+            ->method('getForm')
+            ->will($this->returnValue($form));
+
+        $response = [
+            'Results' => [
+                [
+                    'id' => 123,
+                    'name' => 'foo'
+                ]
+            ]
+        ];
+
+        $altResponse = [
+            'Results' => [
+                [
+                    'id' => 123,
+                    'description' => 'foo'
+                ]
+            ]
+        ];
+
+        $standardListData = [
+            'limit' => 100,
+            'sort' => 'name'
+        ];
+
+        $altListData = [
+            'limit' => 100,
+            'sort' => 'description'
+        ];
+
+        $extendedListData = [
+            'team' => 10,
+            'category' => 100,
+            'limit' => 100,
+            'sort' => 'name'
+        ];
+
+        $taskData = [
+            'isClosed' => 'Y',
             'category' => ['id' => 100],
             'taskSubCategory' => ['id' => 1],
             'assignedToTeam' => ['id' => 10],
@@ -288,7 +429,9 @@ class TaskControllerTest extends AbstractHttpControllerTestCase
             ->will($this->returnValue(true));
 
         $formData = [
-            'details' => [],
+            'details' => [
+                'urgent' => 1
+            ],
             'assignment' => [],
             'id' => 100,
             'version' => 200
