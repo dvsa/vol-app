32635534c30ecc35a6fde50fcec09e597e73bdd1
Rob Caiger <rob@clocal.co.uk>
2014-09-01 15:26:09 +0100

    Added unit tests

--- app/internal/test/Olcs/src/Controller/Journey/Application/VehicleSafety/SafetyControllerTest.php
+++ app/internal/test/Olcs/src/Controller/Journey/Application/VehicleSafety/SafetyControllerTest.php
@@ -19,4 +19,132 @@ class SafetyControllerTest extends ParentTestCase
 {
     protected $controllerName = '\Olcs\Controller\Journey\Application\VehicleSafety\SafetyController';
     protected $routeName = 'Application/VehicleSafety/Safety';
+
+    public function testIndexActionUpdatingStatusWithRows()
+    {
+        $this->goodsOrPsv = 'goods';
+
+        $this->setUpAction(
+            'index',
+            null,
+            array(
+                'licence' => array(
+                    'safetyInsVehicles' => '',
+                    'safetyInsTrailers' => '',
+                    'safetyInsVaries' => '',
+                    'tachographIns' => ''
+                ),
+                'table' => array(
+                    'rows' => 1
+                ),
+                'application' => array(
+                    'id' => 1,
+                    'version' => 1,
+                    'safetyConfirmation' => ''
+                )
+            )
+        );
+
+        $this->controller->setEnabledCsrf(false);
+
+        $response = $this->controller->indexAction();
+
+        $this->assertInstanceOf('Zend\Http\Response', $response);
+    }
+
+    public function testIndexActionUpdatingStatusWithSafetyConfirmation()
+    {
+        $this->goodsOrPsv = 'goods';
+
+        $this->setUpAction(
+            'index',
+            null,
+            array(
+                'licence' => array(
+                    'safetyInsVehicles' => '',
+                    'safetyInsTrailers' => '',
+                    'safetyInsVaries' => '',
+                    'tachographIns' => ''
+                ),
+                'table' => array(
+                    'rows' => 0
+                ),
+                'application' => array(
+                    'id' => 1,
+                    'version' => 1,
+                    'safetyConfirmation' => 'Y'
+                )
+            )
+        );
+
+        $this->controller->setEnabledCsrf(false);
+
+        $response = $this->controller->indexAction();
+
+        $this->assertInstanceOf('Zend\Http\Response', $response);
+    }
+
+    public function testIndexActionUpdatingStatusWithSafetyConfirmationUnchecked()
+    {
+        $this->goodsOrPsv = 'goods';
+
+        $this->setUpAction(
+            'index',
+            null,
+            array(
+                'licence' => array(
+                    'safetyInsVehicles' => '',
+                    'safetyInsTrailers' => '',
+                    'safetyInsVaries' => '',
+                    'tachographIns' => ''
+                ),
+                'table' => array(
+                    'rows' => 0
+                ),
+                'application' => array(
+                    'id' => 1,
+                    'version' => 1,
+                    'safetyConfirmation' => 'N'
+                )
+            )
+        );
+
+        $this->controller->setEnabledCsrf(false);
+
+        $response = $this->controller->indexAction();
+
+        $this->assertInstanceOf('Zend\Http\Response', $response);
+    }
+
+    public function testIndexActionUpdatingStatusWithoutFormChanges()
+    {
+        $this->goodsOrPsv = 'goods';
+
+        $this->setUpAction(
+            'index',
+            null,
+            array(
+                'licence' => array(
+                    'safetyInsVehicles' => '',
+                    'safetyInsTrailers' => '',
+                    'safetyInsVaries' => '',
+                    'tachographIns' => ''
+                ),
+                'table' => array(
+                    'rows' => 0
+                ),
+                'application' => array(
+                    'id' => 1,
+                    'version' => 1,
+                    'safetyConfirmation' => ''
+                )
+            )
+        );
+
+        $this->controller->setEnabledCsrf(false);
+
+        $response = $this->controller->indexAction();
+
+        $this->assertInstanceOf('Zend\Http\Response', $response);
+    }
 }
