51a69cc8a7dcf9082e411ec1f33779ac80b4aaa2
Henry Hayes <henryhayesuk@gmail.com>
2014-09-11 16:48:25 +0100

    Submission table uses correct ID param and Case prohibition controller methods removed

--- app/internal/module/Olcs/src/Table/Tables/submission.table.php
+++ app/internal/module/Olcs/src/Table/Tables/submission.table.php
@@ -33,7 +33,7 @@ return array(
             'title' => 'Submission #',
             'formatter' => function ($row) {
                 return '<a href="' . $this->generateUrl(
-                    array('id' => $row['id'], 'action' => 'edit'),
+                    array('submission' => $row['id'], 'action' => 'edit'),
                     'submission',
                     true
                 ) . '">' . $row['id'] . '</a>';
