218007cc4cec5db6df42900c57d0820209044763
Rob Caiger <rob@clocal.co.uk>
2014-09-18 10:08:37 +0100

    Started re-working Licence OC section

--- app/internal/module/Olcs/src/Controller/Licence/Details/OperatingCentreController.php
+++ app/internal/module/Olcs/src/Controller/Licence/Details/OperatingCentreController.php
@@ -1,26 +1,32 @@
 <?php
 
 /**
- * OperatingCentre Controller
+ * Operating Centre Controller
+ *
+ * Internal - Licence section
  */
 namespace Olcs\Controller\Licence\Details;
 
-use Zend\View\Model\ViewModel;
+use Common\Controller\Traits;
 
 /**
- * OperatingCentre Controller
- *
- * @author Rob Caiger <rob@clocal.co.uk>
+ * Operating Centre Controller
  */
 class OperatingCentreController extends AbstractLicenceDetailsController
 {
-    protected $section = 'operating_centre';
+    use Traits\OperatingCentre\GenericLicenceAuthorisationSection;
 
-    public function indexAction()
-    {
-        $view = new ViewModel();
-        $view->setTemplate('licence/details/placeholder');
+    /**
+     * Set the form name
+     *
+     * @var string
+     */
+    protected $formName = 'application_operating-centres_authorisation';
 
-        return $this->renderView($view);
-    }
+    /**
+     * Setup the section
+     *
+     * @var string
+     */
+    protected $section = 'operating_centres';
 }
