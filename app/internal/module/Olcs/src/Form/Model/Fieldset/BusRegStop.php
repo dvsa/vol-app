cc8f1666851de9ca297a7cf18194341e64cd4923
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-09-19 10:21:44 +0100

    Formatting changes

--- app/internal/module/Olcs/src/Form/Model/Fieldset/BusRegStop.php
+++ app/internal/module/Olcs/src/Form/Model/Fieldset/BusRegStop.php
@@ -12,20 +12,36 @@ class BusRegStop extends Base
 {
     /**
      * @Form\Type("Radio")
-     * @Form\Options({"label": "Use all bus stops", "value_options":{"N":"No", "Y":"Yes"}})
+     * @Form\Options({
+     *      "label": "Use all bus stops",
+     *      "value_options":{
+     *          "N":"No",
+     *          "Y":"Yes"
+     *      }
+     * })
      * @Form\Attributes({"value": "N"})
      */
     public $useAllStops;
 
     /**
      * @Form\Type("Radio")
-     * @Form\Options({"label": "Manoeuvres", "value_options":{"N":"No", "Y":"Yes"}})
+     * @Form\Options({
+     *      "label": "Manoeuvres",
+     *      "value_options":{
+     *          "N":"No",
+     *          "Y":"Yes"
+     *      }
+     * })
      * @Form\Attributes({"value": "N"})
      */
     public $hasManoeuvre = 0;
 
     /**
-     * @Form\Attributes({"id":"manoeuvreDetail","class":"extra-long","name":"manoeuvreDetail"})
+     * @Form\Attributes({
+     *      "id":"manoeuvreDetail",
+     *      "class":"extra-long",
+     *      "name":"manoeuvreDetail"
+     * })
      * @Form\Options({
      *     "label": "Manoeuvres comments",
      *     "label_attributes": {
@@ -36,19 +52,35 @@ class BusRegStop extends Base
      * })
      * @Form\Type("TextArea")
      * @Form\Filter({"name":"Zend\Filter\StringTrim"})
-     * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"min":5,"max":255}})
+     * @Form\Validator({
+     *      "name":"Zend\Validator\StringLength",
+     *      "options":{
+     *          "min":5,
+     *          "max":255
+     *      }
+     * })
      */
     public $manoeuvreDetail = null;
 
     /**
      * @Form\Type("Radio")
-     * @Form\Options({"label": "Need new bus stops", "value_options":{"N":"No", "Y":"Yes"}})
+     * @Form\Options({
+     *      "label": "Need new bus stops",
+     *      "value_options":{
+     *          "N":"No",
+     *          "Y":"Yes"
+     *      }
+     * })
      * @Form\Attributes({"value": "N"})
      */
     public $needNewStop = 0;
 
     /**
-     * @Form\Attributes({"id":"newStopDetail","class":"extra-long","name":"newStopDetail"})
+     * @Form\Attributes({
+     *      "id":"newStopDetail",
+     *      "class":"extra-long",
+     *      "name":"newStopDetail"
+     * })
      * @Form\Options({
      *     "label": "Need new bus stops comments",
      *     "label_attributes": {
@@ -59,19 +91,35 @@ class BusRegStop extends Base
      * })
      * @Form\Type("TextArea")
      * @Form\Filter({"name":"Zend\Filter\StringTrim"})
-     * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"min":5,"max":255}})
+     * @Form\Validator({
+     *      "name":"Zend\Validator\StringLength",
+     *      "options":{
+     *          "min":5,
+     *          "max":255
+     *      }
+     * })
      */
     public $newStopDetail = null;
 
     /**
      * @Form\Type("Radio")
-     * @Form\Options({"label": "No fixed stopping points", "value_options":{"N":"No", "Y":"Yes"}})
+     * @Form\Options({
+     *      "label": "No fixed stopping points",
+     *      "value_options":{
+     *          "N":"No",
+     *          "Y":"Yes"
+     *      }
+     * })
      * @Form\Attributes({"value": "N"})
      */
     public $hasNotFixedStop = 0;
 
     /**
-     * @Form\Attributes({"id":"notFixedStopDetail","class":"extra-long","name":"notFixedStopDetail"})
+     * @Form\Attributes({
+     *      "id":"notFixedStopDetail",
+     *      "class":"extra-long",
+     *      "name":"notFixedStopDetail"
+     * })
      * @Form\Options({
      *     "label": "No fixed stopping points comments",
      *     "label_attributes": {
@@ -82,12 +130,23 @@ class BusRegStop extends Base
      * })
      * @Form\Type("TextArea")
      * @Form\Filter({"name":"Zend\Filter\StringTrim"})
-     * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"min":5,"max":255}})
+     * @Form\Validator({
+     *      "name":"Zend\Validator\StringLength",
+     *      "options":{
+     *          "min":5,
+     *          "max":255
+     *      }
+     * })
      */
     public $notFixedStopDetail = null;
 
     /**
-     * @Form\Attributes({"id":"subsidised","placeholder":"","class":"small"})
+     * @Form\Attributes({
+     *      "id":"subsidised",
+     *      "placeholder":"",
+     *      "class":"small",
+     *      "value":"bs_no"
+     * })
      * @Form\Options({
      *     "label": "Supported by subsidies",
      *     "disable_inarray_validator": false,
@@ -95,10 +154,14 @@ class BusRegStop extends Base
      * })
      * @Form\Type("DynamicSelect")
      */
-    public $subsidised = 'bs_no';
+    public $subsidised = null;
 
     /**
-     * @Form\Attributes({"id":"subsidyDetail","class":"extra-long","name":"subsidyDetail"})
+     * @Form\Attributes({
+     *      "id":"subsidyDetail",
+     *      "class":"extra-long",
+     *      "name":"subsidyDetail"
+     * })
      * @Form\Options({
      *     "label": "Supported by subsidies comments",
      *     "label_attributes": {
@@ -109,7 +172,13 @@ class BusRegStop extends Base
      * })
      * @Form\Type("TextArea")
      * @Form\Filter({"name":"Zend\Filter\StringTrim"})
-     * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"min":5,"max":255}})
+     * @Form\Validator({
+     *      "name":"Zend\Validator\StringLength",
+     *      "options":{
+     *          "min":5,
+     *          "max":255
+     *      }
+     * })
      */
     public $subsidyDetail = null;
 }
