2131e0cfa42c53364afbbc8ee8fa2a84309dd614
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-09-25 10:05:57 +0100

    New impounding controller

---
+++ app/internal/module/Olcs/src/Controller/Cases/Impounding/ImpoundingController.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * Case Impounding Controller
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+namespace Olcs\Controller\Cases\Impounding;
+
+// Olcs
+use Olcs\Controller as OlcsController;
+use Olcs\Controller\Traits as ControllerTraits;
+
+/**
+ * Case Impounding Controller
+ *
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class ImpoundingController extends OlcsController\CrudAbstract
+{
+    use ControllerTraits\CaseControllerTrait;
+
+    /**
+     * Identifier name
+     *
+     * @var string
+     */
+    protected $identifierName = 'id';
+
+    /**
+     * Table name string
+     *
+     * @var string
+     */
+    protected $tableName = 'impounding';
+
+    /**
+     * Holds the form name
+     *
+     * @var string
+     */
+    protected $formName = 'impounding';
+
+    /**
+     * The current page's extra layout, over and above the
+     * standard base template, a sibling of the base though.
+     *
+     * @var string
+     */
+    protected $pageLayout = 'case';
+
+    /**
+     * For most case crud controllers, we use the case/inner-layout
+     * layout file. Except submissions.
+     *
+     * @var string
+     */
+    protected $pageLayoutInner = 'case/inner-layout';
+
+    /**
+     * Holds the service name
+     *
+     * @var string
+     */
+    protected $service = 'Impounding';
+
+    /**
+     * Holds the navigation ID,
+     * required when an entire controller is
+     * represented by a single navigation id.
+     */
+    protected $navigationId = 'case_details_impounding';
+
+    /**
+     * Holds an array of variables for the
+     * default index list page.
+     */
+    protected $listVars = [
+        'case',
+    ];
+
+    /**
+     * Data map
+     *
+     * @var array
+     */
+    protected $dataMap = array(
+        'main' => array(
+            'mapFrom' => array(
+                'application_details',
+                'hearing',
+                'outcome',
+                'base',
+            )
+        )
+    );
+
+    /**
+     * Holds the isAction
+     *
+     * @var boolean
+     */
+    protected $isAction = false;
+
+    /**
+     * Holds the Data Bundle
+     *
+     * @var array
+     */
+    protected $dataBundle = array(
+        'children' => array(
+            'case' => array(
+                'properties' => array(
+                    'id'
+                )
+            ),
+            'presidingTc' => array(
+                'properties' => array(
+                    'id',
+                    'name'
+                )
+            ),
+            'outcome' => array(
+                'properties' => array(
+                    'id',
+                    'name'
+                )
+            ),
+            'impoundingType' => array(
+                'properties' => array(
+                    'id',
+                    'description'
+                )
+            ),
+            'piVenue' => array(
+                'properties' => array(
+                    'id',
+                    'name'
+                )
+            ),
+        )
+    );
+}
