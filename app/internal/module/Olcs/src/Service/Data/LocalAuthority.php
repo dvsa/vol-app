8c37eedaaa61263923f477374ef12da39ff1b79e
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-10-02 15:10:32 +0100

    New services for Local Authority and Traffic Area

--- app/internal/module/Olcs/src/Service/Data/LocalAuthority.php
+++ app/internal/module/Olcs/src/Service/Data/LocalAuthority.php
@@ -1,7 +1,70 @@
 <?php
+
+namespace Olcs\Service\Data;
+
+use Common\Service\Data\AbstractData;
+use Common\Service\Data\ListDataInterface;
+
 /**
- * Created by PhpStorm.
- * User: ian
- * Date: 01/10/2014
- * Time: 14:13
- */ 
+ * Class LocalAuthority
+ * @author Ian Lindsay <ian@hemera-business-services.co.uk>
+ */
+class LocalAuthority extends AbstractData implements ListDataInterface
+{
+    protected $serviceName = 'LocalAuthority';
+
+    /**
+     * Format data!
+     *
+     * @param array $data
+     * @return array
+     */
+    public function formatData(array $data)
+    {
+        $optionData = [];
+
+        foreach ($data as $datum) {
+            $optionData[$datum['id']] = $datum['description'];
+        }
+
+        return $optionData;
+    }
+
+    /**
+     * @param $category
+     * @param bool $useGroups
+     * @return array
+     */
+    public function fetchListOptions($category, $useGroups = false)
+    {
+        $data = $this->fetchListData();
+
+        if (!$data) {
+            return [];
+        }
+
+        return $this->formatData($data);
+    }
+
+    /**
+     * Ensures only a single call is made to the backend for each dataset
+     *
+     * @internal param $category
+     * @return array
+     */
+    public function fetchListData()
+    {
+        if (is_null($this->getData('LocalAuthority'))) {
+
+            $data = $this->getRestClient()->get('', ['limit' => 1000]);
+
+            $this->setData('LocalAuthority', false);
+
+            if (isset($data['Results'])) {
+                $this->setData('LocalAuthority', $data['Results']);
+            }
+        }
+
+        return $this->getData('LocalAuthority');
+    }
+}
