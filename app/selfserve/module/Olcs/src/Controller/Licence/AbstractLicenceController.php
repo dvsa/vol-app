207b83fa39966863cb14b3f1e4a39011c32c6891
Nick Payne <nick@kurai.co.uk>
2014-10-06 15:21:24 +0100

    add basic licence controller and routes

---
+++ app/selfserve/module/Olcs/src/Controller/Licence/AbstractLicenceController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Abstract Licence Controller
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
+namespace Olcs\Controller\Licence;
+
+use Olcs\Controller\AbstractExternalController;
+
+/**
+ * Abstract Licence Controller
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
+abstract class AbstractLicenceController extends AbstractExternalController
+{
+}
