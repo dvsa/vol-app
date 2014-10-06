5abbdf7b647c6610b477360594df5ad393a47bd1
Nick Payne <nick@kurai.co.uk>
2014-10-06 14:50:19 +0100

    add abstract and application section overview view models

---
+++ app/selfserve/module/Olcs/src/View/AbstractSectionOverviewModel.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Abstract Section Overview Model
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
+namespace Olcs\View;
+
+use Common\View\AbstractViewModel;
+
+/**
+ * Abstract Section Overview Model
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
+class AbstractSectionOverviewModel extends AbstractViewModel
+{
+    // @TODO backport code from RC's current 'OverviewSection'
+    // implementation - generic
+}
