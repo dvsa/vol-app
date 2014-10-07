7f4da58fe955561fb517a772fb83bbb1cf1ede3f
Nick Payne <nick@kurai.co.uk>
2014-10-07 16:00:00 +0100

    flesh out type of licence form stub data a bit

--- app/selfserve/module/Olcs/src/Form/Model/Form/TypeOfLicence.php
+++ app/selfserve/module/Olcs/src/Form/Model/Form/TypeOfLicence.php
@@ -7,7 +7,7 @@ use Zend\Form\Annotation as Form;
 /**
  * @codeCoverageIgnore Auto-generated file with no methods
  * @Form\Name("type-of-licence")
- * @Form\Options({"label":"Type of cicence"})
+ * @Form\Options({"label":"Type of licence"})
  * @Form\Attributes({"method":"post"})
  * @Form\Type("Common\Form\Form")
  * @Form\Options({"prefer_form_input_filter": true})
@@ -15,11 +15,25 @@ use Zend\Form\Annotation as Form;
 class TypeOfLicence
 {
     /**
-     * @Form\Name("fields")
-     * @Form\Options({"label":"Appeal Details"})
-     * @Form\ComposedObject("Olcs\Form\Model\Fieldset\Appeal")
+     * @Form\Name("operator-location")
+     * @Form\Options({"label":"Operator location"})
+     * @Form\ComposedObject("Olcs\Form\Model\Fieldset\OperatorLocation")
      */
-    //public $fields = null;
+    // public $operatorLocation = null;
+
+    /**
+     * @Form\Name("operator-type")
+     * @Form\Options({"label":"Operator type"})
+     * @Form\ComposedObject("Olcs\Form\Model\Fieldset\OperatorType")
+     */
+    // public $operatorType = null;
+
+    /**
+     * @Form\Name("licence-type")
+     * @Form\Options({"label":"Licence type"})
+     * @Form\ComposedObject("Olcs\Form\Model\Fieldset\LicenceType")
+     */
+    // public $licenceType = null;
 
     /**
      * @Form\Name("form-actions")
