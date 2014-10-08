84a041a5237cda993a51032e3bb7e9610bd92ee4
Henry Hayes <henryhayesuk@gmail.com>
2014-10-08 13:23:19 +0100

    Basic tests for Stay controller

---
+++ app/internal/test/Olcs/src/Controller/Case/Hearing/StayController.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Stay Test Controller
+ *
+ * @author Craig Reasbeck <craig.reasbeck@valtech.co.uk>
+ */
+
+namespace OlcsTest\Controller;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+use Olcs\Controller\Cases\Hearing\StayController;
+use OlcsTest\Controller\ControllerTestAbstract;
+
+/**
+ * Stay Test Controller
+ */
+class StayControllerTest extends ControllerTestAbstract
+{
+    protected $testClass = 'Olcs\Controller\Cases\Hearing\StayController';
+
+    /**
+     * Isolated test for the redirect action method.
+     */
+    public function testRedirectToIndex()
+    {
+        $routeId = 'case_hearing_appeal';
+
+        $sut = $this->getMock($this->testClass, ['redirectToRoute']);
+        $sut->expects($this->once())
+            ->method('redirectToRoute')
+            ->with($routeId, ['action' => 'details'], [], true)
+            ->will($this->returnValue('return'));
+
+        $this->assertEquals('return', $sut->redirectToIndex());
+    }
+}
