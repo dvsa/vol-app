b62490b5d6832b494327045729fffbbc88db3b65
Nick Payne <nick@kurai.co.uk>
2014-10-09 15:59:00 +0100

    rough start on business details section

--- app/selfserve/module/Olcs/src/Controller/Application/BusinessDetailsController.php
+++ app/selfserve/module/Olcs/src/Controller/Application/BusinessDetailsController.php
@@ -17,11 +17,32 @@ use Zend\Form\Form;
  */
 class BusinessDetailsController extends AbstractApplicationController
 {
+    /**
+     * Business details section
+     */
     public function indexAction()
     {
+        $form = $this->getHelperService('FormHelper')
+            ->createForm('Lva\BusinessDetails')
+            ->setData([]);
+
+        $table = $this->getServiceLocator()
+            ->get('Table')
+            ->buildTable(
+                'application_your-business_business_details-subsidiaries',
+                array(), // @TODO data...
+                array(), // params?
+                false
+            );
+
+        $form->get('table')
+            ->get('table')
+            ->setTable($table);
+
         return new Section(
             array(
-                'title' => 'Business details'
+                'title' => 'Business details',
+                'form' => $form
             )
         );
     }
