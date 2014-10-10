94cf00ee7eb6a60f695ea720aee5789d84097950
Nick Payne <nick@kurai.co.uk>
2014-10-10 15:17:09 +0100

    implement trading names functionality, start on companies house lookup

--- app/selfserve/module/Olcs/src/Controller/Application/BusinessDetailsController.php
+++ app/selfserve/module/Olcs/src/Controller/Application/BusinessDetailsController.php
@@ -55,17 +55,85 @@ class BusinessDetailsController extends AbstractApplicationController
             ->get('table')   // element
             ->setTable($table);
 
-        if ($request->isPost() && $form->isValid()) {
-            if (isset($data['tradingNames'])) {
-                $tradingNames = $this->formatTradingNamesDataForSave($data);
-                $this->getEntityService('TradingNames')->save($tradingNames);
+        if ($request->isPost()) {
+
+            /**
+             * we'll re-use this in a few places, so cache the lookup
+             * just for the sake of legibility
+             */
+            $tradingNames = $data['data']['tradingNames'];
+
+            if (isset($data['data']['companyNumber']['submit_lookup_company'])) {
+                /**
+                 * User has pressed 'Find company' on registered company number
+                 */
+                if (strlen(trim($data['data']['companyNumber']['company_number'])) != 8) {
+
+                    $form->get('data')->get('companyNumber')->setMessages(
+                        array(
+                            'company_number' => array(
+                                'The input must be 8 characters long'
+                            )
+                        )
+                    );
+
+                } else {
+                    $result = $this->getServiceLocator()
+                        ->get('CompaniesHouse')
+                        ->search('numberSearch', $data['data']['companyNumber']['company_number']);
+
+                    if ($result['Count'] == 1) {
+
+                        $companyName = $result['Results'][0]['CompanyName'];
+                        // @TODO reimplement adding name into form
+                        $data['name'] = $companyName;
+
+                    } else {
+
+                        $form->get('data')->get('companyNumber')->setMessages(
+                            array(
+                                'company_number' => array(
+                                    'Sorry, we couldn\'t find any matching companies, please try again or enter your '
+                                    . 'details manually below'
+                                )
+                            )
+                        );
+                    }
+                }
+            } elseif (isset($tradingNames['submit_add_trading_name'])) {
+                /**
+                 * User has pressed 'Add another' on trading names
+                 */
+                $form->setValidationGroup(array('data' => ['tradingNames']));
+
+                if ($form->isValid()) {
+
+                    // remove existing entries from collection and check for empty entries
+                    $names = [];
+                    foreach ($tradingNames['trading_name'] as $key => $val) {
+                        if (strlen(trim($val['text'])) > 0) {
+                            $names[] = $val;
+                        }
+                    }
+                    $names[] = ['text' => ''];
+
+                    $form->get('data')->get('tradingNames')->get('trading_name')->populateValues($names);
+                }
+            } elseif ($form->isValid()) {
+                /**
+                 * Normal submission; save the form data
+                 */
+                if (count($tradingNames['trading_name'])) {
+                    $tradingNames = $this->formatTradingNamesDataForSave($orgId, $data);
+                    $this->getEntityService('TradingNames')->save($tradingNames);
+                }
+
+                $data = $this->formatDataForSave($data);
+                $data['id'] = $orgId;
+                $this->getEntityService('Organisation')->save($data);
+
+                return $this->completeSection('business_details');
             }
-
-            $data = $this->formatDataForSave($data);
-            $data['id'] = $orgId;
-            $this->getEntityService('Organisation')->save($data);
-
-            return $this->completeSection('business_details');
         }
 
         return new Section(
@@ -75,11 +143,11 @@ class BusinessDetailsController extends AbstractApplicationController
             )
         );
     }
-    public function formatTradingNamesDataForSave($data)
+    public function formatTradingNamesDataForSave($organisationId, $data)
     {
         $tradingNames = [];
 
-        foreach ($data['tradingNames']['trading_name'] as $tradingName) {
+        foreach ($data['data']['tradingNames']['trading_name'] as $tradingName) {
             if (trim($tradingName['text']) !== '') {
                 $tradingNames[] = [
                     'name' => $tradingName['text']
@@ -90,8 +158,8 @@ class BusinessDetailsController extends AbstractApplicationController
         $data['tradingNames'] = $tradingNames;
 
         return array(
-            'organisation' => $data['id'],
-            'licence' => $licence['id'],
+            'organisation' => $organisationId,
+            'licence' => $this->getLicenceId(),
             'tradingNames' => $tradingNames
         );
     }
