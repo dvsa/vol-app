47284f468fcb43b1fa47f604cba14188d0353706
Chris Riley <chris.riley@valtech.co.uk>
2014-10-10 08:37:58 +0100

    Added more dummy data

--- app/selfserve/module/Olcs/src/Service/Data/EbsrPack.php
+++ app/selfserve/module/Olcs/src/Service/Data/EbsrPack.php
@@ -16,10 +16,26 @@ class EbsrPack extends AbstractData
     {
         return [
             [
+                'status' => 'Recieved',
+                'filename' => 'PB000679.zip',
+                'submitted' => '2014-10-07'
+            ],
+            [
                 'status' => 'Processing',
                 'filename' => 'PB000678.zip',
                 'submitted' => '2014-10-07'
+            ],
+            [
+                'status' => 'Distributing',
+                'filename' => 'PB000677.zip',
+                'submitted' => '2014-10-07'
+            ],
+            [
+                'status' => 'Complete',
+                'filename' => 'PB000676.zip',
+                'submitted' => '2014-10-07'
             ]
+
         ];
     }
 }
