74df282b93481e802e876c6511bb45bf1652491e
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-10-13 11:20:15 +0100

    undo changes to to PiDefinition

--- app/internal/module/Olcs/src/Service/Data/PublicInquiryDefinition.php
+++ app/internal/module/Olcs/src/Service/Data/PublicInquiryDefinition.php
@@ -34,47 +34,4 @@ class PublicInquiryDefinition extends AbstractPublicInquiryData
         }
         return $optionData;
     }
-
-    /**
-     * @param $context
-     * @param bool $useGroups
-     * @return array
-     */
-    /*public function fetchListOptions($context, $useGroups = false)
-    {
-        $context = empty($context) ?
-            $this->getLicenceContext() : array_merge($context, $this->getLicenceContext());
-        $context['bundle'] = json_encode(['properties' => 'ALL']);
-        $context['order'] = 'sectionCode';
-
-        $data = $this->fetchPublicInquiryData($context);
-
-        if (!$data) {
-            return [];
-        }
-
-        return $this->formatData($data);
-    }*/
-
-    /**
-     * Ensures only a single call is made to the backend for each dataset
-     *
-     * @internal param $category
-     * @return array
-     */
-    /*public function fetchListData($params)
-    {
-        if (is_null($this->getData('PiDefinition'))) {
-
-            $data = $this->getRestClient()->get('', $params);
-
-            $this->setData('PiDefinition', false);
-
-            if (isset($data['Results'])) {
-                $this->setData('PiDefinition', $data['Results']);
-            }
-        }
-
-        return $this->getData('PiDefinition');
-    }*/
 }
