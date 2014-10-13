6c00083a23b6ef94c26f7ec4ded3598e559c1108
Nick Payne <nick@kurai.co.uk>
2014-10-13 14:46:18 +0100

    add external licence details business logic

---
+++ app/selfserve/module/Olcs/src/Controller/Licence/BusinessDetailsController.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * External Licence Business Details Controller
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
+namespace Olcs\Controller\Licence;
+
+use Common\Controller\Traits\Lva;
+
+/**
+ * External Licence Business Details Controller
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
+class BusinessDetailsController extends AbstractLicenceController
+{
+    use Lva\BusinessDetailsTrait;
+}
