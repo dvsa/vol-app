b18b1844c787e5a67f51a83c81c29ec5a465aa6f
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-10-15 13:46:47 +0100

    Added title to form

--- app/internal/module/Olcs/src/Form/Model/Form/PublicInquiryHearing.php
+++ app/internal/module/Olcs/src/Form/Model/Form/PublicInquiryHearing.php
@@ -9,7 +9,7 @@ use Zend\Form\Annotation as Form;
  * @Form\Name("Public inquiry hearing")
  * @Form\Attributes({"method":"post"})
  * @Form\Type("Common\Form\Form")
- * @Form\Options({"prefer_form_input_filter": true})
+ * @Form\Options({"prefer_form_input_filter": true, "label":"Hearing"})
  */
 class PublicInquiryHearing
 {
