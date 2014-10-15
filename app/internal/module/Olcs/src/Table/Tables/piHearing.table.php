12f86c4d17c24462d996ac28f667dd9bcdba0364
Ian Lindsay <ian@hemera-business-services.co.uk>
2014-10-15 13:44:36 +0100

    Table of Pi hearings

---
+++ app/internal/module/Olcs/src/Table/Tables/piHearing.table.php
@@ -0,0 +1,53 @@
+<?php
+
+return array(
+    'variables' => array(
+        'title' => 'Hearings'
+    ),
+    'settings' => array(
+        'crud' => array(
+            'formName' => 'PublicInquiryHearing',
+            'actions' => array(
+                'addHearing' => array('class' => 'primary', 'value' => 'Add Hearing'),
+                'editHearing' => array('requireRows' => true, 'value' => 'Edit Hearing')
+            )
+        ),
+        'paginate' => array(
+            'limit' => array(
+                'default' => 10,
+                'options' => array(10, 25, 50)
+            )
+        )
+    ),
+    'attributes' => array(
+    ),
+    'columns' => array(
+        array(
+            'title' => '&nbsp;',
+            'width' => 'checkbox',
+            'format' => '{{[elements/radio]}}'
+        ),
+        array(
+            'title' => 'Date of Pi',
+            'formatter' => function ($data, $column) {
+                $url = $this->generateUrl(['action' => 'edit', 'id' => $data['id'], 'pi' => $data['pi']['id']], 'case_pi_hearing', true);
+                $column['formatter'] = 'Date';
+                return '<a href="' . $url . '">' . date('d/m/Y', strtotime($data['hearingDate'])) . '</a>';
+            },
+        ),
+        array(
+            'title' => 'Venue',
+            'formatter' => function ($data) {
+                return (isset($data['piVenue']['name']) ? $data['piVenue']['name'] : $data['piVenueOther']);
+            }
+        ),
+        array(
+            'title' => 'Adjourned',
+            'name' => 'isAdjourned'
+        ),
+        array(
+            'title' => 'Cancelled',
+            'name' => 'isCancelled'
+        ),
+    )
+);
