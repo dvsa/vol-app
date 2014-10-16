5455b137c32470621963cf5b427f314b66beb467
Shaun Lizzio <shaun@lizzio.co.uk>
2014-10-16 11:25:02 +0100

    Configured route for new controller showing comment form

---
+++ app/internal/module/Olcs/src/Controller/Cases/Submission/SubmissionSectionCommentController.php
@@ -0,0 +1,138 @@
+<?php
+
+/**
+ * Cases SubmissionSectionComments Controller
+ *
+ * @author Shaun Lizzio <shaun.lizzio@valtech.co.uk>
+ */
+namespace Olcs\Controller\Cases\Submission;
+
+use Olcs\Controller as OlcsController;
+use Zend\View\Model\ViewModel;
+use Olcs\Controller\Cases\AbstractController as AbstractCasesController;
+use Olcs\Controller\Traits as ControllerTraits;
+
+/**
+ * Cases SubmissionSectionComment Controller
+ *
+ * @author Shaun Lizzio <shaun.lizzio@valtech.co.uk>
+ */
+class SubmissionSectionCommentController extends OlcsController\CrudAbstract
+{
+    use ControllerTraits\CaseControllerTrait;
+
+    /**
+     * Identifier name
+     *
+     * @var string
+     */
+    protected $identifierName = 'submissionSectionComment';
+
+    /**
+     * Table name string
+     *
+     * @var string
+     */
+    protected $tableName = 'submissionSectionComment';
+
+    /**
+     * Holds the form name
+     *
+     * @var string
+     */
+    protected $formName = 'comment';
+
+    /**
+     * The current page's extra layout, over and above the
+     * standard base template, a sibling of the base though.
+     *
+     * @var string
+     */
+    protected $pageLayout = 'case';
+
+    protected $detailsView = 'case/submission/details';
+
+    protected $pageLayoutInner = null;
+
+    /**
+     * Holds the service name
+     *
+     * @var string
+     */
+    protected $service = 'Submission';
+
+    /**
+     * Holds an array of variables for the default
+     * index list page.
+     */
+    protected $listVars = [
+        'case',
+    ];
+
+    /**
+     * Data map
+     *
+     * @var array
+     */
+    protected $dataMap = array(
+        'main' => array(
+            'mapFrom' => array(
+                'fields'
+            )
+        )
+    );
+
+    protected $action = false;
+
+    /**
+     * Holds the Data Bundle
+     *
+     * @var array
+     */
+    protected $dataBundle = array(
+        'properties' => 'ALL',
+        'children' => array(
+            'submission' => array(
+                'properties' => 'ALL'
+            )
+        )
+    );
+
+    /**
+     * Holds the navigation ID,
+     * required when an entire controller is
+     * represented by a single navigation id.
+     */
+    protected $navigationId = 'case_submissions';
+
+    /**
+     * Holds all the submission section ref data with descriptions
+     */
+    protected $submissionSectionRefData = array();
+
+    /**
+     * Complete section and save
+     * Redirects to details action.
+     *
+     * @param array $data
+     * @return array
+     */
+    public function processSave($data)
+    {
+        $result = $this->callParentProcessSave($data);
+
+        $id = isset($result['id']) ? $result['id'] : $data['fields']['id'];
+        return $this->redirect()->toRoute('submission', ['action' => 'details', 'submission' => $id], [], true);
+    }
+
+    /**
+     * Call parent process load and return result. Public method to allow unit testing
+     *
+     * @param array $data
+     * @return array
+     */
+    public function callParentSave($data, $service = null)
+    {
+        return parent::save($data, $service);
+    }
+}
