8b8ca5294119d055dd267a649487e66fd322be9c
Nick Payne <nick@kurai.co.uk>
2014-10-16 14:07:03 +0100

    initial operating centre external app behaviour

--- app/selfserve/module/Olcs/src/Controller/Application/OperatingCentresController.php
+++ app/selfserve/module/Olcs/src/Controller/Application/OperatingCentresController.php
@@ -8,6 +8,10 @@
  */
 namespace Olcs\Controller\Application;
 
+use Common\Controller\Traits\Lva;
+use Common\Service\Entity\LicenceEntityService;
+use Zend\Form\Form;
+
 /**
  * Operating Centres Controller
  *
@@ -16,8 +20,51 @@ namespace Olcs\Controller\Application;
  */
 class OperatingCentresController extends AbstractApplicationController
 {
-    public function indexAction()
+    use Lva\OperatingCentresTrait;
+
+    /**
+     * Remove trailer elements for PSV and set up Traffic Area section
+     *
+     * @param \Zend\Form\Form $form
+     * @return \Zend\Form\Form
+     */
+    protected function alterForm(Form $form)
     {
-        return $this->render('operating_centres');
+        // Make the same form alterations that are required for the summary section
+        $form = $this->makeFormAlterations($form, $this->getAlterFormOptions());
+
+        if (empty($this->tableData)) {
+            $form->remove('dataTrafficArea');
+            return $form;
+        }
+
+        $trafficArea = $this->getTrafficArea();
+        $trafficAreaId = $trafficArea ? $trafficArea['id'] : '';
+
+        $dataTrafficAreaFieldset = $form->get('dataTrafficArea');
+
+        if ($trafficAreaId) {
+
+            $nameExistsElement = $dataTrafficAreaFieldset->remove('trafficArea')->get('trafficAreaInfoNameExists');
+
+            $nameExistsElement->setValue(
+                str_replace('%NAME%', $trafficArea['name'], $nameExistsElement->getValue())
+            );
+            return $form;
+        }
+
+        $dataTrafficAreaFieldset->remove('trafficAreaInfoLabelExists')
+            ->remove('trafficAreaInfoNameExists')
+            ->remove('trafficAreaInfoHintExists')
+            ->get('trafficArea')
+            ->setValueOptions($this->getTrafficAreaSectionService()->getTrafficAreaValueOptions());
+
+        return $form;
+    }
+
+    protected function isPsv()
+    {
+        $data = $this->getTypeOfLicenceData();
+        return isset($data['goodsOrPsv']) && $data['goodsOrPsv'] === LicenceEntityService::LICENCE_CATEGORY_PSV;
     }
 }
