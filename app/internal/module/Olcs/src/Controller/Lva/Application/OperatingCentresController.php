16ef0093a3890519f242f9523628cc43f24a7702
Nick Payne <nick@kurai.co.uk>
2014-10-24 08:16:28 +0100

    reinstate basic internal application OC controller

---
+++ app/internal/module/Olcs/src/Controller/Lva/Application/OperatingCentresController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Internal Application Operating Centres Controller
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
+namespace Olcs\Controller\Lva\Application;
+
+use Common\Controller\Lva;
+use Olcs\Controller\Lva\Traits\ApplicationControllerTrait;
+
+/**
+ * Internal Application Operating Centres Controller
+ *
+ * @author Nick Payne <nick.payne@valtech.co.uk>
+ */
+class OperatingCentresController extends Lva\AbstractOperatingCentresController
+{
+    use ApplicationControllerTrait;
+
+    protected $lva = 'application';
+    protected $location = 'internal';
+}
