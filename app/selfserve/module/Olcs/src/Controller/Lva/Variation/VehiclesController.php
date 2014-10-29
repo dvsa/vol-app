37d64098465cb41e6ed8c50ca5861ba0c157d72c
Rob Caiger <rob@clocal.co.uk>
2014-10-29 16:52:21 +0000

    Vehicles

--- app/selfserve/module/Olcs/src/Controller/Lva/Variation/VehiclesController.php
+++ app/selfserve/module/Olcs/src/Controller/Lva/Variation/VehiclesController.php
@@ -1,32 +1,32 @@
 <?php
 
 /**
- * External Application Vehicles Controller
+ * External Variation Vehicles Controller
  *
  * @author Nick Payne <nick.payne@valtech.co.uk>
  * @author Rob Caiger <rob@clocal.co.uk>
  */
-namespace Olcs\Controller\Lva\Application;
+namespace Olcs\Controller\Lva\Variation;
 
 use Olcs\Controller\Lva\AbstractGenericVehiclesController;
-use Olcs\Controller\Lva\Traits\ApplicationControllerTrait;
+use Olcs\Controller\Lva\Traits\VariationControllerTrait;
 use Common\Controller\Lva\Traits;
 
 /**
- * External Application Vehicles Controller
+ * External Variation Vehicles Controller
  *
  * @author Nick Payne <nick.payne@valtech.co.uk>
  * @author Rob Caiger <rob@clocal.co.uk>
  */
 class VehiclesController extends AbstractGenericVehiclesController
 {
-    use ApplicationControllerTrait,
+    use VariationControllerTrait,
         Traits\ApplicationGenericVehiclesControllerTrait,
         Traits\ApplicationGoodsVehiclesControllerTrait {
             Traits\ApplicationGoodsVehiclesControllerTrait::alterTable as traitAlterTable;
         }
 
-    protected $lva = 'application';
+    protected $lva = 'variation';
     protected $location = 'external';
 
     /**
