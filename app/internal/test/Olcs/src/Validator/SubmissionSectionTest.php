b59b2ac427d7ae625bf73945a9744a7b61d2fb09
Shaun Lizzio <shaun@lizzio.co.uk>
2014-10-29 14:25:58 +0000

    Added UT for submission section validator

---
+++ app/internal/test/Olcs/src/Validator/SubmissionSectionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace OlcsTest\Validator;
+
+use Olcs\Validator\SubmissionSection;
+
+/**
+ * Class SubmissionSection Test
+ * @package OlcsTest\Validator
+ */
+class SubmissionSectionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provideIsValid
+     * @param $expected
+     * @param $value
+     */
+    public function testIsValid($value, $expected)
+    {
+        $sut = new SubmissionSection();
+
+        $this->assertEquals($expected, $sut->isValid($value));
+    }
+
+    public function provideIsValid()
+    {
+        return [
+            [[], false],
+            ['', false],
+            [false, false],
+            [['submissionType' => ''], false],
+            [['submissionType' => 'test'], true],
+            [['submissionType' => 'hey-im-set'], true],
+        ];
+    }
+}
