deb5ee83c6efde56b1f5f577908a6ba04ddab53e
Rob Caiger <rob@clocal.co.uk>
2014-10-30 15:16:32 +0000

    Bootstrap

--- app/internal/test/Bootstrap.php
+++ app/internal/test/Bootstrap.php
@@ -22,10 +22,8 @@ class Bootstrap
         $loader = static::initAutoloader();
 
         $loader->addPsr4('OlcsTest\\', __DIR__ . '/Olcs/src');
-        $loader->addPsr4(
-            'CommonTest\\',
-            dirname(__DIR__) . '/vendor/olcs/OlcsCommon/application_test/Common/src/Common'
-        );
+
+        self::getServiceManager();
 
         // Grab the application config
         $config = include dirname(__DIR__) . '/config/application.config.php';
