57e453d41c26a3d1954e2e10d5315eca023d673e
Chris Riley <chris.riley@valtech.co.uk>
2014-10-30 16:44:28 +0000

    Added in category data services, changed doc upload controller to make use of them.

---
+++ app/internal/test/Olcs/src/Service/Data/DocumentSubCategoryTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace OlcsTest\Service\Data;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Olcs\Service\Data\DocumentSubCategory;
+use Mockery as m;
+
+/**
+ * Class DocumentSubCategoryTest
+ * @package OlcsTest\Service\Data
+ */
+class DocumentSubCategoryTest extends TestCase
+{
+    public function testFetchListData()
+    {
+        $results = [
+            'Results' => [
+                ['result1'],
+                ['result2']
+            ]
+        ];
+
+        $mockRestClient = m::mock('\Common\Util\RestClient');
+        $mockRestClient->shouldReceive('get')->once()->with('', m::type('array'))->andReturn($results);
+        $sut = new DocumentSubCategory();
+        $sut->setRestClient($mockRestClient);
+
+        $this->assertEquals($results['Results'], $sut->fetchListData([]));
+        $sut->fetchListData([]);
+    }
+
+    public function testFetchListDataWithCategory()
+    {
+        $results = [
+            'Results' => [
+                ['result1'],
+                ['result2']
+            ]
+        ];
+
+        $mockRestClient = m::mock('\Common\Util\RestClient');
+        $mockRestClient->shouldReceive('get')->once()->with('', ['category' => 'testing'])->andReturn($results);
+        $sut = new DocumentSubCategory();
+        $sut->setRestClient($mockRestClient);
+        $sut->setCategory('testing');
+
+        $this->assertEquals($results['Results'], $sut->fetchListData([]));
+        $sut->fetchListData([]);
+    }
+}
