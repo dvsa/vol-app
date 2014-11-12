f00b3b5bbb5ba0d7d80177a9cd17f496e33a70c6
Rob Caiger <rob@clocal.co.uk>
2014-11-12 11:37:03 +0000

    moved duplicate method into trait

--- app/internal/test/Olcs/src/Controller/Application/ApplicationControllerTest.php
+++ app/internal/test/Olcs/src/Controller/Application/ApplicationControllerTest.php
@@ -14,7 +14,7 @@ use Common\Service\Entity\LicenceEntityService;
 use Common\Service\Data\CategoryDataService;
 use Common\Service\Data\FeeTypeDataService;
 use Common\Service\Entity\FeeEntityService;
-
+use CommonTest\Traits\MockDateTrait;
 /**
  * Application Controller Test
  *
@@ -22,6 +22,8 @@ use Common\Service\Entity\FeeEntityService;
  */
 class ApplicationControllerTest extends PHPUnit_Framework_TestCase
 {
+    use MockDateTrait;
+
     private $sut;
     private $sm;
     private $mockParams;
@@ -431,6 +433,7 @@ class ApplicationControllerTest extends PHPUnit_Framework_TestCase
 
     /**
      * Helper method
+     * @todo when these helper methods are required in more than 1 place, we need to abstract them away
      */
     protected function mockRouteParam($name, $value)
     {
@@ -479,18 +482,5 @@ class ApplicationControllerTest extends PHPUnit_Framework_TestCase
         $mockRedirect = $this->getMock('\Zend\Mvc\Controller\Plugin\Redirect', array('toRoute'));
         $this->pluginManager->setService('Redirect', $mockRedirect);
         return $mockRedirect;
-    }
-
-    /**
-     * Helper method
-     */
-    protected function mockDate($date)
-    {
-        $mockDateHelper = $this->getMock('\stdClass', ['getDate']);
-        $mockDateHelper->expects($this->any())
-            ->method('getDate')
-            ->will($this->returnValue($date));
-
-        $this->sm->setService('Helper\Date', $mockDateHelper);
     }
 }
