102001e596590b5cf06be01f9fa836e58208514d
Shaun Lizzio <shaun@lizzio.co.uk>
2014-11-18 17:26:30 +0000

    check oppositions array is populated

--- app/internal/module/Olcs/src/Service/Data/Submission.php
+++ app/internal/module/Olcs/src/Service/Data/Submission.php
@@ -291,29 +291,31 @@ class Submission extends AbstractData implements CloseableInterface
     protected function filterOppositionsData(array $data = array())
     {
         $dataToReturnArray = array();
-        foreach ($data['application']['oppositions'] as $opposition) {
-            $thisOpposition['id'] = $opposition['id'];
-            $thisOpposition['version'] = $opposition['version'];
-            $thisOpposition['dateReceived'] = $opposition['raisedDate'];
-            $thisOpposition['oppositionType'] = $opposition['opposer']['opposerType']['description'];
-            $thisOpposition['contactName']['forename'] =
-                $opposition['opposer']['contactDetails']['person']['forename'];
-            $thisOpposition['contactName']['familyName'] =
-                $opposition['opposer']['contactDetails']['person']['familyName'];
+        if (isset($data['application']['oppositions']) && is_array($data['application']['oppositions'])) {
+            foreach ($data['application']['oppositions'] as $opposition) {
+                $thisOpposition['id'] = $opposition['id'];
+                $thisOpposition['version'] = $opposition['version'];
+                $thisOpposition['dateReceived'] = $opposition['raisedDate'];
+                $thisOpposition['oppositionType'] = $opposition['opposer']['opposerType']['description'];
+                $thisOpposition['contactName']['forename'] =
+                    $opposition['opposer']['contactDetails']['person']['forename'];
+                $thisOpposition['contactName']['familyName'] =
+                    $opposition['opposer']['contactDetails']['person']['familyName'];
 
-            foreach ($opposition['grounds'] as $ground) {
-                $thisOpposition['grounds'][] = $ground['grounds']['description'];
+                foreach ($opposition['grounds'] as $ground) {
+                    $thisOpposition['grounds'][] = $ground['grounds']['description'];
+                }
+
+                $thisOpposition['isValid'] = $opposition['isValid'];
+                $thisOpposition['isCopied'] = $opposition['isCopied'];
+                $thisOpposition['isInTime'] = $opposition['isInTime'];
+                $thisOpposition['isPublicInquiry'] = $opposition['isPublicInquiry'];
+                $thisOpposition['isWithdrawn'] = $opposition['isWithdrawn'];
+
+                $dataToReturnArray[] = $thisOpposition;
             }
-
-            $thisOpposition['isValid'] = $opposition['isValid'];
-            $thisOpposition['isCopied'] = $opposition['isCopied'];
-            $thisOpposition['isInTime'] = $opposition['isInTime'];
-            $thisOpposition['isPublicInquiry'] = $opposition['isPublicInquiry'];
-            $thisOpposition['isWithdrawn'] = $opposition['isWithdrawn'];
-
-            $dataToReturnArray[] = $thisOpposition;
         }
-
+        
         return $dataToReturnArray;
     }
 
