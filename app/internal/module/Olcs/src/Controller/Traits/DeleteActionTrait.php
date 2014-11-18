085b4c6ad92d8eea1f9832d387a8e0687e061e67
Shaun Lizzio <shaun@lizzio.co.uk>
2014-11-18 11:38:06 +0000

    Added confirm plugin to deleteActionTrait. Added entityDisplayName

--- app/internal/module/Olcs/src/Controller/Traits/DeleteActionTrait.php
+++ app/internal/module/Olcs/src/Controller/Traits/DeleteActionTrait.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Olcs\Controller\Traits;
+use Zend\View\Model\ViewModel;
 
 /**
  * Class DeleteActionTrait
@@ -15,9 +16,18 @@ trait DeleteActionTrait
     {
         $identifierName = $this->getIdentifierName();
         $id = $this->params()->fromRoute($identifierName);
+
+        $response = $this->confirm(
+            'Are you sure you wish to delete this ' . $this->getEntityDisplayName() . '?'
+        );
+
+        if ($response instanceof ViewModel) {
+            return $this->renderView($response);
+        }
+
         $this->makeRestCall($this->getDeleteServiceName(), 'DELETE', ['id' => $id]);
 
-        $this->addErrorMessage('Deleted sucessfully');
+        $this->addErrorMessage('Deleted successfully');
 
         $this->redirectToIndex();
     }
