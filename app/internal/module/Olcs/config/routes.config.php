09f4160827e261e5849b91fa0a460e2b63ba6eed
Shaun Lizzio <shaun@lizzio.co.uk>
2014-11-21 11:15:44 +0000

    Fixed impounding navigation when on edit pages (uses 'id')

--- app/internal/module/Olcs/config/routes.config.php
+++ app/internal/module/Olcs/config/routes.config.php
@@ -586,10 +586,10 @@ $routes = [
     'case_details_impounding' => [
         'type' => 'segment',
         'options' => [
-            'route' => '/case/[:case]/impounding[/:action][/:id]',
+            'route' => '/case/[:case]/impounding[/:action][/:impounding]',
             'constraints' => [
                 'case' => '[0-9]+',
-                'id' => '[0-9]+'
+                'impounding' => '[0-9]+'
             ],
             'defaults' => [
                 'controller' => 'CaseImpoundingController',
