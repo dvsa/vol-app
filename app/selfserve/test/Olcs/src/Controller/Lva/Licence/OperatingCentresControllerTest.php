725b7e7f3b82574968b09cf9684ec68bae7e5601
Nick Payne <nick@kurai.co.uk>
2014-11-25 16:00:22 +0000

    tests covering new licence OC behaviour

--- app/selfserve/test/Olcs/src/Controller/Lva/Licence/OperatingCentresControllerTest.php
+++ app/selfserve/test/Olcs/src/Controller/Lva/Licence/OperatingCentresControllerTest.php
@@ -129,4 +129,169 @@ class OperatingCentresControllerTest extends AbstractLvaControllerTestCase
 
         $this->sut->indexAction();
     }
+
+    public function testBasicPostEditAction()
+    {
+        $form = $this->createMockForm('Lva\OperatingCentre');
+
+        $data = [
+            'applicationOperatingCentre' => [
+                'id' => '1'
+            ],
+            'operatingCentre' => [
+                'id' => '16'
+            ]
+        ];
+
+        $dataFilter = m::mock()
+            ->shouldReceive('has')
+            ->with('noOfVehiclesRequired')
+            ->andReturn(false)
+            ->shouldReceive('has')
+            ->with('noOfTrailersRequired')
+            ->andReturn(false)
+            ->getMock();
+
+        $addressInputFilter = m::mock()
+            ->shouldReceive('get')
+            ->with('postcode')
+            ->andReturn(
+                m::mock()
+                ->shouldReceive('setRequired')
+                ->with(false)
+                ->andReturn(
+                    m::mock()
+                    ->shouldReceive('getValidatorChain')
+                    ->andReturn(
+                        m::mock()
+                        ->shouldReceive('attach')
+                        ->getMock()
+                    )
+                    ->getMock()
+                )
+                ->getMock()
+            )
+            ->getMock();
+
+        $inputFilter = m::mock()
+            ->shouldReceive('get')
+            ->with('data')
+            ->andReturn($dataFilter)
+            ->shouldReceive('get')
+            ->with('address')
+            ->andReturn($addressInputFilter)
+            ->getMock();
+
+        $mockElement = m::mock('\Zend\Form\Element');
+
+        $addressElement = m::mock()
+            ->shouldReceive('get')
+            ->with('searchPostcode')
+            ->andReturn($mockElement)
+            ->shouldReceive('get')
+            ->with('addressLine1')
+            ->andReturn($mockElement)
+            ->shouldReceive('get')
+            ->with('town')
+            ->andReturn($mockElement)
+            ->shouldReceive('get')
+            ->with('postcode')
+            ->andReturn($mockElement)
+            ->shouldReceive('get')
+            ->with('countryCode')
+            ->andReturn($mockElement)
+            ->getMock();
+
+        $form->shouldReceive('setData')
+            ->with([])
+            ->andReturn($form)
+            ->shouldReceive('getInputFilter')
+            ->andReturn($inputFilter)
+            ->shouldReceive('get')
+            ->with('address')
+            ->andReturn($addressElement)
+            ->shouldReceive('has')
+            ->with('advertisements')
+            ->andReturn(false)
+            ->shouldReceive('isValid')
+            ->andReturn(true)
+            ->shouldReceive('getData')
+            ->andReturn($data);
+
+        $this->shouldRemoveAddAnother($form);
+
+        $this->getMockFormHelper()
+            ->shouldReceive('processAddressLookupForm')
+            ->andReturn(false)
+            ->shouldReceive('disableElements')
+            ->with($addressElement)
+            ->shouldReceive('disableValidation')
+            ->with($addressInputFilter)
+            ->shouldReceive('lockElement')
+            ->times(5);
+
+        $this->sut->shouldReceive('getTypeOfLicenceData')
+            ->andReturn(
+                [
+                    'licenceType' => [
+                        'id' => 'ltyp_sn'
+                    ],
+                    'niFlag' => 'N'
+                ]
+            )
+            ->shouldReceive('params')
+            ->with('child_id')
+            ->andReturn(4321)
+            ->shouldReceive('getLicenceId')
+            ->andReturn(7)
+            ->shouldReceive('getIdentifier')
+            ->andReturn(9)
+            ->shouldReceive('handlePostSave')
+            ->andReturn('saved');
+
+        $this->mockEntity('Licence', 'getTrafficArea')
+            ->with(7)
+            ->andReturn(['id' => 'B']);
+
+        $this->mockEntity('LicenceOperatingCentre', 'getOperatingCentresCount')
+            ->with(9)
+            ->andReturn(
+                [
+                    'Count' => 0
+                ]
+            );
+
+        $this->mockEntity('LicenceOperatingCentre', 'getVehicleAuths')
+            ->with(4321)
+            ->andReturn(
+                [
+                    'noOfVehicles' => 0,
+                    'noOfTrailers' => 0
+                ]
+            );
+
+        $this->mockEntity('OperatingCentre', 'save')
+            ->with(
+                [
+                    'id' => '16'
+                ]
+            );
+
+        $this->mockEntity('LicenceOperatingCentre', 'save');
+
+        $removeFields = [
+            'advertisements',
+            'data->sufficientParking',
+            'data->permission'
+        ];
+
+        $this->shouldRemoveElements($form, $removeFields);
+
+        $this->setPost();
+
+        $this->assertEquals(
+            'saved',
+            $this->sut->editAction()
+        );
+    }
 }
