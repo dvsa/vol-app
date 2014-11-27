ae20147dc14cfc792bfa62052aae920eab266535
Shaun Lizzio <shaun@lizzio.co.uk>
2014-11-27 12:52:07 +0000

    Removed current licence from list as not strictly a 'linked' licence

--- app/internal/module/Olcs/src/Filter/SubmissionSection/LinkedLicencesAppNumbers.php
+++ app/internal/module/Olcs/src/Filter/SubmissionSection/LinkedLicencesAppNumbers.php
@@ -18,20 +18,22 @@ class LinkedLicencesAppNumbers extends AbstractSubmissionSectionFilter
     public function filter($data = array())
     {
         $dataToReturnArray = array();
-        if (!empty($data['licence']['organisation']['licences'])) {
+        if (!empty($data['licence']['organisation']['licences']) &&
+            count($data['licence']['organisation']['licences']) > 1) {
             foreach ($data['licence']['organisation']['licences'] as $licence) {
                 $thisRow = array();
-                $thisRow['id'] = $licence['id'];
-                $thisRow['version'] = $licence['version'];
-                $thisRow['licNo'] = $licence['licNo'];
-                $thisRow['status'] = $licence['status']['description'];
-                $thisRow['licenceType'] = $licence['licenceType']['description'];
-                $thisRow['totAuthTrailers'] = $licence['totAuthTrailers'];
-                $thisRow['totAuthVehicles'] = $licence['totAuthVehicles'];
-                $thisRow['vehiclesInPossession'] = $this->calculateVehiclesInPossession($licence);
-                $thisRow['trailersInPossession'] = $this->calculateTrailersInPossession($data['licence']);
-
-                $dataToReturnArray[] = $thisRow;
+                if ($licence['id'] != $data['licence']['id']) {
+                    $thisRow['id'] = $licence['id'];
+                    $thisRow['version'] = $licence['version'];
+                    $thisRow['licNo'] = $licence['licNo'];
+                    $thisRow['status'] = $licence['status']['description'];
+                    $thisRow['licenceType'] = $licence['licenceType']['description'];
+                    $thisRow['totAuthTrailers'] = $licence['totAuthTrailers'];
+                    $thisRow['totAuthVehicles'] = $licence['totAuthVehicles'];
+                    $thisRow['vehiclesInPossession'] = $this->calculateVehiclesInPossession($licence);
+                    $thisRow['trailersInPossession'] = $this->calculateTrailersInPossession($data['licence']);
+                    $dataToReturnArray[] = $thisRow;
+                }
             }
         }
         return $dataToReturnArray;
