09c20f5c24a3d19bd0414cc5020d462997921bb7
Shaun Lizzio <shaun@lizzio.co.uk>
2014-12-15 16:15:27 +0000

    changed context to request 'services' rather than entities

--- app/internal/module/Olcs/src/Form/Model/Fieldset/BusRegisterService.php
+++ app/internal/module/Olcs/src/Form/Model/Fieldset/BusRegisterService.php
@@ -90,7 +90,7 @@ class BusRegisterService extends Base
      *     "label": "Correspondence address",
      *     "disable_inarray_validator": false,
      *     "service_name": "Common\Service\Data\AddressListDataService",
-     *     "context": {"entities":{"licence"},"order":{"correspondence"}}
+     *     "context": {"services":{"licence"},"order":{"correspondence"}}
      * })
      * @Form\Type("DynamicSelect")
      */
