ae9f14dbe03659aa12b7f60c30b48ac0630b942d
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-01-07 10:20:06 +0000

    Fieldset to replace the textareas when publication status is not new

---
+++ app/internal/module/Olcs/src/Form/Model/Fieldset/PublicationNotNew.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Olcs\Form\Model\Fieldset;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * @codeCoverageIgnore Auto-generated file with no methods
+ * @Form\Name("readOnlyText")
+ */
+class PublicationNotNew
+{
+    /**
+     * @Form\Options({"label":"Text 1"})
+     */
+    public $text1 = null;
+
+    /**
+     * @Form\Options({"label":"Text 2"})
+     */
+    public $text2 = null;
+
+    /**
+     * @Form\Options({"label":"Text 3"})
+     */
+    public $text3 = null;
+}
