aacb85e2cdc00c383f53daa20548ec9d353b7054
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-01-07 17:28:18 +0000

    Textarea type used to force readonly field onto new line

--- app/internal/module/Olcs/src/Form/Model/Fieldset/PublicationNotNew.php
+++ app/internal/module/Olcs/src/Form/Model/Fieldset/PublicationNotNew.php
@@ -12,16 +12,19 @@ class PublicationNotNew
 {
     /**
      * @Form\Options({"label":"Text 1"})
+     * @Form\Type("TextArea")
      */
     public $text1 = null;
 
     /**
      * @Form\Options({"label":"Text 2"})
+     * @Form\Type("TextArea")
      */
     public $text2 = null;
 
     /**
      * @Form\Options({"label":"Text 3"})
+     * @Form\Type("TextArea")
      */
     public $text3 = null;
 }
