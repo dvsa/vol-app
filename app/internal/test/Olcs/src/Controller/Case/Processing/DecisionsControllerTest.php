2a50aadc9962585a7b42d708efec45855c3e0174
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-01-16 13:37:13 +0000

    Test for getDAtaForForm and redirectToIndex

--- app/internal/test/Olcs/src/Controller/Case/Processing/DecisionsControllerTest.php
+++ app/internal/test/Olcs/src/Controller/Case/Processing/DecisionsControllerTest.php
@@ -7,6 +7,7 @@ use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Mockery as m;
 use Olcs\TestHelpers\ControllerPluginManagerHelper;
 use Olcs\TestHelpers\ControllerDetailsActionHelper;
+use Olcs\TestHelpers\ControllerRouteMatchHelper;
 
 /**
  * Class DecisionsControllerTest
@@ -24,11 +25,17 @@ class DecisionsControllerTest extends MockeryTestCase
      */
     protected $detailsHelper;
 
+    /**
+     * @var ControllerRouteMatchHelper
+     */
+    protected $routeMatchHelper;
+
     public function setUp()
     {
         $this->sut = new DecisionsController();
         $this->pluginManagerHelper = new ControllerPluginManagerHelper();
         $this->detailsHelper = new ControllerDetailsActionHelper();
+        $this->routeMatchHelper = new ControllerRouteMatchHelper();
 
         parent::setUp();
     }
@@ -51,6 +58,9 @@ class DecisionsControllerTest extends MockeryTestCase
         $this->assertInstanceOf('\Zend\View\Model\ViewModel', $this->sut->detailsAction());
     }
 
+    /**
+     * Tests the details action when no data is found
+     */
     public function testDetailsActionNotFound()
     {
         $id = null;
@@ -66,5 +76,141 @@ class DecisionsControllerTest extends MockeryTestCase
         $this->sut->setEvent($this->detailsHelper->getNotFoundEvent());
 
         $this->assertInstanceOf('\Zend\View\Model\ViewModel', $this->sut->detailsAction());
+    }
+
+    /**
+     * Checks that the decision id is correctly appended to the form data
+     */
+    public function testGetDataForFormEdit()
+    {
+        $decision = 2;
+        $id = 1;
+        $action = 'edit';
+        $mockRestData = ['id' => $id];
+
+        $expected = [
+            'id' => $id,
+            'fields' => [
+                'id' => $id,
+                'decision' => $decision
+            ],
+            'base' => [
+                'id' => $id,
+                'fields' => [
+                    'id' => $id,
+                ],
+            ]
+        ];
+
+        //mock plugin manager
+        $mockPluginManager = $this->pluginManagerHelper->getMockPluginManager(
+            [
+                'params' => 'Params'
+            ]
+        );
+
+        //rest call to return prohibition data
+        $mockRestHelper = m::mock('RestHelper');
+        $mockRestHelper->shouldReceive('makeRestCall')->withAnyArgs()->andReturn($mockRestData);
+
+        //route params
+        $mockParams = $mockPluginManager->get('params', '');
+        $mockParams->shouldReceive('fromRoute')->with('decision')->andReturn($decision);
+        $mockParams->shouldReceive('fromRoute')->with('action')->andReturn($action);
+        $mockParams->shouldReceive('fromRoute')->with('id')->andReturn($id);
+        $this->sut->setPluginManager($mockPluginManager);
+
+        //mock service manager
+        $mockServiceManager = m::mock('\Zend\ServiceManager\ServiceManager');
+        $mockServiceManager->shouldReceive('get')->with('Helper\Rest')->andReturn($mockRestHelper);
+
+        $this->sut->setServiceLocator($mockServiceManager);
+
+        $data = $this->sut->getDataForForm();
+
+        $this->assertEquals($data, $expected);
+    }
+
+    /**
+     * Checks that the decision id is correctly appended to the form data
+     */
+    public function testGetDataForFormAdd()
+    {
+        $decision = 2;
+        $id = 1;
+        $case = 24;
+        $action = 'add';
+        $mockRestData = false;
+
+        $expected = [
+            'case' => $case,
+            'fields' => [
+                'case' => $case,
+                'decision' => $decision
+            ],
+            'base' => [
+                'case' => $case
+            ]
+        ];
+
+        //mock plugin manager
+        $mockPluginManager = $this->pluginManagerHelper->getMockPluginManager(
+            [
+                'params' => 'Params'
+            ]
+        );
+
+        //rest call to return prohibition data
+        $mockRestHelper = m::mock('RestHelper');
+        $mockRestHelper->shouldReceive('makeRestCall')->withAnyArgs()->andReturn($mockRestData);
+
+        //route params
+        $mockParams = $mockPluginManager->get('params', '');
+        $mockParams->shouldReceive('fromRoute')->with('decision')->andReturn($decision);
+        $mockParams->shouldReceive('fromRoute')->with('action')->andReturn($action);
+        $mockParams->shouldReceive('fromRoute')->with('id')->andReturn($id);
+        $mockParams->shouldReceive('fromQuery')->with('case', null)->andReturn($case);
+        $this->sut->setPluginManager($mockPluginManager);
+
+        $event = $this->routeMatchHelper->getMockRouteMatch(array('action' => 'not-found'));
+        $this->sut->setEvent($event);
+
+        //mock service manager
+        $mockServiceManager = m::mock('\Zend\ServiceManager\ServiceManager');
+        $mockServiceManager->shouldReceive('get')->with('Helper\Rest')->andReturn($mockRestHelper);
+
+        $this->sut->setServiceLocator($mockServiceManager);
+
+        $data = $this->sut->getDataForForm();
+
+        $this->assertEquals($data, $expected);
+    }
+
+    /**
+     * Tests redirectToIndex
+     */
+    public function testRedirectToIndex()
+    {
+        $case = 1;
+
+        $mockPluginManager = $this->pluginManagerHelper->getMockPluginManager(
+            ['params' => 'Params', 'redirect' => 'Redirect']
+        );
+
+        $mockParams = $mockPluginManager->get('params', '');
+        $mockParams->shouldReceive('fromRoute')->with('case')->andReturn($case);
+
+        $mockRedirect = $mockPluginManager->get('redirect', '');
+        $mockRedirect->shouldReceive('toRouteAjax')->with(
+            'processing_decisions',
+            ['action'=>'details', 'case' => $case],
+            ['code' => '303'], false
+        )->andReturn('redirectResponse');
+
+        $mockPluginManager->shouldReceive('get')->with('redirect')->andReturn($mockRedirect);
+
+        $this->sut->setPluginManager($mockPluginManager);
+
+        $this->assertEquals('redirectResponse', $this->sut->redirectToIndex());
     }
 }
