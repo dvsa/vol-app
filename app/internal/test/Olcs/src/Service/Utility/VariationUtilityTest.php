3998723010fbd54bc87ade95f3a469ef9ccc36f8
Rob Caiger <rob@clocal.co.uk>
2015-01-16 09:24:55 +0000

    Utility to alter variation data

---
+++ app/internal/test/Olcs/src/Service/Utility/VariationUtilityTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Variation Utility test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+namespace OlcsTest\Service\Utility;
+
+use Olcs\Service\Utility\VariationUtility;
+
+/**
+ * Variation Utility Test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+class VariationUtilityTest extends \PHPUnit_Framework_TestCase
+{
+    protected $sut;
+
+    public function setUp()
+    {
+        $this->sut = new VariationUtility();
+    }
+
+    public function testAlterCreateVariationData()
+    {
+        $data = [
+            'foo' => 'bar',
+            'status' => 'foo'
+        ];
+
+        $expectedData = [
+            'foo' => 'bar',
+            'status' => ApplicationEntityService::APPLICATION_STATUS_UNDER_CONSIDERATION
+        ];
+
+        $this->assertEquals($expectedData, $this->sut->alterCreateVariationData($data));
+    }
+}
