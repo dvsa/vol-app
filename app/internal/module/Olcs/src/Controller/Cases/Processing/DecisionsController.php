dc369319f13c12c19dd7bb8a5aaa5e68462bd8ef
Shaun Lizzio <shaun@lizzio.co.uk>
2015-01-19 16:09:37 +0000

    Removed blank line

--- app/internal/module/Olcs/src/Controller/Cases/Processing/DecisionsController.php
+++ app/internal/module/Olcs/src/Controller/Cases/Processing/DecisionsController.php
@@ -159,7 +159,6 @@ class DecisionsController extends OlcsController\CrudAbstract implements CaseCon
         return $data;
     }
 
-
     public function redirectToIndex()
     {
         return $this->redirectToRouteAjax(
