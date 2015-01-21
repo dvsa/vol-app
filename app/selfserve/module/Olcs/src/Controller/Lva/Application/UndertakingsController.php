97090e464a732cd71c6ec05eac017c85042b756e
Dan Eggleston <dan@stolenegg.com>
2015-01-21 17:44:09 +0000

    OLCS-2236 initial work on external application/variation controllers to populate declaration and undertakings content

--- app/selfserve/module/Olcs/src/Controller/Lva/Application/UndertakingsController.php
+++ app/selfserve/module/Olcs/src/Controller/Lva/Application/UndertakingsController.php
@@ -21,4 +21,52 @@ class UndertakingsController extends Lva\AbstractUndertakingsController
 
     protected $lva = 'application';
     protected $location = 'external';
+
+    /**
+     * @param \Zend\Form\Form
+     */
+    protected function alterFormForLva(\Zend\Form\Form $form)
+    {
+        $fieldSet = $form->get('declarationsAndUndertakings');
+
+        $application = [];
+        $fieldSet->get('undertakings')->setValue($this->getUndertakingsPartial($application));
+        $fieldSet->get('declarations')->setValue($this->getDeclarationsPartial($application));
+    }
+
+    /**
+     * Determine correct partial to use for undertakings html
+     *
+     * (public for unit testing)
+     *
+     * @param array $application application data
+     * @return string
+     */
+    public function getUndertakingsPartial(array $application) {
+        $prefix = 'markup-undertakings-';
+
+        // @TODO switch on PSV / Goods / S,SI,SR,S
+        // options are gv79-standard, gv79-restricted, psv421-standard, psv421-restricted, psv-356 (SR)
+        $part = 'gv79-restricted';
+
+        return $prefix.$part;
+    }
+
+    /**
+     * Determine correct partial to use for undertakings html
+     *
+     * (public for unit testing)
+     *
+     * @param array $application application data
+     * @return string
+     */
+    public function getDeclarationsPartial(array $application) {
+        $prefix = 'markup-declarations-';
+
+        // @TODO switch on PSV / Goods / S,SI,SR,S
+        // options are gv79, psv421, psv-356
+        $part = 'gv79';
+
+        return $prefix.$part;
+    }
 }
--- app/selfserve/module/Olcs/src/Controller/Lva/Variation/UndertakingsController.php
+++ app/selfserve/module/Olcs/src/Controller/Lva/Variation/UndertakingsController.php
@@ -21,4 +21,52 @@ class UndertakingsController extends Lva\AbstractUndertakingsController
 
     protected $lva = 'variation';
     protected $location = 'external';
+
+    /**
+     * @param \Zend\Form\Form
+     */
+    protected function alterFormForLva(\Zend\Form\Form $form)
+    {
+        $fieldSet = $form->get('declarationsAndUndertakings');
+
+        $application = [];
+        $fieldSet->get('undertakings')->setValue($this->getUndertakingsPartial($application));
+        $fieldSet->get('declarations')->setValue($this->getDeclarationsPartial($application));
+    }
+
+    /**
+     * Determine correct partial to use for undertakings html
+     *
+     * (public for unit testing)
+     *
+     * @param array $application application data
+     * @return string
+     */
+    public function getUndertakingsPartial(array $application) {
+        $prefix = 'markup-undertakings-';
+
+        // @TODO switch on PSV / Goods / upgrade
+        // options are gv81-standard, gv81-restricted, gv80a, psv430-431
+        $part = 'psv430-431';
+
+        return $prefix.$part;
+    }
+
+    /**
+     * Determine correct partial to use for undertakings html
+     *
+     * (public for unit testing)
+     *
+     * @param array $application application data
+     * @return string
+     */
+    public function getDeclarationsPartial(array $application) {
+        $prefix = 'markup-declarations-';
+
+        // @TODO switch on PSV / Goods / upgrade
+        // options are gv81-standard, gv81-restricted, gv80a, psv430-431-standard, psv430-431-restricted
+        $part = 'psv430-431-standard';
+
+        return $prefix.$part;
+    }
 }
