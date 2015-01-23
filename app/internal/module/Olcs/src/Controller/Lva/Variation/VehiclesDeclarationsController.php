fc4230adca92d303710b2f10d3b5a7b3d1c0fc5a
Rob Caiger <rob@clocal.co.uk>
2015-01-23 14:42:16 +0000

    Added variation controllers

---
+++ app/internal/module/Olcs/src/Controller/Lva/Variation/VehiclesDeclarationsController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Internal Variation Vehicles Declarations Controller
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+namespace Olcs\Controller\Lva\Variation;
+
+use Common\Controller\Lva;
+use Olcs\Controller\Lva\Traits\VariationControllerTrait;
+
+/**
+ * Internal Variation Vehicles Declarations Controller
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+class VehiclesDeclarationsController extends Lva\AbstractVehiclesDeclarationsController
+{
+    use VariationControllerTrait;
+
+    protected $lva = 'variation';
+    protected $location = 'internal';
+}
