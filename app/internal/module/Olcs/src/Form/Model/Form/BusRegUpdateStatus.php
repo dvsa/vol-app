faae9702eb122ca21e704e3f382c58738bd05a27
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-01-30 14:19:04 +0000

    Forms for updating bus reg statuses

---
+++ app/internal/module/Olcs/src/Form/Model/Form/BusRegUpdateStatus.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Olcs\Form\Model\Form;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * @codeCoverageIgnore Auto-generated file with no methods
+ * @Form\Name("bus-reg-update-status")
+ * @Form\Attributes({"method":"post"})
+ * @Form\Type("Common\Form\Form")
+ * @Form\Options({"prefer_form_input_filter": true, "label":"Update status", "override_form_label": true})
+ */
+class BusRegUpdateStatus
+{
+    /**
+     * @Form\Name("fields")
+     * @Form\ComposedObject("Olcs\Form\Model\Fieldset\BusRegUpdateStatus")
+     */
+    public $fields = null;
+
+    /**
+     * @Form\Name("form-actions")
+     * @Form\Attributes({"class":"actions-container"})
+     * @Form\ComposedObject("Olcs\Form\Model\Fieldset\CancelFormActions")
+     */
+    public $formActions = null;
+
+}
