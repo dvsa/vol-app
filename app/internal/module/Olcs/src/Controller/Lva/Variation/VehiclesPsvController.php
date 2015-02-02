00f41c6abcf21312eab1aa0267183abb9fb07e43
Rob Caiger <rob@clocal.co.uk>
2015-02-02 11:06:33 +0000

    Started tidying up vehicle controllers

--- app/internal/module/Olcs/src/Controller/Lva/Variation/VehiclesPsvController.php
+++ app/internal/module/Olcs/src/Controller/Lva/Variation/VehiclesPsvController.php
@@ -10,7 +10,6 @@ namespace Olcs\Controller\Lva\Variation;
 use Common\Controller\Lva\AbstractVehiclesPsvController;
 use Common\Controller\Lva\Traits;
 use Olcs\Controller\Lva\Traits\VariationControllerTrait;
-use Olcs\Controller\Lva\Traits\LicenceGenericVehiclesControllerTrait;
 
 /**
  * Internal Variation Vehicles PSV Controller
@@ -21,8 +20,8 @@ class VehiclesPsvController extends AbstractVehiclesPsvController
 {
     use VariationControllerTrait,
         Traits\PsvLicenceControllerTrait,
-        // @NOTE: AC says variations behave exactly as per licences, so...
-        Traits\LicenceGenericVehiclesControllerTrait,
+        // @NOTE this at the moment just sets the application id of the licence vehicle
+        Traits\ApplicationGenericVehiclesControllerTrait,
         Traits\PsvGoodsLicenceVariationControllerTrait;
 
     protected $lva = 'variation';
