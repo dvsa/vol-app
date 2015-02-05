8cf67ede55b06f4a019dc28ce93dc46253d7aa60
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-02-05 16:56:55 +0000

    Bus Reg variation reason form

---
+++ app/internal/module/Olcs/src/Form/Model/Form/BusRegVariationReason.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Olcs\Form\Model\Form;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * @codeCoverageIgnore Auto-generated file with no methods
+ * @Form\Name("bus-reg-variation-reason")
+ * @Form\Attributes({"method":"post"})
+ * @Form\Type("Common\Form\Form")
+ * @Form\Options({"prefer_form_input_filter": true, "label":"Grant variation", "override_form_label": true})
+ */
+class BusRegVariationReason
+{
+    /**
+     * @Form\Name("fields")
+     * @Form\ComposedObject("Olcs\Form\Model\Fieldset\BusRegVariationReason")
+     */
+    public $fields = null;
+
+    /**
+     * @Form\Name("form-actions")
+     * @Form\Attributes({"class":"actions-container"})
+     * @Form\ComposedObject("Olcs\Form\Model\Fieldset\CancelFormActions")
+     */
+    public $formActions = null;
+}
