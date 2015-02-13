d2e7935e807c845c94414eafa68db09d80204520
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-02-13 11:42:48 +0000

    Form for Tm Case decision - no further action

---
+++ app/internal/module/Olcs/src/Form/Model/Fieldset/TmCaseNoFurtherAction.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Olcs\Form\Model\Fieldset;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * @codeCoverageIgnore Auto-generated file with no methods
+ * @Form\Name("fields")
+ */
+class TmCaseNoFurtherAction extends CaseBase
+{
+    /**
+     * @Form\Type("Radio")
+     * @Form\Options({
+     *      "label": "MSI",
+     *      "value_options":{
+     *          "N":"No",
+     *          "Y":"Yes"
+     *      },
+     *      "fieldset-attributes" : {
+     *          "class":"inline"
+     *      }
+     * })
+     */
+    public $isMsi;
+
+    /**
+     * @Form\Options({
+     *     "label": "Date of decision",
+     *     "create_empty_option": true,
+     *     "render_delimiters": false
+     * })
+     * @Form\Type("DateSelect")
+     * @Form\Validator({"name":"Date", "options":{"format":"Y-m-d"}})
+     * @Form\Filter({"name": "DateSelectNullifier"})
+     */
+    public $decisionDate = null;
+
+    /**
+     * @Form\Required(false)
+     * @Form\Attributes({"required":false})
+     * @Form\Options({
+     *     "label": "Date of notification (to TM)",
+     *     "create_empty_option": true,
+     *     "render_delimiters": false
+     * })
+     * @Form\Type("DateSelect")
+     * @Form\AllowEmpty(true)
+     *
+     * @Form\Validator({"name": "ValidateIf",
+     *      "options":{
+     *          "context_field": "decisionDate",
+     *          "context_values": {"--"},
+     *          "context_truth": false,
+     *          "allow_empty" : true,
+     *          "validators": {
+     *              {"name": "Date", "options": {"format": "Y-m-d"}},
+     *              {
+     *                  "name": "DateCompare",
+     *                  "options": {
+     *                      "compare_to":"decisionDate",
+     *                      "compare_to_label":"Date of decision",
+     *                      "operator": "gte",
+     *                  }
+     *              }
+     *          }
+     *      }
+     * })
+     *
+     * @Form\Filter({"name": "DateSelectNullifier"})
+     */
+    public $notifiedDate = null;
+
+    /**
+     * @Form\Required(false)
+     * @Form\Attributes({"class":"extra-long"})
+     * @Form\Options({"label":"Reason why no further action"})
+     * @Form\Type("Textarea")
+     * @Form\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"max":500}})
+     */
+    public $noFurtherActionReason = null;
+
+    /**
+     * @Form\Attributes({"value":""})
+     * @Form\Type("Hidden")
+     */
+    public $decision = null;
+}
