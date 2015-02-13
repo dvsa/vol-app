f82b1bb7839220ddfb7239d81c1d2da39ed95eee
Shaun Lizzio <shaun@lizzio.co.uk>
2015-02-13 12:58:08 +0000

    Fixed table link to legacyOffence page

--- app/internal/module/Olcs/src/Controller/Cases/Conviction/OffenceController.php
+++ app/internal/module/Olcs/src/Controller/Cases/Conviction/OffenceController.php
@@ -141,4 +141,15 @@ class OffenceController extends OlcsController\CrudAbstract implements
      * @var string
      */
     protected $tableViewPlaceholderName = 'table';
+
+    /**
+     * Master details option.
+     */
+    public function detailsAction()
+    {
+        if ($this->getRequest()->isXmlHttpRequest()) {
+            $this->pageLayoutInner = null;
+        }
+        return parent::detailsAction();
+    }
 }
