abcda2fbc7357d48a4b8ace728084048bf7bf45f
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-02-16 16:10:34 +0000

    Fixed form titles

--- app/internal/module/Olcs/src/Form/Model/Form/BusRegQuality.php
+++ app/internal/module/Olcs/src/Form/Model/Form/BusRegQuality.php
@@ -9,7 +9,7 @@ use Zend\Form\Annotation as Form;
  * @Form\Name("bus-reg-quality")
  * @Form\Attributes({"method":"post"})
  * @Form\Type("Common\Form\Form")
- * @Form\Options({"prefer_form_input_filter": true, "label":"Bus Registration Quality", "override_form_label":true})
+ * @Form\Options({"prefer_form_input_filter": true, "label":"Quality schemes"})
  */
 class BusRegQuality
 {
