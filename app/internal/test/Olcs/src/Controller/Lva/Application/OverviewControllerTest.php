e8e3dcce4bb14066691e8b77eabd96f85995b1d0
Dan Eggleston <dan@stolenegg.com>
2015-02-19 16:44:02 +0000

    OLCS-5171 more controller tests

--- app/internal/test/Olcs/src/Controller/Lva/Application/OverviewControllerTest.php
+++ app/internal/test/Olcs/src/Controller/Lva/Application/OverviewControllerTest.php
@@ -35,26 +35,7 @@ class OverviewControllerTest extends AbstractLvaControllerTestCase
 
         $this->sut->shouldReceive('params')->with('application')->andReturn($applicationId);
 
-        $form = $this->createMockForm('ApplicationOverview');
-
-        $trackingFieldset = m::mock();
-        $form->shouldReceive('get')->with('tracking')->andReturn($trackingFieldset);
-
-        $sections = [
-            'type_of_licence',
-            'business_type',
-            'business_details',
-            'addresses',
-            'people',
-        ];
-        $this->sut->shouldReceive('getAccessibleSections')->once()->andReturn($sections);
-        $trackingFieldset
-            ->shouldReceive('add')
-            ->times(count($sections))
-            ->with(m::type('\Common\Form\Elements\InputFilters\SelectEmpty'));
-
-        $this->mockEntity('ApplicationTracking', 'getValueOptions')
-            ->andReturn(['status_options_array']);
+        $form = $this->getMockForm();
 
         $trackingData = [
           'addressesStatus'              => null,
@@ -87,6 +68,202 @@ class OverviewControllerTest extends AbstractLvaControllerTestCase
             ->with($applicationId)
             ->andReturn($trackingData);
 
+        $formData = ['tracking' => $trackingData];
+        $form
+            ->shouldReceive('setData')
+            ->with($formData)
+            ->andReturnSelf();
+
+        $this->mockRender();
+
+        $this->sut->indexAction();
+
+        $this->assertEquals('overview', $this->view);
+    }
+
+    public function testIndexPostValidSave()
+    {
+        $postData = [
+            'id' => '', // we don't use the application id or version (yet!)
+            'version' => '',
+            'details' => [],
+            'tracking' => [
+                'id' => 1,
+                'version' => 3,
+                'typeOfLicenceStatus' => '',
+                'businessTypeStatus' => '1',
+                'businessDetailsStatus' => '2',
+                'addressesStatus' => '3',
+                'peopleStatus' => '',
+            ],
+            'form-actions' => [
+                'save' => ''
+            ],
+        ];
+
+        $this->setPost($postData);
+
+        $form = $this->getMockForm();
+
+        $form->shouldReceive('setData')
+            ->once()
+            ->with($postData)
+            ->andReturnSelf();
+
+        $form->shouldReceive('isValid')->once()->andReturn(true);
+
+        $expectedSaveData = [
+                'id' => 1,
+                'version' => 3,
+                'businessTypeStatus' => '1',
+                'businessDetailsStatus' => '2',
+                'addressesStatus' => '3',
+        ];
+
+        $this->mockService('Entity\ApplicationTracking', 'save')
+            ->once()
+            ->with($expectedSaveData);
+
+        $this->sut->shouldReceive('addSuccessMessage')->once();
+        $this->sut->shouldReceive('reload')->andReturn('REDIRECT');
+
+        $this->assertEquals('REDIRECT', $this->sut->indexAction());
+    }
+
+    public function testIndexPostValidContinue()
+    {
+        $applicationId = 69;
+
+        $this->sut->shouldReceive('params')->with('application')->andReturn($applicationId);
+
+        $postData = [
+            'id' => '',
+            'version' => '',
+            'details' => [],
+            'tracking' => [
+                'id' => 1,
+                'version' => 3,
+                'typeOfLicenceStatus' => '',
+                'businessTypeStatus' => '1',
+                'businessDetailsStatus' => '2',
+                'addressesStatus' => '3',
+                'peopleStatus' => '',
+            ],
+            'form-actions' => [
+                'saveAndContinue' => ''
+            ],
+        ];
+
+        $this->setPost($postData);
+
+        $form = $this->getMockForm();
+
+        $form->shouldReceive('setData')
+            ->once()
+            ->with($postData)
+            ->andReturnSelf();
+
+        $form->shouldReceive('isValid')->once()->andReturn(true);
+
+        $this->mockService('Entity\ApplicationTracking', 'save')
+            ->once()
+            ->with(m::type('array'));
+
+        $this->sut->shouldReceive('addSuccessMessage')->once();
+
+        $this->sut->shouldReceive('redirect')->andReturn(
+            m::mock()
+                ->shouldReceive('toRoute')
+                    ->with('lva-application/type_of_licence', ['application' => $applicationId])
+                    ->andReturn('REDIRECT')
+                ->getMock()
+        );
+
+        $this->assertEquals('REDIRECT', $this->sut->indexAction());
+    }
+
+    public function testIndexPostCancel()
+    {
+        $postData = [
+            'id' => '',
+            'version' => '',
+            'details' => [],
+            'tracking' => [
+                'id' => 1,
+                'version' => 3,
+                'typeOfLicenceStatus' => '',
+                'businessTypeStatus' => '1',
+                'businessDetailsStatus' => '2',
+                'addressesStatus' => '3',
+                'peopleStatus' => '',
+            ],
+            'form-actions' => [
+                'cancel' => ''
+            ],
+        ];
+
+        $this->setPost($postData);
+
+        $form = $this->getMockForm();
+
+        $form->shouldReceive('setData')->never();
+
+        $this->sut->shouldReceive('addSuccessMessage')->once();
+        $this->sut->shouldReceive('reload')->andReturn('REDIRECT');
+
+        $this->assertEquals('REDIRECT', $this->sut->indexAction());
+    }
+
+    public function testIndexPostInvalid()
+    {
+        $postData = [];
+
+        $this->setPost($postData);
+
+        $form = $this->getMockForm();
+
+        $form->shouldReceive('setData')
+            ->once()
+            ->with($postData)
+            ->andReturnSelf();
+
+        $form->shouldReceive('isValid')->once()->andReturn(false);
+
+        $this->mockService('Entity\ApplicationTracking', 'save')->never();
+
+        $applicationId = 69;
+        $this->sut->shouldReceive('params')->with('application')->andReturn($applicationId);
+
+        $this->mockRender();
+
+        $this->sut->indexAction();
+
+        $this->assertEquals('overview', $this->view);
+    }
+
+    protected function getMockForm()
+    {
+        $form = $this->createMockForm('ApplicationOverview');
+
+        $trackingFieldset = m::mock();
+        $form->shouldReceive('get')->with('tracking')->andReturn($trackingFieldset);
+
+        $sections = [
+            'type_of_licence',
+            'business_type',
+            'business_details',
+            'addresses',
+            'people',
+        ];
+        $this->sut->shouldReceive('getAccessibleSections')->once()->andReturn($sections);
+        $trackingFieldset
+            ->shouldReceive('add')
+            ->times(count($sections))
+            ->with(m::type('\Common\Form\Elements\InputFilters\SelectEmpty'));
+
+        $this->mockEntity('ApplicationTracking', 'getValueOptions')
+            ->andReturn(['status_options_array']);
+
         // assert button label is modified
         $saveButton = m::mock()
             ->shouldReceive('setLabel')
@@ -101,16 +278,6 @@ class OverviewControllerTest extends AbstractLvaControllerTestCase
 
         $form->shouldReceive('get')->with('form-actions')->andReturn($buttonFieldset);
 
-        $formData = ['tracking' => $trackingData];
-        $form
-            ->shouldReceive('setData')
-            ->with($formData)
-            ->andReturnSelf();
-
-        $this->mockRender();
-
-        $this->sut->indexAction();
-
-        $this->assertEquals('overview', $this->view);
+        return $form;
     }
 }
