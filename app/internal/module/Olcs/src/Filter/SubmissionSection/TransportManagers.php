6f5376c016d4feac19c92d37a4ba60b8c056e30e
Shaun Lizzio <shaun@lizzio.co.uk>
2015-02-20 09:56:47 +0000

    Extracted TMs attached to applications also

--- app/internal/module/Olcs/src/Filter/SubmissionSection/TransportManagers.php
+++ app/internal/module/Olcs/src/Filter/SubmissionSection/TransportManagers.php
@@ -8,6 +8,8 @@ namespace Olcs\Filter\SubmissionSection;
  */
 class TransportManagers extends AbstractSubmissionSectionFilter
 {
+    private $dataToReturnArray = array('tables' => array('transport-managers' => array()));
+
     /**
      * Filters data for transport-managers section
      * @param array $data
@@ -15,37 +17,63 @@ class TransportManagers extends AbstractSubmissionSectionFilter
      */
     public function filter($data = array())
     {
-        $dataToReturnArray = array();
+
         if (!empty($data['licence']['tmLicences'])) {
-            foreach ($data['licence']['tmLicences'] as $tmLicence) {
+            $this->extractTmData(
+                $data['licence']['tmLicences'],
+                $data['licence']['licNo']
+            );
+        }
+
+        if (!empty($data['licence']['organisation']['licences'])) {
+            foreach ($data['licence']['organisation']['licences'] as $licence) {
+                if (!empty($licence['applications'])) {
+                    foreach ($licence['applications'] as $application) {
+                        if (!empty($application['transportManagers'])) {
+                            $this->extractTmData(
+                                $application['transportManagers'],
+                                $application['licence']['licNo']
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
+        return $this->dataToReturnArray;
+    }
+
+    private function extractTmData($data, $licenceNo)
+    {
+        if (!empty($data)) {
+            foreach ($data as $tmData) {
+                echo '******** PROCESSING TM ********';
                 $thisRow = array();
-                $thisRow['licNo'] = $data['licence']['licNo'];
-                $thisRow['id'] = $tmLicence['transportManager']['id'];
-                $thisRow['version'] = $tmLicence['transportManager']['version'];
-                $thisRow['tmType'] = $tmLicence['transportManager']['tmType']['description'];
-                $thisRow['title'] = $tmLicence['transportManager']['workCd']['person']['title'];
-                $thisRow['forename'] = $tmLicence['transportManager']['workCd']['person']['forename'];
-                $thisRow['familyName'] = $tmLicence['transportManager']['workCd']['person']['familyName'];
-                $thisRow['dob'] = $tmLicence['transportManager']['workCd']['person']['birthDate'];
+                $thisRow['licNo'] = $licenceNo;
+                $thisRow['id'] = $tmData['transportManager']['id'];
+                $thisRow['version'] = $tmData['transportManager']['version'];
+                $thisRow['tmType'] = $tmData['transportManager']['tmType']['description'];
+                $thisRow['title'] = $tmData['transportManager']['workCd']['person']['title'];
+                $thisRow['forename'] = $tmData['transportManager']['workCd']['person']['forename'];
+                $thisRow['familyName'] = $tmData['transportManager']['workCd']['person']['familyName'];
+                $thisRow['dob'] = $tmData['transportManager']['workCd']['person']['birthDate'];
 
                 $thisRow['qualifications'] = array();
-                foreach ($tmLicence['transportManager']['qualifications'] as $qualification) {
+                foreach ($tmData['transportManager']['qualifications'] as $qualification) {
                     $thisRow['qualifications'][] = $qualification['qualificationType']['description'];
                 }
 
                 $thisRow['otherLicences'] = array();
 
-                foreach ($tmLicence['transportManager']['otherLicences'] as $otherLicence) {
+                foreach ($tmData['transportManager']['otherLicences'] as $otherLicence) {
                     $thisOtherRow = array();
                     $thisOtherRow['licNo'] = $otherLicence['licNo'];
                     $thisOtherRow['applicationId'] = $otherLicence['application']['id'];
                     $thisRow['otherLicences'][] = $thisOtherRow;
                 }
 
-                $dataToReturnArray['tables']['transport-managers'][] = $thisRow;
+                $this->dataToReturnArray['tables']['transport-managers'][] = $thisRow;
             }
         }
-
-        return $dataToReturnArray;
     }
 }
