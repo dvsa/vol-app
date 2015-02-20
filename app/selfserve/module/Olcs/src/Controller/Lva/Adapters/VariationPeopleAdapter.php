7aae951e809678e5731dd561f1db6a385a6a5e3d
Nick Payne <nick@kurai.co.uk>
2015-02-20 09:15:02 +0000

    initial variation logic

--- app/selfserve/module/Olcs/src/Controller/Lva/Adapters/VariationPeopleAdapter.php
+++ app/selfserve/module/Olcs/src/Controller/Lva/Adapters/VariationPeopleAdapter.php
@@ -8,24 +8,28 @@
 namespace Olcs\Controller\Lva\Adapters;
 
 use Zend\Form\Form;
-use Common\Controller\Lva\Adapters\AbstractAdapter;
+use Common\Controller\Lva\Adapters\AbstractPeopleAdapter;
 use Common\Service\Entity\OrganisationEntityService;
-use Common\Controller\Lva\Interfaces\PeopleAdapterInterface;
 
 /**
  * External Variation People Adapter
  *
  * @author Nick Payne <nick.payne@valtech.co.uk>
  */
-class VariationPeopleAdapter extends AbstractAdapter implements PeopleAdapterInterface
+class VariationPeopleAdapter extends AbstractPeopleAdapter
 {
-    public function addMessages($orgId)
-    {
-    }
+    protected $tableConfig = 'lva-variation-people';
 
     public function alterFormForOrganisation(Form $form, $table, $orgId, $orgType)
     {
-        return $this->getServiceLocator()->get('Lva\People')->lockOrganisationForm($form, $table, $orgId);
+        // @TODO think about an abstract method to check org type is EITHER partnership or sole trader
+        // (even here; sure, ST will never apply but the check doesn't hurt) as it's always used as
+        // a behavioural switch when both false and true depending on the adapter
+        if ($orgType === OrganisationEntityService::ORG_TYPE_PARTNERSHIP) {
+            return $this->getServiceLocator()->get('Lva\People')->lockOrganisationForm($form, $table, $orgId);
+        }
+
+        // otherwise go ahead with variation logic @TODO
     }
 
     public function alterAddOrEditFormForOrganisation(Form $form, $orgId, $orgType)
@@ -36,5 +40,10 @@ class VariationPeopleAdapter extends AbstractAdapter implements PeopleAdapterInt
     public function canModify($orgId)
     {
         return false;
+    }
+
+    public function attachMainScripts()
+    {
+        $this->getServiceLocator()->get('Script')->loadFile('lva-crud-delta');
     }
 }
