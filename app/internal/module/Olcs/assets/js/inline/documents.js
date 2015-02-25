5f52dd25741c4882528cb01a335d92a260089831
Rob Caiger <rob@clocal.co.uk>
2015-02-25 15:12:53 +0000

    Moved JS from documents.js to tableHandler

--- app/internal/module/Olcs/assets/js/inline/documents.js
+++ app/internal/module/Olcs/assets/js/inline/documents.js
@@ -13,12 +13,4 @@ OLCS.ready(function() {
     dest: form + " #documentSubCategory",
     url: "/list/document-sub-categories"
   });
-
-  OLCS.conditionalButton({
-    form: ".table__form",
-    label: "Delete",
-    predicate: function(length, callback) {
-      callback(length < 1);
-    }
-  });
 });
