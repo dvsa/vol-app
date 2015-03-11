893810f7f7650a254a24e7a489e9bff4620d3074
Nick Payne <nick@kurai.co.uk>
2015-03-11 13:10:26 +0000

    convert generate document form to annotations

---
+++ app/internal/module/Olcs/src/Form/Model/Fieldset/GenerateDocumentBookmarks.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Olcs\Form\Model\Fieldset;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * @codeCoverageIgnore No methods
+ * @Form\Options({
+ *     "label": "documents.bookmarks",
+ * })
+ * @Form\Name("bookmarks")
+ */
+class GenerateDocumentBookmarks
+{
+    /**
+    * We can't populate our bookmarks statically
+    * from config. They're one to many with the
+    * template the user chooses, and each bookmark
+    * has many child paragraphs. As such we have to
+    * build them up in the controller
+    */
+}
