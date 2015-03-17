21092c478250e8232ebbfde9c484c9a1d417fe07
Dan Eggleston <dan@stolenegg.com>
2015-03-17 17:02:59 +0000

    OLCS-4852 make application sidebar nav consistent with other areas

--- app/internal/module/Olcs/src/Listener/RouteParam/Application.php
+++ app/internal/module/Olcs/src/Listener/RouteParam/Application.php
@@ -9,18 +9,22 @@ use Zend\EventManager\ListenerAggregateInterface;
 use Zend\EventManager\ListenerAggregateTrait;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
 use Common\View\Helper\PluginManagerAwareTrait as ViewHelperManagerAwareTrait;
 use Common\Service\Data\ApplicationAwareTrait;
+use Common\Service\Entity\ApplicationEntityService;
 
 /**
  * Class Cases
  * @package Olcs\Listener\RouteParam
  */
-class Application implements ListenerAggregateInterface, FactoryInterface
+class Application implements ListenerAggregateInterface, FactoryInterface, ServiceLocatorAwareInterface
 {
     use ListenerAggregateTrait;
     use ApplicationAwareTrait;
     use ViewHelperManagerAwareTrait;
+    use ServiceLocatorAwareTrait;
 
     /**
      * Attach one or more listeners
@@ -48,6 +52,22 @@ class Application implements ListenerAggregateInterface, FactoryInterface
 
         $placeholder = $this->getViewHelperManager()->get('placeholder');
         $placeholder->getContainer('application')->set($application);
+
+        $sidebarNav = $this->getServiceLocator()->get('right-sidebar');
+
+        $status = $this->getServiceLocator()->get('Entity\Application')->getStatus($id);
+
+        $showGrantButton = $this->shouldShowGrantButton($status);
+
+        if ($showGrantButton) {
+            $showUndoGrantButton = false;
+        } else {
+            $showUndoGrantButton = $this->shouldShowUndoGrantButton($id, $status);
+        }
+
+        $sidebarNav->findById('application-decisions-grant')->setVisible($showGrantButton);
+        $sidebarNav->findById('application-decisions-undo-grant')->setVisible($showUndoGrantButton);
+
     }
 
     /**
@@ -64,5 +84,27 @@ class Application implements ListenerAggregateInterface, FactoryInterface
         );
 
         return $this;
+    }
+
+    protected function shouldShowGrantButton($status)
+    {
+        return ($status === ApplicationEntityService::APPLICATION_STATUS_UNDER_CONSIDERATION);
+    }
+
+    protected function shouldShowUndoGrantButton($applicationId, $status)
+    {
+        $applicationType = $this->getServiceLocator()->get('Entity\Application')->getApplicationType($applicationId);
+
+        if ($applicationType === ApplicationEntityService::APPLICATION_TYPE_NEW
+            && $status === ApplicationEntityService::APPLICATION_STATUS_GRANTED
+        ) {
+            $applicationService = $this->getServiceLocator()->get('Entity\Application');
+
+            $category = $applicationService->getCategory($applicationId);
+
+            return ($category === LicenceEntityService::LICENCE_CATEGORY_GOODS_VEHICLE);
+        }
+
+        return false;
     }
 }
