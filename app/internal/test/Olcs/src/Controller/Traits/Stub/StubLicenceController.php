a9f77b10d67060463f468b8eb5919bf449a53d91
Dan Eggleston <dan@stolenegg.com>
2015-03-25 13:42:01 +0000

    OLCS-7854 remove commented out code

--- app/internal/test/Olcs/src/Controller/Traits/Stub/StubLicenceController.php
+++ app/internal/test/Olcs/src/Controller/Traits/Stub/StubLicenceController.php
@@ -16,21 +16,4 @@ class StubLicenceController implements ServiceLocatorAwareInterface
 {
     use LicenceControllerTrait,
         ServiceLocatorAwareTrait;
-
-    // private $params;
-
-    // public function doRender($view)
-    // {
-    //     return $this->render($view);
-    // }
-
-    // public function setParams($params)
-    // {
-    //     $this->params = $params;
-    // }
-
-    // public function params($name)
-    // {
-    //     return isset($this->params[$name]) ? $this->params[$name] : null;
-    // }
 }
