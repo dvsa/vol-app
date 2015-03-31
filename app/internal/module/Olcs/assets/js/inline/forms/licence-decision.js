f295e55ceee3292fe5a97861cd81aee68756d69a
Nick Payne <nick@kurai.co.uk>
2015-03-31 09:28:27 +0100

    force a cascadeForm check on curtailment show, ensure curtail button appears in correct container

--- app/internal/module/Olcs/assets/js/inline/forms/licence-decision.js
+++ app/internal/module/Olcs/assets/js/inline/forms/licence-decision.js
@@ -1,15 +1,31 @@
 $(function() {
   "use strict";
 
+  var formId = "#LicenceStatusDecisionCurtail";
+
+  function isChecked(result) {
+    return function() {
+      return OLCS.formHelper.isChecked("licence-decision-affect-immediate", "immediateAffect") === result;
+    };
+  }
+
   OLCS.cascadeForm({
+    form: formId,
     cascade: false,
     rulesets: {
-      "form-actions": function() {
-        return OLCS.formHelper.isChecked("licence-decision-affect-immediate", "immediateAffect");
-      },
-      "licence-decision": function() {
-        return !OLCS.formHelper.isChecked("licence-decision-affect-immediate", "immediateAffect");
-      }
+      "form-actions": isChecked(true),
+      "licence-decision": isChecked(false)
     }
+  });
+
+  // @FIXME: a bug in how we render and then execute inline script means the
+  // cascade rules above fire too early when showing in a modal. This causes
+  // it to wrongly evaluate the current visible state of some elements as 'hidden'
+  // when in fact they're visible but hidden by virtue of the container (the modal)
+  // not yet being displayed.
+  // By hooking into the show event of the modal we can force the cascade form to
+  // check any changes when the form is actually displayed
+  OLCS.eventEmitter.once("show:modal", function() {
+    $(formId).change();
   });
 });
