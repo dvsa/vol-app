8ed9e7ff3bd632649d7397d2c2fb32061fb9c420
Shaun Lizzio <shaun@lizzio.co.uk>
2015-04-01 14:36:52 +0100

    CS fix

--- app/internal/module/Olcs/src/Controller/CrudAbstract.php
+++ app/internal/module/Olcs/src/Controller/CrudAbstract.php
@@ -678,7 +678,7 @@ abstract class CrudAbstract extends CommonController\AbstractSectionController i
         if (property_exists($this, 'navigationId')) {
             $this->setPlaceholder('navigationId', $this->navigationId);
         }
-        
+
         if (!is_null($pageLayoutInner)) {
 
             // This is a zend\view\variables object - cast it to an array.
