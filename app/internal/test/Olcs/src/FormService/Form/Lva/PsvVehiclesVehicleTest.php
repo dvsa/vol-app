20853a5a9a2456188399a647009ee81c9eb754f9
Rob Caiger <rob@clocal.co.uk>
2015-04-02 19:43:32 +0100

    Added unit tests

---
+++ app/internal/test/Olcs/src/FormService/Form/Lva/PsvVehiclesVehicleTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Psv Vehicles Vehicle Test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+namespace OlcsTest\FormService\Form\Lva;
+
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Olcs\FormService\Form\Lva\PsvVehiclesVehicle;
+
+/**
+ * Psv Vehicles Vehicle Test
+ *
+ * @author Rob Caiger <rob@clocal.co.uk>
+ */
+class PsvVehiclesVehicleTest extends MockeryTestCase
+{
+    protected $sut;
+
+    public function setUp()
+    {
+        $this->sut = new PsvVehiclesVehicle();
+    }
+
+    public function testAlterFormNoop()
+    {
+        $mockForm = m::mock();
+        $this->assertNull($this->sut->alterForm($mockForm));
+    }
+}
