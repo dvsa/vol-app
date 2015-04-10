da66c235bcaadc359edea16e0417f178577972d7
Piotr Filak <piotr.filak@valtech.co.uk>
2015-04-10 15:29:42 +0100

    OLCS-4958 - Unit tests

--- app/internal/test/Olcs/src/Controller/Case/Submission/DecisionControllerTest.php
+++ app/internal/test/Olcs/src/Controller/Case/Submission/DecisionControllerTest.php
@@ -55,4 +55,73 @@ class DecisionControllerTest extends AbstractHttpControllerTestCase
 
         $this->assertSame($outData, $sut->processLoad($inData));
     }
+
+    /**
+     * @dataProvider processSaveDataProvider
+     */
+    public function testProcessSave($isOk)
+    {
+        $id = 1;
+        $submissionId = 123;
+        $caseId = 12;
+        $data = ['fields' => ['id' => $id]];
+
+        $params = $this->getMockParams('fromRoute');
+        $params->expects($this->any())->method('fromRoute')->will(
+            $this->returnValueMap(
+                [
+                    ['id', $id],
+                    ['submission', $submissionId],
+                    ['case', $caseId],
+                ]
+            )
+        );
+
+        $mockedResponse = $this->getMock('stdClass', ['isOk'], array(), '', false, false);
+        $mockedResponse->expects($this->once())->method('isOk')
+            ->will($this->returnValue($isOk));
+
+        $mockedDecisionBusinessService = $this->getMock('stdClass', ['process'], array(), '', false, false);
+        $mockedDecisionBusinessService->expects($this->once())->method('process')
+            ->with(
+                [
+                    'id' => $id,
+                    'data' => $data['fields'],
+                    'submissionId' => $submissionId,
+                    'caseId' => $caseId,
+                ]
+            )
+            ->will($this->returnValue($mockedResponse));
+
+        $mockedBusinessServiceManager = $this->getMock('stdClass', ['get'], array(), '', false, false);
+        $mockedBusinessServiceManager->expects($this->once())->method('get')
+            ->with('Cases\Submission\Decision')->will($this->returnValue($mockedDecisionBusinessService));
+
+        $mockedServiceLocator = $this->getMock('stdClass', ['get'], array(), '', false, false);
+        $mockedServiceLocator->expects($this->once())->method('get')
+            ->with('BusinessServiceManager')->will($this->returnValue($mockedBusinessServiceManager));
+
+        $sut = $this->getSut(
+            ['getServiceLocator', 'params', 'addSuccessMessage', 'addErrorMessage', 'redirectToRoute']
+        );
+        $sut->expects($this->once())->method('getServiceLocator')
+            ->will($this->returnValue($mockedServiceLocator));
+        $sut->expects($this->any())->method('params')
+            ->will($this->returnValue($params));
+        $sut->expects($this->exactly($isOk ? 1 : 0))->method('addSuccessMessage');
+        $sut->expects($this->exactly($isOk ? 0 : 1))->method('addErrorMessage');
+        $sut->expects($this->once())->method('redirectToRoute');
+
+        $sut->processSave($data);
+    }
+
+    public function processSaveDataProvider()
+    {
+        return [
+            // success
+            [true],
+            // error
+            [false],
+        ];
+    }
 }
