812af80425743a23295d833492c8d56d034c290f
Rob Caiger <rob@clocal.co.uk>
2015-04-20 13:40:40 +0100

    Added class so JS postcode component kicks in

--- app/internal/module/Admin/src/Form/Model/Fieldset/OfficeAddress.php
+++ app/internal/module/Admin/src/Form/Model/Fieldset/OfficeAddress.php
@@ -9,6 +9,9 @@ use Common\Form\Model\Fieldset\Address;
  * @Form\Name("officeAddress")
  * @Form\Type("\Common\Form\Elements\Types\Address")
  * @Form\Options({"label":"Office Address"})
+ * @Form\Attributes({
+ *     "class": "address js-postcode-search"
+ * })
  */
 class OfficeAddress extends Address
 {
--- app/internal/module/Olcs/src/Form/Model/Fieldset/RequestorsAddress.php
+++ app/internal/module/Olcs/src/Form/Model/Fieldset/RequestorsAddress.php
@@ -9,6 +9,9 @@ use Common\Form\Model\Fieldset\Address;
  * @Form\Name("requestorsAddress")
  * @Form\Type("\Common\Form\Elements\Types\Address")
  * @Form\Options({"label":"Requestors Address"})
+ * @Form\Attributes({
+ *     "class": "address js-postcode-search"
+ * })
  */
 class RequestorsAddress extends Address
 {
