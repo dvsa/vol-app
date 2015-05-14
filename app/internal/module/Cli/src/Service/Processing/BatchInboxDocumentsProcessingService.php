fbdc37c4ce9b13bd5443aa8c09da76184cfc9ad4
Nick Payne <nick@kurai.co.uk>
2015-05-14 10:36:50 +0100

    email and print reminders when due

--- app/internal/module/Cli/src/Service/Processing/BatchInboxDocumentsProcessingService.php
+++ app/internal/module/Cli/src/Service/Processing/BatchInboxDocumentsProcessingService.php
@@ -10,6 +10,8 @@ namespace Cli\Service\Processing;
 use Common\Service\Entity\LicenceStatusRuleEntityService;
 use Common\Service\Entity\LicenceEntityService;
 use Common\BusinessService\Response;
+use Common\Service\Helper\UrlHelperService;
+use Common\Service\File\File;
 
 /**
  * Batch process inbox documents and send reminder emails
@@ -28,25 +30,89 @@ class BatchInboxDocumentsProcessingService extends AbstractBatchProcessingServic
      */
     public function process()
     {
+        $ciEntityService = $this->getServiceLocator()->get('Entity\CorrespondenceInbox');
         $minDate         = $this->getDate('P1M');
         $maxPrintDate    = $this->getDate('P1W');
         $maxReminderDate = $this->getDate('P2D');
 
-        $printList = $this->getServiceLocator()
-            ->get('Entity\CorrespondenceInbox')
-            ->getAllRequiringPrint($minDate, $maxPrintDate);
+        $printList = $ciEntityService->getAllRequiringPrint($minDate, $maxPrintDate);
+        $emailList = $ciEntityService->getAllRequiringReminder($minDate, $maxReminderDate);
 
-        // print each item in printList
-        // update each item in print list to flag it as printed
+        $this->outputLine('Found ' . count($printList) . ' records to print');
+        $this->outputLine('Found ' . count($emailList) . ' records to email');
 
-        $emailList = $this->getServiceLocator()
-            ->get('Entity\CorrespondenceInbox')
-            ->getAllRequiringReminder($minDate, $maxReminderDate);
+        $updates = [];
 
-        // email each item in emailList
-        // ^^ will require explicitly telling the email service about
-        // our custom template
-        // update each item in email list to flag it as emailed
+        foreach ($emailList as $row) {
+            $licence = $row['licence'];
+            $organisation = $licence['organisation'];
+
+            $users = $this->getServiceLocator()
+                ->get('Entity\Organisation')
+                ->getAdminEmailAddresses($organisation['id']);
+
+            if (empty($users)) {
+                $this->log('No admin email addresses for licence ' . $licence['id']);
+                continue;
+            }
+
+            $url = $this->getServiceLocator()->get('Helper\Url')->fromRouteWithHost(
+                UrlHelperService::EXTERNAL_HOST,
+                'correspondence_inbox'
+            );
+
+            $this->outputLine('Sending email reminder for licence ' . $licence['id'] . ' to ' . implode($users, ','));
+
+            $this->getServiceLocator()
+                ->get('Email')
+                ->sendTemplate(
+                    // never translate to welsh
+                    false,
+                    // default from name is fine
+                    null,
+                    // default from address is fine
+                    null,
+                    $users,
+                    'email.inbox-reminder',
+                    'markup-email-inbox-reminder',
+                    [$licence['licNo'], $url]
+                );
+
+            $updates[] = [
+                'id' => $row['id'],
+                'emailReminderSent' => 'Y',
+                '_OPTIONS_' => ['force' => true]
+            ];
+        }
+
+        foreach ($printList as $row) {
+            $licence = $row['licence'];
+            $document = $row['document'];
+
+            $file = new File();
+            $file->fromData($document);
+
+            $this->outputLine('Printing document for licence ' . $licence['id']);
+
+            $this->getServiceLocator()
+                ->get('PrintScheduler')
+                ->enqueueFile($file, $document['description']);
+
+            $updates[] = [
+                'id' => $row['id'],
+                'printed' => 'Y',
+                '_OPTIONS_' => ['force' => true]
+            ];
+        }
+
+        if (count($updates)) {
+            $this->outputLine('Updating ' . count($updates). ' records');
+            $ciEntityService->multiUpdate($updates);
+        } else {
+            $this->outputLine('No records to update');
+        }
+
+        return self::EXIT_CODE_SUCCESS;
     }
 
     private function getDate($interval)
