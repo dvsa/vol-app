cc4121ff3afe946634b83151f27ffad55fbc0b75
Shaun Lizzio <shaun@lizzio.co.uk>
2015-05-15 11:38:30 +0100

    CS fixes

--- app/internal/module/Olcs/src/Service/Data/Submission.php
+++ app/internal/module/Olcs/src/Service/Data/Submission.php
@@ -283,9 +283,7 @@ class Submission extends AbstractData implements CloseableInterface
      */
     public function filterSectionData($sectionId)
     {
-        $filteredSectionData = [];
         $filter = $this->getFilter();
-        $method = 'filter' . ucfirst($filter->filter($sectionId)) . 'Data';
 
         // load filter class
         $filteredSectionData = $this->getFilterManager()
