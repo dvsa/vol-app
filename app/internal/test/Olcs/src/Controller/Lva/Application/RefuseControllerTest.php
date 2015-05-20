ac260fe770c6c94d5ddd4acafd6d122c36d044a1
Mat Evans <mat.evans@valtech.co.uk>
2015-05-20 17:22:11 +0100

    OLCS-9061 Redirect to application overview after refuse, withdraw, etc

--- app/internal/test/Olcs/src/Controller/Lva/Application/RefuseControllerTest.php
+++ app/internal/test/Olcs/src/Controller/Lva/Application/RefuseControllerTest.php
@@ -43,7 +43,6 @@ class RefuseControllerTest extends AbstractLvaControllerTestCase
     public function testIndexActionWithPostConfirm()
     {
         $id = 69;
-        $licenceId = 100;
 
         $this->sut->shouldReceive('params')->with('application')->andReturn($id);
 
@@ -80,14 +79,9 @@ class RefuseControllerTest extends AbstractLvaControllerTestCase
             ->with('SUCCESS MESSAGE')
             ->once();
 
-        $this->mockEntity('Application', 'getLicenceIdForApplication')
-            ->with($id)
-            ->once()
-            ->andReturn($licenceId);
-
         $redirect = m::mock();
         $this->sut->shouldReceive('redirect->toRouteAjax')
-            ->with('lva-licence/overview', ['licence' => $licenceId])
+            ->with('lva-application/overview', ['application' => $id])
             ->andReturn($redirect);
 
         $this->assertSame($redirect, $this->sut->indexAction());
