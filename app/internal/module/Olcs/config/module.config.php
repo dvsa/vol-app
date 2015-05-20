445ec0e74fada643cfc484b56401c9f88e35a66e
Joshua Curtis <josh.curtis@valtech.co.uk>
2015-05-20 11:09:35 +0100

    Updated so that the transport consultant service sits in common.

--- app/internal/module/Olcs/config/module.config.php
+++ app/internal/module/Olcs/config/module.config.php
@@ -650,7 +650,6 @@ return array(
             'Lva\LicenceOverview' => 'Olcs\BusinessService\Service\Lva\LicenceOverview',
             'Lva\SaveApplicationChangeOfEntity' => 'Olcs\BusinessService\Service\Lva\SaveApplicationChangeOfEntity',
             'Lva\GracePeriod' => 'Olcs\BusinessService\Service\Lva\GracePeriod',
-            'Lva\TransportConsultant' => 'Olcs\BusinessService\Service\Lva\TransportConsultant',
             'InspectionRequest' => 'Olcs\BusinessService\Service\InspectionRequest',
             'InspectionRequestUpdate' => 'Olcs\BusinessService\Service\InspectionRequestUpdate',
             'Cases\Penalty\ErruAppliedPenaltyResponse'
