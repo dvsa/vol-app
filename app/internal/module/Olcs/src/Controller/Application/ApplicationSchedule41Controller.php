de30201b7db71f517eb437ee12616df3879cd7bd
Joshua Curtis <josh.curtis@valtech.co.uk>
2015-05-21 16:18:49 +0100

    Working schedule4/1

--- app/internal/module/Olcs/src/Controller/Application/ApplicationSchedule41Controller.php
+++ app/internal/module/Olcs/src/Controller/Application/ApplicationSchedule41Controller.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * ApplicationSchedule41Controller.php
+ */
 namespace Olcs\Controller\Application;
 
 use Common\Controller\Lva\Traits\CrudTableTrait;
@@ -10,10 +13,24 @@ use Olcs\Controller\Lva\Traits\ApplicationControllerTrait;
 use Olcs\Controller\AbstractController;
 use Zend\View\Model\ViewModel;
 
+/**
+ * Class ApplicationSchedule41Controller
+ *
+ * Application41 schedule controller.
+ *
+ * @package Olcs\Controller\Application
+ *
+ * @author Josh Curtis <josh.curtis@valtech.co.uk>
+ */
 class ApplicationSchedule41Controller extends AbstractController
 {
     use ApplicationControllerTrait;
 
+    /**
+     * Search for a licence by licence number.
+     *
+     * @return string|\Zend\Http\Response|ViewModel
+     */
     public function licenceSearchAction()
     {
         $request = $this->getRequest();
@@ -62,6 +79,11 @@ class ApplicationSchedule41Controller extends AbstractController
         return $this->renderView($view);
     }
 
+    /**
+     * Transfer operating centres from a licence to and application.
+     *
+     * @return string|ViewModel
+     */
     public function transferAction()
     {
         $request = $this->getRequest();
@@ -86,6 +108,13 @@ class ApplicationSchedule41Controller extends AbstractController
         return $this->renderView($view);
     }
 
+    /**
+     * Is the licence valid according to the Ac.
+     *
+     * @param array $params
+     *
+     * @return \Zend\Http\Response
+     */
     private function isLicenceValid(array $params)
     {
         $licence = (count($params['Results']) > 0 ? $params['Results'][0] : false);
@@ -110,6 +139,13 @@ class ApplicationSchedule41Controller extends AbstractController
         }
     }
 
+    /**
+     * Return a redirect response with an error message.
+     *
+     * @param $message
+     *
+     * @return \Zend\Http\Response
+     */
     protected function redirectWithError($message)
     {
         $this->flashMessenger()
@@ -123,6 +159,13 @@ class ApplicationSchedule41Controller extends AbstractController
         );
     }
 
+    /**
+     * Get the operating centres table for transfering.
+     *
+     * @param $data
+     *
+     * @return mixed
+     */
     public function getOcTable($data)
     {
         return $this->getServiceLocator()
@@ -133,25 +176,42 @@ class ApplicationSchedule41Controller extends AbstractController
             );
     }
 
+    /**
+     * Format the operating centre data for the table.
+     *
+     * @param $data
+     *
+     * @return array
+     */
     public function formatDataForTable($data)
     {
-        $operatingCentres = array_map(function($operatingCentre) {
-            return array(
-                'id' => $operatingCentre['operatingCentre']['id'],
-                'address' => $operatingCentre['operatingCentre']['address'],
-                'noOfVehiclesRequired' => $operatingCentre['noOfVehiclesRequired'],
-                'noOfTrailersRequired' => $operatingCentre['noOfTrailersRequired'],
-                'operatingCentre' => $operatingCentre['operatingCentre'],
-                'conditions' => $operatingCentre['operatingCentre']['conditionUndertakings'],
-                'undertakings' => $operatingCentre['operatingCentre']['conditionUndertakings']
-            );
-        }, $data['Results']);
+        $operatingCentres = array_map(
+            function ($operatingCentre) {
+                return array(
+                    'id' => $operatingCentre['operatingCentre']['id'],
+                    'address' => $operatingCentre['operatingCentre']['address'],
+                    'noOfVehiclesRequired' => $operatingCentre['noOfVehiclesRequired'],
+                    'noOfTrailersRequired' => $operatingCentre['noOfTrailersRequired'],
+                    'operatingCentre' => $operatingCentre['operatingCentre'],
+                    'conditions' => $operatingCentre['operatingCentre']['conditionUndertakings'],
+                    'undertakings' => $operatingCentre['operatingCentre']['conditionUndertakings']
+                );
+            },
+            $data['Results']
+        );
 
         return $operatingCentres;
     }
 
-    public function checkForRedirect()
+    /**
+     * No-op.
+     *
+     * @param $lvaId
+     *
+     * @return bool
+     */
+    public function checkForRedirect($lvaId)
     {
-        return false;
+        unset($lvaId);
     }
 }
