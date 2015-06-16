c01a7df021f0dab5f4d2736e8dde717e6e276a09
Chris Riley <chris.riley@valtech.co.uk>
2015-06-16 10:48:20 +0100

    Added Add and Edit methods to the internal controller abstract, changed bus service details to use

--- app/internal/module/Olcs/src/Controller/AbstractInternalController.php
+++ app/internal/module/Olcs/src/Controller/AbstractInternalController.php
@@ -17,11 +17,18 @@ use Common\Service\Cqrs\Response;
 /**
  * Abstract class to extend for BASIC list/edit/delete functions
  *
+ * @TODO generic methodology for adding scripts to actions
+ * @TODO delete action
+ * @TODO method to alter form depending on data retrieved
+ * @TODO define post add/edit/delete redirect location as a parameter?
+ * @TODO review navigation stuff...
+ *
  * @method ViewBuilderInterface viewBuilder()
  * @method Plugin\Script script()
  * @method Plugin\Placeholder placeholder()
  * @method Plugin\Table table()
  * @method Response handleQuery(QueryInterface $query)
+ * @method Response handleCommand(QueryInterface $query)
  */
 abstract class AbstractInternalController extends AbstractActionController
 {
@@ -31,6 +38,263 @@ abstract class AbstractInternalController extends AbstractActionController
      * represented by a single navigation id.
      */
     protected $navigationId = '';
+
+    /*
+     * Variables for controlling table/list rendering
+     * tableName and listDto are required,
+     * listVars probably needs to be defined every time but will work without
+     */
+    protected $tableViewPlaceholderName = 'table';
+    protected $tableViewTemplate = 'partials/table';
+    protected $defaultTableSortField = 'id';
+    protected $tableName = '';
+    protected $listDto = '';
+    protected $listVars = [];
+
+    /**
+     * Variables for controlling details view rendering
+     * details view template and itemDto are required.
+     */
+    protected $detailsViewTemplate = 'pages/case/offence';
+    protected $detailsViewPlaceholderName = 'details';
+    protected $itemDto = '';
+    protected $itemParams = ['id'];
+
+    /**
+     * Variables for controlling edit view rendering
+     * all these variables are required
+     * itemDto (see above) is also required.
+     */
+    protected $formClass = '';
+    protected $updateCommand = '';
+    protected $mapperClass = '';
+
+    /**
+     * Variables for controlling edit view rendering
+     * all these variables are required
+     * itemDto (see above) is also required.
+     */
+    protected $createCommand = '';
+
+    public function indexAction()
+    {
+        return $this->index(
+            $this->listDto,
+            $this->listVars,
+            $this->defaultTableSortField,
+            $this->tableViewPlaceholderName,
+            $this->tableName,
+            $this->tableViewTemplate
+        );
+    }
+
+    public function detailsAction()
+    {
+        return $this->details(
+            $this->itemDto,
+            $this->itemParams,
+            $this->detailsViewPlaceholderName,
+            $this->detailsViewTemplate
+        );
+    }
+
+    public function addAction()
+    {
+        return $this->add(
+            $this->formClass,
+            $this->createCommand,
+            $this->mapperClass
+        );
+    }
+
+    public function editAction()
+    {
+        return $this->edit(
+            $this->formClass,
+            $this->itemDto,
+            $this->itemParams,
+            $this->updateCommand,
+            $this->mapperClass
+        );
+    }
+
+    final protected function index($listDto, $paramNames, $defaultSort, $tableViewPlaceholderName, $tableName, $tableViewTemplate)
+    {
+        $listParams = $this->getListParams($paramNames, $defaultSort);
+        $response = $this->handleQuery($listDto::create($listParams));
+
+        if ($response->isNotFound()) {
+            return $this->notFoundAction();
+        }
+
+        if ($response->isClientError() || $response->isServerError()) {
+            $this->getServiceLocator()->get('Helper\FlashMessenger')->addErrorMessage('unknown-error');
+        }
+
+        if ($response->isOk()) {
+            $data = $response->getResult();
+
+            $this->placeholder()->setPlaceholder(
+                $tableViewPlaceholderName,
+                $this->table()->buildTable($tableName, $data, $listParams)
+            );
+        }
+
+        return $this->viewBuilder()->buildViewFromTemplate($tableViewTemplate);
+    }
+
+    final protected function details($itemDto, $paramNames, $detailsViewPlaceHolderName, $detailsViewTemplate)
+    {
+        $params = $this->getItemParams($paramNames);
+
+        $response = $this->handleQuery($itemDto::create($params));
+
+        if ($response->isNotFound()) {
+            return $this->notFoundAction();
+        }
+
+        if ($response->isClientError() || $response->isServerError()) {
+            $this->getServiceLocator()->get('Helper\FlashMessenger')->addErrorMessage('unknown-error');
+        }
+
+        if ($response->isOk()) {
+            $data = $response->getResult();
+
+            if (isset($data)) {
+                $this->placeholder()->setPlaceholder($detailsViewPlaceHolderName, $data);
+            }
+        }
+
+        return $this->viewBuilder()->buildViewFromTemplate($detailsViewTemplate);
+    }
+
+    final protected function add($formClass, $createCommand, $mapperClass)
+    {
+        $request = $this->getRequest();
+        $form = $this->getServiceLocator()->get('Helper\Form')->createForm($formClass);
+        $this->placeholder()->setPlaceholder('form', $form);
+
+        if ($request->isPost()) {
+            $data = $request->getPost();
+            $form->setData($data);
+
+            if ($form->isValid()) {
+                $commandData = $mapperClass::mapFromForm($form->getData());
+                $response = $this->handleCommand($createCommand::create($commandData));
+
+                if ($response->isServerError()) {
+                    $this->getServiceLocator()->get('Helper\FlashMessenger')->addErrorMessage('unknown-error');
+                }
+
+                if ($response->isClientError()) {
+                    $flashErrors = $mapperClass::mapFromErrors($form, $response->getResult());
+                    foreach ($flashErrors as $error) {
+                        $this->getServiceLocator()->get('Helper\FlashMessenger')->addErrorMessage($error);
+                    }
+                }
+
+                if ($response->isOk()) {
+                    $this->getServiceLocator()->get('Helper\FlashMessenger')->addSuccessMessage('Created record');
+                    return $this->redirectToIndex();
+                }
+            }
+        }
+
+        return $this->viewBuilder()->buildViewFromTemplate('pages/crud-form');
+    }
+
+    final protected function edit($formClass, $itemDto, $paramNames, $updateCommand, $mapperClass)
+    {
+        $request = $this->getRequest();
+        $form = $this->getServiceLocator()->get('Helper\Form')->createForm($formClass);
+        $this->placeholder()->setPlaceholder('form', $form);
+
+        if ($request->isPost()) {
+            $data = $request->getPost();
+            $form->setData($data);
+
+            if ($form->isValid()) {
+                $commandData = $mapperClass::mapFromForm($form->getData());
+                $response = $this->handleCommand($updateCommand::create($commandData));
+
+                if ($response->isServerError()) {
+                    $this->getServiceLocator()->get('Helper\FlashMessenger')->addErrorMessage('unknown-error');
+                }
+
+                if ($response->isClientError()) {
+                    $flashErrors = $mapperClass::mapFromErrors($form, $response->getResult());
+                    foreach ($flashErrors as $error) {
+                        $this->getServiceLocator()->get('Helper\FlashMessenger')->addErrorMessage($error);
+                    }
+                }
+
+                if ($response->isOk()) {
+                    $this->getServiceLocator()->get('Helper\FlashMessenger')->addSuccessMessage('Updated record');
+                    return $this->redirectToIndex();
+                }
+            }
+        } else {
+            $itemParams = $this->getItemParams($paramNames);
+            $response = $this->handleQuery($itemDto::create($itemParams));
+
+            if ($response->isNotFound()) {
+                return $this->notFoundAction();
+            }
+
+            if ($response->isClientError() || $response->isServerError()) {
+                $this->getServiceLocator()->get('Helper\FlashMessenger')->addErrorMessage('unknown-error');
+            }
+
+            if ($response->isOk()) {
+                $result = $response->getResult();
+                $formData = $mapperClass::mapFromResult($result);
+                $form->setData($formData);
+            }
+        }
+
+        return $this->viewBuilder()->buildViewFromTemplate('pages/crud-form');
+    }
+
+    private function getListParams($paramNames, $defaultSort)
+    {
+        $params = [
+            'page'    => $this->params()->fromQuery('page', 1),
+            'sort'    => $this->params()->fromQuery('sort', $defaultSort),
+            'order'   => $this->params()->fromQuery('order', 'DESC'),
+            'limit'   => $this->params()->fromQuery('limit', 10),
+        ];
+
+        foreach ((array) $paramNames as $varName) {
+            $params[$varName] = $this->params()->fromRoute($varName);
+        }
+
+        return $params;
+    }
+
+    private function getItemParams($paramNames)
+    {
+        $params = [];
+
+        foreach ((array) $paramNames as $key => $varName) {
+            if (is_int($key)) {
+                $params[$varName] = $this->params()->fromRoute($varName);
+            } else {
+                $params[$key] = $this->params()->fromRoute($varName);
+            }
+        }
+
+        return $params;
+    }
+
+    protected function redirectToIndex()
+    {
+        return $this->redirect()->toRoute(
+            null,
+            ['action'=>'index'],
+            ['code' => '303'], // Why? No cache is set with a 303 :)
+            true
+        );
+    }
 
     /**
      * @codeCoverageIgnore this is part of the event system.
@@ -52,7 +316,7 @@ abstract class AbstractInternalController extends AbstractActionController
      *
      * @return boolean true
      */
-    public function setNavigationCurrentLocation()
+    final public function setNavigationCurrentLocation()
     {
         $navigation = $this->getServiceLocator()->get('Navigation');
         if (!empty($this->navigationId)) {
@@ -60,113 +324,5 @@ abstract class AbstractInternalController extends AbstractActionController
         }
 
         return true;
-    }
-
-    /*
-     * Variables for controlling table/list rendering
-     * tableName and listDto are required,
-     * listVars probably needs to be defined every time but will work without
-     */
-    protected $tableViewPlaceholderName = 'table';
-    protected $tableViewTemplate = 'partials/table';
-    protected $defaultTableSortField = 'id';
-    protected $tableName = '';
-    protected $listDto = '';
-    protected $listVars = [];
-
-    private function getListParams()
-    {
-        $params = [
-            'page'    => $this->params()->fromQuery('page', 1),
-            'sort'    => $this->params()->fromQuery('sort', $this->defaultTableSortField),
-            'order'   => $this->params()->fromQuery('order', 'DESC'),
-            'limit'   => $this->params()->fromQuery('limit', 10),
-        ];
-
-        foreach ((array) $this->listVars as $varName) {
-            $params[$varName] = $this->params()->fromRoute($varName);
-        }
-
-        return $params;
-    }
-
-    public function indexAction()
-    {
-        $listParams = $this->getListParams();
-        $dto = $this->listDto;
-        $response = $this->handleQuery($dto::create($listParams));
-
-        if ($response->isNotFound()) {
-            return $this->notFoundAction();
-        }
-
-        if ($response->isClientError() || $response->isServerError()) {
-            $this->getServiceLocator()->get('Helper\FlashMessenger')->addErrorMessage('unknown-error');
-        }
-
-        if ($response->isOk()) {
-            $data = $response->getResult();
-
-            $this->placeholder()->setPlaceholder(
-                $this->tableViewPlaceholderName,
-                $this->table()->buildTable($this->tableName, $data, $listParams)
-            );
-        }
-
-        return $this->viewBuilder()->buildViewFromTemplate($this->tableViewTemplate);
-    }
-
-    /**
-     * Variables for controlling details view rendering
-     * details view template and itemDto are required.
-     */
-    protected $detailsViewTemplate = 'pages/case/offence';
-    protected $detailsViewPlaceholderName = 'details';
-    protected $itemDto = '';
-    protected $itemParams = ['id'];
-
-    /**
-     * Method to display details of a legacy offence
-     * @return array|ViewModel
-     */
-    public function detailsAction()
-    {
-        $dto = $this->itemDto;
-        $itemParams = $this->getItemParams();
-
-        $response = $this->handleQuery($dto::create($itemParams));
-
-        if ($response->isNotFound()) {
-            return $this->notFoundAction();
-        }
-
-        if ($response->isClientError() || $response->isServerError()) {
-            $this->getServiceLocator()->get('Helper\FlashMessenger')->addErrorMessage('unknown-error');
-        }
-
-        if ($response->isOk()) {
-            $data = $response->getResult();
-
-            if (isset($data)) {
-                $this->setPlaceholder($this->detailsViewPlaceholderName, $data);
-            }
-        }
-
-        return $this->viewBuilder()->buildViewFromTemplate($this->detailsViewTemplate);
-    }
-
-    /**
-     * Gets a single legacy offence by case and legacy offence ID
-     * @return Response
-     */
-    private function getItemParams()
-    {
-        $params = [];
-
-        foreach ((array) $this->itemParams as $varName) {
-            $params[$varName] = $this->params()->fromRoute($varName);
-        }
-
-        return $params;
     }
 }
--- app/internal/module/Olcs/src/Controller/Bus/Details/BusDetailsServiceController.php
+++ app/internal/module/Olcs/src/Controller/Bus/Details/BusDetailsServiceController.php
@@ -7,28 +7,57 @@
  */
 namespace Olcs\Controller\Bus\Details;
 
-use Dvsa\Olcs\Transfer\Command\Bus\UpdateServiceDetails as UpdateServiceDetailsCommand;
+use Dvsa\Olcs\Transfer\Command\Bus\UpdateServiceDetails;
+use Dvsa\Olcs\Transfer\Query\Bus\BusReg;
+use Olcs\Controller\AbstractInternalController;
+use \Olcs\Data\Mapper\BusReg as BusRegMapper;
+use Olcs\Controller\Interfaces\BusRegControllerInterface;
+use Olcs\Controller\Interfaces\PageInnerLayoutProvider;
+use Olcs\Controller\Interfaces\PageLayoutProvider;
+use Olcs\Form\Model\Form\BusServiceNumberAndType;
 
 /**
  * Bus Details Service Controller
  *
  * @author Ian Lindsay <ian@hemera-business-services.co.uk>
  */
-class BusDetailsServiceController extends BusDetailsController
+class BusDetailsServiceController extends AbstractInternalController
+    implements BusRegControllerInterface, PageLayoutProvider , PageInnerLayoutProvider
 {
-    protected $item = 'service';
-
-    /* properties required by CrudAbstract */
-    protected $formName = 'bus-service-number-and-type';
-
-    protected $inlineScripts = ['bus-servicenumbers'];
-
-    public function processSave($data)
+    public function getPageInnerLayout()
     {
-        $command = new UpdateServiceDetailsCommand();
-        $command->exchangeArray($data);
-        $response = $this->handleCommand($command);
+        return 'layout/bus-registration-subsection';
+    }
 
-        return $response;
+    public function getPageLayout()
+    {
+        return 'layout/bus-registrations-section';
+    }
+
+
+    protected $section = 'details';
+    protected $subNavRoute = 'licence_bus_details';
+    protected $navigationId = 'licence_bus_details';
+
+    protected $inlineScripts = ['forms/bus-details-ta'];
+
+
+    protected $itemDto = BusReg::class;
+    protected $itemParams = ['id' => 'busRegId'];
+    protected $formClass = BusServiceNumberAndType::class;
+    protected $updateCommand = UpdateServiceDetails::class;
+    protected $mapperClass = BusRegMapper::class;
+    public function editAction()
+    {
+        /*if ($this->isFromEbsr() || !$this->isLatestVariation()) {
+            $form->setOption('readonly', true);
+        }*/
+        return $this->edit(
+            $this->formClass,
+            $this->itemDto,
+            $this->itemParams,
+            $this->updateCommand,
+            $this->mapperClass
+        );
     }
 }
