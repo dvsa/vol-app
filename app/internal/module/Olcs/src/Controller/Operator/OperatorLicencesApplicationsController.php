1015ea65fb26a53d71b6d5e057d8c674730b72a5
Mat Evans <mat.evans@valtech.co.uk>
2015-07-23 10:48:02 +0100

    OLCS-9267 Operator Licences and Applications

--- app/internal/module/Olcs/src/Controller/Operator/OperatorLicencesApplicationsController.php
+++ app/internal/module/Olcs/src/Controller/Operator/OperatorLicencesApplicationsController.php
@@ -1,26 +1,84 @@
 <?php
 
-/**
- * Operator Licences & Applications Controller
- *
- * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
- */
 namespace Olcs\Controller\Operator;
 
+use Olcs\Controller\AbstractInternalController;
+use Olcs\Controller\Interfaces\CaseControllerInterface;
+use Olcs\Controller\Interfaces\PageInnerLayoutProvider;
+use Olcs\Controller\Interfaces\PageLayoutProvider;
+
 /**
- * Operator Licences & Applications Controller
+ * OperatorLicencesApplicationsController Controller
  *
- * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
+ * @author Mat Evans <mat.evans@valtech.co.uk>
  */
-class OperatorLicencesApplicationsController extends OperatorController
+class OperatorLicencesApplicationsController extends AbstractInternalController implements CaseControllerInterface,
+ PageLayoutProvider,
+ PageInnerLayoutProvider
 {
-    /**
-     * @var string
-     */
-    protected $section = 'licences_applications';
+    public function getPageLayout()
+    {
+        return 'layout/operator-subsection';
+    }
+
+    public function getPageInnerLayout()
+    {
+        return 'pages/operator/licences-and-applications';
+    }
+
+    public function indexAction()
+    {
+        /**
+         * Both methods return the same view
+         */
+        $this->setupLicencesTable();
+
+        return $this->setupEnvironmentComplaintsTable();
+    }
 
     /**
-     * @var string
+     * Create a table of Licences
+     *
+     * @return \Zend\View\Model\ViewModel
      */
-    protected $subNavRoute = 'operator_profile';
+    private function setupLicencesTable()
+    {
+        /* @var $request \Zend\Http\Request */
+        $request = $this->getRequest();
+        // exclude certain licence statuses
+        $request->getQuery()->set('excludeStatuses', ['lsts_not_submitted', 'lsts_consideration', 'lsts_granted']);
+        // order by licNo
+        $request->getQuery()->set('sort', 'licNo');
+
+        return $this->index(
+            \Dvsa\Olcs\Transfer\Query\Licence\GetList::class,
+            ['organisation'],
+            'id',
+            'licencesTable',
+            'operator-licences',
+            $this->tableViewTemplate
+        );
+    }
+
+    /**
+     * Create a table of Applications
+     *
+     * @return \Zend\View\Model\ViewModel
+     */
+    private function setupEnvironmentComplaintsTable()
+    {
+        /* @var $request \Zend\Http\Request */
+        $request = $this->getRequest();
+        // order by created date
+        $request->getQuery()->set('sort', 'createdOn');
+
+        return $this->index(
+            \Dvsa\Olcs\Transfer\Query\Application\GetList::class,
+            ['organisation'],
+            'id',
+            'applicationsTable',
+            'operator-applications',
+            $this->tableViewTemplate
+        );
+    }
 }
