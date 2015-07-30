0726c01a889e57bf887f0eda378dea38cbdaa645
Shaun Lizzio <shaun@lizzio.co.uk>
2015-07-30 08:16:35 +0100

    Working partners operating centre table

--- app/selfserve/module/Olcs/src/Table/Tables/entity-view-operating-centres-partner.table.php
+++ app/selfserve/module/Olcs/src/Table/Tables/entity-view-operating-centres-partner.table.php
@@ -6,27 +6,39 @@ return array(
     'attributes' => array(),
     'columns' => array(
         array(
-            'title' => 'search-result-label-operating-centre',
+            'title' => 'entity-view-label-operating-centre',
             'formatter' => 'Address',
             'name' => 'operatingCentre->address'
         ),
         array(
-            'title' => 'search-result-label-vehicles',
-            'formatter' => function ($data, $column) {
-                if (empty($data['noOfVehiclesPossessed'])) {
-                    return 'N/A';
-                }
-                return $data['noOfVehiclesPossessed'];
+            'title' => 'entity-view-table-header-interim',
+            'formatter' => 'yesno',
+            'name' => 'isInterim'
+        ),
+        array(
+            'title' => 'entity-view-table-header-vehicles-authorised',
+            'name' => 'noOfVehiclesPossessed'
+        ),
+        array(
+            'title' => 'entity-view-table-header-trailers-authorised',
+            'name' => 'noOfTrailersPossessed'
+        ),
+        array(
+            'title' => 'entity-view-table-header-removed',
+            'formatter' => function ($data) {
+                return !empty($data['removedDate']) ?
+                    'Yes' : 'No';
             }
         ),
         array(
-            'title' => 'search-result-label-trailers',
-            'formatter' => function ($data, $column) {
-                if (empty($data['noOfTrailersPossessed'])) {
-                    return 'N/A';
-                }
-                return $data['noOfTrailersPossessed'];
-            }
+            'title' => 'entity-view-table-header-date-added',
+            'formatter' => 'Date',
+            'name' => 'createdOn'
+        ),
+        array(
+            'title' => 'entity-view-table-header-date-removed',
+            'formatter' => 'Date',
+            'name' => 'deletedDate'
         )
     )
 );
