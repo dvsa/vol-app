ec21aee207dc829e52feb20f0f80cf1ae5620f8d
Rob Caiger <rob@clocal.co.uk>
2015-07-30 16:24:34 +0100

    Migration of PSV Vehicles

---
+++ app/selfserve/module/Olcs/src/Table/Tables/lva-psv-vehicles-medium.table.php
@@ -0,0 +1,48 @@
+<?php
+
+$translationPrefix = 'application_vehicle-safety_vehicle-psv-medium.table';
+
+return array(
+    'variables' => array(
+        'title' => $translationPrefix . '.title',
+        'empty_message' => $translationPrefix . '.empty_message',
+        'required_label' => 'vehicle',
+        'within_form' => true
+    ),
+    'settings' => array(
+        'crud' => array(
+            'actions' => array(
+                'add' => array('class' => 'primary'),
+                'delete' => array('class' => 'secondary', 'requireRows' => true),
+                'transfer' => array(
+                    'label' => 'Transfer', 'class' => 'secondary js-require--multiple', 'requireRows' => true
+                )
+            )
+        )
+    ),
+    'attributes' => array(
+    ),
+    'columns' => array(
+        array(
+            'title' => $translationPrefix . '.vrm',
+            'stack' => 'vehicle->vrm',
+            'formatter' => 'StackValue',
+            'action' => 'edit',
+            'type' => 'Action',
+        ),
+        array(
+            'title' => $translationPrefix . '.specified',
+            'formatter' => 'Date',
+            'name' => 'specifiedDate'
+        ),
+        array(
+            'title' => $translationPrefix . '.removed',
+            'formatter' => 'Date',
+            'name' => 'removalDate'
+        ),
+        array(
+            'width' => 'checkbox',
+            'type' => 'Checkbox'
+        )
+    )
+);
