3e949ed838ce1ff813e788e28adb755979613678
Rob Caiger <rob@clocal.co.uk>
2015-09-03 13:35:05 +0100

    Fixed tests

--- app/internal/test/Olcs/src/FormService/Form/Lva/PsvVehiclesTest.php
+++ app/internal/test/Olcs/src/FormService/Form/Lva/PsvVehiclesTest.php
@@ -7,6 +7,7 @@
  */
 namespace OlcsTest\FormService\Form\Lva;
 
+use Mockery as m;
 use Olcs\FormService\Form\Lva\PsvVehicles;
 
 /**
@@ -17,4 +18,40 @@ use Olcs\FormService\Form\Lva\PsvVehicles;
 class PsvVehiclesTest extends AbstractLvaFormServiceTestCase
 {
     protected $classToTest = PsvVehicles::class;
+
+    public function testGetForm()
+    {
+        // Mocks
+        $mockForm = m::mock(\Common\Form\Form::class);
+
+        $this->formHelper->shouldReceive('createForm')
+            ->andReturn($mockForm)
+            ->shouldReceive('remove')
+            ->once()
+            ->with($mockForm, 'shareInfo');
+
+        $mockForm
+            ->shouldReceive('get')
+            ->with('form-actions')
+            ->once()
+            ->andReturn(
+                m::mock()
+                    ->shouldReceive('get')
+                    ->with('save')
+                    ->once()
+                    ->andReturn(
+                        m::mock()
+                            ->shouldReceive('setLabel')
+                            ->with('internal.save.button')
+                            ->once()
+                            ->getMock()
+                    )
+                    ->getMock()
+            )
+            ->getMock();
+
+        $form = $this->sut->getForm();
+
+        $this->assertSame($mockForm, $form);
+    }
 }
