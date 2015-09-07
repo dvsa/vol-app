0cf6ce7596fff6ead823abefe3c807eb619b890a
Alex P <alex.peshkov@valtech.co.uk>
2015-09-07 12:16:11 +0100

    OLCS-9922 Enhance continuation pages/processing to include PSV small, medium and large vehicles

---
+++ app/internal/test/Olcs/src/Data/Mapper/ContinuationTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace OlcsTest\Data\Mapper;
+
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Olcs\Data\Mapper\Continuation as Sut;
+use Zend\Form\Form;
+
+/**
+ * Continuation Mapper Test
+ */
+class ContinuationTest extends MockeryTestCase
+{
+    public function testMapFromErrors()
+    {
+        $errors = [
+            'totAuthVehicles' => [['error1']],
+            'general' => ['error2'],
+        ];
+        $expected = [
+            'general' => ['error2']
+        ];
+        $messages = [
+            'fields' => ['totalVehicleAuthorisation' => ['error1']]
+        ];
+        $mockForm = m::mock(Form::class)->makePartial()
+            ->shouldReceive('setMessages')
+            ->with($messages)
+            ->once()
+            ->getMock();
+
+        $this->assertEquals($expected, Sut::mapFromErrors($mockForm, $errors));
+    }
+}
