d57c64418d2e3993505e816c1c0f3cca83228aea
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-09-28 17:36:15 +0100

    PublicationLink data mapper

---
+++ app/internal/module/Olcs/src/Data/Mapper/PublicationLink.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Olcs\Data\Mapper;
+
+use Common\Data\Mapper\MapperInterface;
+use Zend\Form\FormInterface;
+
+/**
+ * Class Generic Mapper for forms with Fields field set
+ * @package Olcs\Data\Mapper
+ */
+class PublicationLink implements MapperInterface
+{
+    /**
+     * Should map data from a result array into an array suitable for a form
+     *
+     * @param array $data
+     * @return array
+     */
+    public static function mapFromResult(array $data)
+    {
+        $readOnly = [
+            'typeArea' => $data['publication']['pubType'] . ' / ' . $data['publication']['trafficArea']['name'],
+            'publicationNo' => $data['publication']['publicationNo'],
+            'status' => $data['publication']['pubStatus']['description'],
+            'section' => $data['publicationSection']['description'],
+            'trafficArea' => $data['publication']['trafficArea']['name'],
+            'publicationDate' => date('d/m/Y', strtotime($data['publication']['pubDate']))
+        ];
+
+        $textFields = [
+            'text1' => $data['text1'],
+            'text2' => $data['text2'],
+            'text3' => $data['text3']
+        ];
+
+        if ($data['isNew']) {
+            $base = [
+                'id' => $data['id'],
+                'version' => $data['version']
+            ];
+
+            $formData = [
+                'fields' => array_merge($textFields, $base)
+            ];
+        } else {
+            $formData = [
+                'readOnlyText' => $textFields
+            ];
+        }
+
+        $formData['readOnly'] = $readOnly;
+
+        return $formData;
+    }
+
+    /**
+     * Should map form data back into a command data structure
+     *
+     * @param array $data
+     * @return array
+     */
+    public static function mapFromForm(array $data)
+    {
+        return $data['fields'];
+    }
+
+    /**
+     * Should map errors onto the form, any global errors should be returned so they can be added
+     * to the flash messenger
+     *
+     * @param FormInterface $form
+     * @param array $errors
+     * @return array
+     */
+    public static function mapFromErrors(FormInterface $form, array $errors)
+    {
+        return $errors;
+    }
+}
---
+++ app/internal/test/Olcs/src/Data/Mapper/PublicationLinkTest.php
@@ -0,0 +1,129 @@
+<?php
+namespace OlcsTest\Data\Mapper;
+
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Olcs\Data\Mapper\PublicationLink as Sut;
+use Zend\Form\FormInterface;
+
+/**
+ * PublicationLink Test
+ */
+class PublicationLinkTest extends MockeryTestCase
+{
+    /**
+     * Tests mapFromForm
+     */
+    public function testMapFromForm()
+    {
+        $inData = ['fields' => ['field' => 'data']];
+        $expected = ['field' => 'data'];
+
+        $this->assertEquals($expected, Sut::mapFromForm($inData));
+    }
+
+    /**
+     * Tests mapFromErrors
+     */
+    public function testMapFromErrors()
+    {
+        $mockForm = m::mock(FormInterface::class);
+        $errors = ['field' => 'data'];
+
+        $this->assertEquals($errors, Sut::mapFromErrors($mockForm, $errors));
+    }
+
+    /**
+     * test map from result
+     *
+     * @dataProvider mapFromResultProvider
+     *
+     * @param $inputData
+     * @param $expectedOutput
+     */
+    public function testMapFromResultStatusIsNew($inputData, $expectedOutput)
+    {
+        $this->assertEquals($expectedOutput, Sut::mapFromResult($inputData));
+    }
+
+    /**
+     * Data provider for mapFromResult
+     *
+     * @return array
+     */
+    public function mapFromResultProvider()
+    {
+        $pubType = 'pub type';
+        $trafficArea = 'trafficArea';
+        $publicationNo = 12345;
+        $pubStatus = 'status';
+        $pubSection = 'section';
+        $pubDate = '2015-12-25';
+        $formatPubDate = date('d/m/Y', strtotime($pubDate));
+        $text1 = 'text1';
+        $text2 = 'text2';
+        $text3 = 'text3';
+        $id = 11;
+        $version = 22;
+
+        $inputDataNew = [
+            'id' => $id,
+            'version' => $version,
+            'publication' => [
+                'pubType' => $pubType,
+                'trafficArea' => [
+                    'name' => $trafficArea
+                ],
+                'publicationNo' => $publicationNo,
+                'pubStatus' => [
+                    'description' => $pubStatus
+                ],
+                'pubDate' => $pubDate
+            ],
+            'publicationSection' => [
+                'description' => $pubSection
+            ],
+            'text1' => $text1,
+            'text2' => $text2,
+            'text3' => $text3,
+            'isNew' => true
+        ];
+
+        $inputDataNotNew = $inputDataNew;
+        $inputDataNotNew['isNew'] = false;
+
+        $readOnly = [
+            'typeArea' => $pubType . ' / ' . $trafficArea,
+            'publicationNo' => $publicationNo,
+            'status' => $pubStatus,
+            'section' => $pubSection,
+            'trafficArea' => $trafficArea,
+            'publicationDate' => $formatPubDate
+        ];
+
+        $expectedIsNewOutput = [
+            'fields' => [
+                'id' => $id,
+                'version' => $version,
+                'text1' => $text1,
+                'text2' => $text2,
+                'text3' => $text3
+            ],
+            'readOnly' => $readOnly
+        ];
+
+        $expectedNotNewOutput = [
+            'readOnlyText' => [
+                'text1' => $text1,
+                'text2' => $text2,
+                'text3' => $text3
+            ],
+            'readOnly' => $readOnly
+        ];
+
+        return [
+            [$inputDataNew, $expectedIsNewOutput],
+            [$inputDataNotNew, $expectedNotNewOutput]
+        ];
+    }
+}
