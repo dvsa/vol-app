6c3197b92c3229a1c22b4bbeffb26f44b7bc5150
Dan Eggleston <dan@stolenegg.com>
2015-10-06 16:59:30 +0100

    OLCS-10678 move cpid pages into reports section

--- app/internal/module/Admin/src/Controller/PaymentProcessingFeesController.php
+++ app/internal/module/Admin/src/Controller/PaymentProcessingFeesController.php
@@ -5,13 +5,15 @@
  */
 namespace Admin\Controller;
 
+use Common\Controller\AbstractActionController;
+use Olcs\Controller\Interfaces\LeftViewProvider;
 use Olcs\Controller\Traits\FeesActionTrait;
 use Zend\View\Model\ViewModel;
 
 /**
  * Payment Processing Fees Controller
  */
-class PaymentProcessingFeesController extends PaymentProcessingController
+class PaymentProcessingFeesController extends AbstractActionController implements LeftViewProvider
 {
     use FeesActionTrait;
 
