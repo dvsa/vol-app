c10b5d9ad698eaf860da8178a3ccc06a3f0b6b90
Rob Caiger <rob@clocal.co.uk>
2015-10-13 09:40:08 +0100

    Removed methods from concretes

--- app/selfserve/module/Olcs/src/Controller/Lva/Application/UndertakingsController.php
+++ app/selfserve/module/Olcs/src/Controller/Lva/Application/UndertakingsController.php
@@ -38,16 +38,12 @@ class UndertakingsController extends Lva\AbstractUndertakingsController
 
     protected function formatDataForForm($applicationData)
     {
-        $licenceType = $applicationData['licenceType']['id'];
         $goodsOrPsv  = $applicationData['goodsOrPsv']['id'];
-        $niFlag      = $applicationData['niFlag'];
 
         $output = array(
             'declarationsAndUndertakings' => array(
-                'declarationConfirmation' => $applicationData['declarationConfirmation'],
                 'version' => $applicationData['version'],
                 'id' => $applicationData['id'],
-                'undertakings' => $this->getUndertakingsPartial($goodsOrPsv, $licenceType, $niFlag),
             )
         );
 
@@ -68,69 +64,10 @@ class UndertakingsController extends Lva\AbstractUndertakingsController
     {
         parent::updateForm($form, $applicationData);
 
-        $licenceType = $applicationData['licenceType']['id'];
         $goodsOrPsv  = $applicationData['goodsOrPsv']['id'];
 
         if (!($goodsOrPsv === Licence::LICENCE_CATEGORY_GOODS_VEHICLE)) {
             $this->getServiceLocator()->get('Helper\Form')->remove($form, 'interim');
         }
-
-        if ($licenceType === Licence::LICENCE_TYPE_SPECIAL_RESTRICTED
-            && $goodsOrPsv === Licence::LICENCE_CATEGORY_PSV
-        ) {
-            // override label
-            $form->get('declarationsAndUndertakings')
-                ->get('declarationConfirmation')->setLabel('markup-declarations-psv356');
-        }
-    }
-
-    /**
-     * Determine correct partial to use for undertakings html
-     *
-     * Valid partials are:
-     *  gv79-standard, gv79-restricted,
-     *  gvni79-standard, gvni79-restricted,
-     *  psv421-standard, psv421-restricted, psv-356
-     *
-     * (public for unit testing)
-     *
-     * @param string $goodsOrPsv
-     * @param string $licenceType
-     * @return string
-     */
-    public function getUndertakingsPartial($goodsOrPsv, $licenceType, $niFlag)
-    {
-        $part = $this->getPartialPrefix($goodsOrPsv);
-        if ($niFlag == 'Y') {
-            $part .= 'ni';
-        }
-
-        $part .= $this->getSuffix($goodsOrPsv, $licenceType);
-
-        $nonRestricted = [
-            Licence::LICENCE_TYPE_STANDARD_NATIONAL,
-            Licence::LICENCE_TYPE_STANDARD_INTERNATIONAL
-        ];
-
-        if (in_array($licenceType, $nonRestricted)) {
-            $part .= '-standard';
-        }
-
-        if ($licenceType === Licence::LICENCE_TYPE_RESTRICTED) {
-            $part .= '-restricted';
-        }
-
-        return 'markup-undertakings-' . $part;
-    }
-
-    protected function getSuffix($goodsOrPsv, $licenceType)
-    {
-        if ($goodsOrPsv === Licence::LICENCE_CATEGORY_PSV) {
-            if ($licenceType === Licence::LICENCE_TYPE_SPECIAL_RESTRICTED) {
-                return 356;
-            }
-            return 421;
-        }
-        return 79;
     }
 }
