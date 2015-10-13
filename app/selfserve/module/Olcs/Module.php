b57c005212c93957e5e5063e9460e2bcdbb23456
Rob Caiger <rob@clocal.co.uk>
2015-10-13 11:59:00 +0100

    Removed session config from Module.php as it's never used

--- app/selfserve/module/Olcs/Module.php
+++ app/selfserve/module/Olcs/Module.php
@@ -8,8 +8,6 @@
 
 namespace Olcs;
 
-use Zend\Session\SessionManager;
-use Zend\Session\Container;
 use Zend\Mvc\ModuleRouteListener;
 
 /**
@@ -48,69 +46,5 @@ class Module
         $eventManager = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
-    }
-
-    public function bootstrapSession($e)
-    {
-        $session = $e->getApplication()
-            ->getServiceManager()
-            ->get('Zend\Session\SessionManager');
-        $session->start();
-
-        $container = new Container('initialized');
-        if (!isset($container->init)) {
-            $session->regenerateId(true);
-            $container->init = 1;
-        }
-    }
-
-    public function getServiceConfig()
-    {
-        return array(
-            'factories' => array(
-                'Zend\Session\SessionManager' => function ($sm) {
-                    $config = $sm->get('config');
-                    if (isset($config['session'])) {
-                        $session = $config['session'];
-
-                        $sessionConfig = null;
-                        if (isset($session['config'])) {
-                            $class = isset($session['config']['class'])
-                                ? $session['config']['class']
-                                : 'Zend\Session\Config\SessionConfig';
-                            $options = isset($session['config']['options']) ? $session['config']['options'] : array();
-                            $sessionConfig = new $class();
-                            $sessionConfig->setOptions($options);
-                        }
-
-                        $sessionStorage = null;
-                        if (isset($session['storage'])) {
-                            $class = $session['storage'];
-                            $sessionStorage = new $class();
-                        }
-
-                        $sessionSaveHandler = null;
-                        if (isset($session['save_handler'])) {
-                            // class should be fetched from service manager since it will require constructor arguments
-                            $sessionSaveHandler = $sm->get($session['save_handler']);
-                        }
-
-                        $sessionManager = new SessionManager($sessionConfig, $sessionStorage, $sessionSaveHandler);
-
-                        if (isset($session['validator'])) {
-                            $chain = $sessionManager->getValidatorChain();
-                            foreach ($session['validator'] as $validator) {
-                                $validator = new $validator();
-                                $chain->attach('session.validate', array($validator, 'isValid'));
-                            }
-                        }
-                    } else {
-                        $sessionManager = new SessionManager();
-                    }
-                    Container::setDefaultManager($sessionManager);
-                    return $sessionManager;
-                }
-            )
-        );
     }
 }
