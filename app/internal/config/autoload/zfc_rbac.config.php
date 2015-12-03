e7d4ddeb1025f900fcdcf2c01be52aa556b279c3
Shaun Lizzio <shaun@lizzio.co.uk>
2015-12-03 14:46:43 +0000

    Configured admin-menu to use Zend navigation

--- app/internal/config/autoload/zfc_rbac.config.php
+++ app/internal/config/autoload/zfc_rbac.config.php
@@ -1,16 +1,25 @@
 <?php
-
+/*
+ * RoutePermissionGuard - Configuration is '<route_id>' => '<permission_name>'. Order is important, once
+ * a route is matched, all others are ignored.
+ * @see https://github.com/ZF-Commons/zfc-rbac/blob/master/docs/README.md
+ *
+ * NOTE: Any zfc_rbac configuration within any Module will be merged with other modules, hence this
+ * global file where the order can be controlled and easily debugged.
+ *
+ */
 return [
     'guards' => [
         'ZfcRbac\Guard\RoutePermissionsGuard' =>[
-            // OLCS Module pages
+            // OLCS Module Routes
             'case_processing_notes' => ['internal-notes'],
             '*case*' => ['internal-case'],
             '*documents*' => ['internal-documents'],
             '*docs*' => ['internal-documents'],
             'fetch_tmp_document' => ['internal-documents'],
             'note' => ['internal-notes'],
-            // cli module route
+
+            // CLI module Routes
             'batch-licence-status' => ['*'],
             'batch-cns' => ['*'],
             'process-queue' => ['*'],
@@ -20,14 +29,15 @@ return [
             'not-found' => ['*'],
             'server-error' => ['*'],
 
-            // Admin Module Pages
+            // Admin Module Routes
             'admin-dashboard/admin-financial-standing*' => ['internal-admin'],
             'admin-dashboard/admin-payment-processing*' => ['internal-admin'],
             'admin-dashboard/admin-system-message' => ['internal-admin'],
             'admin-dashboard/admin-public-holiday' => ['internal-admin'],
             'admin-dashboard/admin-team-management' => ['internal-admin'],
-
-            //'admin-dashboard/*' => ['internal-user'],
+            'admin-dashboard/admin-partner-management' => ['internal-admin'],
+            'admin-dashboard/admin-printer-management' => ['internal-admin'],
+            'admin-dashboard/admin-user-management' => ['internal-admin'],
 
             // Global route rule needs to be last
             '*' => ['internal-view'],
