2a63c17524a0886a63b4259dce1cfaca074010dd
Rob Caiger <rob@clocal.co.uk>
2015-12-14 09:19:48 +0000

    Updated unit test

--- app/internal/test/Olcs/src/FormService/Form/Lva/FinancialHistoryTest.php
+++ app/internal/test/Olcs/src/FormService/Form/Lva/FinancialHistoryTest.php
@@ -7,6 +7,8 @@
  */
 namespace OlcsTest\FormService\Form\Lva;
 
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Olcs\FormService\Form\Lva\FinancialHistory;
 
 /**
@@ -14,7 +16,42 @@ use Olcs\FormService\Form\Lva\FinancialHistory;
  *
  * @author Dan Eggleston <dan@stolenegg.com>
  */
-class FinancialHistoryTest extends AbstractLvaFormServiceTestCase
+class FinancialHistoryTest extends MockeryTestCase
 {
-    protected $classToTest = FinancialHistory::class;
+    protected $sut;
+
+    protected $formHelper;
+
+    protected $fsm;
+
+    public function setUp()
+    {
+        $this->formHelper = m::mock('\Common\Service\Helper\FormHelperService');
+        $this->fsm = m::mock('\Common\FormService\FormServiceManager')->makePartial();
+
+        $this->sut = new FinancialHistory();
+        $this->sut->setFormHelper($this->formHelper);
+        $this->sut->setFormServiceLocator($this->fsm);
+    }
+
+    public function testGetForm()
+    {
+        $request = m::mock();
+
+        // Mocks
+        $mockForm = m::mock();
+
+        $formActions = m::mock();
+        $formActions->shouldReceive('get->setLabel')->once();
+
+        $mockForm->shouldReceive('get')->with('form-actions')->andReturn($formActions);
+
+        $this->formHelper->shouldReceive('createFormWithRequest')
+            ->with('Lva\FinancialHistory', $request)
+            ->andReturn($mockForm);
+
+        $form = $this->sut->getForm($request);
+
+        $this->assertSame($mockForm, $form);
+    }
 }
