6b6c75c8d2caed238955f2dea28c9874e43d13c1
Alex P <alex.peshkov@valtech.co.uk>
2016-01-08 14:32:06 +0000

    OLCS-11619 ImpoundingLegislation data service migration

--- app/internal/test/Olcs/src/Service/Data/PaymentTypeTest.php
+++ app/internal/test/Olcs/src/Service/Data/PaymentTypeTest.php
@@ -5,37 +5,59 @@ namespace OlcsTest\Service\Data;
 use Olcs\Service\Data\PaymentType;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Mockery as m;
+use CommonTest\Service\Data\AbstractDataServiceTestCase;
 
 /**
  * Class PaymentTypeTest
  * @package OlcsTest\Service\Data
  */
-class PaymentTypeTest extends MockeryTestCase
+class PaymentTypeTest extends AbstractDataServiceTestCase
 {
     public function testFetchListData()
     {
-        $this->markTestSkipped();
         $data = [
-            [
-                'id' => 'fpm_cash',
-                'description' => 'Cash'
-            ], [
-                'id' => 'will_be_filtered',
-                'description' => 'Foo'
-            ], [
-                'id' => 'fpm_card_offline',
-                'description' => 'Will Be Overridden'
+            'results' => [
+                [
+                    'id' => 'fpm_cash',
+                    'description' => 'Cash'
+                ], [
+                    'id' => 'will_be_filtered',
+                    'description' => 'Foo'
+                ], [
+                    'id' => 'fpm_card_offline',
+                    'description' => 'Will Be Overridden'
+                ]
             ]
         ];
 
-        $mockRestClient = m::mock('\Common\Util\RestClient')
-            ->shouldReceive('get')
-            ->with('/category/fee_pay_method')
+        $mockTransferAnnotationBuilder = m::mock()
+            ->shouldReceive('createQuery')->once()->andReturn('query')
+            ->once()
+            ->getMock();
+
+        $mockResponse = m::mock()
+            ->shouldReceive('isOk')
+            ->andReturn(true)
+            ->once()
+            ->shouldReceive('getResult')
             ->andReturn($data)
+            ->twice()
             ->getMock();
 
         $sut = new PaymentType();
-        $sut->setRestClient($mockRestClient);
+        $this->mockHandleQuery($sut, $mockTransferAnnotationBuilder, $mockResponse, $data);
+        $this->mockServiceLocator
+            ->shouldReceive('get')
+            ->with('LanguagePreference')
+            ->andReturn(
+                m::mock()
+                    ->shouldReceive('getPreference')
+                    ->andReturn('en')
+                    ->once()
+                    ->getMock()
+            )
+            ->once()
+            ->getMock();
 
         $result = [
             [
