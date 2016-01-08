da7e279525509c221fcd9c1ddbf1b8805b8a15c7
Alex P <alex.peshkov@valtech.co.uk>
2016-01-08 14:41:04 +0000

    OLCS-11619 slight code improvements

--- app/internal/test/Olcs/src/Service/Data/PaymentTypeTest.php
+++ app/internal/test/Olcs/src/Service/Data/PaymentTypeTest.php
@@ -5,59 +5,29 @@ namespace OlcsTest\Service\Data;
 use Olcs\Service\Data\PaymentType;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Mockery as m;
-use CommonTest\Service\Data\AbstractDataServiceTestCase;
 
 /**
  * Class PaymentTypeTest
  * @package OlcsTest\Service\Data
  */
-class PaymentTypeTest extends AbstractDataServiceTestCase
+class PaymentTypeTest extends MockeryTestCase
 {
     public function testFetchListData()
     {
         $data = [
-            'results' => [
-                [
-                    'id' => 'fpm_cash',
-                    'description' => 'Cash'
-                ], [
-                    'id' => 'will_be_filtered',
-                    'description' => 'Foo'
-                ], [
-                    'id' => 'fpm_card_offline',
-                    'description' => 'Will Be Overridden'
-                ]
+            [
+                'id' => 'fpm_cash',
+                'description' => 'Cash'
+            ], [
+                'id' => 'will_be_filtered',
+                'description' => 'Foo'
+            ], [
+                'id' => 'fpm_card_offline',
+                'description' => 'Will Be Overridden'
             ]
         ];
-
-        $mockTransferAnnotationBuilder = m::mock()
-            ->shouldReceive('createQuery')->once()->andReturn('query')
-            ->once()
-            ->getMock();
-
-        $mockResponse = m::mock()
-            ->shouldReceive('isOk')
-            ->andReturn(true)
-            ->once()
-            ->shouldReceive('getResult')
-            ->andReturn($data)
-            ->twice()
-            ->getMock();
-
         $sut = new PaymentType();
-        $this->mockHandleQuery($sut, $mockTransferAnnotationBuilder, $mockResponse, $data);
-        $this->mockServiceLocator
-            ->shouldReceive('get')
-            ->with('LanguagePreference')
-            ->andReturn(
-                m::mock()
-                    ->shouldReceive('getPreference')
-                    ->andReturn('en')
-                    ->once()
-                    ->getMock()
-            )
-            ->once()
-            ->getMock();
+        $sut->setData('fee_pay_method', $data);
 
         $result = [
             [
