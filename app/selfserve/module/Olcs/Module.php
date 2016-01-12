2ff0fc62c56b5362b81f193f7e3f84b50352b5ac
Shaun Lizzio <shaun@lizzio.co.uk>
2016-01-12 14:16:56 +0000

    Configure Sessions

--- app/selfserve/module/Olcs/Module.php
+++ app/selfserve/module/Olcs/Module.php
@@ -10,6 +10,9 @@ namespace Olcs;
 
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
+use Zend\Session\Config\SessionConfig;
+use Zend\Session\Container;
+use Zend\Session\SessionManager;
 
 /**
  * Module.php
@@ -58,5 +61,20 @@ class Module
 
         $cookieBannerListener = $sm->get('CookieBannerListener');
         $cookieBannerListener->attach($eventManager, 1);
+
+        $this->initSession(array(
+            'remember_me_seconds' => 180,
+            'use_cookies' => true,
+            'cookie_httponly' => true,
+        ));
+    }
+
+    public function initSession($config)
+    {
+        $sessionConfig = new SessionConfig();
+        $sessionConfig->setOptions($config);
+        $sessionManager = new SessionManager($sessionConfig);
+        $sessionManager->start();
+        Container::setDefaultManager($sessionManager);
     }
 }
