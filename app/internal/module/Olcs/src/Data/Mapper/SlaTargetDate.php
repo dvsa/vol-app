fc0eae15d6566bfaa6900ad29241cc4874a299c8
Shaun Lizzio <shaun@lizzio.co.uk>
2016-02-02 14:42:19 +0000

    unset uneccessary field

--- app/internal/module/Olcs/src/Data/Mapper/SlaTargetDate.php
+++ app/internal/module/Olcs/src/Data/Mapper/SlaTargetDate.php
@@ -42,6 +42,7 @@ class SlaTargetDate implements MapperInterface
      */
     public static function mapFromForm(array $data)
     {
+        unset($data['fields']['entityTypeHtml']);
         return $data['fields'];
     }
 
