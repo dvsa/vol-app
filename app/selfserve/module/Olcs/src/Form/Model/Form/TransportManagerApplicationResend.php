447006f8f32e9d13c650c7706ae32c23b1c5e2bd
Edmund Reed <esr360@live.com>
2016-02-25 11:33:58 +0000

    adding aria-labels

--- app/selfserve/module/Olcs/src/Form/Model/Form/TransportManagerApplicationResend.php
+++ app/selfserve/module/Olcs/src/Form/Model/Form/TransportManagerApplicationResend.php
@@ -13,13 +13,22 @@ class TransportManagerApplicationResend
 {
     /**
      * @Form\Attributes({"id":"emailAddress","placeholder":"","class":"medium", "readonly":"readonly"})
-     * @Form\Options({"label":"transport-managers-email-address"})
+     * @Form\Options({
+     *     "label": "transport-managers-email-address",
+     *     "label_attributes": {
+     *         "aria-label": "Enter Transport Manager's email address"
+     *     }
+     * })
      * @Form\Type("Text")
      */
     protected $emailAddress;
 
     /**
-     * @Form\Attributes({"type":"submit","class":"action--primary large"})
+     * @Form\Attributes({
+     *     "type":"submit",
+     *     "class":"action--primary large",
+     *     "aria-label": "Confirm resend link to Transport Manger"
+     * })
      * @Form\Type("\Common\Form\Elements\InputFilters\ActionButton")
      * @Form\Options({
      *     "label": "resend-link"
