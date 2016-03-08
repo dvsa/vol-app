a89ff185622445070cc11ee9eeac8b6875f1c0c9
Alex P <alex.peshkov@valtech.co.uk>
2016-03-08 16:44:43 +0000

    OLCS-10151 remove actionLinks for people table on external licence

--- app/selfserve/module/Olcs/src/Controller/Lva/Licence/PeopleController.php
+++ app/selfserve/module/Olcs/src/Controller/Lva/Licence/PeopleController.php
@@ -10,6 +10,7 @@ namespace Olcs\Controller\Lva\Licence;
 
 use Common\Controller\Lva;
 use Olcs\Controller\Lva\Traits\LicenceControllerTrait;
+use Zend\Form\Form;
 
 /**
  * External Licence People Controller
@@ -23,4 +24,10 @@ class PeopleController extends Lva\AbstractPeopleController
 
     protected $lva = 'licence';
     protected $location = 'external';
+
+    protected function alterFormForLva(Form $form)
+    {
+        $table = $form->get('table')->get('table')->getTable();
+        $table->removeColumn('actionLinks');
+    }
 }
