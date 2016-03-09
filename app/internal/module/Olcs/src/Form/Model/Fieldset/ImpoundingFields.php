35a2642fbcb7371ab591570dc72a143e3323cf00
Piotr Filak <piotr.filak@valtech.co.uk>
2016-03-09 09:40:29 +0000

    OLCS-11898 - Impounding validation update

--- app/internal/module/Olcs/src/Form/Model/Fieldset/ImpoundingFields.php
+++ app/internal/module/Olcs/src/Form/Model/Fieldset/ImpoundingFields.php
@@ -49,10 +49,25 @@ class ImpoundingFields
      * })
      * 
      * @Form\Type("Text")
-     * @Form\Filter({"name":"Common\Filter\Vrm"})
-     * @Form\Validator({"name":"Common\Form\Elements\Validators\Vrm"})
+     * @Form\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"min":1,"max":20}})
      */
     public $vrm = null;
+
+    /**
+     * @Form\Attributes({"id":"birthDate"})
+     * @Form\Options({
+     *     "label": "Birth date",
+     *     "create_empty_option": true,
+     *     "render_delimiters": false
+     * })
+     * @Form\Required(false)
+     * @Form\Type("DateSelect")
+     * @Form\Validator({"name": "Date", "options": {"format": "Y-m-d"}})
+     * @Form\Validator({"name": "\Common\Form\Elements\Validators\DateNotInFuture"})
+     * @Form\Filter({"name": "DateSelectNullifier"})
+     */
+    public $birthDate = null;
 
     /**
      * @Form\Attributes({"id":"impoundingLegislationTypes","placeholder":"","multiple":"multiple",
