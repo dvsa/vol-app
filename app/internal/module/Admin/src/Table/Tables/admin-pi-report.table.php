71d9806a6cc0a5d0ece64481605953cdfae2547b
Piotr Filak <piotr.filak@valtech.co.uk>
2016-04-15 08:58:09 +0100

    OLCS-2011 - PI report page - more formatters

--- app/internal/module/Admin/src/Table/Tables/admin-pi-report.table.php
+++ app/internal/module/Admin/src/Table/Tables/admin-pi-report.table.php
@@ -22,78 +22,21 @@ return array(
         array(
             'title' => 'Case Id',
             'formatter' => function ($data) {
-                if (empty($data['pi']['case']['id'])) {
-                    return '';
-                }
-
-                return sprintf(
-                    '<a href="%s">%s</a>',
-                    $this->generateUrl(
-                        array(
-                            'case' => $data['pi']['case']['id']
-                        ),
-                        'case',
-                        false
-                    ),
-                    $data['pi']['case']['id']
+                return $this->callFormatter(
+                    [
+                        'formatter' => 'CaseLink',
+                    ],
+                    $data['pi']['case']
                 );
             }
         ),
         array(
             'title' => 'Record',
-            'formatter' => function ($data) {
-                if (!empty($data['pi']['case']['licence'])) {
-                    return sprintf(
-                        '<a href="%s">%s</a> (%s)',
-                        $this->generateUrl(
-                            array(
-                                'licence' => $data['pi']['case']['licence']['id']
-                            ),
-                            'licence',
-                            false
-                        ),
-                        $data['pi']['case']['licence']['licNo'],
-                        $data['pi']['case']['licence']['status']['description']
-                    );
-                } elseif (!empty($data['pi']['case']['transportManager'])) {
-                    return sprintf(
-                        '<a href="%s">TM %s</a> (%s)',
-                        $this->generateUrl(
-                            array(
-                                'transportManager' => $data['pi']['case']['transportManager']['id']
-                            ),
-                            'transport-manager',
-                            false
-                        ),
-                        $data['pi']['case']['transportManager']['id'],
-                        $data['pi']['case']['transportManager']['tmStatus']['description']
-                    );
-                }
-                return '';
-            }
+            'formatter' => 'PiReportRecord'
         ),
         array(
             'title' => 'Name',
-            'formatter' => function ($data) {
-                if (!empty($data['pi']['case']['licence']['organisation'])) {
-                    // display org linked to the licence
-                    return $this->callFormatter(
-                        [
-                            'formatter' => 'OrganisationLink',
-                        ],
-                        $data['pi']['case']['licence']
-                    );
-                } elseif (!empty($data['pi']['case']['transportManager']['homeCd']['person'])) {
-                    // display TM details
-                    return $this->callFormatter(
-                        [
-                            'formatter' => 'Name',
-                        ],
-                        $data['pi']['case']['transportManager']['homeCd']['person']
-                    );
-                }
-                return '';
-            }
+            'formatter' => 'PiReportName'
         ),
         array(
             'title' => 'PI Date & Time',
