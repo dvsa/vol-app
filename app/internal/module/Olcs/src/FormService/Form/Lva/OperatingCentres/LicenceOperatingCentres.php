8f4fa0547f215ca0b568ca377ccdd75a8d75145e
Mat Evans <mat.evans@valtech.co.uk>
2016-04-21 15:56:59 +0100

    OLCS-7653 Traffic area always editable on Internal Operating centres page

---
+++ app/internal/module/Olcs/src/FormService/Form/Lva/OperatingCentres/LicenceOperatingCentres.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Olcs\FormService\Form\Lva\OperatingCentres;
+
+use Common\FormService\Form\Lva\OperatingCentres\AbstractOperatingCentres;
+use Zend\Form\Form;
+
+/**
+ * Licence Operating Centres
+ */
+class LicenceOperatingCentres extends AbstractOperatingCentres
+{
+    protected function allowChangingTrafficArea($trafficAreaId)
+    {
+        // Traffic area can be changed as long as its not Northern Irelend
+        return ($trafficAreaId !== 'N');
+    }
+}
