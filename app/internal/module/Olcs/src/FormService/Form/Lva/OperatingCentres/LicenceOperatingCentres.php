109abed60669e7a9de16489d7f435820eae43afa
Mat Evans <mat.evans@valtech.co.uk>
2016-04-26 14:31:53 +0100

    OLCS-7653 Only allow chaning traffic area on new applications

--- app/internal/module/Olcs/src/FormService/Form/Lva/OperatingCentres/LicenceOperatingCentres.php
+++ app/internal/module/Olcs/src/FormService/Form/Lva/OperatingCentres/LicenceOperatingCentres.php
@@ -11,20 +11,4 @@ use Zend\Form\Form;
  */
 class LicenceOperatingCentres extends AbstractOperatingCentres
 {
-    /**
-     * @see AbstractOperatingCentres::allowChangingTrafficArea
-     */
-    protected function allowChangingTrafficArea($trafficAreaId)
-    {
-        // Traffic area can be changed as long as its not Northern Irelend
-        return ($trafficAreaId !== RefData::NORTHERN_IRELAND_TRAFFIC_AREA_CODE);
-    }
-
-    /**
-     * @see AbstractOperatingCentres::removeTrafficAreaElements
-     */
-    protected function removeTrafficAreaElements($data)
-    {
-        return false;
-    }
 }
