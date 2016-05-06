17cd7fda104b2fcdc96c120f65d6d33992816039
Shaun Lizzio <shaun@lizzio.co.uk>
2016-05-06 11:41:59 +0100

    implemented Dto

--- app/internal/module/Olcs/src/Service/Data/PublicInquiryReason.php
+++ app/internal/module/Olcs/src/Service/Data/PublicInquiryReason.php
@@ -1,13 +1,22 @@
 <?php
 
 namespace Olcs\Service\Data;
+use Dvsa\Olcs\Transfer\Query\Reason\ReasonList as ReasonListDto;
+use Common\Service\Data\ListDataInterface;
+use Zend\ServiceManager\FactoryInterface;
+use Common\Service\Data\LicenceServiceTrait;
 
 /**
  * Class PublicInquiryReason
+ *
  * @package Olcs\Service\Data
  */
-class PublicInquiryReason extends AbstractPublicInquiryData
+class PublicInquiryReason extends AbstractPublicInquiryData implements ListDataInterface, FactoryInterface
 {
+    protected $listDto = ReasonListDto::class;
+    protected $sort = 'sectionCode';
+    protected $order = 'ASC';
+
     /**
      * @var string
      */
