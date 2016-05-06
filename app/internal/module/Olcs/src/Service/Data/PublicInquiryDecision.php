07fe3b4e3eb1eadf7356735c7e956df6d69a4017
Shaun Lizzio <shaun@lizzio.co.uk>
2016-05-06 10:29:16 +0100

    Implemented DTO in data service

--- app/internal/module/Olcs/src/Service/Data/PublicInquiryDecision.php
+++ app/internal/module/Olcs/src/Service/Data/PublicInquiryDecision.php
@@ -2,12 +2,20 @@
 
 namespace Olcs\Service\Data;
 
+use Dvsa\Olcs\Transfer\Query\Decision\DecisionList as DecisionListDto;
+use Common\Service\Data\ListDataInterface;
+use Zend\ServiceManager\FactoryInterface;
+
 /**
  * Class PublicInquiryDecision
  * @package Olcs\Service\Data
  */
-class PublicInquiryDecision extends AbstractPublicInquiryData
+class PublicInquiryDecision extends AbstractPublicInquiryData implements ListDataInterface, FactoryInterface
 {
+    protected $listDto = DecisionListDto::class;
+    protected $sort = 'sectionCode';
+    protected $order = 'ASC';
+
     /**
      * @var string
      */
