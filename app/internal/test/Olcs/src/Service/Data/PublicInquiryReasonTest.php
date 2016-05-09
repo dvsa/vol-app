377d27d30aa0983b1ea88b275fdaf067d41b0b84
Shaun Lizzio <shaun@lizzio.co.uk>
2016-05-09 14:55:06 +0100

    UT Abstract method

--- app/internal/test/Olcs/src/Service/Data/PublicInquiryReasonTest.php
+++ app/internal/test/Olcs/src/Service/Data/PublicInquiryReasonTest.php
@@ -4,6 +4,7 @@ namespace OlcsTest\Service\Data;
 
 use Olcs\Service\Data\PublicInquiryReason;
 use CommonTest\Service\Data\AbstractDataServiceTestCase;
+use Dvsa\Olcs\Transfer\Query\Reason\ReasonList as Qry;
 use Mockery as m;
 
 /**
@@ -64,5 +65,110 @@ class PublicInquiryReasonTest extends AbstractDataServiceTestCase
         $this->mockHandleQuery($sut, $mockTransferAnnotationBuilder, $mockResponse, $results);
 
         $this->assertEmpty($sut->fetchListData([]));
+    }
+
+    /**
+     * @dataProvider provideFetchListOptions
+     *
+     * @param $niFlag
+     * @param $goodsOrPsv
+     * @param $expectedList
+     */
+    public function testFetchListOptions($niFlag, $goodsOrPsv, $expectedList)
+    {
+        $mockLicenceService = m::mock('\Common\Service\Data\Licence');
+        $mockLicenceService
+            ->shouldReceive('getId')
+            ->once()
+            ->andReturn(7)
+            ->shouldReceive('fetchLicenceData')
+            ->andReturn(
+                [
+                    'id' => 7,
+                    'niFlag'=> $niFlag,
+                    'goodsOrPsv' => ['id'=> $goodsOrPsv],
+                    'trafficArea' => ['id'=> 'B']
+                ]
+            )
+            ->once()
+            ->getMock();
+
+        $results = ['results' => $expectedList];
+        $params = [
+            'sort' => 'sectionCode',
+            'order' => 'ASC',
+            'niFlag' => $niFlag,
+            'goodsOrPsv' => $goodsOrPsv,
+        ];
+
+        $mockTransferAnnotationBuilder = m::mock()
+            ->shouldReceive('createQuery')->once()->andReturnUsing(
+                function ($dto) use ($params) {
+                    $this->assertEquals($params['sort'], $dto->getSort());
+                    $this->assertEquals($params['order'], $dto->getOrder());
+                    return 'query';
+                }
+            )
+            ->once()
+            ->getMock();
+
+        $mockResponse = m::mock()
+            ->shouldReceive('isOk')
+            ->andReturn(true)
+            ->once()
+            ->shouldReceive('getResult')
+            ->andReturn($results)
+            ->once()
+            ->getMock();
+
+        $sut = new PublicInquiryReason();
+        $sut->setLicenceService($mockLicenceService);
+
+        $this->mockHandleQuery($sut, $mockTransferAnnotationBuilder, $mockResponse, $results);
+
+        $this->assertEquals($this->getSingleExpected(), $sut->fetchListOptions($params));
+    }
+
+    /**
+     * Data provider for testFetchListOptions
+     *
+     * @return array
+     */
+    public function provideFetchListOptions()
+    {
+        return [
+            ['Y', 'lcat_psv', $this->getSingleSource()],
+            ['N', 'lcat_psv', $this->getSingleSource()],
+            ['Y', 'lcat_gv', $this->getSingleSource()],
+            ['N', 'lcat_gv', $this->getSingleSource()]
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    protected function getSingleExpected()
+    {
+        $expected = [
+            'val-1' => 'Value 1',
+            'val-2' => 'Value 2',
+            'val-3' => 'Value 3',
+        ];
+
+        return $expected;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getSingleSource()
+    {
+        $source = [
+            0 => ['id' => 'val-1', 'description' => 'Value 1'],
+            1 => ['id' => 'val-2', 'description' => 'Value 2'],
+            2 => ['id' => 'val-3', 'description' => 'Value 3']
+        ];
+
+        return $source;
     }
 }
