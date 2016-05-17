5fb8b1bae2470e8988704d7ce57a7aef7c22f3c7
Piotr Filak <piotr.filak@valtech.co.uk>
2016-05-17 14:02:06 +0100

    OLCS-12795 - Add an SI

---
+++ app/internal/module/Olcs/src/Form/Model/Fieldset/SiFields.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Olcs\Form\Model\Fieldset;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * @codeCoverageIgnore Auto-generated file with no methods
+ * @Form\Name("fields")
+ */
+class SiFields extends CaseBase
+{
+    /**
+     * @Form\Attributes({"id":"notificationNumber"})
+     * @Form\Options({"label": "Notification Number"})
+     * @Form\Required(false)
+     * @Form\Type("Text")
+     * @Form\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"max":36}})
+     */
+    public $notificationNumber = null;
+
+    /**
+     * @Form\Attributes({"id":"siCategoryType","class":"chosen-select-medium"})
+     * @Form\Options({
+     *     "label": "Type",
+     *     "empty_option": "",
+     *     "disable_inarray_validator": false,
+     *     "service_name": "Common\Service\Data\SiCategoryType"
+     * })
+     * @Form\Required(true)
+     * @Form\Type("DynamicSelect")
+     */
+    public $siCategoryType;
+
+    /**
+     * @Form\Attributes({"id":"infringementDate"})
+     * @Form\Options({
+     *     "label": "Date of infringement"
+     * })
+     * @Form\Required(true)
+     * @Form\Type("DateSelect")
+     * @Form\Filter({"name": "DateSelectNullifier"})
+     * @Form\Validator({"name": "\Common\Validator\Date"})
+     * @Form\Validator({"name":"Date","options":{"format":"Y-m-d"}})
+     * @Form\Validator({"name":"Common\Form\Elements\Validators\DateNotInFuture"})
+     */
+    public $infringementDate;
+
+    /**
+     * @Form\Attributes({"id":"checkDate"})
+     * @Form\Options({
+     *     "label": "Date of check"
+     * })
+     * @Form\Required(true)
+     * @Form\Type("DateSelect")
+     * @Form\Filter({"name": "DateSelectNullifier"})
+     * @Form\Validator({"name": "\Common\Validator\Date"})
+     * @Form\Validator({"name":"Date","options":{"format":"Y-m-d"}})
+     * @Form\Validator({"name":"Common\Form\Elements\Validators\DateNotInFuture"})
+     */
+    public $checkDate;
+
+    /**
+     * @Form\Attributes({"id":"memberStateCode","class":"chosen-select-medium"})
+     * @Form\Options({
+     *     "label": "Member state",
+     *     "empty_option": "",
+     *     "disable_inarray_validator": false,
+     *     "service_name": "Common\Service\Data\Country",
+     *     "category": "isMemberState",
+     *     "use_groups": false
+     * })
+     * @Form\Required(true)
+     * @Form\Type("DynamicSelect")
+     */
+    public $memberStateCode;
+
+    /**
+     * @Form\Attributes({"id":"reason","class":"extra-long"})
+     * @Form\Options({"label":"Reason"})
+     * @Form\Required(false)
+     * @Form\Type("TextArea")
+     * @Form\Filter({"name":"Zend\Filter\StringTrim"})
+     * @Form\Validator({"name":"Zend\Validator\StringLength","options":{"max":5000}})
+     */
+    public $reason = null;
+}
