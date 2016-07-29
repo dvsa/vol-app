3e8f695187555dd70af97098171096f19e1cc009
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2016-07-29 11:29:41 +0100

    OLCS-13217: set timeout for cqrs client to 300 in local; fix annotations;

--- app/internal/config/autoload/global.php
+++ app/internal/config/autoload/global.php
@@ -4,7 +4,6 @@ return array(
     'application-name' => 'internal',
     'cqrs_client' => [
         'adapter' => \Zend\Http\Client\Adapter\Curl::class,
-        'timeout' => 60,
     ],
     'form_row' => [
         'render_date_hint' => false
