e6b4e64fc9c516acaa8931820b29a522a4cd6d9d
Piotr Filak <piotr.filak@valtech.co.uk>
2016-09-09 15:10:11 +0100

    OLCS-12415 - Delete CLI module from Internal

--- app/internal/config/application.cli.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$applicationConfig = require __DIR__ .'/application.config.php';
-
-// push Cli module onto the stack
-$applicationConfig['modules'][] = 'Cli';
-
-return $applicationConfig;
