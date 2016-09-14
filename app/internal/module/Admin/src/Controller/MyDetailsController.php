6b6ae40c1c52cc405621e7593cc0316b7a8e8a6f
Piotr Filak <piotr.filak@valtech.co.uk>
2016-09-14 14:42:55 +0100

    OLCS-13785 - coding standards

--- app/internal/module/Admin/src/Controller/MyDetailsController.php
+++ app/internal/module/Admin/src/Controller/MyDetailsController.php
@@ -1,16 +1,13 @@
 <?php
 
-/**
- * My Details Controller
- */
 namespace Admin\Controller;
 
+use Admin\Form\Model\Form\MyDetails as Form;
 use Dvsa\Olcs\Transfer\Command\MyAccount\UpdateMyAccount as UpdateDto;
 use Dvsa\Olcs\Transfer\Query\MyAccount\MyAccount as ItemDto;
 use Olcs\Controller\AbstractInternalController;
 use Olcs\Controller\Interfaces\LeftViewProvider;
 use Olcs\Data\Mapper\MyDetails as Mapper;
-use Admin\Form\Model\Form\MyDetails as Form;
 use Zend\View\Model\ViewModel;
 
 /**
@@ -24,19 +21,6 @@ class MyDetailsController extends AbstractInternalController implements LeftView
      * represented by a single navigation id.
      */
     protected $navigationId = 'admin-dashboard/admin-your-account';
-
-    public function getLeftView()
-    {
-        $view = new ViewModel(
-            [
-                'navigationId' => 'admin-dashboard/admin-your-account',
-                'navigationTitle' => 'Your account'
-            ]
-        );
-        $view->setTemplate('admin/sections/admin/partials/generic-left');
-
-        return $view;
-    }
 
     /**
      * Variables for controlling details view rendering
@@ -56,11 +40,39 @@ class MyDetailsController extends AbstractInternalController implements LeftView
 
     protected $editContentTitle = 'Your account';
 
+    /**
+     * Get left view
+     *
+     * @return ViewModel
+     */
+    public function getLeftView()
+    {
+        $view = new ViewModel(
+            [
+                'navigationId' => 'admin-dashboard/admin-your-account',
+                'navigationTitle' => 'Your account'
+            ]
+        );
+        $view->setTemplate('admin/sections/admin/partials/generic-left');
+
+        return $view;
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Zend\Http\Response
+     */
     public function indexAction()
     {
         return $this->redirectToIndex();
     }
 
+    /**
+     * Edit action
+     *
+     * @return \Zend\View\Model\ViewModel|\Zend\Http\Response
+     */
     public function editAction()
     {
         $this->placeholder()->setPlaceholder('pageTitle', 'Your account');
@@ -68,7 +80,12 @@ class MyDetailsController extends AbstractInternalController implements LeftView
         return parent::editAction();
     }
 
-    public function redirectToIndex()
+    /**
+     * Redirect to index
+     *
+     * @return \Zend\Http\Response
+     */
+    private function redirectToIndex()
     {
         return $this->redirect()->toRouteAjax(
             'admin-dashboard/admin-your-account/details',
