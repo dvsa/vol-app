13970e5af206a7fe7c8dca1002cfbc5a6f11878b
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2016-10-19 14:29:55 +0100

    OLCS-14102: remove unused config entry

--- app/internal/config/autoload/global.php
+++ app/internal/config/autoload/global.php
@@ -6,9 +6,6 @@ return array(
         'adapter' => \Zend\Http\Client\Adapter\Curl::class,
         'timeout' => 60,
     ],
-    'form_row' => [
-        'render_date_hint' => false
-    ],
     'soflomo_purifier' => [
         'config' => [
             'Cache.SerializerPath' => sys_get_temp_dir(),
