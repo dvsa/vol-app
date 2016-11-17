6991bedf4478f1633aeac882780da34ea8485198
Alex P <alex.peshkov@valtech.co.uk>
2016-11-17 10:06:17 +0000

    OLCS-14141 legacy controllers deleted

--- app/internal/module/Olcs/config/routes.config.php
+++ app/internal/module/Olcs/config/routes.config.php
@@ -2918,16 +2918,6 @@ $routes['lva-application']['child_routes'] = array_merge(
                 )
             )
         ),
-        'undertakings' => array(
-            'type' => 'segment',
-            'options' => array(
-                'route' => 'undertakings[/]',
-                'defaults' => array(
-                    'controller' => 'LvaApplication/Undertakings',
-                    'action' => 'index'
-                )
-            )
-        ),
         'publish' => array(
             'type' => 'segment',
             'options' => array(
