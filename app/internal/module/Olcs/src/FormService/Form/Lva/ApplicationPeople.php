fc866ae0a320125b65ad6f34a245fec2f6a14107
Mat Evans <mat.evans@valtech.co.uk>
2016-12-07 09:40:42 +0000

    Revert "Fix unit test failures"
    This reverts commit 6a410575ee06395dd511cc076fea47ecca06d7df.

--- app/internal/module/Olcs/src/FormService/Form/Lva/ApplicationPeople.php
+++ app/internal/module/Olcs/src/FormService/Form/Lva/ApplicationPeople.php
@@ -12,4 +12,17 @@ use Common\Form\Form;
  */
 class ApplicationPeople extends CommonApplicationPeople
 {
+    /**
+     * Alter form
+     *
+     * @param Form  $form   form
+     * @param array $params params
+     *
+     * @return void
+     */
+    public function alterForm($form, $params = [])
+    {
+        parent::alterForm($form, $params);
+        $this->removeFormAction($form, 'save');
+    }
 }
