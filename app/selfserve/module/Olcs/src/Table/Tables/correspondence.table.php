66785a390f4bade3fc64c4875157740af6b39c5d
Alex P <alex.peshkov@valtech.co.uk>
2017-01-24 14:43:46 +0000

    OLCS-14643 Add date to 'Document' list on Self service

--- app/selfserve/module/Olcs/src/Table/Tables/correspondence.table.php
+++ app/selfserve/module/Olcs/src/Table/Tables/correspondence.table.php
@@ -21,14 +21,26 @@ return array(
     'attributes' => [],
     'columns' => array(
         array(
+            'title' => 'dashboard-correspondence.table.column.issuedDate',
+            'formatter' => function ($row) {
+                if (isset($row['correspondence']['document']['issuedDate'])) {
+                    return date(\DATE_FORMAT, strtotime($row['correspondence']['document']['issuedDate']));
+                }
+                return '';
+            },
+            'sort' => 'd.issuedDate'
+        ),
+        array(
             'title' => 'dashboard-correspondence.table.column.title',
             'name' => 'correspondence',
             'formatter' => 'AccessedCorrespondence',
+            'sort' => 'd.description'
         ),
         array(
             'title' => 'dashboard-correspondence.table.column.reference',
             'name' => 'licence',
             'formatter' => 'LicenceNumberLink',
+            'sort' => 'l.licNo'
         ),
     )
 );
