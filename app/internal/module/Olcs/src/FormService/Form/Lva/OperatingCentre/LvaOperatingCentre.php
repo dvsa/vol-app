5351463b752578343c524c192332c835cee27444
Mat Evans <mat.evans@valtech.co.uk>
2017-01-25 11:24:28 +0000

    OLCS-15024 Remove upload file validator for Internal

--- app/internal/module/Olcs/src/FormService/Form/Lva/OperatingCentre/LvaOperatingCentre.php
+++ app/internal/module/Olcs/src/FormService/Form/Lva/OperatingCentre/LvaOperatingCentre.php
@@ -10,6 +10,7 @@ namespace Olcs\FormService\Form\Lva\OperatingCentre;
 use Common\FormService\Form\Lva\OperatingCentre\CommonOperatingCentre;
 use Zend\Form\Form;
 use Zend\Validator\Identical as ValidatorIdentical;
+use Common\Validator\ValidateIf;
 
 /**
  * Lva Internal Operating Centres
@@ -18,10 +19,20 @@ use Zend\Validator\Identical as ValidatorIdentical;
  */
 class LvaOperatingCentre extends CommonOperatingCentre
 {
+    /**
+     * Alter the Form
+     *
+     * @param Form  $form   Form to alter
+     * @param array $params Data
+     *
+     * @return void
+     */
     public function alterForm(Form $form, array $params)
     {
         $this->getFormHelper()->removeValidator($form, 'data->permission', ValidatorIdentical::class);
         $this->getFormHelper()->removeValidator($form, 'data->sufficientParking', ValidatorIdentical::class);
+        // On Internal uploading the advert isn't mandatory
+        $this->getFormHelper()->removeValidator($form, 'advertisements->uploadedFileCount', ValidateIf::class);
         parent::alterForm($form, $params);
     }
 }
