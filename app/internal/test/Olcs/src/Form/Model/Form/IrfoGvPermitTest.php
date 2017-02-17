8b28cb86b36d645435854c74a4f428099241e74f
Piotr Filak <piotr.filak@valtech.co.uk>
2017-02-17 09:51:10 +0000

    OLCS-15318 - Implement UT form validation for SS

--- app/internal/test/Olcs/src/Form/Model/Form/IrfoGvPermitTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/IrfoGvPermitTest.php
@@ -3,6 +3,7 @@
 namespace OlcsTest\Form\Model\Form;
 
 use Olcs\TestHelpers\FormTester\AbstractFormValidationTestCase;
+use Zend\Validator;
 
 /**
  * Class IrfoGvPermitTest
@@ -15,4 +16,104 @@ class IrfoGvPermitTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Olcs\Form\Model\Form\IrfoGvPermit::class;
+
+    public function testIrfoGvPermitType()
+    {
+        $element = ['fields', 'irfoGvPermitType'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementAllowEmpty($element, false);
+        $this->assertFormElementDynamicSelect($element);
+    }
+
+    public function testYearRequired()
+    {
+        $element = ['fields', 'yearRequired'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementAllowEmpty($element, false);
+        $this->assertFormElementNumber($element);
+    }
+
+    public function testInForceDate()
+    {
+        $element = ['fields', 'inForceDate'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementAllowEmpty($element, false);
+        $this->assertFormElementDate($element);
+    }
+
+    public function testExpiryDate()
+    {
+        $element = ['fields', 'expiryDate'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementAllowEmpty($element, false);
+
+        $this->assertFormElementValid(
+            $element,
+            ['day' => 2, 'month' => '2', 'year' => 2010],
+            ['fields' => ['inForceDate' => ['day' => 1, 'month' => '2', 'year' => 2010]]]
+        );
+        $this->assertFormElementNotValid(
+            $element,
+            ['day' => 1, 'month' => '2', 'year' => 2010],
+            \Common\Validator\AbstractCompare::NOT_GTE,
+            ['fields' => ['inForceDate' => ['day' => 2, 'month' => '2', 'year' => 2010]]]
+        );
+    }
+
+    public function testIsFeeExempt()
+    {
+        $element = ['fields', 'isFeeExempt'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementAllowEmpty($element, false);
+        $this->assertFormElementCheckbox($element);
+    }
+
+    public function testExemptionDetails()
+    {
+        $element = ['fields', 'exemptionDetails'];
+        $this->assertFormElementRequired($element, false);
+        $this->assertFormElementAllowEmpty($element, true);
+        $this->assertFormElementText($element);
+
+        $this->assertFormElementAllowEmpty($element, false, ['fields' => ['isFeeExempt' => 'Y']]);
+        $this->assertFormElementText($element, 0, 255, ['fields' => ['isFeeExempt' => 'Y']]);
+    }
+
+    public function testNoOfCopies()
+    {
+        $element = ['fields', 'noOfCopies'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementAllowEmpty($element, false);
+        $this->assertFormElementNumber($element);
+    }
+
+    public function testOrganisation()
+    {
+        $element = ['fields', 'organisation'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testId()
+    {
+        $element = ['fields', 'id'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testVersion()
+    {
+        $element = ['fields', 'version'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testSubmit()
+    {
+        $element = ['form-actions', 'submit'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testCancel()
+    {
+        $element = ['form-actions', 'cancel'];
+        $this->assertFormElementActionButton($element);
+    }
 }
