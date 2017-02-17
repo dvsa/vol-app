9a3dfb610c423dcbf022de6d9d8ab2571da3ee22
Mat Evans <mat.evans@valtech.co.uk>
2017-02-17 11:20:45 +0000

    OLCS-15195 Add test for form

--- app/internal/test/Olcs/src/Form/Model/Form/AddUnlicensedGoodsVehicleTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/AddUnlicensedGoodsVehicleTest.php
@@ -15,4 +15,32 @@ class AddUnlicensedGoodsVehicleTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Olcs\Form\Model\Form\AddUnlicensedGoodsVehicle::class;
+
+    public function testDataId()
+    {
+        $element = ['data', 'id'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testDataVersion()
+    {
+        $element = ['data', 'version'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testDataVrm()
+    {
+        $element = ['data', 'vrm'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementAllowEmpty($element, false);
+        $this->assertFormElementText($element, 1, 20);
+    }
+
+    public function testDataMakeModel()
+    {
+        $element = ['data', 'platedWeight'];
+        $this->assertFormElementRequired($element, false);
+        $this->assertFormElementAllowEmpty($element, true);
+        $this->assertFormElementNumber($element, 0, 999999);
+    }
 }
