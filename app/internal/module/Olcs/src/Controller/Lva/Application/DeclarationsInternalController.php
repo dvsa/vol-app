b41af357528de4cb790d04aa1bd78b58cb548449
Mat Evans <mat.evans@valtech.co.uk>
2017-03-01 12:04:06 +0000

    OLCS-10380 Add Verify signature details to application declaration page

--- app/internal/module/Olcs/src/Controller/Lva/Application/DeclarationsInternalController.php
+++ app/internal/module/Olcs/src/Controller/Lva/Application/DeclarationsInternalController.php
@@ -15,4 +15,38 @@ class DeclarationsInternalController extends \Olcs\Controller\Lva\AbstractDeclar
 
     protected $lva = 'application';
     protected $location = 'internal';
+
+    /**
+     * Alter the form
+     *
+     * @param \Common\Form\Form $form The form
+     * @param null              $data Optional array of data
+     *
+     * @return null
+     */
+    protected function alterFormForLva(\Common\Form\Form $form, $data = null)
+    {
+        // Get signature data from application declaration DTO
+        $response = $this->handleQuery(
+            \Dvsa\Olcs\Transfer\Query\Application\Declaration::create(['id' => $this->getIdentifier()])
+        );
+        // If all ok and verify signature exists on the application
+        if ($response->isOk() && $response->getResult()['signature']) {
+            // Add signature details into form
+            $signatureDetails = $response->getResult()['signature'];
+            $form->get('declarations')->get('verifySignatureText')->setValue(
+                sprintf(
+                    'This application has been digitally signed on %s by %s with date of birth %s',
+                    (new \DateTime($signatureDetails['date']))->format(\DATE_FORMAT),
+                    $signatureDetails['name'],
+                    (new \DateTime($signatureDetails['dob']))->format(\DATE_FORMAT)
+                )
+            );
+
+            // Change checkbox label
+            $form->get('declarations')->get('declarationConfirmation')->setLabel('Signature accepted');
+        }
+
+        parent::alterFormForLva($form, $data);
+    }
 }
