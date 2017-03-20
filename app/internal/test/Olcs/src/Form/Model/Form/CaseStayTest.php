cdc633819a1ce640a9514055ef5899779b78d499
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-20 14:10:00 +0000

    OLCS-16103 case stay form test

--- app/internal/test/Olcs/src/Form/Model/Form/CaseStayTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/CaseStayTest.php
@@ -15,4 +15,34 @@ class CaseStayTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Olcs\Form\Model\Form\CaseStay::class;
+
+    public function testId()
+    {
+        $element = ['fields', 'id'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testVersion()
+    {
+        $element = ['fields', 'version'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testCase()
+    {
+        $element = ['fields', 'case'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testSubmit()
+    {
+        $element = ['form-actions', 'submit'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testCancel()
+    {
+        $element = ['form-actions', 'cancel'];
+        $this->assertFormElementActionButton($element);
+    }
 }
