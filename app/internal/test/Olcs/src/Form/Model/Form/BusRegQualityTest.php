59feb80391b5b52932810e60bd4aea2b599643c2
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-20 13:57:43 +0000

    OLCS-16102 form tests

--- app/internal/test/Olcs/src/Form/Model/Form/BusRegQualityTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/BusRegQualityTest.php
@@ -3,6 +3,7 @@
 namespace OlcsTest\Form\Model\Form;
 
 use Olcs\TestHelpers\FormTester\AbstractFormValidationTestCase;
+use Zend\Form\Element\Radio;
 
 /**
  * Class BusRegQualityTest
@@ -15,4 +16,82 @@ class BusRegQualityTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Olcs\Form\Model\Form\BusRegQuality::class;
+
+
+    public function testId()
+    {
+        $element = ['fields', 'id'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testVersion()
+    {
+        $element = ['fields', 'version'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testStatus()
+    {
+        $element = ['fields', 'status'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testIsLatestVariation()
+    {
+        $element = ['fields', 'isLatestVariation'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testIsTxcApp()
+    {
+        $element = ['fields', 'isTxcApp'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testIsQualityPartnership()
+    {
+        $element = ['fields', 'isQualityPartnership'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementType($element, Radio::class);
+    }
+
+    public function testQualityPartnershipDetails()
+    {
+        $element = ['fields', 'qualityPartnershipDetails'];
+        $this->assertFormElementRequired($element, false);
+        $this->assertFormElementText($element, 0, 4000);
+    }
+
+    public function testIsQualityContract()
+    {
+        $element = ['fields', 'isQualityContract'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementType($element, Radio::class);
+    }
+
+    public function testQualityPartnershipFacilitiesUsed()
+    {
+        $element = ['fields', 'qualityPartnershipFacilitiesUsed'];
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementType($element, Radio::class);
+    }
+
+    public function testQualityContractDetails()
+    {
+        $element = ['fields', 'qualityContractDetails'];
+        $this->assertFormElementRequired($element, false);
+        $this->assertFormElementText($element, 0, 4000);
+    }
+
+    public function testSubmit()
+    {
+        $element = ['form-actions', 'submit'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testCancel()
+    {
+        $element = ['form-actions', 'cancel'];
+        $this->assertFormElementActionButton($element);
+    }
 }
--- app/internal/test/Olcs/src/Form/Model/Form/BusRegVariationReasonTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/BusRegVariationReasonTest.php
@@ -15,4 +15,22 @@ class BusRegVariationReasonTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Olcs\Form\Model\Form\BusRegVariationReason::class;
+
+    public function testReason()
+    {
+        $element = ['fields', 'variationReasons'];
+        $this->assertFormElementDynamicSelect($element, false);
+    }
+
+    public function testSubmit()
+    {
+        $element = ['form-actions', 'submit'];
+        $this->assertFormElementActionButton($element);
+    }
+
+    public function testCancel()
+    {
+        $element = ['form-actions', 'cancel'];
+        $this->assertFormElementActionButton($element);
+    }
 }
