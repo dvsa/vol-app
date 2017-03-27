23563ccab48a0edda7809eabefa0608874631346
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-27 13:57:01 +0100

    OLCS-16108 form tests

--- app/internal/test/Olcs/src/Form/Model/Form/Lva/AddGoodsVehicleTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/Lva/AddGoodsVehicleTest.php
@@ -15,4 +15,83 @@ class AddGoodsVehicleTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Olcs\Form\Model\Form\Lva\AddGoodsVehicle::class;
+
+    public function testId()
+    {
+        $this->assertFormElementHidden(['data', 'id']);
+    }
+
+    public function testVersion()
+    {
+        $this->assertFormElementHidden(['data', 'version']);
+    }
+
+    public function testVrm()
+    {
+        $this->assertFormElementVrm(['data', 'vrm']);
+    }
+
+    public function testPlatedWeight()
+    {
+        $this->assertFormElementVehiclePlatedWeight(['data', 'platedWeight']);
+    }
+
+    public function testLicenceVehicleId()
+    {
+        $this->assertFormElementHidden(['licence-vehicle', 'id']);
+    }
+
+    public function testLicenceVehicleVersion()
+    {
+        $this->assertFormElementHidden(['licence-vehicle', 'version']);
+    }
+
+    public function testLicenceVehicleReceivedDate()
+    {
+        $element = ['licence-vehicle', 'receivedDate'];
+        $this->assertFormElementDate($element);
+        $this->assertFormElementRequired($element, false);
+    }
+
+    public function testLicenceVehicleSpecifiedDate()
+    {
+        $element = ['licence-vehicle', 'specifiedDate'];
+        $this->assertFormElementDate($element);
+        $this->assertFormElementRequired($element, false);
+    }
+
+    public function testLicenceVehicleRemovalDate()
+    {
+        $element = ['licence-vehicle', 'removalDate'];
+        $this->assertFormElementDate($element);
+        $this->assertFormElementRequired($element, false);
+    }
+
+    public function testDiscNumber()
+    {
+        $element = ['licence-vehicle', 'discNo'];
+        $this->assertFormElementText($element);
+        $this->assertFormElementRequired($element, false);
+    }
+
+    public function testAddAnother()
+    {
+        $this->assertFormElementActionButton(
+            ['form-actions', 'addAnother']
+        );
+    }
+
+    public function testSubmit()
+    {
+        $this->assertFormElementActionButton(
+            ['form-actions', 'submit']
+        );
+    }
+
+    public function testCancel()
+    {
+        $this->assertFormElementActionButton(
+            ['form-actions', 'cancel']
+        );
+    }
 }
--- app/internal/test/Olcs/src/Form/Model/Form/Lva/EditGoodsVehicleLicenceTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/Lva/EditGoodsVehicleLicenceTest.php
@@ -15,4 +15,112 @@ class EditGoodsVehicleLicenceTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Olcs\Form\Model\Form\Lva\EditGoodsVehicleLicence::class;
+
+    public function testId()
+    {
+        $this->assertFormElementHidden(['data', 'id']);
+    }
+
+    public function testVersion()
+    {
+        $this->assertFormElementHidden(['data', 'version']);
+    }
+
+    public function testVrm()
+    {
+        // Disabled field on form.  No validation required.
+        $this->assertFormElementRequired(['data', 'vrm'], false);
+    }
+
+    public function testPlatedWeight()
+    {
+        $this->assertFormElementVehiclePlatedWeight(['data', 'platedWeight']);
+    }
+
+    public function testLicenceVehicleId()
+    {
+        $element = ['licence-vehicle', 'id'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testLicenceVehicleVersion()
+    {
+        $element = ['licence-vehicle', 'version'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testLicenceVehicleReceivedDate()
+    {
+        $element = ['licence-vehicle', 'receivedDate'];
+        $this->assertFormElementDate($element);
+        $this->assertFormElementRequired($element, false);
+    }
+
+    public function testLicenceVehicleSpecifiedDate()
+    {
+        $element = ['licence-vehicle', 'specifiedDate'];
+        $this->assertFormElementDate($element);
+        $this->assertFormElementRequired($element, false);
+    }
+
+    public function testLicenceVehicleRemovalDate()
+    {
+        $element = ['licence-vehicle', 'removalDate'];
+        $this->assertFormElementDate($element);
+        $this->assertFormElementRequired($element, false);
+    }
+
+    public function testWarningLetterSeedDate()
+    {
+        $this->assertFormElementDate([
+            'licence-vehicle',
+            'warningLetterSeedDate',
+        ]);
+    }
+
+    public function testWarningLetterSentDate()
+    {
+        $this->assertFormElementDate([
+            'licence-vehicle',
+            'warningLetterSentDate',
+        ]);
+    }
+
+    public function testDiscNumber()
+    {
+        $element = ['licence-vehicle', 'discNo'];
+        $this->assertFormElementText($element);
+        $this->assertFormElementRequired($element, false);
+    }
+
+    public function testVehicleHistory()
+    {
+        $element = ['vehicle-history-table', 'table'];
+        $this->assertFormElementTable($element);
+        $this->assertFormElementRequired($element, false);
+        $this->assertFormElementAllowEmpty($element, true);
+
+        $element = ['vehicle-history-table', 'action'];
+        $this->assertFormElementHidden($element);
+
+        $element = ['vehicle-history-table', 'rows'];
+        $this->assertFormElementHidden($element);
+
+        $element = ['vehicle-history-table', 'id'];
+        $this->assertFormElementHidden($element);
+    }
+
+    public function testSubmit()
+    {
+        $this->assertFormElementActionButton(
+            ['form-actions', 'submit']
+        );
+    }
+
+    public function testCancel()
+    {
+        $this->assertFormElementActionButton(
+            ['form-actions', 'cancel']
+        );
+    }
 }
