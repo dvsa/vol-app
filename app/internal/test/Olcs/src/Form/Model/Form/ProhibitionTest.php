b4f069e08450ff120469c882f13ff2c6b2f7504c
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-28 09:53:42 +0100

    OLCS-16111 form tests

--- app/internal/test/Olcs/src/Form/Model/Form/ProhibitionTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/ProhibitionTest.php
@@ -3,6 +3,8 @@
 namespace OlcsTest\Form\Model\Form;
 
 use Olcs\TestHelpers\FormTester\AbstractFormValidationTestCase;
+use Common\Form\Elements\Custom\OlcsCheckbox;
+use Common\Validator\DateCompare;
 
 /**
  * Class ProhibitionTest
@@ -15,4 +17,93 @@ class ProhibitionTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Olcs\Form\Model\Form\Prohibition::class;
+
+    public function testProhibitionDate()
+    {
+        $element = ['fields', 'prohibitionDate'];
+        $this->assertFormElementDate($element);
+    }
+
+    public function testVrm()
+    {
+        $element = ['fields', 'vrm'];
+        $this->assertFormElementRequired($element, false);
+    }
+
+    public function testIsTrailer()
+    {
+        $element = ['fields', 'isTrailer'];
+        $this->assertFormElementType($element, OlcsCheckbox::class);
+        $this->assertFormElementRequired($element, true);
+    }
+
+    public function testProhibitionType()
+    {
+        $this->assertFormElementDynamicSelect(
+            ['fields', 'prohibitionType'],
+            true
+        );
+    }
+
+    public function testClearedDate()
+    {
+        $element = ['fields', 'clearedDate'];
+        $this->assertFormElementNotValid(
+            $element,
+            [
+                'year'  => date('Y'),
+                'month' => date('m'),
+                'day'   => date('j'),
+            ],
+            ['invalidField']
+        );
+
+        // This date cannot be
+        $this->assertFormElementNotValid(
+            $element,
+            [
+                'year'  => date('Y'),
+                'month' => date('m'),
+                'day'   => date('j'),
+            ],
+            [
+                'invalidField',
+                DateCompare::NOT_GTE,
+            ],
+            [
+                'fields' => [
+                    'prohibitionDate' => [
+                        'year'  => date('Y') + 1,
+                        'month' => date('m'),
+                        'day'   => date('j'),
+                    ],
+                ],
+            ]
+        );
+    }
+
+    public function testImposedAt()
+    {
+        $this->assertFormElementRequired(['fields', 'imposedAt'], false);
+    }
+
+    public function testId()
+    {
+        $this->assertFormElementHidden(['fields', 'id']);
+    }
+
+    public function testVersion()
+    {
+        $this->assertFormElementHidden(['fields', 'version']);
+    }
+
+    public function testSubmit()
+    {
+        $this->assertFormElementActionButton(['form-actions', 'submit']);
+    }
+
+    public function testCancel()
+    {
+        $this->assertFormElementActionButton(['form-actions', 'cancel']);
+    }
 }
