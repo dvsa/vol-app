61ff46f660b3c23470215f4b45f174861e0755bf
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-28 14:43:06 +0100

    OLCS-16114 form tests apart from submission

--- app/internal/test/Olcs/src/Form/Model/Form/SubmissionSectionEditCommentTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/SubmissionSectionEditCommentTest.php
@@ -15,4 +15,29 @@ class SubmissionSectionEditCommentTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Olcs\Form\Model\Form\SubmissionSectionEditComment::class;
+
+    public function testComment()
+    {
+        $this->assertFormElementRequired(['fields', 'comment'], false);
+    }
+
+    public function testId()
+    {
+        $this->assertFormElementHidden(['fields', 'id']);
+    }
+
+    public function testVersion()
+    {
+        $this->assertFormElementHidden(['fields', 'version']);
+    }
+
+    public function testSubmit()
+    {
+        $this->assertFormElementActionButton(['form-actions', 'submit']);
+    }
+
+    public function testCancel()
+    {
+        $this->assertFormElementActionButton(['form-actions', 'cancel']);
+    }
 }
