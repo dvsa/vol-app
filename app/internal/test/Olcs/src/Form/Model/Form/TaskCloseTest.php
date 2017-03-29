4936964c0dc401c4e3af496e4be47dc941be588d
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-03-29 09:34:03 +0100

    OLCS-16115 form tests

--- app/internal/test/Olcs/src/Form/Model/Form/TaskCloseTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/TaskCloseTest.php
@@ -15,4 +15,19 @@ class TaskCloseTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Olcs\Form\Model\Form\TaskClose::class;
+
+    public function testLabel()
+    {
+        $this->assertFormElementHtml(['details', 'label']);
+    }
+
+    public function testClose()
+    {
+        $this->assertFormElementActionButton(['form-actions', 'close']);
+    }
+
+    public function testCancel()
+    {
+        $this->assertFormElementActionButton(['form-actions', 'cancel']);
+    }
 }
--- app/internal/test/Olcs/src/Form/Model/Form/TasksHomeTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/TasksHomeTest.php
@@ -3,6 +3,7 @@
 namespace OlcsTest\Form\Model\Form;
 
 use Olcs\TestHelpers\FormTester\AbstractFormValidationTestCase;
+use Zend\Form\Element\Select;
 
 /**
  * Class TasksHomeTest
@@ -15,4 +16,70 @@ class TasksHomeTest extends AbstractFormValidationTestCase
      * @var string The class name of the form being tested
      */
     protected $formName = \Olcs\Form\Model\Form\TasksHome::class;
+
+    public function testAssignedToTeam()
+    {
+        $element = ['assignedToTeam'];
+        $this->assertFormElementType($element, Select::class);
+        $this->assertFormElementRequired($element, true);
+    }
+
+    public function testAssignedToUser()
+    {
+        $element = ['assignedToUser'];
+        $this->assertFormElementType($element, Select::class);
+        $this->assertFormElementRequired($element, true);
+    }
+
+    public function testCategory()
+    {
+        $this->assertFormElementDynamicSelect(['category'], true);
+    }
+
+    public function testTaskSubCategory()
+    {
+        $this->assertFormElementDynamicSelect(['taskSubCategory'], true);
+    }
+
+    public function testDate()
+    {
+        $this->assertFormElementDynamicSelect(['date']);
+    }
+
+    public function testStatus()
+    {
+        $this->assertFormElementDynamicSelect(['status']);
+    }
+
+    public function testShowTasks()
+    {
+        $element = ['showTasks'];
+        $this->assertFormElementType($element, Select::class);
+        $this->assertFormElementRequired($element, true);
+    }
+
+    public function testUrgent()
+    {
+        $this->assertFormElementRequired(['urgent'], true);
+    }
+
+    public function testFilter()
+    {
+        $this->assertFormElementActionButton(['filter']);
+    }
+
+    public function testSort()
+    {
+        $this->assertFormElementHidden(['sort']);
+    }
+
+    public function testOrder()
+    {
+        $this->assertFormElementHidden(['order']);
+    }
+
+    public function testLimit()
+    {
+        $this->assertFormElementHidden(['limit']);
+    }
 }
