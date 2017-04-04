49130adcee31382304cda0f3e0b445a9addd50c6
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-04-04 15:53:37 +0100

    OLCS-16227 added comment

--- app/internal/test/Olcs/src/Form/Model/Form/SubmissionTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/SubmissionTest.php
@@ -16,6 +16,11 @@ class SubmissionTest extends AbstractFormValidationTestCase
      */
     protected $formName = \Olcs\Form\Model\Form\Submission::class;
 
+    /**
+     * @internal Skipping 'submissionSections' because the element is mainly
+     * powered by Javascript.  It's dynamic and also dynamically populated.
+     * See this link for example: http://olcs-internal.olcs.gov.uk/case/28/submission/add/
+     */
     public function testSubmissionSections()
     {
         $element = ['fields', 'submissionSections'];
