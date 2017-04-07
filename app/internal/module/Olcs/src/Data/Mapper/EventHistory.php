eb4975fed9d0792cefb27f775b46cc5268946712
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2017-04-07 17:18:25 +0100

    OLCS-16239: convert dates from utc to WEB Timezone;

--- app/internal/module/Olcs/src/Data/Mapper/EventHistory.php
+++ app/internal/module/Olcs/src/Data/Mapper/EventHistory.php
@@ -19,17 +19,35 @@ class EventHistory
         $result['details'] = $data['eventHistoryType']['description'];
         $result['info'] = $data['eventData'];
 
-        $dateTime = \DateTime::createFromFormat(\DateTime::ISO8601, $data['eventDatetime']);
-        $result['date'] = $dateTime->format('H:i, d/m/y');
+        $result['date'] = date(\DATETIME_FORMAT, strtotime($data['eventDatetime']));
 
         $result['by'] = isset($data['user']['contactDetails']['person']['forename']) &&
             isset($data['user']['contactDetails']['person']['familyName']) ?
             $data['user']['contactDetails']['person']['forename'] . ' ' .
             $data['user']['contactDetails']['person']['familyName'] : $data['user']['loginId'];
 
+        //  prepare details
+        $details = isset($data['eventHistoryDetails']) ? $data['eventHistoryDetails'] : [];
+
+        $dateFields = ['open_date', 'closed_date', 'deleted_date'];
+        array_walk(
+            $details,
+            function(&$item) use ($dateFields) {
+                if (!in_array($item['name'], $dateFields)) {
+                    return;
+                }
+
+                foreach (['newValue', 'oldValue'] as $key) {
+                    if ($item[$key] !== null) {
+                        $item[$key] = date(\DATETIME_FORMAT, strtotime($item['newValue'] . ' UTC'));
+                    }
+                }
+            }
+        );
+
         return [
             'readOnlyData' => $result,
-            'eventHistoryDetails' => isset($data['eventHistoryDetails']) ? $data['eventHistoryDetails'] : []
+            'eventHistoryDetails' => $details,
         ];
     }
 }
