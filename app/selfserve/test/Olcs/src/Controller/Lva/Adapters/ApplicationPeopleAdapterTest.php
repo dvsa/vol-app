e1f6bcc900d2a1fede04e818cc4aed64298fb55e
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2017-05-15 13:58:11 +0100

    OLCS-16580: canModify function fix; fix unit tests; annotations;

--- app/selfserve/test/Olcs/src/Controller/Lva/Adapters/ApplicationPeopleAdapterTest.php
+++ app/selfserve/test/Olcs/src/Controller/Lva/Adapters/ApplicationPeopleAdapterTest.php
@@ -1,128 +1,83 @@
 <?php
 
-/**
- * Internal / Common Application People Adapter Test
- *
- * @author Nick Payne <nick.payne@valtech.co.uk>
- */
 namespace OlcsTest\Controller\Lva\Adapters;
 
+use Common\Controller\Lva\AbstractController;
 use Mockery as m;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Olcs\Controller\Lva\Adapters\ApplicationPeopleAdapter;
 use Common\Service\Entity\OrganisationEntityService;
 
 /**
- * Internal / Common Application People Adapter Test
- *
- * @author Nick Payne <nick.payne@valtech.co.uk>
+ * @covers \Olcs\Controller\Lva\Adapters\ApplicationPeopleAdapter
  */
 class ApplicationPeopleAdapterTest extends MockeryTestCase
 {
-    protected $sut;
-    protected $sm;
-
-    public function setUp()
+    /**
+     * @dataProvider dpTestCanModify
+     */
+    public function testCanModify($type, $isInForce, $expect)
     {
-        $this->sm = m::mock('\Zend\ServiceManager\ServiceManager')->makePartial();
-        $this->sm->setAllowOverride(true);
-
-        $this->sut = new ApplicationPeopleAdapter();
-        $this->sut->setServiceLocator($this->sm);
-    }
-
-    public function testAlterFormForOrganisationDoesNotAlterFormWithoutInForceLicences()
-    {
-        $this->sm->setService(
-            'Entity\Organisation',
-            m::mock()
-            ->shouldReceive('hasInForceLicences')
-            ->with(123)
-            ->andReturn(false)
-            ->getMock()
-        );
-
-        $form = m::mock('Zend\Form\Form');
-        $table = m::mock();
-
-        $this->assertNull($this->sut->alterFormForOrganisation($form, $table, 123));
-    }
-
-    public function testAlterAddOrEditFormForOrganisationDoesNotAlterFormWithInForceLicences()
-    {
-        $this->sm->setService(
-            'Entity\Organisation',
-            m::mock()
-            ->shouldReceive('hasInForceLicences')
-            ->with(123)
-            ->andReturn(true)
-            ->shouldReceive('getType')
-            ->with(123)
-            ->andReturn(
-                [
+        $data = [
+            'licence' => [
+                'organisation' => [
+                    'hasInforceLicences' => $isInForce,
                     'type' => [
-                        'id' => OrganisationEntityService::ORG_TYPE_OTHER
-                    ]
-                ]
-            )
-            ->getMock()
-        );
+                        'id' => $type,
+                    ],
+                ],
+            ],
+        ];
 
-        $form = m::mock('Zend\Form\Form');
+        $mockResp = m::mock(\Zend\Http\Response::class);
+        $mockResp
+            ->shouldReceive('isOk')->andReturn(true)
+            ->shouldReceive('getResult')->andReturn($data);
 
-        $this->assertNull($this->sut->alterAddOrEditFormForOrganisation($form, 123));
+        /** @var ApplicationPeopleAdapter | m\MockInterface $sut */
+        $sut = m::mock(ApplicationPeopleAdapter::class)
+            ->makePartial()
+            ->shouldAllowMockingProtectedMethods();
+        $sut->shouldReceive('handleQuery')->andReturn($mockResp);
+
+        $sut->loadPeopleData(AbstractController::LVA_APP, 999);
+
+        static::assertEquals($expect, $sut->canModify());
     }
 
-    public function testAlterAddOrEditFormForOrganisationDoesNotAlterFormWithoutInForceLicences()
+    public function dpTestCanModify()
     {
-        $this->sm->setService(
-            'Entity\Organisation',
-            m::mock()
-            ->shouldReceive('hasInForceLicences')
-            ->with(123)
-            ->andReturn(false)
-            ->getMock()
-        );
-
-        $form = m::mock('Zend\Form\Form');
-
-        $this->assertNull($this->sut->alterAddOrEditFormForOrganisation($form, 123));
-    }
-
-    public function testCanModifyWithNoInForceLicences()
-    {
-        $this->sm->setService(
-            'Entity\Organisation',
-            m::mock()
-            ->shouldReceive('hasInForceLicences')
-            ->with(123)
-            ->andReturn(false)
-            ->getMock()
-        );
-
-        $this->assertTrue($this->sut->canModify(123));
-    }
-
-    public function testCanModifyNormalOrganisation()
-    {
-        $this->sm->setService(
-            'Entity\Organisation',
-            m::mock()
-            ->shouldReceive('hasInForceLicences')
-            ->with(123)
-            ->andReturn(true)
-            ->shouldReceive('getType')
-            ->with(123)
-            ->andReturn(
-                [
-                    'type' => [
-                        'id' => OrganisationEntityService::ORG_TYPE_OTHER
-                    ]
-                ]
-            )
-            ->getMock()
-        );
-
-        $this->assertTrue($this->sut->canModify(123));
+        return [
+            'ltd & Not InForce' => [
+                'type' => OrganisationEntityService::ORG_TYPE_REGISTERED_COMPANY,
+                'inForce' => false,
+                'expect' => true,
+            ],
+            'llp & Not InForce' => [
+                'type' => OrganisationEntityService::ORG_TYPE_LLP,
+                'inForce' => false,
+                'expect' => true,
+            ],
+            'other & Not InForce' => [
+                'type' => OrganisationEntityService::ORG_TYPE_OTHER,
+                'inForce' => false,
+                'expect' => true,
+            ],
+            'ltd & InForce' => [
+                'type' => OrganisationEntityService::ORG_TYPE_REGISTERED_COMPANY,
+                'inForce' => true,
+                'expect' => false,
+            ],
+            'SoleTraider & Not InForce' => [
+                'type' => OrganisationEntityService::ORG_TYPE_SOLE_TRADER,
+                'inForce' => false,
+                'expect' => false,
+            ],
+            ' & Not InForce' => [
+                'type' => OrganisationEntityService::ORG_TYPE_PARTNERSHIP,
+                'inForce' => false,
+                'expect' => false,
+            ],
+        ];
     }
 }
