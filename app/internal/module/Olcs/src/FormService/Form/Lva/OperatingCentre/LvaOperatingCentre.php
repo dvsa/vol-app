fea67f43e905c85644ccb729dfbb61e780c59472
Alex P <alex.peshkov@valtech.co.uk>
2017-05-16 15:32:48 +0100

    OLCS-16500 unit tests

--- app/internal/module/Olcs/src/FormService/Form/Lva/OperatingCentre/LvaOperatingCentre.php
+++ app/internal/module/Olcs/src/FormService/Form/Lva/OperatingCentre/LvaOperatingCentre.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * Lva Internal Operating Centres
- *
- * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
- */
 namespace Olcs\FormService\Form\Lva\OperatingCentre;
 
 use Common\FormService\Form\Lva\OperatingCentre\CommonOperatingCentre;
