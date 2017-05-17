f851e0424076f83d428c583f5839c61e655e6ecb
Dmitrij Golubev <dgolubev@DGolubev-pc.local>
2017-05-17 09:54:17 +0100

    Revert "OLCS-16246: add to config CSRF timeout; fix createForm() usage;"
    This reverts commit 8445840c66f8e3e0a420f652912207f93f409387.

--- app/selfserve/config/autoload/global.php
+++ app/selfserve/config/autoload/global.php
@@ -6,8 +6,4 @@ return [
         'adapter' => \Common\Service\Cqrs\Adapter\Curl::class,
         'timeout' => 60,
     ],
-    //  CSFR Form settings
-    'csrf' => [
-        'timeout' => 5400,  //  90 min; should match to auth timeout
-    ],
 ];
