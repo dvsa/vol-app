1f79528d1b62c6bc155a1593d1dca9fcf509c4a6
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-08-07 16:43:13 +0100

    OLCS-16655 table config added for the new community licences page

---
+++ app/internal/module/Olcs/src/Table/Tables/community.licence.table.php
@@ -0,0 +1,55 @@
+<?php
+
+return array(
+    'variables' => array(
+        'title' => 'Community Licence'
+    ),
+    'settings' => array(
+        'paginate' => array(
+            'limit' => array(
+                'default' => 10,
+                'options' => array(10, 25, 50)
+            )
+        ),
+        'crud' => array(
+            'actions' => array(
+                'add' => array('class' => 'action--secondary', 'value' => 'Add'),
+                'office-licence-add' => array('class' => 'action--secondary', 'value' => 'Add office licence')
+            )
+        ),
+    ),
+    'attributes' => array(
+    ),
+    'columns' => array(
+        array(
+            'title' => 'Prefix',
+            'sort' => 'prefix',
+            'name' => 'serialNoPrefix',
+        ),
+        array(
+            'title' => 'Date Issued',
+            'formatter' => 'Date',
+            'sort' => 'specifiedDate',
+            'name' => 'specifiedDate',
+        ),
+        array(
+            'title' => 'Issue number',
+            'sort' => 'issueNo',
+            'name' => 'issueNo',
+        ),
+        array(
+            'title' => 'Status',
+            'name' => 'status.description',
+            'formatter' => function ($row) {
+                return isset($row['status']['description']) ? $row['status']['description'] : null;
+            }
+        ),
+        [
+            'width' => 'checkbox',
+            'type' => 'Checkbox',
+            'data-attributes' => [
+                'filename'
+            ],
+        ],
+    )
+);
