fc0a8949b4d1c710df3527229cd8df7b266012d9
Alex Peshkov <alex.peshkov@valtech.co.uk>
2017-08-24 10:26:57 +0100

    OLCS-17302 unit tests fixed

--- app/selfserve/test/Olcs/src/FormService/Form/Lva/ApplicationFinancialEvidenceTest.php
+++ app/selfserve/test/Olcs/src/FormService/Form/Lva/ApplicationFinancialEvidenceTest.php
@@ -9,6 +9,7 @@ use Olcs\FormService\Form\Lva\ApplicationFinancialEvidence;
 use Zend\Form\Form;
 use Zend\Http\Request;
 use OlcsTest\FormService\Form\Lva\Traits\ButtonsAlterations;
+use OlcsTest\Bootstrap;
 
 /**
  * Application Financial Evidence Form Test
@@ -24,17 +25,49 @@ class ApplicationFinancialEvidenceTest extends MockeryTestCase
      */
     protected $sut;
 
+    /** @var  \Common\FormService\FormServiceManager */
+    protected $fsm;
+    /** @var  var FormHelperService */
     protected $fh;
+    /** @var  m\MockInterface */
+    protected $urlHelper;
+    /** @var  m\MockInterface */
+    protected $translator;
 
     public function setUp()
     {
         $this->fh = m::mock(FormHelperService::class)->makePartial();
+        $this->fsm = m::mock(\Common\FormService\FormServiceManager::class)->makePartial();
+        $this->urlHelper = m::mock();
+        $this->translator = m::mock();
+
+        $sm = Bootstrap::getServiceManager();
+        $sm->setService('Helper\Url', $this->urlHelper);
+        $sm->setService('Helper\Translation', $this->translator);
+
+        $this->fsm->shouldReceive('getServiceLocator')->andReturn($sm);
+
         $this->sut = new ApplicationFinancialEvidence();
         $this->sut->setFormHelper($this->fh);
+        $this->sut->setFormServiceLocator($this->fsm);
     }
 
     public function testAlterForm()
     {
+        $this->translator
+            ->shouldReceive('translateReplace')
+            ->with('lva-financial-evidence-evidence.hint', ['FOO'])
+            ->andReturn('BAR')
+            ->once()
+            ->getMock();
+
+        $this->urlHelper
+            ->shouldReceive('fromRoute')
+            ->with('guides/guide', ['guide' => 'financial-evidence'], [], true)
+            ->andReturn('FOO')
+            ->once()
+            ->getMock();
+
         $mockSaveButton = m::mock()
             ->shouldReceive('setLabel')
             ->with('lva.external.save_and_return.link')
@@ -106,9 +139,12 @@ class ApplicationFinancialEvidenceTest extends MockeryTestCase
                             ->getMock()
                     )
                     ->once()
+                    ->shouldReceive('setOption')
+                    ->with('hint', 'BAR')
+                    ->once()
                     ->getMock()
             )
-            ->times(3)
+            ->once()
             ->getMock();
 
         $mockRequest = m::mock(Request::class);
