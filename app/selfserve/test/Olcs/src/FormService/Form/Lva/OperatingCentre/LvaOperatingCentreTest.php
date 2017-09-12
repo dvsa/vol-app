0a0d989908a8db64e3fe8281a3446aaaa6f25eb0
Alex Peshkov <alex.peshkov@valtech.co.uk>
2017-09-12 16:58:33 +0100

    OLCS-17744 unused code removed

--- app/selfserve/test/Olcs/src/FormService/Form/Lva/OperatingCentre/LvaOperatingCentreTest.php
+++ app/selfserve/test/Olcs/src/FormService/Form/Lva/OperatingCentre/LvaOperatingCentreTest.php
@@ -170,7 +170,6 @@ class LvaOperatingCentreTest extends MockeryTestCase
 
     /**
      * @dataProvider gbParamsProvider
-     * @group test123
      */
     public function testAlterFormGb($params)
     {
