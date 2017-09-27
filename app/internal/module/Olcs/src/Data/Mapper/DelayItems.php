b312b9669d265c6acf182f232387786cad3c80c5
Gaurav Malhotra <gaurav.malhotra@valtech.co.uk>
2017-09-27 11:33:56 +0100

    OLCS-17160 mapper for delayitems

--- app/internal/module/Olcs/src/Data/Mapper/DelayItems.php
+++ app/internal/module/Olcs/src/Data/Mapper/DelayItems.php
@@ -14,7 +14,9 @@ class DelayItems implements MapperInterface
     /**
      * Should map data from a result array into an array suitable for a form
      *
-     * @param array $data
+     * @param array $data Data from command
+     *
+     * @return array
      */
     public static function mapFromResult(array $data)
     {
@@ -29,7 +31,8 @@ class DelayItems implements MapperInterface
     /**
      * Should map form data back into a command data structure
      *
-     * @param array $data
+     * @param array $data Data from form
+     *
      * @return array
      */
     public static function mapFromForm(array $data)
@@ -45,8 +48,9 @@ class DelayItems implements MapperInterface
      * Should map errors onto the form, any global errors should be returned so they can be added
      * to the flash messenger
      *
-     * @param FormInterface $form
-     * @param array $errors
+     * @param FormInterface $form   Form interface
+     * @param array         $errors array response from errors
+     *
      * @return array
      */
     public static function mapFromErrors(FormInterface $form, array $errors)
