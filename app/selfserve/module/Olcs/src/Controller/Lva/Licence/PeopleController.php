9a70ca6a6695f3660225c75adcc26ec3e68ff231
Shaun Hare <shaun@shaunhare.co.uk>
2017-10-05 11:07:21 +0100

    added basic skeleton for testand ensured working

--- app/selfserve/module/Olcs/src/Controller/Lva/Licence/PeopleController.php
+++ app/selfserve/module/Olcs/src/Controller/Lva/Licence/PeopleController.php
@@ -32,4 +32,24 @@ class PeopleController extends Lva\AbstractPeopleController
         $table = $form->get('table')->get('table')->getTable();
         $table->removeColumn('actionLinks');
     }
+
+
+    public function addAction()
+    {
+
+
+        //@todo get the variation type and check
+        /*
+            Controller needs to check if the new type of variation already exists (i expect we will know this by using a new id parameter in the route)
+
+            Create a new DTO in transfer eg Dvsa\Olcs\Transfer\Command\Licence\CreatePersonVariation
+            Create a new Command handler in backend Dvsa\Olcs\Api\Domain\CommandHandler\Licence\CreatePersonVariation
+            Link them up in Api/config
+
+            Create a form (probably very similar to the application one "olcs-common/Common/src/Common/Form/Model/Form/Lva/Person.php")
+
+            There are already DTO handlers for adding a person to a licence, this will be because some type of organisation will already similar functionality, so we need to either modify these or create new ones.
+         */
+
+    }
 }
