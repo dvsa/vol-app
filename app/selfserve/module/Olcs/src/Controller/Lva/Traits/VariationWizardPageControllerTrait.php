1bb957abc68b678351a031960a0af6008d6074f6
Richard Ward <richard.ward@bjss.com>
2017-11-14 16:07:32 +0000

    Fix cancel action on wizard pages for director change
    Was only working under very limited circumstances before, and only
    if form validation passed
    
    OLCS-18189

--- app/selfserve/module/Olcs/src/Controller/Lva/Traits/VariationWizardPageControllerTrait.php
+++ app/selfserve/module/Olcs/src/Controller/Lva/Traits/VariationWizardPageControllerTrait.php
@@ -2,17 +2,20 @@
 
 namespace Olcs\Controller\Lva\Traits;
 
+use Common\Controller\Lva\AbstractController;
 use Common\Controller\Plugin\Redirect;
 use Common\Service\Cqrs\Response as CqrsResponse;
 use Dvsa\Olcs\Transfer\Command\CommandInterface;
 use Zend\Http\Request;
 use Zend\Http\Response;
+use Zend\Mvc\Controller\Plugin\Params;
 
 /**
  * Trait for use in an AbstractController that forms part of a variation wizard
  * @method CqrsResponse handleCommand(CommandInterface $query)
  * @method Request getRequest()
  * @method Redirect redirect()
+ * @method Params params(string $param = null, mixed $default = null)
  */
 trait VariationWizardPageControllerTrait
 {
@@ -46,6 +49,18 @@ trait VariationWizardPageControllerTrait
     abstract protected function getStartRoute();
 
     /**
+     * Check if a button has been pressed
+     *
+     * @see AbstractController::isButtonPressed
+     *
+     * @param string $button button
+     * @param array  $data   data
+     *
+     * @return bool
+     */
+    abstract protected function isButtonPressed($button, $data = []);
+
+    /**
      * Ensure this controller is being called with a suitable variation
      *
      * @return null|mixed
@@ -58,16 +73,10 @@ trait VariationWizardPageControllerTrait
         if ($this->fetchDataForLva()['variationType']['id'] !== $this->getVariationType()) {
             return $this->notFoundAction();
         }
-        return null;
-    }
-
-    public function indexAction()
-    {
-        $formActions = $this->getRequest()->getPost('form-actions');
-        if (is_array($formActions) and array_key_exists('cancel',$formActions)) {
+        if ($this->isButtonPressed('cancel') && $this->params('action') === 'index') {
             return $this->handleCancelRedirect();
         }
-        return parent::indexAction();
+        return null;
     }
 
     /**
