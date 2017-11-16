ffaea680eec7abb579c1fa6bdecc71d17e178e5d
Shaun Hare <shaun.hare@dvsa.gov.uk>
2017-11-16 12:01:35 +0000

    fixing parameter issue causing failing build

--- app/internal/module/Olcs/src/FormService/Form/Lva/ConvictionsPenalties.php
+++ app/internal/module/Olcs/src/FormService/Form/Lva/ConvictionsPenalties.php
@@ -5,6 +5,7 @@
  *
  * @author Dan Eggleston <dan@stolenegg.com>
  */
+
 namespace Olcs\FormService\Form\Lva;
 
 use Common\FormService\Form\Lva\ConvictionsPenalties as CommonConvictionsPenalties;
@@ -19,12 +20,14 @@ class ConvictionsPenalties extends CommonConvictionsPenalties
     /**
      * Make form alterations
      *
-     * @param \Zend\Form\Form $form
+     * @param \Zend\Form\Form $form   form
+     * @param array           $params params
+     *
      * @return \Zend\Form\Form
      */
-    protected function alterForm($form)
+    protected function alterForm($form, array $params)
     {
-        parent::alterForm($form);
+        parent::alterForm($form, $params);
 
         $form->get('form-actions')->get('save')->setLabel('internal.save.button');
 
