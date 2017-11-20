7b4b261095738144309f07d7e580bbe700541320
Parth <parth.vyas@dvsa.gov.uk>
2017-11-20 15:45:59 +0000

    remove space

--- app/selfserve/module/Olcs/src/Controller/Lva/Traits/VariationWizardPageFormActionsTrait.php
+++ app/selfserve/module/Olcs/src/Controller/Lva/Traits/VariationWizardPageFormActionsTrait.php
@@ -37,7 +37,6 @@ trait VariationWizardPageFormActionsTrait
         $formActions = $form->get('form-actions');
         $formActions->remove('save');
         $formActions->get('saveAndContinue')->setLabel($this->getSubmitActionText());
-
         $cancelButton = $formActions->get('cancel');
         $currentCancelButtonClass = $cancelButton->getAttribute("class");
         $cancelButton->setAttribute("class", $currentCancelButtonClass." action--cancel");
