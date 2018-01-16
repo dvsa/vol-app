febe95707c7adb11e07b58e4e6ae9cf2351ed479
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-01-16 11:25:48 +0000

    adding form test - correcting labels and ensuring no mandatory fields

---
+++ app/internal/test/Olcs/src/Form/Model/Form/RevocationsSlaTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace OlcsTest\Form\Model\Form;
+
+
+use Olcs\TestHelpers\FormTester\AbstractFormValidationTestCase;
+
+class RevocationsSlaTest extends AbstractFormValidationTestCase
+{
+    protected $formName = \Olcs\Form\Model\Form\RevocationsSla::class;
+
+
+}
