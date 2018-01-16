6a4bf9b89c5561dc95f264b3ea718b5f21da5ff7
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-01-16 11:34:06 +0000

    test wip

--- app/internal/test/Olcs/src/Form/Model/Form/RevocationsSlaTest.php
+++ app/internal/test/Olcs/src/Form/Model/Form/RevocationsSlaTest.php
@@ -10,5 +10,18 @@ class RevocationsSlaTest extends AbstractFormValidationTestCase
 {
     protected $formName = \Olcs\Form\Model\Form\RevocationsSla::class;
 
+    public function testId()
+    {
+        $this->assertFormElementHidden(['fields', 'id']);
+    }
 
+    public function testVersion()
+    {
+        $this->assertFormElementHidden(['fields', 'version']);
+    }
+
+    public function testisSubmissionRequiredForApproval()
+    {
+        $this->assertFormElementDynamicRadio(['fields', 'isSubmissionRequiredForApproval', false]);
+    }
 }
