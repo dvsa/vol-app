fdf0c2ac9eb63a02167028c708ccd996b82eaa37
Richard Ward <richard.ward@bjss.com>
2018-01-18 13:07:00 +0000

    Disable trailers section/pages for NI selfserve OLCS-18720

--- app/selfserve/module/Olcs/src/Controller/Lva/Licence/TrailersController.php
+++ app/selfserve/module/Olcs/src/Controller/Lva/Licence/TrailersController.php
@@ -8,6 +8,7 @@ namespace Olcs\Controller\Lva\Licence;
 
 use Common\Controller\Lva;
 use Olcs\Controller\Lva\Traits\LicenceControllerTrait;
+use Zend\Mvc\MvcEvent;
 
 /**
  * Class TrailersController
@@ -16,7 +17,7 @@ use Olcs\Controller\Lva\Traits\LicenceControllerTrait;
  *
  * @package Olcs\Controller\Lva\Licence
  *
- * @author Josh Curtis <josh.curtis@valtech.co.uk>
+ * @author  Josh Curtis <josh.curtis@valtech.co.uk>
  */
 class TrailersController extends Lva\AbstractTrailersController
 {
@@ -24,4 +25,18 @@ class TrailersController extends Lva\AbstractTrailersController
 
     protected $lva = 'licence';
     protected $location = 'external';
+
+    /**
+     * Prevent access to NI:w
+     *
+     * @param MvcEvent $e
+     *
+     * @return array|null|\Zend\Http\Response
+     */
+    public function onDispatch(MvcEvent $e)
+    {
+        return $this->fetchDataForLva()['niFlag'] === 'Y'
+            ? $this->notFoundAction()
+            : parent::onDispatch($e);
+    }
 }
