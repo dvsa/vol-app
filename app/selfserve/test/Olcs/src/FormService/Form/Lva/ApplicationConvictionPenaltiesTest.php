9fa55293ce28f858dde2b5516e932076913182ea
Parth <parth.vyas@dvsa.gov.uk>
2018-03-14 12:26:08 +0000

    Fix tests

--- app/selfserve/test/Olcs/src/FormService/Form/Lva/ApplicationConvictionPenaltiesTest.php
+++ app/selfserve/test/Olcs/src/FormService/Form/Lva/ApplicationConvictionPenaltiesTest.php
@@ -8,6 +8,13 @@ use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Olcs\FormService\Form\Lva\ApplicationConvictionsPenalties;
 use Zend\Form\Form;
 use OlcsTest\FormService\Form\Lva\Traits\ButtonsAlterations;
+use Common\Form\Model\Form\Lva\Fieldset\ConvictionsPenaltiesData;
+use Common\Service\Helper\TranslationHelperService;
+use Common\Form\Model\Form\Lva\Fieldset\ConvictionsPenaltiesReadMoreLink;
+use Common\Form\Elements\InputFilters\ActionLink;
+use Common\FormService\FormServiceManager;
+use Zend\Form\Element;
+use Zend\Di\ServiceLocator;
 
 /**
  * Application Convictions and Penalties Form Test
@@ -25,6 +32,8 @@ class ApplicationConvictionsPenaltiesTest extends MockeryTestCase
 
     protected $fh;
 
+    protected $formName = 'Lva\ConvictionsPenalties';
+
     public function setUp()
     {
         $this->fh = m::mock(FormHelperService::class)->makePartial();
@@ -32,21 +41,90 @@ class ApplicationConvictionsPenaltiesTest extends MockeryTestCase
         $this->sut->setFormHelper($this->fh);
     }
 
+    public function checkAlterForm($guidePath, $guideName)
+    {
+        $translator = m::mock(TranslationHelperService::class);
+        $translator
+            ->shouldReceive('translate')
+            ->andReturn($guideName);
+
+        $mockUrl = m::mock();
+        $mockUrl
+            ->shouldReceive('fromRoute')
+            ->with(
+                'guides/guide',
+                ['guide' => $guideName]
+            )
+            ->once()
+            ->andReturn($guidePath);
+        $this->formHelper = m::mock(FormHelperService::class);
+        $this->fsm = m::mock(FormServiceManager::class)->makePartial();
+
+
+        $dataTable = m::mock(ConvictionsPenaltiesData::class);
+        $dataTable
+            ->shouldReceive('add')
+            ->with(anInstanceOf(Element::class), hasKeyValuePair('priority', integerValue()));
+
+        $ConvictionsReadMoreLink = m::mock(ConvictionsPenaltiesReadMoreLink::class);
+        $ConvictionsReadMoreLink
+            ->shouldReceive('get')
+            ->with('readMoreLink')->andReturn(
+                m::mock(ActionLink::class)->shouldReceive('setValue')->once()->with($guidePath)->getMock()
+            )->getMock();
+
+        $form = m::mock(Form::class);
+        $form
+            ->shouldReceive('get')->with('data')->andReturn($dataTable)
+            ->shouldReceive('get')->with('convictionsReadMoreLink')->andReturn(
+                $ConvictionsReadMoreLink
+            )->getMock();
+
+        $mockServiceLocator = m::mock(ServiceLocator::class);
+
+
+        $mockServiceLocator->shouldReceive('get')->with('Helper\Translation')->once()->andReturn($translator);
+        $mockServiceLocator->shouldReceive('get')->with('Helper\Url')->once()->andReturn($mockUrl);
+
+        $this->formHelper
+            ->shouldReceive('createForm')
+            ->once()
+            ->with($this->formName)
+            ->andReturn($form);
+
+        $this->fsm
+            ->shouldReceive('getServiceLocator')
+            ->andReturn($mockServiceLocator);
+
+        $this->sut->setFormHelper($this->formHelper);
+        $this->sut->setFormServiceLocator($this->fsm);
+
+        $this->mockAlterButtons($form, $this->formHelper);
+
+        $actual = $this->sut->getForm();
+
+        $this->assertSame($form, $actual);
+    }
+
+    public function checkGetFormNi()
+    {
+        $this->checkAlterForm(
+            '/guides/convictions-and-penalties-guidance-ni/',
+            'convictions-and-penalties-guidance-ni'
+        );
+    }
+
+    public function checkGetFormGb()
+    {
+        $this->checkAlterForm(
+            '/guides/convictions-and-penalties-guidance-gb/',
+            'convictions-and-penalties-guidance-gb'
+        );
+    }
+
     public function testAlterForm()
     {
-        $mockForm = m::mock(Form::class);
-
-        $this->fh->shouldReceive('createForm')
-            ->once()
-            ->with('Lva\ConvictionsPenalties')
-            ->andReturn($mockForm)
-            ->getMock();
-
-        $this->mockAlterButtons($mockForm, $this->fh);
-
-        $form = $this->sut->getForm();
-
-        $this->assertSame($mockForm, $form);
+        $this->checkGetFormGb();
+        $this->checkGetFormNi();
     }
 }
-
