09a51a46152fe3067929ff6cb661899320170121
vagrant <vagrant@olcs-v2.(none)>
2018-07-03 13:37:34 +0000

    removed unused files and code

--- app/selfserve/module/Permits/src/Permits/Form/Model/Form/CabotageForm.php
+++ app/selfserve/module/Permits/src/Permits/Form/Model/Form/CabotageForm.php
@@ -6,7 +6,7 @@ use Zend\Form\Annotation as Form;
 /**
  * @Form\Name("Cabotage")
  * @Form\Attributes({"method":"post"})
- * @Form\Type("Permits\Form\Form")
+ * @Form\Type("Common\Form\Form")
  */
 class CabotageForm
 {
--- app/selfserve/module/Permits/src/Permits/Form/PermitApplicationForm.php
+++ app/selfserve/module/Permits/src/Permits/Form/PermitApplicationForm.php
@@ -18,45 +18,45 @@ class PermitApplicationForm extends Form
         $this->inputFilter = null;
 
         $this->add(array(
-            'name' => 'id',
-            'type' => 'Hidden',
+          'name' => 'id',
+          'type' => 'Hidden',
         ));
 
         $this->add(array(
-            'name' => 'intensity',
-            'type' => 'Hidden',//number
+          'name' => 'intensity',
+          'type' => 'Hidden',//number
         ));
 
         $this->add(array(
-            'type' => 'MultiCheckBox',//MultiCheckBox
-            'name' => 'sectors',
-            ));
+          'type' => 'MultiCheckBox',//MultiCheckBox
+          'name' => 'sectors',
+        ));
 
         $this->add(array(
-            'type' => 'Hidden',//Radio
-            'name' => 'restrictedCountries',
-            'options' => array(
-                'value_options' => array(
-                    '1' => 'Yes',
-                    '0' => 'No',
-                ),
+          'type' => 'Hidden',//Radio
+          'name' => 'restrictedCountries',
+          'options' => array(
+            'value_options' => array(
+              '1' => 'Yes',
+              '0' => 'No',
             ),
+          ),
         ));
 
         $this->add(array(
-            'type' => 'MultiCheckBox',
-            'name' => 'restrictedCountriesList',
+          'type' => 'MultiCheckBox',
+          'name' => 'restrictedCountriesList',
 
         ));
 
         $this->add(array(
-            'name' => 'submit',
-            'type' => 'Submit',
-            'attributes' => array(
-                'value' => 'Accept and continue',
-                'id' => 'submitbutton',
-                'class' => 'action--primary large',
-            ),
+          'name' => 'submit',
+          'type' => 'Submit',
+          'attributes' => array(
+            'value' => 'Accept and continue',
+            'id' => 'submitbutton',
+            'class' => 'action--primary large',
+          ),
         ));
     }
 
