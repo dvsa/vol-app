76a1f3d046f10c4a3eb92aa06dfb2cbe74aee54f
Kollol Shamsuddin <kol.shamsuddin@capgemini.com>
2018-07-11 14:18:56 +0100

    For for restricted countries

--- app/selfserve/module/Permits/src/Permits/Form/Model/Form/RestrictedCountriesForm.php
+++ app/selfserve/module/Permits/src/Permits/Form/Model/Form/RestrictedCountriesForm.php
@@ -12,10 +12,6 @@ class RestrictedCountriesForm
 {
     /**
      * @Form\Name("Fields")
-     * @Form\Options({
-     *     "label" : "permits.page.field.restricted-countries",
-     *     "hint" : "markup-ecmt-restricted-countries",
-     * })
      * @Form\ComposedObject("Permits\Form\Model\Fieldset\RestrictedCountries")
      */
     public $fields = null;
@@ -25,6 +21,4 @@ class RestrictedCountriesForm
      * @Form\ComposedObject("Permits\Form\Model\Fieldset\Submit")
      */
     public $submitButton = null;
-
-
 }
