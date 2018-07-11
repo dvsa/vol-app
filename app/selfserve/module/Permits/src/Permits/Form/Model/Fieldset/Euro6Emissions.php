2f2d35efde76809c617d60607319300b2c03d3e3
Jason de Jonge <dejongej@Jasons-MacBook-Pro.local>
2018-07-11 09:36:15 +0100

    woops left a problem in, fixed now

--- app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/Euro6Emissions.php
+++ app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/Euro6Emissions.php
@@ -17,14 +17,15 @@ class Euro6Emissions
      *    "id" : "MeetsEuro6",
      * })
      * @Form\Options({
+     *      "label": "Choose one licence below",
      *      "fieldset-attributes": {"id": "ecmt-licence"},
      *      "fieldset-data-group": "licence-type",
      *      "label_attributes": {"class": "form-control form-control--radio"},
      *      "value_options": {
-     *          OB2013691 (North East),
-     *          OC010019897 (North West),
-     *          PB5553691 (South East),
-     *          PC010119896 (South West),
+     *          "OB2013691 (North East)",
+     *          "OC010019897 (North West)",
+     *          "PB5553691 (South East)",
+     *          "PC010119896 (South West)",
      *      },
      * })
      * @Form\Type("Radio")
