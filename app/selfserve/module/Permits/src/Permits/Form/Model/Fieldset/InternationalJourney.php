cbbcd85adfa1d6137a115fdd8314d19b8fb9da30
Kollol Shamsuddin <kol.shamsuddin@capgemini.com>
2018-07-12 15:22:18 +0100

    Form and fieldset for international journey page

---
+++ app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/InternationalJourney.php
@@ -0,0 +1,34 @@
+<?php
+namespace Permits\Form\Model\Fieldset;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * @codeCoverageIgnore Auto-generated file with no methods
+ * @Form\Name("InternationalJourney")
+ */
+class InternationalJourney
+{
+    /**
+     * @Form\Name("InternationalJourney")
+     * @Form\Required(true)
+     * @Form\Attributes({
+     *   "class" : "input--trips",
+     *    "id" : "InternationalJourney",
+     * })
+     * @Form\Options({
+     *      "label": "",
+     *      "fieldset-attributes": {"id": "international-journey"},
+     *      "fieldset-data-group": "percentage-type",
+     *      "label_attributes": {"class": "form-control form-control--radio"},
+     *      "value_options": {
+     *          "Less than 60%",
+     *          "From 60% to 90%",
+     *          "More than 90%",
+     *      },
+     * })
+     * @Form\Type("Radio")
+     */
+    public $ecmtLicence = null;
+
+}
