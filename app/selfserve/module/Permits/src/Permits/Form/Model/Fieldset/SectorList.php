90d3cfffe89d70f959bbac22c3656c23209c7e0c
Jason de Jonge <dejongej@Jasons-MacBook-Pro.local>
2018-07-12 10:50:29 +0100

    created sector list form with unit test

---
+++ app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/SectorList.php
@@ -0,0 +1,38 @@
+<?php
+namespace Permits\Form\Model\Fieldset;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * @codeCoverageIgnore Auto-generated file with no methods
+ * @Form\Name("SectorList")
+ * @Form\Attributes({
+ *     "class" : "sector-list guidance"
+ * })
+ */
+class SectorList
+{
+    /**
+     * @Form\Name("SectorList")
+     * @Form\Required(false)
+     * @Form\Attributes({
+     *   "class" : "input--trips",
+     *    "id" : "EcmtLicence",
+     * })
+     * @Form\Options({
+     *      "label": "Which sector do you mainly specialise in?",
+     *      "fieldset-attributes": {"id": "sector-list"},
+     *      "fieldset-data-group": "sector-list",
+     *      "label_attributes": {"class": "form-control form-control--radio"},
+     *      "value_options": {
+     *          "Food products, beverages and tobacco, products of agriculture,
+     *                      hunting and forests, fish and other fishing products",
+     *          "Unrefined coal and lignite, crude petroleum and natural gas",
+     *          "Textiles and textile products, leather and leather products",
+     *      },
+     * })
+     * @Form\Type("Radio")
+     */
+    public $SectorList = null;
+
+}
---
+++ app/selfserve/module/Permits/src/Permits/Form/Model/Form/SpecialistHaulageForm.php
@@ -0,0 +1,24 @@
+<?php
+namespace Permits\Form\Model\Form;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * @Form\Name("SpecialistHaulage")
+ * @Form\Attributes({"method":"post"})
+ * @Form\Type("Common\Form\Form")
+ */
+class SpecialistHaulageForm
+{
+    /**
+     * @Form\Name("Fields")
+     * @Form\ComposedObject("Permits\Form\Model\Fieldset\SpecialistHaulage")
+     */
+    public $fields = null;
+
+    /**
+     * @Form\Name("Submit")
+     * @Form\ComposedObject("Permits\Form\Model\Fieldset\Submit")
+     */
+    public $submitButton = null;
+}
