0d9b5ed864a5637d02b632a260911c0bf19b6d30
Juan Carlos Ortega Contreras <juan.ortega-contreras@capgemini.com>
2018-07-26 11:00:46 +0100

    corrections as per MR comments

--- app/selfserve/module/Permits/src/Permits/Controller/PermitsController.php
+++ app/selfserve/module/Permits/src/Permits/Controller/PermitsController.php
@@ -13,8 +13,6 @@ use Dvsa\Olcs\Transfer\Query\Organisation\Organisation;
 use Dvsa\Olcs\Transfer\Command\Permits\CreateEcmtPermits;
 use Dvsa\Olcs\Transfer\Command\Permits\CreateEcmtPermitApplication;
 use Dvsa\Olcs\Transfer\Command\Permits\UpdateEcmtPermitApplication;
-use Dvsa\Olcs\Transfer\Command\Permits\UpdateEcmtEmissions;
-use Dvsa\Olcs\Transfer\Command\Permits\UpdateEcmtCabotage;
 use Zend\Mvc\MvcEvent;
 use Zend\Http\Header\Referer as HttpReferer;
 use Zend\Http\PhpEnvironment\Request as HttpRequest;
@@ -26,7 +24,6 @@ use Zend\Session\Container; // We need this when using sessions
 
 use Olcs\Controller\Lva\Traits\ExternalControllerTrait;
 use Permits\View\Helper\EcmtSection;
-
 
 class PermitsController extends AbstractOlcsController implements ToggleAwareInterface
 {
@@ -770,26 +767,4 @@ class PermitsController extends AbstractOlcsController implements ToggleAwareInt
         return $response->getResult();
     }
 
-    /**
-     * Returns an array for the update command
-     *
-     * @param $id application id
-     * @param $data array
-     * @return array
-     */
-    private function generateApplicationData($id, $data)
-    {
-        $application = $id;
-        $key = key($data);
-        $value = $data[$key];
-        $applicationData = [
-          'id' => $id,
-          $key => $value,
-          'status' => $application['status']['id'],
-          'paymentStatus' => $application['paymentStatus']['id'],
-          'permitType' => $application['permitType']['id'],
-          'licence' => $application['licence']['id']
-        ];
-        return $applicationData;
-    }
 }
