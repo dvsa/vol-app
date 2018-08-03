8c95d868a6d40715fdcc3f192e5b7f9c1bb0c322
Andy Newton <andrew.newton@capgemini.com>
2018-08-03 11:46:56 +0100

    OLCS-20573 - Implement IRHP Tab condionally on licence type.

--- app/internal/test/Olcs/src/Listener/NavigationToggleTest.php
+++ app/internal/test/Olcs/src/Listener/NavigationToggleTest.php
@@ -3,6 +3,7 @@
 namespace OlcsTest\Listener;
 
 use Common\FeatureToggle;
+use Dvsa\Olcs\Transfer\Query\Licence\Licence;
 use Common\Service\Cqrs\Query\QuerySender;
 use Mockery\Adapter\Phpunit\MockeryTestCase as TestCase;
 use Olcs\Listener\NavigationToggle;
@@ -59,9 +60,15 @@ class NavigationToggleTest extends TestCase
     /**
      * @dataProvider dpDispatch
      */
-    public function testOnDispatch($adminPermitsEnabled)
+    public function testOnDispatch($adminPermitsEnabled, $internalPermitEnabled, $params)
     {
+        $licence['goodsOrPsv']['id'] = 'lcat_psv';
+        if (!empty($params)) {
+            $licence['goodsOrPsv']['id'] = 'lcat_gv';
+        }
+
         $permitsKey = 'admin-dashboard/admin-permits';
+        $irhpPermitsKey = 'licence_irhp_permits';
         $userData = [ 'disableDataRetentionRecords' => false ];
 
         $userObject = new User();
@@ -73,10 +80,23 @@ class NavigationToggleTest extends TestCase
 
         $page = new Uri();
         $permitsPage = new Uri();
+        $ihrpPermitsPage = new Uri();
 
         $this->mockQuerySender->shouldReceive('featuresEnabled')
             ->with([FeatureToggle::ADMIN_PERMITS])
             ->andReturn($adminPermitsEnabled);
+
+        $this->mockQuerySender->shouldReceive('featuresEnabled')
+            ->with([FeatureToggle::INTERNAL_PERMITS])
+            ->andReturn($internalPermitEnabled);
+
+        $mockLicence = m::mock(Licence::class);
+        $mockLicence->shouldReceive('getResult')
+            ->andReturn($licence);
+
+        $this->mockQuerySender->shouldReceive('send')
+            ->andReturn($mockLicence)
+            ->zeroOrMoreTimes();
 
         $this->mockNavigation
             ->shouldReceive('findBy')
@@ -88,23 +108,34 @@ class NavigationToggleTest extends TestCase
             ->with('id', $permitsKey)
             ->andReturn($permitsPage);
 
+        $this->mockNavigation
+            ->shouldReceive('findBy')
+            ->with('id', $irhpPermitsKey)
+            ->andReturn($ihrpPermitsPage);
+
         /** @var \Zend\Mvc\MvcEvent | m\MockInterface $mockEvent */
         $mockEvent = m::mock(\Zend\Mvc\MvcEvent::class);
+
+        $mockEvent->shouldReceive('getRouteMatch->getParams')
+            ->andReturn($params);
 
         $this->sut->createService($this->mockSm);
         $this->sut->onDispatch($mockEvent);
 
         $isVisible = $this->mockNavigation->findBy('id', 'admin-dashboard/admin-data-retention')->getVisible();
 
+        $isIrhpVisible = $this->mockNavigation->findBy('id', $irhpPermitsKey)->getVisible();
+
         $this->assertTrue($isVisible);
         $this->assertEquals($adminPermitsEnabled, $this->mockNavigation->findBy('id', $permitsKey)->getVisible());
+        $this->assertEquals($isIrhpVisible, $internalPermitEnabled);
     }
 
     public function dpDispatch(): array
     {
         return [
-            [true],
-            [false]
+            [true, true, ['licence' => 7]],
+            [false, false, []]
         ];
     }
 }
