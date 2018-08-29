5fd00e25bc4873488a0d9d762eee109a01750fa4
Jason de Jonge <dejongej@Jasons-MacBook-Pro.local>
2018-08-29 12:08:41 +0100

    set SectorList to required so that validation works

--- app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/SectorList.php
+++ app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/SectorList.php
@@ -12,7 +12,7 @@ class SectorList
 
     /**
      * @Form\Name("SectorList")
-     * @Form\Required(false)
+     * @Form\Required(true)
      * @Form\Options({
      *     "fieldset-attributes": {"id": "sector-list"},
      *     "fieldset-data-group": "sector-list",
