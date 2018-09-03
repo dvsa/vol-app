8db268b92c4aa992b600435c654fb013c6ec5cbc
Kollol Shamsuddin <kol.shamsuddin@capgemini.com>
2018-09-03 16:49:51 +0100

    [OLCS-21379] Declined permit fieldset is added

---
+++ app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/DeclinedPermitFieldset.php
@@ -0,0 +1,28 @@
+<?php
+namespace Permits\Form\Model\Fieldset;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * @codeCoverageIgnore Auto-generated file with no methods
+ */
+class DeclinedPermitFieldset
+{
+    /**
+     * @Form\Required(true)
+     * @Form\Attributes({
+     *   "class" : "input--confirm-decline",
+     *   "id" : "DeclinedPermit",
+     * })
+     * @Form\Options({
+     *   "checked_value": "1",
+     *   "unchecked_value": "0",
+     *   "label": "permits.form.declined_permit.label",
+     *   "label_attributes": {"class": "form-control form-control--checkbox"},
+     *   "must_be_value": "1",
+     *   "not_checked_message": "permits.form.declined_permit.error_message"
+     * })
+     * @Form\Type("\Common\Form\Elements\InputFilters\SingleCheckbox")
+     */
+    public $confirmDecline = null;
+}
