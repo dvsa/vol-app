678a1ae9c41a2b365d196b3ab96dece35fcec5f1
Kollol Shamsuddin <kol.shamsuddin@capgemini.com>
2018-09-05 09:56:32 +0100

    [OLCS-21379] unit test for decline aplication form

---
+++ app/selfserve/test/Permits/src/Form/Model/Form/DeclineApplicationFormTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PermitsTest\Form\Model\Form;
+
+use Olcs\TestHelpers\FormTester\AbstractFormValidationTestCase;
+use Zend\Validator;
+
+/**
+ * Class DeclineApplicationFormTest
+ *
+ * @group FormTests
+ */
+class DeclineApplicationFormTest extends AbstractFormValidationTestCase
+{
+    /**
+     * @var string The class name of the form being tested
+     */
+    protected $formName = \Permits\Form\Model\Form\DeclineApplicationFormTest::class;
+
+    public function testDeclineApplicationFieldset()
+    {
+        $element = ['Fields', 'DeclineApplicationFieldset'];
+
+        $this->assertFormElementRequired($element, true);
+        $this->assertFormElementAllowEmpty($element, false);
+        $this->assertFormElementType($element, "\Common\Form\Elements\InputFilters\SingleCheckbox");
+
+        $this->assertFormElementValid($element, 'Yes');
+        $this->assertFormElementNotValid($element, 'No', [Validator\Identical::NOT_SAME]);
+        $this->assertFormElementNotValid($element, 'X', [Validator\Identical::NOT_SAME]);
+    }
+
+    public function testSubmit()
+    {
+        $element = ['Submit', 'SubmitButton'];
+        $this->assertFormElementActionButton($element);
+        $this->assertFormElementType($element, "Zend\Form\Element\Submit");
+    }
+
+}
