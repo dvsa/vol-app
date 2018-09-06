a19e208bd14c3f746b6cc49a97bc2bf22704ee9f
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-09-06 13:23:50 +0100

    change transport_manager to transport_managers

--- app/selfserve/module/Olcs/config/module.config.php
+++ app/selfserve/module/Olcs/config/module.config.php
@@ -1387,7 +1387,7 @@ return array(
 
                 'lva-application/transport_manager_details*' => ['selfserve-tm'],
                 'lva-variation/transport_manager_details*' => ['selfserve-tm'],
-                'lva-transport_manager/*' =>['selfserve-tm'],
+                'lva-transport_managers/*' =>['selfserve-tm'],
                 'lva-*' => ['selfserve-lva'],
                 'search*' => ['*'],
                 'index' => ['*'],
