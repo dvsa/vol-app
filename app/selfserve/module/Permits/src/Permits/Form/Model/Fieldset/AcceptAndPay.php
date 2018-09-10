e0acfba46c8a5a96eca16b391b4200cbb6c411f2
Tonci Vidovic <tonci.vidovic@capgemini.com>
2018-09-10 11:43:26 +0100

    OLCS-21379 form buttons float left

--- app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/AcceptAndPay.php
+++ app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/AcceptAndPay.php
@@ -6,28 +6,24 @@ use Zend\Form\Annotation as Form;
 /**
  * @codeCoverageIgnore Auto-generated file with no methods
  * @Form\Name("AcceptAndPay")
+ * @Form\Attributes({"class":"actions-container"})
  */
 class AcceptAndPay
 {
     /**
      * @Form\Name("SubmitButton")
-     * @Form\Attributes({
-     *     "class":"action--primary large",
-     *     "id":"submit-accept-button",
-     *     "value":"permits.page.accept.and.pay",
-     * })
-     * @Form\Type("Zend\Form\Element\Submit")
+     * @Form\Attributes({"id":"submit-accept-button","type":"submit","class":"action--primary action--external large"})
+     * @Form\Options({"label": "permits.page.accept.and.pay"})
+     * @Form\Type("\Common\Form\Elements\InputFilters\ActionButton")
      */
     public $submit = null;
 
     /**
      * @Form\Name("DeclineButton")
-     * @Form\Attributes({
-     *     "class":"action--primary large",
-     *     "id":"submit-decline-button",
-     *     "value":"permits.page.decline.permits",
-     * })
-     * @Form\Type("Zend\Form\Element\Submit")
+     *
+     * @Form\Attributes({"id":"submit-decline-button","type":"submit","class":"action--secondary large"})
+     * @Form\Options({"label": "permits.page.decline.permits"})
+     * @Form\Type("\Common\Form\Elements\InputFilters\ActionButton")
      */
     public $decline = null;
 }
