bef76da5365942a0e18ad522c22f2cf85d8f222f
Scott Callaway <github@scottcallaway.co.uk>
2018-09-24 16:47:31 +0100

    OLCS-21035 - Add keypress handler to capture the user pressing the ENTER key and moved displaying of alert box logic into separate function

--- app/internal/module/Admin/assets/js/inline/irhp-permit-stock.js
+++ app/internal/module/Admin/assets/js/inline/irhp-permit-stock.js
@@ -12,11 +12,25 @@ OLCS.ready(function() {
   // Add an on click handlder to the save button to display a popup
   // if the user is editing the form.
   $(document).on("click", '#save', function (e) {
+    showEditAlert(e);
+  });
+
+  // Bind a keypress handler forw when the user presses the 'ENTER'
+  // key instead of clicking the submit button.
+  $(document).keypress(function (event) {
+    if (event.which === 13) {
+      showEditAlert(event);
+    }
+  });
+
+  // Show an alert box to the user when they try to edit an IRHP Permit
+  // Stock.
+  function showEditAlert(e) {
     if (isEditing) {
       if (!confirm("Are you sure you want to edit details of existing stock?")) {
         e.preventDefault();
         e.stopImmediatePropagation();
       }
     }
-  });
+  }
 });
