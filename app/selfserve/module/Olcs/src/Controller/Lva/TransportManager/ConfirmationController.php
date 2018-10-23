a6284a9c20a27ba68961292c6ec2670324947733
Parth <parth.vyas@dvsa.gov.uk>
2018-10-23 16:00:45 +0100

    Modify isTransportManagerRole to return true for operator users

--- app/selfserve/module/Olcs/src/Controller/Lva/TransportManager/ConfirmationController.php
+++ app/selfserve/module/Olcs/src/Controller/Lva/TransportManager/ConfirmationController.php
@@ -96,7 +96,7 @@ class ConfirmationController extends AbstractController
             );
         }
     }
-
+// fix here
     /**
      * is the logged in user just TM, eg not an admin
      *
@@ -104,8 +104,10 @@ class ConfirmationController extends AbstractController
      */
     private function isTransportManagerRole()
     {
-        return ($this->isGranted(RefData::PERMISSION_SELFSERVE_TM_DASHBOARD) &&
-            !$this->isGranted(RefData::PERMISSION_SELFSERVE_LVA));
+        if($this->isGranted(RefData::PERMISSION_SELFSERVE_TM_DASHBOARD)){
+            return true;
+        }
+            return false;
     }
 
     private function getSignatureDate($signature)
