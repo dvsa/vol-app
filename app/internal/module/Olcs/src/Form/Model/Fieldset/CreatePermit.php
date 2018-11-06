86ef8e5931976a62104eed76b5c24044842f96a5
Jason de Jonge <dejongej@Jasons-MacBook-Pro.local>
2018-11-06 17:16:31 +0000

    set breakchainonfailure on form validator to prevent further validators being called OLCS-22366

--- app/internal/module/Olcs/src/Form/Model/Fieldset/CreatePermit.php
+++ app/internal/module/Olcs/src/Form/Model/Fieldset/CreatePermit.php
@@ -65,7 +65,13 @@ class CreatePermit extends Base
      *     "allow_empty" : true,
      * })
      * @Form\Validator({"name":"Zend\Validator\Digits"})
-     * @Form\Validator({"name":"Zend\Validator\GreaterThan", "options": {"min": 0}})
+     * @Form\Validator({
+     *      "name":"Zend\Validator\GreaterThan",
+     *      "options": {
+     *          "min": 0,
+     *          "breakchainonfailure": true
+     *      }
+     * })
      * @Form\Validator({
      *     "name": "NumberCompare",
      *     "options": {
