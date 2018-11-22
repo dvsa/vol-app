d4db275c3a4211f9eef32ad55f552aa7f51303a1
James Hodgson <james@tdchodgy.co.uk>
2018-11-22 12:01:47 +0000

    Add page content OLCS-22213

--- app/selfserve/module/Olcs/src/Controller/Licence/Surrender/DeclarationController.php
+++ app/selfserve/module/Olcs/src/Controller/Licence/Surrender/DeclarationController.php
@@ -7,9 +7,12 @@ class DeclarationController extends AbstractSurrenderController
     public function indexAction()
     {
         $params = [
-            'title' => '',
+            'title' => 'licence.surrender.declaration.title',
             'licNo' => $this->licence['licNo'],
-            'content' => '',
+            'content' => [
+                'markup' => 'markup-licence-surrender-declaration',
+                'data' => [$this->licence['licNo']]
+            ],
             'backLink' => $this->getBackLink('lva-licence'),
         ];
 
