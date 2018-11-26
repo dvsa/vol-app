214e0599fc43e088f648e33ba44add3f9ae0d0a9
Piotr Filak <piotr.filak@capgemini.com>
2018-11-26 13:10:21 +0000

    OLCS-19279 - Print IRHP Permits Confirm Screen

---
+++ app/internal/module/Admin/src/Table/Tables/admin-irhp-permit-print-confirm.table.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'variables' => [
+        'title' => 'Printer must be loaded with the templates for the following Permit Numbers and in this order.',
+    ],
+    'settings' => [
+        'crud' => [
+            'actions' => [
+                'print' => [
+                    'requireRows' => false,
+                    'label' => 'Confirm',
+                    'class' => 'action--primary'
+                ],
+                'cancel' => [
+                    'requireRows' => false,
+                    'class' => 'action--secondary'
+                ]
+            ]
+        ],
+    ],
+    'attributes' => [
+    ],
+    'columns' => [
+        [
+            'title' => 'Sequence Number',
+            'name' => 'sequenceNumber'
+        ],
+        [
+            'title' => 'Permit Number',
+            'name' => 'permitNumberWithPrefix'
+        ],
+        [
+            'title' => 'Operator Name',
+            'formatter' => function ($data) {
+                return $data['irhpPermitApplication']['ecmtPermitApplication']['licence']['organisation']['name'];
+            }
+        ],
+    ]
+];
