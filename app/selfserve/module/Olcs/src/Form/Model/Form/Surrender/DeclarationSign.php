bd60a523c40a129cca31f0e0579c2ed07f7cff74
James Hodgson <james@tdchodgy.co.uk>
2018-11-26 08:37:48 +0000

    Move form header out of partial OLCS-22213

--- app/selfserve/module/Olcs/src/Form/Model/Form/Surrender/DeclarationSign.php
+++ app/selfserve/module/Olcs/src/Form/Model/Form/Surrender/DeclarationSign.php
@@ -11,6 +11,12 @@ use Zend\Form\Annotation as Form;
 class DeclarationSign
 {
     /**
+     * @Form\Attributes({"value": "licence.surrender.declaration.form.header"})
+     * @Form\Type("Common\Form\Elements\Types\HtmlTranslated")
+     */
+    public $header = null;
+
+    /**
      * @Form\Attributes({"value": "markup-declaration-for-verify","data-container-class":"declarationForVerify"})
      * @Form\Type("Common\Form\Elements\Types\GuidanceTranslated")
      */
