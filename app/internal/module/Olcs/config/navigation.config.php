16d4b4c558c858d1fd6179bdf8b92b0eb36c2777
Andy Newton <andy@vitri.ltd>
2018-11-30 17:01:02 +0000

    OLCS-22746 - ICW Navigation fix

--- app/internal/module/Olcs/config/navigation.config.php
+++ app/internal/module/Olcs/config/navigation.config.php
@@ -571,15 +571,6 @@ $nav = [
                                     ],
                                 ],
                                 [
-                                    'id' => 'licence_irhp_permits-add',
-                                    'label' => 'Application details',
-                                    'route' => 'licence/permits/add',
-                                    'use_route_match' => true,
-                                    'params' => [
-                                        'action' => 'add',
-                                    ],
-                                ],
-                                [
                                     'id' => 'licence_irhp_permits-documents',
                                     'label' => 'Docs & attachments',
                                     'route' => 'licence/irhp-docs',
