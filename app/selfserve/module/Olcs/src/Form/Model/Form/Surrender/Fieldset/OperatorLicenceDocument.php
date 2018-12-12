1d22365599a49bb5f4b7cd357472b31591522574
Igor Stimoli <igor.stimoli@bjss.com>
2018-12-12 17:09:22 +0000

    OLCS-22256: changes to OperatorLicence form

--- app/selfserve/module/Olcs/src/Form/Model/Form/Surrender/Fieldset/OperatorLicenceDocument.php
+++ app/selfserve/module/Olcs/src/Form/Model/Form/Surrender/Fieldset/OperatorLicenceDocument.php
@@ -5,6 +5,7 @@ namespace Olcs\Form\Model\Form\Surrender\Fieldset;
 use Zend\Form\Annotation as Form;
 
 /**
+ * @Form\Type("\Common\Form\Elements\Types\RadioVertical")
  * @Form\Name("operator-licence-document")
  */
 class OperatorLicenceDocument
@@ -22,40 +23,36 @@ class OperatorLicenceDocument
      *          "possession": {
      *              "label": "licence.surrender.operator_licence.possession.label",
      *              "value": "possession",
-     *              "attributes": {"data-aria-controls":"conditional-surrender-licence-possession", "id":"surrender-licence-possession"},
-     *              "childContent" : {
-     *                  "content": "\Olcs\Form\Model\Form\Surrender\Fieldset\LicenceInPossession",
-     *                  "attributes": {"id":"conditional-surrender-licence-possession","class":"govuk-radios__conditional govuk-radios__conditional--hidden"}
-     *              }
+     *              "attributes": {"data-aria-controls":"conditional-surrender-licence-possession", "id":"surrender-licence-possession"}
      *          },
      *          "lost": {
      *              "label": "licence.surrender.operator_licence.lost.label",
      *              "value": "lost",
-     *              "attributes": {"data-aria-controls":"conditional-surrender-licence-lost", "id":"surrender-licence-lost"},
-     *              "childContent" : {
-     *                  "content": "\Olcs\Form\Model\Form\Surrender\Fieldset\LicenceLost",
-     *                  "attributes": {"id":"conditional-surrender-licence-lost","class":"govuk-radios__conditional govuk-radios__conditional--hidden"}
-     *              },
+     *              "attributes": {"data-aria-controls":"conditional-surrender-licence-lost", "id":"surrender-licence-lost"}
      *          },
      *          "stolen": {
      *              "label": "licence.surrender.operator_licence.stolen.label",
      *              "value": "stolen",
-     *              "attributes": {"data-aria-controls":"conditional-surrender-licence-stolen", "id":"surrender-licence-stolen"},
-     *              "childContent" : {
-     *                  "content": "\Olcs\Form\Model\Form\Surrender\Fieldset\LicenceStolen",
-     *                  "attributes": {"id":"conditional-surrender-licence-stolen","class":"govuk-radios__conditional govuk-radios__conditional--hidden"}
-     *              }
+     *              "attributes": {"data-aria-controls":"conditional-surrender-licence-stolen", "id":"surrender-licence-stolen"}
      *          }
      *      }
      * })
      * @Form\Type("\Common\Form\Elements\Types\Radio")
      */
-    public $licenceDocumentOptions = null;
+    public $radio = null;
 
     /**
-     * @Form\Name("form-actions")
-     * @Form\ComposedObject("Common\Form\Model\Fieldset\SaveButton")
-     * @Form\Attributes({"class":"actions-container"})
+     * @Form\ComposedObject("\Olcs\Form\Model\Form\Surrender\Fieldset\LicenceInPossession")
      */
-    public $formActions = null;
+    public $possessionContent = null;
+
+    /**
+     * @Form\ComposedObject("\Olcs\Form\Model\Form\Surrender\Fieldset\LicenceLost")
+     */
+    public $lostContent;
+
+    /**
+     * @Form\ComposedObject("\Olcs\Form\Model\Form\Surrender\Fieldset\LicenceStolen")
+     */
+    public $stolenContent;
 }
--- app/selfserve/module/Olcs/src/Form/Model/Form/Surrender/OperatorLicence.php
+++ app/selfserve/module/Olcs/src/Form/Model/Form/Surrender/OperatorLicence.php
@@ -13,4 +13,11 @@ class OperatorLicence
      * @Form\ComposedObject("Olcs\Form\Model\Form\Surrender\Fieldset\OperatorLicenceDocument")
      */
     public $operatorLicenceDocument = null;
+
+    /**
+     * @Form\Name("form-actions")
+     * @Form\ComposedObject("Common\Form\Model\Fieldset\SaveButton")
+     * @Form\Attributes({"class":"actions-container"})
+     */
+    public $formActions = null;
 }
