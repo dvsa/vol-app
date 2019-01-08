de3276439e2ca1d1eab8fed988add14d2aaef015
Jonathan Thomas <jonathan@opalise.co.uk>
2019-01-08 09:02:02 +0000

    OLCS-22907 ss new bilateral permits number of permits per country page

--- app/selfserve/module/Olcs/src/Controller/AbstractSelfserveController.php
+++ app/selfserve/module/Olcs/src/Controller/AbstractSelfserveController.php
@@ -138,7 +138,9 @@ abstract class AbstractSelfserveController extends AbstractOlcsController
      *
      * @var array
      */
-    protected $postConfig = [];
+    protected $postConfig = [
+        'saveAndReturnStep' => EcmtSection::ROUTE_APPLICATION_OVERVIEW
+    ];
 
     /**
      * Redirect parameters
@@ -270,7 +272,13 @@ abstract class AbstractSelfserveController extends AbstractOlcsController
                     }
                 }
 
-                return $this->handleSaveAndReturnStep($this->postParams, $config['step'], $this->redirectParams, $this->redirectOptions);
+                return $this->handleSaveAndReturnStep(
+                    $this->postParams,
+                    $config['step'],
+                    $config['saveAndReturnStep'],
+                    $this->redirectParams,
+                    $this->redirectOptions
+                );
             }
         }
     }
@@ -542,20 +550,28 @@ abstract class AbstractSelfserveController extends AbstractOlcsController
      * for wider selfserve use. Permits needs to start using VOL standard buttons before this can be truly reusable
      *
      * @param array $submittedData - an array of the data submitted by the form
-     * @param string $nextStep - the EcmtSection:: route to be taken if the form was submitted normally
-     *
+     * @param string $nextStep - the route to be taken if the form was submitted normally
+     * @param string $saveAndReturnStep - the route to be taken is the form was submitted using save and return
      * @param array $params
      * @param array $options
      * @return HttpResponse
      */
-    protected function handleSaveAndReturnStep(array $submittedData, string $nextStep, array $params = [], array $options = []): HttpResponse
-    {
+    protected function handleSaveAndReturnStep(
+        array $submittedData,
+        string $nextStep,
+        $saveAndReturnStep = EcmtSection::ROUTE_APPLICATION_OVERVIEW,
+        array $params = [],
+        array $options = []
+    ): HttpResponse {
         if (array_key_exists('SubmitButton', $submittedData['Submit'])) {
-            //Form was submitted normally so continue on chosen path
-            return $this->nextStep($nextStep, $params, $options);
+            // Form was submitted normally so continue on chosen path
+            $step = $nextStep;
+        } else {
+            // A button other than the primary submit button was clicked so return to overview
+            $step = $saveAndReturnStep;
         }
-        //A button other than the primary submit button was clicked so return to overview
-        return $this->nextStep(EcmtSection::ROUTE_APPLICATION_OVERVIEW, $params, $options);
+
+        return $this->nextStep($step, $params, $options);
     }
 
     /**
