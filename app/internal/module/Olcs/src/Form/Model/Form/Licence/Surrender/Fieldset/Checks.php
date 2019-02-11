ba17ac2ce52e4101ea8edb1f6e1df7e5f49343d9
James Hodgson <james@tdchodgy.co.uk>
2019-02-11 15:25:52 +0000

    Add bus reg and open cases checkboxes

--- app/internal/module/Olcs/src/Form/Model/Form/Licence/Surrender/Fieldset/Checks.php
+++ app/internal/module/Olcs/src/Form/Model/Form/Licence/Surrender/Fieldset/Checks.php
@@ -6,6 +6,39 @@ use Zend\Form\Annotation as Form;
 
 class Checks
 {
+
+    /**
+     * @Form\Type("Checkbox")
+     * @Form\Attributes({
+     *     "class":"surrenderChecks__checkbox",
+     *     "disabled": true,
+     *     "checked": true,
+     * })
+     * @Form\Options({
+     *     "label": "There are no open cases associated with this licence",
+     *     "label_options": {
+     *          "label_position": "append"
+     *     }
+     * })
+     */
+    public $openCases = null;
+
+    /**
+     * @Form\Type("Checkbox")
+     * @Form\Attributes({
+     *     "class":"surrenderChecks__checkbox",
+     *     "disabled": true,
+     *     "checked": true,
+     * })
+     * @Form\Options({
+     *     "label": "There are no active bus registrations associated with this licence",
+     *     "label_options": {
+     *          "label_position": "append"
+     *     }
+     * })
+     */
+    public $busRegistrations = null;
+
     /**
      * @Form\Type("Checkbox")
      * @Form\Attributes({
