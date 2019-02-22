8476cac606600a56ed015500ac0aa333fab2869a
Parth <parth.vyas@dvsa.gov.uk>
2019-02-22 13:41:42 +0000

    OLCS-22990: Modify markup for surrender summary box.

--- app/internal/module/Olcs/config/module.config.php
+++ app/internal/module/Olcs/config/module.config.php
@@ -392,7 +392,7 @@ return array(
             'formSubmissionSections' => Olcs\Form\View\Helper\SubmissionSections::class,
             'submissionSectionDetails' => Olcs\View\Helper\SubmissionSectionDetails::class,
             'submissionSectionOverview' => Olcs\View\Helper\SubmissionSectionOverview::class,
-            'surrenderDetails' => Olcs\View\Helper\SurrenderDetails::class,
+            Olcs\View\Helper\SurrenderDetails::class => Olcs\View\Helper\SurrenderDetails::class,
         ],
         'delegators' => array(
             'formElement' => array('Olcs\Form\View\Helper\FormElementDelegatorFactory')
