1d4b709640b4e5afe24a13fe34514c6d19118cf9
Tonci Vidovic <tonci.vidovic@capgemini.com>
2019-03-29 12:01:41 +0000

    OLCS-23972 unit test

--- app/selfserve/test/Permits/src/Data/Mapper/LicencesAvailableTest.php
+++ app/selfserve/test/Permits/src/Data/Mapper/LicencesAvailableTest.php
@@ -320,6 +320,117 @@ class LicencesAvailableTest extends TestCase
                         'selected' => false,
                     ]
                 ]
+            ],
+            'bilateral permits' => [
+                'data' => [
+                    'irhpPermitType' => [
+                        'id' => RefData::IRHP_BILATERAL_PERMIT_TYPE_ID,
+                    ],
+                    'licencesAvailable' => [
+                        'eligibleLicences' => [
+                            'result' => [
+                                0 => [
+                                    'id' => 7,
+                                    'licNo' => 'OB1234567',
+                                    'trafficArea' => 'North East of England',
+                                    'totAuthVehicles' => 12,
+                                    'licenceType' => [
+                                        'description' => 'Standard International',
+                                        'displayOrder' => null,
+                                        'id' => 'ltyp_si',
+                                        'olbsKey' => 'SI',
+                                        'parent' => null,
+                                        'refDataCategoryId' => 'lic_type',
+                                        'version' => 1,
+                                    ],
+                                    'restricted' => false,
+                                    'canMakeEcmtApplication' => true,
+                                    'canMakeBilateralApplication' => true,
+                                    'canMakeMultilateralApplication' => true,
+                                ],
+                                1 => [
+                                    'id' => 70,
+                                    'licNo' => 'OG7654321',
+                                    'trafficArea' => 'Wales',
+                                    'totAuthVehicles' => 12,
+                                    'licenceType' => [
+                                        'description' => 'Standard International',
+                                        'displayOrder' => null,
+                                        'id' => 'ltyp_si',
+                                        'olbsKey' => 'SI',
+                                        'parent' => null,
+                                        'refDataCategoryId' => 'lic_type',
+                                        'version' => 1,
+                                    ],
+                                    'restricted' => false,
+                                    'canMakeEcmtApplication' => true,
+                                    'canMakeBilateralApplication' => false,
+                                    'canMakeMultilateralApplication' => true,
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                'expected' => [
+                    'irhpPermitType' => [
+                        'id' => RefData::IRHP_BILATERAL_PERMIT_TYPE_ID,
+                    ],
+                    'licencesAvailable' => [
+                        'eligibleLicences' => [
+                            'result' => [
+                                0 => [
+                                    'id' => 7,
+                                    'licNo' => 'OB1234567',
+                                    'trafficArea' => 'North East of England',
+                                    'totAuthVehicles' => 12,
+                                    'licenceType' => [
+                                        'description' => 'Standard International',
+                                        'displayOrder' => null,
+                                        'id' => 'ltyp_si',
+                                        'olbsKey' => 'SI',
+                                        'parent' => null,
+                                        'refDataCategoryId' => 'lic_type',
+                                        'version' => 1,
+                                    ],
+                                    'restricted' => false,
+                                    'canMakeEcmtApplication' => true,
+                                    'canMakeBilateralApplication' => true,
+                                    'canMakeMultilateralApplication' => true,
+                                ],
+                                1 => [
+                                    'id' => 70,
+                                    'licNo' => 'OG7654321',
+                                    'trafficArea' => 'Wales',
+                                    'totAuthVehicles' => 12,
+                                    'licenceType' => [
+                                        'description' => 'Standard International',
+                                        'displayOrder' => null,
+                                        'id' => 'ltyp_si',
+                                        'olbsKey' => 'SI',
+                                        'parent' => null,
+                                        'refDataCategoryId' => 'lic_type',
+                                        'version' => 1,
+                                    ],
+                                    'restricted' => false,
+                                    'canMakeEcmtApplication' => true,
+                                    'canMakeBilateralApplication' => false,
+                                    'canMakeMultilateralApplication' => true,
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                'expectedValueOptions' => [
+                    7 => [
+                        'value' => 7,
+                        'label' => 'OB1234567',
+                        'label_attributes' => [
+                            'class' => 'govuk-label govuk-radios__label govuk-label--s',
+                        ],
+                        'hint' => 'Standard International (North East of England)',
+                        'selected' => false,
+                    ]
+                ]
             ]
         ];
     }
