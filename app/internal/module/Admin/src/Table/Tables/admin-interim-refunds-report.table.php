cdc27b2117b17bd20d252ce9eb345cd70158fd4b
James Hodgson <james@tdchodgy.co.uk>
2019-06-05 10:44:29 +0100

    Fix formatting and also show operator name

--- app/internal/module/Admin/src/Table/Tables/admin-interim-refunds-report.table.php
+++ app/internal/module/Admin/src/Table/Tables/admin-interim-refunds-report.table.php
@@ -1,47 +1,53 @@
 <?php
 
 return array(
-    'variables' => array(
+    'variables' => [
         'title' => 'Interim Refunds'
-    ),
-    'settings' => array(
-        'paginate' => array(
-            'limit' => array(
+    ],
+    'settings' => [
+        'paginate' => [
+            'limit' => [
                 'default' => 25,
-                'options' => array(10, 25, 50)
-            ),
-        )
-    ),
-    'attributes' => array(
-    ),
-    'columns' => array(
-        array(
+                'options' => [10, 25, 50]
+            ],
+        ]
+    ],
+    'attributes' => [],
+    'columns' => [
+        [
             'title' => 'Licence Number',
             'formatter' => 'InternalLicenceNumberLink',
-        ),
-        array(
+        ],
+        [
             'title' => 'Operator Name',
-            'name' => 'operator',
-        ),
-        array(
+            'formatter' => function ($data) {
+                return $this->callFormatter(
+                    [
+                        'formatter' => 'OrganisationLink',
+                    ],
+                    $data['licence']
+                );
+            }
+        ],
+        [
             'title' => 'Date Fee Received',
             'name' => 'invoicedDate',
             'formatter' => 'Date'
-        ),
-        array(
+        ],
+        [
             'title' => 'Fee Amount',
             'name' => 'amount',
             'formatter' => 'FeeAmount'
-        ),
+        ],
 
-        array(
+        [
             'title' => 'Date of Refund',
             'name' => 'refundDate',
-        ),
-        array(
+        ],
+        [
             'title' => 'Refund Status',
             'name' => 'feeStatus',
             'formatter' => 'RefData'
-        ),
-    )
+        ],
+    ]
 );
