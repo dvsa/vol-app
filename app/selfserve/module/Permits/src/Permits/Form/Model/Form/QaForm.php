9c98e58b21f30a390b12a2399f8d7c324e4ecd6c
Jonathan Thomas <jonathan@opalise.co.uk>
2019-07-11 22:48:14 +0100

    OLCS-25035 ss permit usage page for short term ecmt updates

--- app/selfserve/module/Permits/src/Permits/Form/Model/Form/QaForm.php
+++ app/selfserve/module/Permits/src/Permits/Form/Model/Form/QaForm.php
@@ -6,10 +6,16 @@ use Zend\Form\Annotation as Form;
 /**
  * @Form\Name("Qa")
  * @Form\Attributes({"method":"post"})
- * @Form\Type("Common\Form\Form")
+ * @Form\Type("Common\Form\QaForm")
  */
 class QaForm
 {
+    /**
+     * @Form\Name("qa")
+     * @Form\Type("Zend\Form\Fieldset")
+     */
+    public $fieldset = null;
+
     /**
      * @Form\Name("Submit")
      * @Form\ComposedObject("Permits\Form\Model\Fieldset\Submit")
