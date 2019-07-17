cb3190f9f5498e52e667b4251a04985333364905
Piotr Filak <piotr.filak@capgemini.com>
2019-07-17 12:32:11 +0100

    OLCS-24992 - SS: View valid permits page for Short Term ECMT permits

---
+++ app/selfserve/module/Permits/src/Permits/Table/Tables/valid-irhp-permits-ecmt-short-term.table.php
@@ -0,0 +1,76 @@
+<?php
+
+use Common\RefData;
+use Common\Util\Escape;
+
+return array(
+    'variables' => array(),
+    'settings' => array(
+        'paginate' => array(
+            'limit' => array(
+                'default' => 10,
+                'options' => array(10, 25, 50)
+            ),
+        ),
+    ),
+    'attributes' => array(),
+    'columns' => array(
+        array(
+            'title' => 'permits.irhp.valid.permits.table.permit-no',
+            'name' => 'permitNumber',
+            'formatter' => function ($row) {
+                return '<b>' . Escape::html($row['permitNumber']) . '</b>';
+            },
+        ),
+        array(
+            'title' => 'permits.irhp.valid.permits.table.application-no',
+            'name' => 'irhpApplication',
+            'stack' => 'irhpPermitApplication->relatedApplication->id',
+            'formatter' => 'StackValue',
+        ),
+        array(
+            'title' => 'permits.irhp.valid.permits.table.emissions-standard',
+            'name' => 'emissionsCategory',
+            'stack' => 'irhpPermitRange->emissionsCategory->description',
+            'formatter' => 'StackValue',
+        ),
+        array(
+            'title' => 'permits.irhp.valid.permits.table.issue-date',
+            'name' => 'issueDate',
+            'formatter' => 'Date',
+        ),
+        array(
+            'title' => 'permits.irhp.valid.permits.table.use-by-date',
+            'name' => 'useByDate',
+            'formatter' => function ($row) {
+                return $this->callFormatter(
+                    [
+                        'name' => 'useByDate',
+                        'formatter' => 'Date',
+                    ],
+                    [
+                        'useByDate' => $row['irhpPermitRange']['irhpPermitStock']['validTo'] ?? null,
+                    ]
+                );
+            }
+        ),
+        array(
+            'title' => 'status',
+            'name' => 'status',
+            'formatter' => function ($row) {
+                return $this->callFormatter(
+                    [
+                        'name' => 'status',
+                        'formatter' => 'RefDataStatus',
+                    ],
+                    [
+                        'status' => [
+                            'id' => RefData::PERMIT_VALID,
+                            'description' => RefData::PERMIT_VALID
+                        ],
+                    ]
+                );
+            }
+        ),
+    )
+);
