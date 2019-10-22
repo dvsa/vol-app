59b3ccb9e0cc3769cbe562aa0735f668cbb32bb9
Ian Lindsay <ian@hemera-business-services.co.uk>
2019-10-22 16:46:03 +0100

    olcs-25971 correct redirection of window closed and permits exhausted messages

--- app/selfserve/module/Permits/src/Permits/View/Helper/IrhpApplicationSection.php
+++ app/selfserve/module/Permits/src/Permits/View/Helper/IrhpApplicationSection.php
@@ -18,6 +18,7 @@ class IrhpApplicationSection extends AbstractHelper
     const ROUTE_YEAR = 'permits/year';
     const ROUTE_STOCK = 'permits/stock';
     const ROUTE_WINDOW_CLOSED = 'permits/window-closed';
+    const ROUTE_PERMITS_EXHAUSTED = 'permits/exhausted';
     const ROUTE_NOT_ELIGIBLE = 'permits/not-eligible';
     const ROUTE_NO_LICENCES = 'permits/no-licences';
     const ROUTE_ADD_LICENCE = 'permits/add-licence';
