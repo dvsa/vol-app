8731b8f1b4c31165a29be5b3e43917cc7814a972
Jonathan Thomas <jonathan@opalise.co.uk>
2020-01-17 14:39:51 +0000

    OLCS-24622 accessibility bug fee page submit page should have role button and not link

--- app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/SubmitAndPay.php
+++ app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/SubmitAndPay.php
@@ -14,7 +14,7 @@ class SubmitAndPay
      * @Form\Attributes({
      *     "class":"govuk-button",
      *     "value":"permits.button.submit-and-pay",
-     *     "role":"link"
+     *     "role":"button"
      * })
      * @Form\Type("Zend\Form\Element\Submit")
      */
