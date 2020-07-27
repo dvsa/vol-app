f2bbedb158a4121a668db1af00f17ab311eab314
Piotr Filak <piotr.filak@capgemini.com>
2020-07-27 12:39:14 +0100

    OLCS-27520 - Accessibility bug : Radio button text shouldn't be in bold

--- app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/Stock.php
+++ app/selfserve/module/Permits/src/Permits/Form/Model/Fieldset/Stock.php
@@ -15,7 +15,6 @@ class Stock
      *     "radios_wrapper_attributes": {"data-module":"radios"}
      * })
      * @Form\Options({
-     *      "label_attributes":{"class": "govuk-label govuk-radios__label govuk-label--s"},
      *      "input_class": "Common\Form\Input\StockInput"
      * })
      * @Form\Type("DynamicRadio")
