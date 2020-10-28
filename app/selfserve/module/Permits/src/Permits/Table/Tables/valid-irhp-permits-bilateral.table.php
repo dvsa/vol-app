9d53ae676e8b2a8acc705513bc333ee17bdb616c
Piotr Filak <piotr.filak@capgemini.com>
2020-10-28 10:19:17 +0000

    OLCS-27670 - SS and Internal - Bilateral - Morocco - updates to issued permits page

--- app/selfserve/module/Permits/src/Permits/Table/Tables/valid-irhp-permits-bilateral.table.php
+++ app/selfserve/module/Permits/src/Permits/Table/Tables/valid-irhp-permits-bilateral.table.php
@@ -61,18 +61,8 @@ return array(
         ),
         array(
             'title' => 'permits.irhp.valid.permits.table.expiry-date',
-            'name' => 'expiryDate',
-            'formatter' => function ($row) {
-                return $this->callFormatter(
-                    [
-                        'name' => 'expiryDate',
-                        'formatter' => 'Date',
-                    ],
-                    [
-                        'expiryDate' => $row['irhpPermitRange']['irhpPermitStock']['validTo'] ?? null,
-                    ]
-                );
-            }
+            'name' => 'ceasedDate',
+            'formatter' => 'Date',
         ),
         array(
             'title' => 'status',
