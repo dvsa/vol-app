1088d9aef47c1be9c50700ec2f8766bacf5228a9
James Hodgson <james.hodgson@dvsa.gov.uk>
2020-11-02 11:51:49 +0000

    VOL-139: Add search to new vehicle remove page

---
+++ app/selfserve/module/Olcs/src/Form/Model/Form/Vehicle/ListVehicleSearch.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Olcs\Form\Model\Form\Vehicle;
+
+use Zend\Form\Annotation as Form;
+
+/**
+ * @Form\Name("vehicle-search")
+ * @Form\Attributes({"method":"get","class":"filters form__search"})
+ * @Form\Type("Common\Form\Form")
+ * @Form\Options({"prefer_form_input_filter": true})
+ */
+class ListVehicleSearch
+{
+    /**
+     * @Form\Name("vehicleSearch")
+     * @Form\Options({
+     *     "label": "licence.vehicle.table.search.label",
+     *     "legend": "licence.vehicle.table.search.legend",
+     * })
+     * @Form\Type("\Common\Form\Elements\Types\VehicleTableSearch")
+     */
+    public $vrmSearch = null;
+}
