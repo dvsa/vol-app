d2f765875dd99bb29b0100285f11520738d51805
James Wragg <james.wragg@dvsa.gov.uk>
2021-01-25 10:19:25 +0000

    VOL-1027: Moved to storing DigitalSignature in cache instead of session

--- app/selfserve/module/Olcs/src/Controller/GdsVerifyController.php
+++ app/selfserve/module/Olcs/src/Controller/GdsVerifyController.php
@@ -2,63 +2,155 @@
 
 namespace Olcs\Controller;
 
-use Olcs\Logging\Log\Logger;
-use Olcs\View\Model\Dashboard;
 use Common\Controller\Lva\AbstractController;
+use Common\Exception\BadRequestException;
+use Dvsa\Olcs\Transfer\Command\GdsVerify\ProcessSignatureResponse;
+use Dvsa\Olcs\Transfer\Query\GdsVerify\GetAuthRequest;
+use Exception;
+use Laminas\Cache\Storage\Adapter\Redis;
+use Laminas\Cache\Storage\StorageInterface;
+use Laminas\Http\Response as HttpResponse;
+use Laminas\Mvc\MvcEvent;
+use Laminas\View\Model\ViewModel;
+use Olcs\DTO\Verify\DigitalSignature;
+use Olcs\Logging\Log\Logger;
+use RuntimeException;
+use ZfcRbac\Exception\UnauthorizedException;
 
 /**
  * GdsVerifyController Controller
  */
 class GdsVerifyController extends AbstractController
 {
+    const CACHE_PREFIX = "verify:";
+
     /**
-     * Display Form to initaite the GDS Verify identification process
+     * @var StorageInterface
+     */
+    private $cache;
+
+    public function onDispatch(MvcEvent $e)
+    {
+        $this->cache = $this->getServiceLocator()->get(Redis::class);
+        return parent::onDispatch($e);
+    }
+
+    /**
+     * Display Form to initiate the GDS Verify identification process
      *
-     * @return \Laminas\View\Model\ViewModel
+     * @return ViewModel
+     * @throws Exception
      */
     public function initiateRequestAction()
     {
-        $types = $this->getTypeOfRequest($this->params()->fromRoute());
-        $form = $this->getServiceLocator()->get('Helper\Form')->createForm('VerifyRequest');
-        $this->handleType($types);
+        $response = $this->handleQuery(GetAuthRequest::create([]));
 
-
-        $response = $this->handleQuery(\Dvsa\Olcs\Transfer\Query\GdsVerify\GetAuthRequest::create([]));
-        if ($response->isOk()) {
-            $result = $response->getResult();
-            if ($result['enabled'] !== true) {
-                throw new \RuntimeException('Verify is currently disabled');
-            }
-            $form->setAttribute('action', $result['url']);
-            $form->get('SAMLRequest')->setValue($result['samlRequest']);
+        if (!$response->isOk()) {
+            throw new Exception("Bad response received from 'GetAuthRequest'", $response->getStatusCode());
         }
+
+        $result = $response->getResult();
+        if ($result['enabled'] !== true) {
+            throw new RuntimeException('Verify is currently disabled');
+        }
+
+        $verifyRequestId = $this->getRootAttributeFromSaml($result['samlRequest'], 'ID');
+        $this->createAndStoreDigitalSignature(
+            $this->getTypeOfRequest($this->params()->fromRoute()),
+            $verifyRequestId
+        );
+        $this->whitelistUserVerifyRequest($verifyRequestId);
+
+        $form = $this->getServiceLocator()->get('Helper\Form')->createForm('VerifyRequest');
+        $form->setAttribute('action', $result['url']);
+        $form->get('SAMLRequest')->setValue($result['samlRequest']);
 
         $this->getServiceLocator()->get('Script')->loadFile('verify-request');
 
-        return new \Laminas\View\Model\ViewModel(array('form' => $form));
+        return new ViewModel(array('form' => $form));
+    }
+
+    /**
+     * Process the request from GDS Verify and forwards to Process Signature Action.
+     *
+     * This is required due to SameSite Cookies and not compromising by converting our cookies to third-party.
+     *
+     * @return HttpResponse
+     * @throws UnauthorizedException|Exception
+     */
+    public function processResponseAction(): HttpResponse
+    {
+        $samlResponse = $this->getRequest()->getPost('SAMLResponse', null);
+        if (is_null($samlResponse)) {
+            throw new UnauthorizedException('Missing samlResponse');
+        }
+
+        $id = $this->getRootAttributeFromSaml($samlResponse, 'InResponseTo');
+        $verifyJourneyKey = $this->buildVerifyJourneyKey($id);
+        if (!empty($this->cache->removeItems([$verifyJourneyKey]))) {
+            throw new UnauthorizedException('Invalid verify journey id');
+        }
+
+        $key = $this->generateSamlKeyFromResponse($samlResponse);
+        $this->cache->setItem($key, $samlResponse);
+
+        return $this->redirect()->toRoute(
+            'verify/process-signature',
+            [],
+            [
+                'query' => [
+                    'ref' => explode(':', $key)[1]
+                ]
+            ]
+        );
     }
 
     /**
      * Process the GDS Verify SAML response
      *
-     * @return \Laminas\Http\Response
+     * @return HttpResponse
+     * @throws BadRequestException
+     * @throws Exception
      */
-    public function processResponseAction()
+    public function processSignatureAction(): HttpResponse
     {
-        $session = new \Olcs\Session\DigitalSignature();
+        $ref = $this->getRequest()->getQuery('ref');
+        if (!$this->validateRedisSamlResponseReferenceKey($ref)) {
+            throw new BadRequestException("Query parameter 'ref' ({$ref}) is not a valid SHA1");
+        }
 
-        Logger::debug("DigitalSignature retrieved:", $session->getArrayCopy());
+        $samlResponse = $this->cache->getItem($this->buildSamlKeyFromRef($ref));
+        $inResponseTo = $this->getRootAttributeFromSaml($samlResponse, 'InResponseTo');
 
-        $applicationId = $session->hasApplicationId() ? $session->getApplicationId() : false;
-        $continuationDetailId = $session->hasContinuationDetailId() ? $session->getContinuationDetailId() : false;
-        $transportManagerApplicationId = $session->hasTransportManagerApplicationId() ? $session->getTransportManagerApplicationId() : false;
-        $licenceId = $session->hasLicenceId() ? $session->getLicenceId() : false;
-        $lva = $session->hasLva() ? $session->getLva() : 'application';
-        $role = $session->hasRole() ? $session->getRole() : null;
+        $signature = $this->retrieveDigitalSignature($inResponseTo);
+        Logger::debug("DigitalSignature retrieved:", $signature->toArray());
 
-        $dto = \Dvsa\Olcs\Transfer\Command\GdsVerify\ProcessSignatureResponse::create(
-            ['samlResponse' => $this->getRequest()->getPost('SAMLResponse')]
-        );
+        $applicationId = $signature->getApplicationId() ?? false;
+        $continuationDetailId = $signature->getContinuationDetailId() ?? false;
+        $transportManagerApplicationId = $signature->getTransportManagerApplicationId() ?? false;
+        $licenceId = $signature->getLicenceId() ?? false;
+        $lva = $signature->getLva() ?? 'application';
+        $role = $signature->getRole() ?? null;
+        $verifyRequestId = $signature->getVerifyId() ?? null;
+
+        if (empty($verifyRequestId)) {
+            throw new BadRequestException("There is no `verifyId` on DigitalSignature");
+        }
+
+        if (empty($inResponseTo)) {
+            throw new BadRequestException("There is no `inResponseTo` in the samlResponse");
+        }
+
+        if ($verifyRequestId !== $inResponseTo) {
+            throw new UnauthorizedException("SamlResponse({$inResponseTo}) does not match SamlRequest({$verifyRequestId})");
+        }
+
+        $this->cache->removeItems([
+            $this->buildActiveUserKey($this->currentUser()->getIdentity()->getUsername()),
+            $this->buildSamlKeyFromRef($ref)
+        ]);
+
+        $dto = ProcessSignatureResponse::create(['samlResponse' => $samlResponse]);
 
         if ($applicationId) {
             $dto->setApplication($applicationId);
@@ -76,12 +168,10 @@ class GdsVerifyController extends AbstractController
             $dto->setLicence($licenceId);
         }
 
-        $session->getManager()->getStorage()->clear(\Olcs\Session\DigitalSignature::SESSION_NAME);
         $response = $this->handleCommand($dto);
         if (!$response->isOk()) {
             $this->getServiceLocator()->get('Helper\FlashMessenger')->addErrorMessage('undertakings_not_signed');
         }
-
 
         if ($applicationId && !$transportManagerApplicationId) {
             return $this->redirect()->toRoute(
@@ -119,39 +209,167 @@ class GdsVerifyController extends AbstractController
             );
         }
 
-        throw new \RuntimeException('There was an error processing the signature response');
+        throw new RuntimeException('There was an error processing the signature response');
     }
 
     /**
-     * verificationType
+     * Create a DigitalSignature and store in redis
      *
      * @param array $types
+     * @param string $verifyId
+     * @throw \RuntimeException
      */
-    private function handleType(array $types): void
+    private function createAndStoreDigitalSignature(array $types, string $verifyId)
     {
-        $session = new \Olcs\Session\DigitalSignature();
-
         if (empty($types)) {
-            throw new \RuntimeException(
+            throw new RuntimeException(
                 'An entity identifier needs to be present, this is used to to calculate where'
                 . ' to return to after completing Verify'
             );
         }
 
-        foreach ($types as $key => $value) {
-            $methodName = 'set' . ucfirst($key);
-            if (method_exists($session, $methodName)) {
-                call_user_func([$session, $methodName], $value);
-            }
-        }
-
-        Logger::debug("DigitalSignature created:", $session->getArrayCopy());
+        $types[DigitalSignature::KEY_VERIFY_ID] = $verifyId;
+        $digitalSignature = new DigitalSignature($types);
+        $this->cache->setItem($this->buildDigitalSignatureKey($verifyId), $digitalSignature->toArray());
+        Logger::debug("DigitalSignature created:", $digitalSignature->toArray());
     }
 
+    /**
+     * @param $params
+     * @return array
+     */
     private function getTypeOfRequest($params): array
     {
         // remove controller and action keys from params
-        $types = array_diff_assoc($params, ['controller' => self::class, 'action' => 'initiate-request']);
-        return $types;
+        return array_diff_assoc($params, ['controller' => self::class, 'action' => 'initiate-request']);
+    }
+
+    /**
+     * Generate cache key for the samlResponse to be stored under
+     *
+     * @param string $samlResponse
+     * @return string
+     */
+    private function generateSamlKeyFromResponse(string $samlResponse): string
+    {
+        $key = sha1($samlResponse);
+        return static::CACHE_PREFIX . "samlResponse:" . $key;
+    }
+
+    /**
+     * Build cache key for storing/retrieving SAML response based on a ref
+     *
+     * @param string $ref
+     * @return string
+     */
+    private function buildSamlKeyFromRef(string $ref): string
+    {
+        return static::CACHE_PREFIX . "samlResponse:" . $ref;
+    }
+
+    /**
+     * Build cache key to whitelist this verify journey
+     *
+     * @param string $id
+     * @return string
+     */
+    private function buildVerifyJourneyKey(string $id): string
+    {
+        return static::CACHE_PREFIX . "activeJourneys:" . $id;
+    }
+
+    /**
+     * Build cache key for whitelisting user for verify
+     *
+     * @param string $username
+     * @return string
+     */
+    private function buildActiveUserKey(string $username): string
+    {
+        return static::CACHE_PREFIX . "activeUsers:" . $username;
+    }
+
+    /**
+     * Build cache key for storing/retrieving a digital signature
+     * @param string $verifyId
+     * @return string
+     */
+    private function buildDigitalSignatureKey(string $verifyId): string
+    {
+        return static::CACHE_PREFIX . "digitalSignature:" . $verifyId;
+    }
+
+    /**
+     * Extract an attribute from a SAML XML String Document
+     *
+     * @param string $samlString
+     * @param string $attributeName
+     * @return string
+     * @throws Exception
+     */
+    protected function getRootAttributeFromSaml(string $samlString, string $attributeName): string
+    {
+        $samlString = base64_decode($samlString);
+        $samlString = simplexml_load_string($samlString);
+
+        if ($samlString === false) {
+            throw new Exception("Unable to parse SAML XML String");
+        }
+
+        $attributes = (array)$samlString->attributes();
+
+        if (!array_key_exists($attributeName, $attributes['@attributes'])) {
+            throw new Exception("SAML XML String Document does not contain attribute '{$attributeName}' in the root.");
+        }
+
+        return (string)$attributes['@attributes'][$attributeName];
+    }
+
+    /**
+     * Whitelist this users journey for verify
+     *
+     * @param string $verifyId
+     */
+    protected function whitelistUserVerifyRequest(string $verifyId): void
+    {
+        $activeUserKey = $this->buildActiveUserKey($this->currentUser()->getIdentity()->getUsername());
+        $previousVerifyId = $this->cache->getItem($activeUserKey);
+        if (!is_null($previousVerifyId)) {
+            $this->cache->removeItems([
+                    $this->buildVerifyJourneyKey($previousVerifyId),
+                    $activeUserKey
+                ]
+            );
+        }
+
+        $this->cache->addItems([
+            $activeUserKey => $verifyId,
+            $this->buildVerifyJourneyKey($verifyId) => true
+        ]);
+    }
+
+    private function validateRedisSamlResponseReferenceKey($key): bool
+    {
+        // Essentially, we verify the reference key is a SHA1.
+        return (bool)preg_match('/^[0-9a-f]{40}$/i', $key);
+    }
+
+    /**
+     * Retrieve a digital signature from redis based on a verify request id
+     *
+     * @param string $inResponseTo
+     * @return DigitalSignature
+     * @throws Exception
+     */
+    protected function retrieveDigitalSignature(string $inResponseTo): DigitalSignature
+    {
+        $signatureRedisKey = $this->buildDigitalSignatureKey($inResponseTo);
+        $signature = $this->cache->getItem($signatureRedisKey);
+        if (empty($signature)) {
+            throw new Exception("DigitalSignatureRedisKey '{$signatureRedisKey}' not found in redis.");
+        }
+
+        $this->cache->removeItem($signatureRedisKey);
+        return new DigitalSignature($signature);
     }
 }
