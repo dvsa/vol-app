c8723cfd2940eaf7706779c6ac86f32492b0364b
James Wragg <james.wragg@dvsa.gov.uk>
2021-02-23 11:08:01 +0000

    VOL-1050: Integrate OCRS checkox with new vehicle management list page

---
+++ app/selfserve/module/Olcs/src/Form/Model/Form/Vehicle/OCRSOptIn.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Olcs\Form\Model\Form\Vehicle;
+
+use Laminas\Form\Annotation as Form;
+
+/**
+ * @Form\Name("ocrs-opt-in")
+ * @Form\Type("Common\Form\Form")
+ */
+class OCRSOptIn
+{
+    /**
+     * @Form\Type("OlcsCheckbox")
+     * @Form\Attributes({"class":"govuk-checkboxes__item"})
+     * @Form\Options({
+     *     "label":"licence.vehicle.list.form.orcs.checkbox.label",
+     *     "label_attributes": {"class": "form-control form-control--checkbox form-control--confirm"},
+     *     "checked_value":"Y",
+     *     "unchecked_value":"N",
+     * })
+     * @Form\Validator({
+     *     "name":"Laminas\Validator\InArray",
+     *     "options": {
+     *         "haystack": {
+     *             "Y", "N"
+     *         },
+     *         "strict": true,
+     *         "messages": {
+     *             Laminas\Validator\InArray::NOT_IN_ARRAY: "licence.vehicle.list.form.orcs.checkbox.invalid-value"
+     *         }
+     *     }
+     * })
+     */
+    public $ocrsCheckbox = null;
+
+    /**
+     * @Form\Attributes({"type":"submit","class":"action--secondary large"})
+     * @Form\Options({"label":"licence.vehicle.list.form.orcs.submit.label"})
+     * @Form\Type("\Laminas\Form\Element\Button")
+     */
+    public $submit = null;
+}
