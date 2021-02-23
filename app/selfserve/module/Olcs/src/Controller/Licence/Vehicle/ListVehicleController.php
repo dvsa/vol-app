74331689ad9c903e15a6fea0d1933641856c5102
James Wragg <james.wragg@dvsa.gov.uk>
2021-02-23 11:22:56 +0000

    Fix: VOL-1050

--- app/selfserve/module/Olcs/src/Controller/Licence/Vehicle/ListVehicleController.php
+++ app/selfserve/module/Olcs/src/Controller/Licence/Vehicle/ListVehicleController.php
@@ -139,7 +139,7 @@ class ListVehicleController
                 'vrm' => $urlQueryData['vehicleSearch'][AbstractInputSearch::ELEMENT_INPUT_NAME] ?? null,
             ]));
 
-            $shareVehicleInfoState = (($licence['organisation']['confirmShareVehicleInfo'] ?? 'N') === 'Y');
+            $shareVehicleInfoState = $licence['organisation']['confirmShareVehicleInfo'];
 
             $response = $this->renderHtmlResponse($request, [
                 'title' => $this->isSearchResultsPage($request) ? 'licence.vehicle.list.search.header' : 'licence.vehicle.list.header',
