7727b7172b50b8472f908118f79451ec45b3e1fc
James Wragg <james.wragg@dvsa.gov.uk>
2022-05-17 11:39:46 +0100

    VOL-3110: When creating a user, usernames must conform to to stricter validation

--- app/selfserve/module/Olcs/src/Form/Model/Fieldset/UserRegistration.php
+++ app/selfserve/module/Olcs/src/Form/Model/Fieldset/UserRegistration.php
@@ -20,7 +20,8 @@ class UserRegistration
      * @Form\Attributes({"id":"username","placeholder":"","class":"medium", "required":false})
      * @Form\Type("Text")
      * @Form\Filter({"name":"Laminas\Filter\StringTrim"})
-     * @Form\Validator({"name":"Dvsa\Olcs\Transfer\Validators\Username"})
+     * @Form\Filter({"name":"Laminas\Filter\StringToLower"})
+     * @Form\Validator({"name":"Dvsa\Olcs\Transfer\Validators\UsernameCreate"})
      */
     public $loginId = null;
 
