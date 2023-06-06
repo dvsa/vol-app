c3882ac0ce7179259359f4dba8a3296fe1245549
Andy Newton <andy.newton@dvsa.gov.uk>
2023-06-06 10:51:25 +0100

    AddressFactory returning correct type now

--- app/internal/module/Olcs/src/View/Helper/AddressFactory.php
+++ app/internal/module/Olcs/src/View/Helper/AddressFactory.php
@@ -32,6 +32,9 @@ class AddressFactory implements \Laminas\ServiceManager\FactoryInterface
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null): Address
     {
+        if (method_exists($container, 'getServiceLocator') && $container->getServiceLocator()) {
+            $container = $container->getServiceLocator();
+        }
         $addressFormatter = $container->get(FormatterPluginManager::class)->get(AddressFormatter::class);
         return new Address($addressFormatter);
     }
