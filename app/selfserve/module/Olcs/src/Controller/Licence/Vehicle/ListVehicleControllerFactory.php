8e4902b4f719f23aefc6b8238ffd8b70f86d23e2
Bal Hayre <baljinder.hayre@dvsa.gov.uk>
2023-06-09 15:35:48 +0100

    VOL-3711(olcs-selfserve): alteration of Permits/controllerFactories and any other factories left from OLCS directory

--- app/selfserve/module/Olcs/src/Controller/Licence/Vehicle/ListVehicleControllerFactory.php
+++ app/selfserve/module/Olcs/src/Controller/Licence/Vehicle/ListVehicleControllerFactory.php
@@ -42,7 +42,7 @@ class ListVehicleControllerFactory implements FactoryInterface
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null): Dispatcher
     {
-        if (method_exists($container, 'getServiceLocator')) {
+        if (method_exists($container, 'getServiceLocator') && $container->getServiceLocator()) {
             $sl = $container->getServiceLocator();
         } else {
             $sl = $container;
