26fc9d04559992b6a291da18ea67ae2c3bf1c704
Bal Hayre <baljinder.hayre@dvsa.gov.uk>
2023-06-13 15:07:54 +0100

    VOL-3711(olcs-selfserve): Changes from $mainServiceLocator to $container - code review

--- app/selfserve/module/Olcs/src/Controller/Cookie/SettingsControllerFactory.php
+++ app/selfserve/module/Olcs/src/Controller/Cookie/SettingsControllerFactory.php
@@ -31,12 +31,12 @@ class SettingsControllerFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null) : SettingsController
     {
         if (method_exists($container, 'getServiceLocator') && $container->getServiceLocator()) {
-            $mainServiceLocator = $container->getServiceLocator();
+            $container = $container->getServiceLocator();
         }
         return new SettingsController(
-            $mainServiceLocator->get('CookieCurrentPreferencesProvider'),
-            $mainServiceLocator->get('CookieSetCookieArrayGenerator'),
-            $mainServiceLocator->get('CookiePreferencesFactory')
+            $container->get('CookieCurrentPreferencesProvider'),
+            $container->get('CookieSetCookieArrayGenerator'),
+            $container->get('CookiePreferencesFactory')
         );
     }
 }
