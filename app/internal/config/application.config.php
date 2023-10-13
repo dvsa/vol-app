4aba9459195f255dfd71aaba4965d7482a7f53ce
Saul Wilcox <saul.wilcox@dvsa.gov.uk>
2023-10-13 09:53:09 +0000

    VOL-4502: Config - Add laminas-config-cloud-parameters module

--- app/internal/config/application.config.php
+++ app/internal/config/application.config.php
@@ -2,6 +2,7 @@
 $config = array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
+        'Dvsa\LaminasConfigCloudParameters',
         'Dvsa\Olcs\Auth',
         'Dvsa\Olcs\Utils',
         'Dvsa\Olcs\Transfer',
