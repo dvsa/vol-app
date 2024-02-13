2493c0e423dcec3617011b0987806b8e4e66ed5a
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2024-02-13 11:51:39 +0000

    chore: add `prettier` and `lint-staged` (#9)

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,11 +1,11 @@
-import type {UserConfig} from '@commitlint/types';
+import type { UserConfig } from "@commitlint/types";
 import * as fs from "fs";
 
 const Configuration: UserConfig = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'scope-enum': [2, 'always', fs.readdirSync('app').filter((file) => fs.statSync(`app/${file}`).isDirectory())],
-  }
+    "scope-enum": [2, "always", fs.readdirSync("app").filter((file) => fs.statSync(`app/${file}`).isDirectory())],
+  },
 };
 
 module.exports = Configuration;
