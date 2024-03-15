9ab47d18712ca4aa71408f9652ccc4ecd4f08eb0
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2024-03-15 16:00:34 +0000

    fix: tweak secret template to work for production (dvsa/olcs-internal#120)

--- app/internal/config/autoload/config-paramater.global.php
+++ app/internal/config/autoload/config-paramater.global.php
@@ -9,12 +9,19 @@ use Dvsa\Olcs\Transfer\Query\CacheableMediumTermQueryInterface;
 
 $environment = getenv('ENVIRONMENT_NAME');
 
+// This logic will be moved to environment variables with the migration to ECS.
+$isProduction = strtoupper($environment) === 'APP';
+$isProductionAccount = in_array(strtoupper($environment), ['INT', 'PP', 'APP']);
+
 $providers = [];
 
 if (!empty($environment)) {
+    // The `int` environment is actually `nduint` in AWS Secrets Manager.
+    $secretsManagerEnvironmentName = ($environment === 'int' ? 'nduint' : $environment);
+
     $providers = [
         SecretsManager::class => [
-            sprintf('DEVAPP%s-BASE-SM-APPLICATION-INTERNAL', strtoupper($environment)),
+            sprintf('%sAPP%s-BASE-SM-APPLICATION-INTERNAL', ($isProductionAccount ? "" : "DEV"), ($isProduction ? "" : strtoupper($secretsManagerEnvironmentName))),
         ],
         ParameterStore::class => [
             sprintf('/applicationparams/%s/', strtolower($environment)),
