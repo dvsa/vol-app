fb1fb0c91281953bd9dde2a6d90bc03c118ac8f8
Wade Womersley <155439365+wadedvsa@users.noreply.github.com>
2024-03-20 11:58:28 +0000

    feat: Allow conversation status filtering (dvsa/olcs-internal#125)
    * feat: Allow conversation status filtering
    
    * chore: olcs-transfer fump

--- app/internal/module/Olcs/src/Controller/Messages/AbstractConversationListController.php
+++ app/internal/module/Olcs/src/Controller/Messages/AbstractConversationListController.php
@@ -8,12 +8,16 @@ use Laminas\View\Model\ViewModel;
 use Olcs\Controller\AbstractInternalController;
 use Olcs\Controller\Interfaces\LeftViewProvider;
 use Olcs\Controller\Interfaces\MessagingControllerInterface;
+use Olcs\Form\Model\Form\ConversationFilter;
 
 abstract class AbstractConversationListController extends AbstractInternalController implements LeftViewProvider, ToggleAwareInterface, MessagingControllerInterface
 {
     protected $tableName = 'conversations-list';
     protected $tableViewTemplate = 'pages/table';
     protected $toggleConfig = ['default' => [FeatureToggle::MESSAGING]];
+    protected $inlineScripts = [
+        'indexAction' => ['forms/filter'],
+    ];
 
     /**
      * Get left view
@@ -22,8 +26,12 @@ abstract class AbstractConversationListController extends AbstractInternalContro
      */
     public function getLeftView()
     {
+        $filterForm = $this->getForm(ConversationFilter::class);
+        $filterForm->setData($this->getRequest()->getQuery());
+
         $view = new ViewModel(['navigationId' => $this->navigationId]);
         $view->setTemplate('sections/messages/partials/left');
+        $this->placeholder()->setPlaceholder('tableFilters', $filterForm);
 
         return $view;
     }
@@ -39,5 +47,18 @@ abstract class AbstractConversationListController extends AbstractInternalContro
         $view->setTemplate('sections/application/partials/right');
 
         return $view;
+    }
+
+    public function modifyListQueryParameters($parameters)
+    {
+        $filterForm = $this->getForm(ConversationFilter::class);
+        $filterForm->setData($this->getRequest()->getQuery());
+
+        $status = $filterForm->get('status')->getValue();
+        if ($filterForm->isValid() && $status !== '') {
+            $parameters['statuses'] = [$status];
+        }
+
+        return parent::modifyListQueryParameters($parameters);
     }
 }
