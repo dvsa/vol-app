5afb98bb1931b9dc9b75409f887987582b7bc32b
chris lawrence <clorro@amazon.co.uk>
2024-04-18 14:11:16 +0100

    feature(terraform): adding efs configuration for api service

--- infra/terraform/modules/service/ecs.tf
+++ infra/terraform/modules/service/ecs.tf
@@ -61,3 +61,61 @@ module "ecs_service" {
   security_group_ids    = var.services[each.key].security_group_ids
   subnet_ids            = var.services[each.key].subnet_ids
 }
+
+module "efs" {
+  for_each = var.services
+
+  source  = "terraform-aws-modules/efs/aws"
+  version = "1.6.2"
+
+  name           = "vol-app-${var.environment}-${each.key}-efs"
+  creation_token = "vol-app-${var.environment}-${each.key}-token"
+  encrypted      = true
+
+  attach_policy                      = true
+  bypass_policy_lockout_safety_check = false
+  policy_statements = [
+    {
+      sid = "vol-app-${var.environment}-${each.key}-policy"
+      actions = [
+        "elasticfilesystem:ClientMount",
+        "elasticfilesystem:ClientWrite",
+      ]
+      principals = [
+        {
+          type        = "Service"
+          identifiers = ["ecs-tasks.amazonaws.com"]
+        }
+      ]
+    }
+  ]
+
+  # mount_targets              = { for k, v in zipmap(module.vpc.fgms_vpc_az, var.services[each.key].subnet_ids) : k => { subnet_id = v } }
+  # security_group_description = "${var.services[each.key]} EFS security group"
+  # security_group_vpc_id      = module.vpc.fgms_vpc_id
+  # security_group_rules = {
+  #   vpc = {
+  #     # relying on the defaults provdied for EFS/NFS (2049/TCP + ingress)
+  #     description = "NFS ingress from VPC private subnets"
+  #     cidr_blocks = module.vpc.fgms_private_subnets_cidr
+  #   }
+  # }
+
+  access_points = {
+    data_cache = {
+      root_directory = {
+        path = "/${var.services[each.key]}/data/cache"
+        creation_info = {
+          owner_gid   = 1001
+          owner_uid   = 1001
+          permissions = "755"
+        }
+      }
+    }
+  }
+
+  enable_backup_policy = false
+
+  create_replication_configuration = false
+
+}
