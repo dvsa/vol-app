929fc623ef7f4af599669d623291ea4a3924f208
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2024-04-25 10:53:05 +0100

    docs: tweak GitHub OIDC subject comment

--- infra/terraform/accounts/nonprod/main.tf
+++ infra/terraform/accounts/nonprod/main.tf
@@ -30,7 +30,7 @@ module "account" {
 
   github_oidc_subjects = concat(
     [
-      "dvsa/vol-app:ref:refs/heads/main",         # `.github/workflows/docker.yaml`
+      "dvsa/vol-app:ref:refs/heads/main",         # `.github/workflows/docker.yaml` & `.github/workflows/assets.yaml`.
       "dvsa/vol-app:environment:account-nonprod", # `.github/workflows/deploy-account.yaml`.
     ],
     [
