1caf3d4112ac7d32484f3c4fd46e48257615d815
C MARSTON <chris.marston@dvsa.gov.uk>
2024-06-20 16:01:46 +0100

    feat(terraform): vol5504 add single batch job

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -240,12 +240,8 @@ module "service" {
       job_config = [{
         command = "/var/www/html/vendor/bin/laminas --container=/var/www/html/config/container-cli.php queue:process-queue"
         image   = "054614622558.dkr.ecr.eu-west-1.amazonaws.com/vol-app/cli:latest"
+        memory              = 2048
       }]
-      memory              = 2048
-      subnets             = data.aws_subnets.this["API"].ids
-      security_group_ids  = [
-        data.aws_security_group.this["API"].id
-      ]
     }
   }
 }
