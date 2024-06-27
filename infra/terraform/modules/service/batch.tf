601b65c198228fb38609b9c8ba58938ba2ee9288
C MARSTON <chris.marston@dvsa.gov.uk>
2024-06-27 15:57:07 +0100

    feat(terraform): vol5504 add single batch job

--- infra/terraform/modules/service/batch.tf
+++ infra/terraform/modules/service/batch.tf
@@ -104,9 +104,13 @@ module "batch" {
         fargatePlatformConfiguration = {
           platformVersion = "LATEST"
         },
-/*        resourceRequirements = [
-          { type = "VCPU", value = 1 },
-          { type = "MEMORY", value = each.value["memory"] }
+        resourceRequirements = [
+          { type = "VCPU" 
+            value = 1 
+          },
+          { type = "MEMORY" 
+            value = each.value["memory"] 
+          }
         ],
         jobRoleArn = "arn:aws:iam::054614622558:role/vol-app-dev-api-service-20240418150301367500000003"
         #### CW Log group to be created later
