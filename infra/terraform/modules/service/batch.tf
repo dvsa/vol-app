62ff4d12dcbbaca7ce24ab953de4b8de9f18f4a2
C MARSTON <chris.marston@dvsa.gov.uk>
2024-07-12 13:29:25 +0100

    feat(terraform): vol5506 fix variables

--- infra/terraform/modules/service/batch.tf
+++ infra/terraform/modules/service/batch.tf
@@ -1,6 +1,6 @@
 locals {
 
-  job_definitions = { for job in var.batch.job : job.name => {
+  jobs = { for job in var.batch.job : job.name => {
     name = job.name 
     type = "container"
     propagate_tags        = true
