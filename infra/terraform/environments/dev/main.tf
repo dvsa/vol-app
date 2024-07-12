dd9396391f65f891d2fe37b7d09d72c656021c4a
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2024-07-12 10:46:09 +0100

    Revert "fix(terraform): add new job"
    This reverts commit 11f374ab90dd973eb8da5da308166b4404d7368d.

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -251,11 +251,6 @@ module "service" {
         commands = ["queue:process-queue"],
         memory   = 1024,
       },
-      {
-        name     = "another-command",
-        commands = ["another:command"],
-        memory   = 1024,
-      },
     ]
   }
 }
