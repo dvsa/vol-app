0594180da8d14e2c490c333d1c9246783cc58c48
Mathew Beales <matt.beales@dvsa.gov.uk>
2024-08-01 16:36:44 +0100

    feat(terraform): add if statement to scheduled jobs

--- infra/terraform/modules/service/batch.tf
+++ infra/terraform/modules/service/batch.tf
@@ -76,12 +76,12 @@ locals {
   } }
 
   schedules = { for job in var.batch.jobs : job.name => {
-    if job.schedule != "" then
+    if job.schedule != "" {
       description         = "Schedule for ${job.name}"
       schedule_expression = job.schedule
       arn                 = "arn:aws:scheduler:::aws-sdk:batch:submitJob"
       input               = jsonencode({ "jobName" : "${job.name}", "jobQueue" : "vol-app-${var.environment}-default", "jobDefinition" : "arn:aws:batch:eu-west-1:054614622558:job-definition/${job.name}"})
-    endif
+    }
   } }
 }
 
