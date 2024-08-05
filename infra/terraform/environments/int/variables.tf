1cb067f99f68e47df9ad50df6e42931ce2129e8f
cmarstondvsa <82583107+cmarstondvsa@users.noreply.github.com>
2024-08-05 17:19:07 +0100

    feat(terraform): vol5237 create int environment (#214)
    * feat(terraform): vol5237 create int environment
    
    * feat(terraform): vol5237 create int environment
    
    * feat(terraform): vol5237 create int environment

---
+++ infra/terraform/environments/int/variables.tf
@@ -0,0 +1,28 @@
+variable "assets_version" {
+  type        = string
+  description = "The version of the assets"
+}
+
+variable "api_image_tag" {
+  type        = string
+  description = "The tag of the API image to deploy"
+  default     = "latest"
+}
+
+variable "selfserve_image_tag" {
+  type        = string
+  description = "The tag of the selfserve image to deploy"
+  default     = "latest"
+}
+
+variable "internal_image_tag" {
+  type        = string
+  description = "The tag of the internal image to deploy"
+  default     = "latest"
+}
+
+variable "cli_image_tag" {
+  type        = string
+  description = "The tag of the cli image to deploy"
+  default     = "latest"
+}
