df6456f4f41dcda62faaa24a77b48ee7f0dc431c
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2024-08-12 12:16:14 +0100

    fix(terraform): remove specific CloudFront S3 policy condition

--- infra/terraform/modules/service/cdn.tf
+++ infra/terraform/modules/service/cdn.tf
@@ -223,12 +223,6 @@ data "aws_iam_policy_document" "s3_policy" {
       type        = "Service"
       identifiers = ["cloudfront.amazonaws.com"]
     }
-
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceArn"
-      values   = [module.cloudfront.cloudfront_distribution_arn]
-    }
   }
 }
 
