d86c063788454090c0b42e01e56eb4a6527ae321
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2024-08-12 15:06:08 +0100

    fix(terraform): pass bucket id instead of ARN

--- infra/terraform/modules/account/main.tf
+++ infra/terraform/modules/account/main.tf
@@ -20,6 +20,6 @@ data "aws_iam_policy_document" "s3_policy" {
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = module.assets[0].s3_bucket_arn
+  bucket = module.assets[0].s3_bucket_id
   policy = data.aws_iam_policy_document.s3_policy.json
 }
