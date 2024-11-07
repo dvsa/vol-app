1de9af35213a1a0e6509105fd33c4a8285b16998
William Shelley <william.shelley@dvsa.gov.uk>
2024-11-07 13:46:37 +0000

    feat: 5261 provision prod account (#431)
    * feat: added prod account tf
    
    * fix: remove envs before creation
    
    * fix: enable prod account planning
    
    * fix: re-added envs
    
    * fix: account reference
    
    * fix: provider version
    
    * fix: alter provider to fix clash
    
    ---------
    
    Co-authored-by: laurence.barker@dvsa.gov.uk <laurence.barker@dvsa.gov.uk>

---
+++ infra/terraform/accounts/prod/remote-state.tf
@@ -0,0 +1,56 @@
+# This imports the remote state so that it can be maintained by the account module.
+module "account-remote-state" {
+  source = "../../modules/remote-state"
+
+  identifier = "vol-app"
+}
+
+import {
+  to = module.account-remote-state.module.dynamodb_table.aws_dynamodb_table.this[0]
+  id = "vol-app-146997448015-terraform-state-lock"
+}
+
+import {
+  to = module.account-remote-state.module.s3[0].aws_s3_bucket.this[0]
+  id = "vol-app-146997448015-terraform-state"
+}
+
+import {
+  to = module.account-remote-state.module.s3[0].aws_s3_bucket_policy.this[0]
+  id = "vol-app-146997448015-terraform-state"
+}
+
+import {
+  to = module.account-remote-state.module.s3[0].aws_s3_bucket_lifecycle_configuration.this[0]
+  id = "vol-app-146997448015-terraform-state"
+}
+
+import {
+  to = module.account-remote-state.module.s3[0].aws_s3_bucket_ownership_controls.this[0]
+  id = "vol-app-146997448015-terraform-state"
+}
+
+import {
+  to = module.account-remote-state.module.s3[0].aws_s3_bucket_server_side_encryption_configuration.this[0]
+  id = "vol-app-146997448015-terraform-state"
+}
+
+import {
+  to = module.account-remote-state.module.s3[0].aws_s3_bucket_public_access_block.this[0]
+  id = "vol-app-146997448015-terraform-state"
+}
+
+import {
+  to = module.account-remote-state.module.s3[0].aws_s3_bucket_versioning.this[0]
+  id = "vol-app-146997448015-terraform-state"
+}
+
+import {
+  to = module.account-remote-state.module.dynamodb_state_lock_policy[0].aws_iam_policy.policy[0]
+  id = "arn:aws:iam::146997448015:policy/vol-app-146997448015-terraform-state-lock-policy"
+}
+
+import {
+  to = module.account-remote-state.module.s3_state_policy[0].aws_iam_policy.policy[0]
+  id = "arn:aws:iam::146997448015:policy/vol-app-146997448015-terraform-state-policy"
+}
