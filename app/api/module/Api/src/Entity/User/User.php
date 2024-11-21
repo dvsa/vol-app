fd9f8a516ae7287b38d6d88f8626a9b741141540
Ian Lindsay <6673081+ilindsay@users.noreply.github.com>
2024-11-21 16:32:02 +0000

    fix: reinstate transport manager ability to have TC role (#465)

--- app/api/module/Api/src/Entity/User/User.php
+++ app/api/module/Api/src/Entity/User/User.php
@@ -65,6 +65,7 @@ class User extends AbstractUser implements OrganisationProviderInterface
             RoleEntity::ROLE_OPERATOR_TM,
         ],
         self::USER_TYPE_TRANSPORT_MANAGER => [
+            RoleEntity::ROLE_OPERATOR_TC,
             RoleEntity::ROLE_OPERATOR_ADMIN,
             RoleEntity::ROLE_OPERATOR_USER,
             RoleEntity::ROLE_OPERATOR_TM,
@@ -103,6 +104,7 @@ class User extends AbstractUser implements OrganisationProviderInterface
             self::PERMISSION_TM => [RoleEntity::ROLE_OPERATOR_TM],
         ],
         self::USER_TYPE_TRANSPORT_MANAGER => [
+            self::PERMISSION_TC => [RoleEntity::ROLE_OPERATOR_TC],
             self::PERMISSION_ADMIN => [RoleEntity::ROLE_OPERATOR_ADMIN],
             self::PERMISSION_USER => [RoleEntity::ROLE_OPERATOR_USER],
             self::PERMISSION_TM => [RoleEntity::ROLE_OPERATOR_TM],
