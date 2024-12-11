656118f3ed20c5c432179c0ab0a1179a9ec0b031
William Shelley <william.shelley@dvsa.gov.uk>
2024-12-11 16:18:10 +0000

    fix: added missing var

--- infra/terraform/environments/int/variables.tf
+++ infra/terraform/environments/int/variables.tf
@@ -22,3 +22,8 @@ variable "cli_image_tag" {
   type        = string
   description = "The tag of the cli image to deploy"
 }
+
+variable "search_image_tag" {
+  type        = string
+  description = "The tag of the search image to deploy"
+}
