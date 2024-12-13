13f85f8d194ad7172ef77ef15cc452af6167ac83
Andrew Newton <andy.newton@dvsa.gov.uk>
2024-12-13 17:18:26 +0000

    feat: liquibase runs in containerised environments via aws batch. WIP (#496)
    * feat: liquibase runs in containerised environments via aws batch. WIP
    
    * feat: liquibase container work

---
+++ infra/docker/liquibase/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+cd /liquibase/changelog
+
+LIQUIBASE_OPTS="--driver=com.mysql.cj.jdbc.Driver \
+  --classpath=/liquibase/changelog/mysql-connector-java-8.0.21/mysql-connector-java-8.0.21.jar \
+  --url=jdbc:mysql://${DB_HOST}:${DB_PORT}/${DB_NAME} \
+  --username=${DB_USER} \
+  --password=${DB_PASSWORD} \
+  --changelog-file=changesets/OLCS.xml \
+  --log-level=info"
+
+if [[ "$1" == "--dry-run" ]]; then
+    echo "Running in dry-run mode - showing pending changes:"
+    liquibase ${LIQUIBASE_OPTS} status --verbose
+    liquibase ${LIQUIBASE_OPTS} update-sql
+else
+    echo "Running migrations..."
+    liquibase ${LIQUIBASE_OPTS} update
+fi
