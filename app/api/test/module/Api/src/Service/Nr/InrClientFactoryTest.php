a2b2d843361db6ea890d61c2fa42c808bd6a01d9
Ahmed Mohamed <ahmed.mohamed2@dvsa.gov.uk>
2025-01-02 15:18:58 +0000

    feat: pair coding oauth2 with shaun

--- app/api/test/module/Api/src/Service/Nr/InrClientFactoryTest.php
+++ app/api/test/module/Api/src/Service/Nr/InrClientFactoryTest.php
@@ -2,12 +2,14 @@
 
 namespace Dvsa\OlcsTest\Api\Service\Nr;
 
+use Dvsa\Olcs\Api\Service\AccessToken\Provider;
 use Dvsa\Olcs\Api\Service\Nr\InrClient;
 use Dvsa\Olcs\Api\Service\Nr\InrClientFactory;
 use Dvsa\Olcs\Api\Service\Nr\InrClientInterface;
 use Dvsa\Olcs\Utils\Client\ClientAdapterLoggingWrapper;
 use Laminas\Http\Client as RestClient;
 use Laminas\Http\Client\Adapter\Curl;
+use Laminas\ServiceManager\ServiceLocatorInterface;
 use Mockery as m;
 use Mockery\Adapter\Phpunit\MockeryTestCase as TestCase;
 use Psr\Container\ContainerInterface;
@@ -27,18 +29,31 @@ class InrClientFactoryTest extends TestCase
 
     public function testCreateService()
     {
+        $oauth2 = 'options';
         $config = [
             'nr' => [
                 'inr_service' => [
                     'uri' => 'http://testServiceAddress',
                     'adapter' => Curl::class,
                     'options' => []
-                ]
+                ],
+                'oauth2' => $oauth2
+
             ]
         ];
 
-        $mockSl = m::mock(ContainerInterface::class);
+        $restClient = m::mock(RestClient::class)
+            ->shouldReceive('getHeaders')->andReturn(['Authorization' => 'Bearer token']);
+
+        $mockTokenProvider = m::mock(Provider::class)
+            ->expects('getToken')->withNoArgs()->andReturn('token');
+
+
+        $mockSl = m::mock(ServiceLocatorInterface::class);
+        var_dump($mockSl);
         $mockSl->shouldReceive('get')->with('config')->andReturn($config);
+        $mockSl->shouldReceive('get')->with(RestClient::class)->andReturn($restClient);
+        $mockSl->shouldReceive('build')->with(Provider::class, $oauth2)->andReturn($mockTokenProvider);
 
         $sut = new InrClientFactory();
 
@@ -49,9 +64,12 @@ class InrClientFactoryTest extends TestCase
         $wrapper = $restClient->getAdapter();
         $curl = $wrapper->getAdapter();
 
+
         $this->assertInstanceOf(InrClientInterface::class, $service);
         $this->assertInstanceOf(RestClient::class, $restClient);
         $this->assertInstanceOf(ClientAdapterLoggingWrapper::class, $wrapper);
         $this->assertInstanceOf(Curl::class, $curl);
+        $this->assertEquals('Bearer token', $restClient->getHeader('Authorization'));
+
     }
 }
