e827e85c065906ffdd3f5e356635e399777da7bd
William Shelley <william.shelley@dvsa.gov.uk>
2025-02-26 15:41:25 +0000

    fix: more tidying and improvements

--- infra/docker/search/entrypoint.sh
+++ infra/docker/search/entrypoint.sh
@@ -5,22 +5,22 @@
 # Templates
 declare -a templates=("address" "application" "busreg" "case" "irfo" "licence" "person" "psv_disc" "publication" "user" "vehicle_current" "vehicle_removed")
 
-# To do: update settings file settings.sh using env vars
-
 # Install logstash pipeline configs
 
 for i in "${templates[@]}"
 do
     # Create /usr/share/logstash/pipeline/$i.conf
-    /usr/share/logstash/config/generate-conf.sh -c /usr/share/logstash/config/settings.sh -i $i
+    /usr/share/logstash/config/generate-conf.sh -c /usr/share/logstash/config/settings.sh -i $i 
     
     # Add to pipelines.yaml
-    # echo -e '- pipeline.id: $i\n  path.config: \"/usr/share/logstash/pipline/${i}.conf\"' >> "/usr/share/logstash/config/pipelines.yml"
+    echo -e "- pipeline.id: $i\n  path.config: \"/usr/share/logstash/pipline/${i}.conf\"" >> "/usr/share/logstash/pipeline/pipelines.yml"
 
-
-    # /usr/share/logstash/build.sh -c /usr/share/logstash/config/settings.sh -d 60 -l $i
+    # build indexes
+    /usr/share/logstash/build.sh -c /usr/share/logstash/config/settings.sh -d 60 -l $i
 done
 
+# Start logstash
+#/usr/local/bin/docker-entrypoint
 # keep container running
 tail -f /dev/null
     
