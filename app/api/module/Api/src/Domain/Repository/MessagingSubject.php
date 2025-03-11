6e7c54393762d99b3d7a8713c85fa925c1f6ccbc
ahmed7752 <ahmed.mohamed2@dvsa.gov.uk>
2025-03-11 12:39:07 +0000

    feat: filter by active categories

--- app/api/module/Api/src/Domain/Repository/MessagingSubject.php
+++ app/api/module/Api/src/Domain/Repository/MessagingSubject.php
@@ -2,9 +2,20 @@
 
 namespace Dvsa\Olcs\Api\Domain\Repository;
 
+use Doctrine\ORM\QueryBuilder;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
 use Dvsa\Olcs\Api\Entity\Messaging\MessagingSubject as Entity;
 
 class MessagingSubject extends AbstractRepository
 {
     protected $entity = Entity::class;
+
+    /**
+    * Apply filters
+    */
+    protected function applyListFilters(QueryBuilder $qb, QueryInterface $query)
+    {
+        $qb->Where($qb->expr()->eq($this->alias . '.isActive',':isActive'))
+            ->setParameter('isActive', $query->getIsActive());
+    }
 }
