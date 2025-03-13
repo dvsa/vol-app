f33c2ed9ac97e76969409ce75a00c1b972f4a0a1
Ahmed Mohamed <ahmed.mohamed2@dvsa.gov.uk>
2025-03-13 14:05:22 +0000

    feat: filter messaging subjects by active categories VOL-6069 (#693)
    * feat: filter by active categories
    
    * Update app/api/module/Api/src/Domain/Repository/MessagingSubject.php
    
    Co-authored-by: Ian Lindsay <6673081+ilindsay@users.noreply.github.com>
    
    * feat: use the correct get method
    
    * fix: onlyActive should be getOnlyActive
    
    * chore: bump olcs-common and olcs-transfer to latest
    
    ---------
    
    Co-authored-by: Ian Lindsay <6673081+ilindsay@users.noreply.github.com>
    Co-authored-by: Ian Lindsay <ian@hemera-business-services.co.uk>

--- app/api/module/Api/src/Domain/Repository/MessagingSubject.php
+++ app/api/module/Api/src/Domain/Repository/MessagingSubject.php
@@ -2,9 +2,22 @@
 
 namespace Dvsa\Olcs\Api\Domain\Repository;
 
+use Doctrine\ORM\QueryBuilder;
+use Dvsa\Olcs\Transfer\Query\QueryInterface;
 use Dvsa\Olcs\Api\Entity\Messaging\MessagingSubject as Entity;
 
 class MessagingSubject extends AbstractRepository
 {
     protected $entity = Entity::class;
+
+    /**
+    * Apply filters
+    */
+    protected function applyListFilters(QueryBuilder $qb, QueryInterface $query)
+    {
+        if (method_exists($query, 'getOnlyActive') && $query->getOnlyActive()) {
+            $qb->Where($qb->expr()->eq($this->alias . '.isActive',':isActive'))
+                ->setParameter('isActive', 1);
+        }
+    }
 }
