c93b8ddb2ea712b92b2abf1650e2da30a67fe5c3
William Shelley <william.shelley@dvsa.gov.uk>
2025-03-20 09:55:48 +0000

    fix: updated settings

--- infra/docker/search/config/settings.sh
+++ infra/docker/search/config/settings.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 ELASTIC_HOST=elasticsearch-dev.olcs.mgt.mtpdvsa
-DBHOST=testhost #"$DB_HOST"
-DBNAME=testname #"$DB_NAME"
-DBUSER=testuser #"$DB_USER"
-DBPASSWORD=testpass #"$DB_PASSWORD"
+DBHOST="$DB_HOST"
+DBNAME="$DB_NAME"
+DBUSER="$DB_USER"
+DBPASSWORD="$DB_PASSWORD"
 LOGSTASH_PATH=/etc/logstash/
