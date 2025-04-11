96fec9dd0975f2958153944f4fdc7e58b8679e7e
Andrew Newton <andy.newton@dvsa.gov.uk>
2025-04-11 10:50:04 +0100

    chore: specify null provider to resolve tflint errors

--- infra/terraform/environments/prep/provider.tf
+++ infra/terraform/environments/prep/provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.72.1"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
+    }
   }
 
   required_version = ">= 1.0"
