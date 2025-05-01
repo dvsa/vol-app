488cc9a326abf519d6381ee7e7bbaa46d87321e6
Andrew Newton <andy.newton@dvsa.gov.uk>
2025-05-01 15:40:13 +0100

    Revert "fix: correct lb logic"
    This reverts commit a9d6b3595e4f9e778d807501b10db4f19d29c607.

--- infra/terraform/modules/service/ecs.tf
+++ infra/terraform/modules/service/ecs.tf
@@ -132,13 +132,13 @@ module "ecs_service" {
       memory_reservation = 100
     }
   }
-  load_balancer = var.services[each.key].listener_rule_enable ? {
+  load_balancer = {
     service = {
       target_group_arn = aws_lb_target_group.this[each.key].arn
       container_name   = each.key
       container_port   = 8080
     }
-  } : {}
+  }
 
   create_security_group = false
   security_group_ids    = var.services[each.key].security_group_ids
