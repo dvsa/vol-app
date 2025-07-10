09a0e837a3b785f7a4c86abf742b8562d0460f4f
William Shelley <william.shelley@dvsa.gov.uk>
2025-07-10 09:34:55 +0100

    fix: comment schedules

--- infra/terraform/environments/prod/backend.tf
+++ infra/terraform/environments/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "vol-app-146997448015-terraform-state"
-    dynamodb_table = "vol-app-146997448015-prep-terraform-state-lock"
+    dynamodb_table = "vol-app-146997448015-prod-terraform-state-lock"
     encrypt        = true
     key            = "prod.tfstate"
     region         = "eu-west-1"
