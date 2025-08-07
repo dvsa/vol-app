0392f95039bd38316529f9757c1f38eeb1b64505
William Shelley <william.shelley@dvsa.gov.uk>
2025-08-07 11:31:20 +0100

    fix: syntax

--- infra/terraform/modules/service/ecs.tf
+++ infra/terraform/modules/service/ecs.tf
@@ -193,13 +193,13 @@ module "ecs_service" {
       memory_reservation = 100
     }
   }
-  load_balancer = (
+  load_balancer = [
     {
       target_group_arn = aws_lb_target_group.this[each.key].arn
       container_name   = each.key
       container_port   = 8080
     }
-  )
+  ]
 
   create_security_group = false
   security_group_ids    = var.services[each.key].security_group_ids
