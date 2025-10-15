7982b3c02f62f0cbfe2a8c2c6dced68c96cae644
William Shelley <william.shelley@dvsa.gov.uk>
2025-10-15 10:22:12 +0100

    fix: modified sas script

--- infra/docker/cli/scripts/sas_mi_extract.sh
+++ infra/docker/cli/scripts/sas_mi_extract.sh
@@ -36,7 +36,7 @@ mysqldump_bin=$(which mysqldump)
 mysql_bin=$(which mysql)
 
 TABLES=$(/usr/bin/mysql -h ${READDB_HOST} -u olcsbatch -p ${BATCH_DB_PASSWORD} --skip-column-names -e "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA=database() AND TABLE_TYPE='BASE TABLE' AND TABLE_NAME NOT LIKE '%_hist' AND TABLE_NAME NOT IN ('translation_key_category_link', 'translation_key', 'translation_key_location', 'translation_key_tag_link','DR_EXPECTED_NULLS', 'translation_key_text','replacement','replacement_tag_link','replacement_category_link','dr_table_counts','dr_expected_deletes')" OLCS_RDS_OLCSDB )
-/usr/bin/mysqldump -h ${READDB_HOST} -u olcsbatch -p ${BATCH_DB_PASSWORD} --skip-triggers --set-gtid-purged=OFF --no-create-db --no-tablespaces OLCS_RDS_OLCSDB ${TABLES} > /mnt/data/olcsdump/olcsdump-${DATE}.dmp
+/usr/bin/mysqldump -h ${READDB_HOST} -u olcsbatch -p"${BATCH_DB_PASSWORD}" --skip-triggers --no-create-db --no-tablespaces OLCS_RDS_OLCSDB ${TABLES} > /mnt/data/olcsdump/olcsdump-${DATE}.dmp
 
 if [ $? -ne 0 ]; then
     echoerr "ERROR: Database dump failed"
