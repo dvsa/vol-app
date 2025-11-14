2b17dc218ded19553d80ae827e16f998195fb1bf
Andrew Newton <fibble@users.noreply.github.com>
2025-11-14 12:12:09 +0000

    fix: revert global misedit

--- app/api/config/autoload/config.global.php
+++ app/api/config/autoload/config.global.php
@@ -281,28 +281,28 @@ return [
         ]
     ]),
     'mail' => ($isProductionAccount && \Aws\Credentials\CredentialProvider::shouldUseEcs())
-        ? [
-            'type' => '\Laminas\Mail\Transport\Smtp',
-            'options' => [
-                'name' => '%olcs_email_host%',
-                'host' => '%olcs_email_host%',
+    ? [
+        'type' => '\Laminas\Mail\Transport\Smtp',
+        'options' => [
+            'name' => '%olcs_email_host%',
+            'host' => '%olcs_email_host%',
+            'port' => '%olcs_email_port%',
+            'connection_config' => [
+                'username' => 'null',
+                'password' => 'null',
                 'port' => '%olcs_email_port%',
-                'connection_config' => [
-                    'username' => 'null',
-                    'password' => 'null',
-                    'port' => '%olcs_email_port%',
-                ],
             ],
-        ]
-        : ($isProductionAccount ? [] : [
-            'type' => \Dvsa\Olcs\Email\Transport\MultiTransport::class,
-            'options' => [
-                'transport' => [
-                    ['type' => 'SMTP', 'options' => ['name' => '%olcs_email_host%', 'host' => '%olcs_email_host%', 'port' => '%olcs_email_port%']],
-                    ['type' => \Dvsa\Olcs\Email\Transport\S3File::class, 'options' => ['bucket' => 'devapp-olcs-pri-olcs-autotest-s3', 'key' => '%domain%/email']],
-                ]
-            ],
-        ]),
+        ],
+    ]
+    : ($isProductionAccount ? [] : [
+        'type' => \Dvsa\Olcs\Email\Transport\MultiTransport::class,
+        'options' => [
+            'transport' => [
+                ['type' => 'SMTP', 'options' => ['name' => '%olcs_email_host%', 'host' => '%olcs_email_host%', 'port' => '%olcs_email_port%']],
+                ['type' => \Dvsa\Olcs\Email\Transport\S3File::class, 'options' => ['bucket' => 'devapp-olcs-pri-olcs-autotest-s3', 'key' => '%domain%/email']],
+            ]
+        ],
+    ]),
 
     'mailboxes' => [
         // IMAP connection to a the mailbox for reading inspection request emails
