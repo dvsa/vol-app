6d7f4bb316d7cc093e9f78a305fa8cb7c6b403ae
Teja Vaddala <teja.vaddala@dvsa.gov.uk>
2026-03-04 21:37:38 +0000

    feat: added new bookmarks

---
+++ app/api/module/Api/src/Service/Document/Bookmark/Alias/LetterDateAdd21Days02.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dvsa\Olcs\Api\Service\Document\Bookmark\Alias;
+
+use Dvsa\Olcs\Api\Service\Document\Bookmark\LetterDateAdd21Days;
+
+class LetterDateAdd21Days02 extends LetterDateAdd21Days
+{
+ //
+}
