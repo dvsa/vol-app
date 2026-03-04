979d3d93b76901071c11e64219b193f8324ef694
Teja Vaddala <teja.vaddala@dvsa.gov.uk>
2026-03-04 22:34:54 +0000

    feat: added doc blocks

--- app/api/module/Api/src/Service/Document/Bookmark/Alias/LetterDateAdd21Days01.php
+++ app/api/module/Api/src/Service/Document/Bookmark/Alias/LetterDateAdd21Days01.php
@@ -4,6 +4,11 @@ namespace Dvsa\Olcs\Api\Service\Document\Bookmark\Alias;
 
 use Dvsa\Olcs\Api\Service\Document\Bookmark\LetterDateAdd21Days;
 
+/**
+ * Alias of LetterDateAdd21Days
+ *
+ * @author Teja Vaddala <teja.vaddala@dvsa.gov.uk>
+ */
 class LetterDateAdd21Days01 extends LetterDateAdd21Days
 {
  //
