65abeff56b1d85b3ff5d0383ffa836bf4391ebb1
Teja Vaddala <teja.vaddala@dvsa.gov.uk>
2026-03-05 11:30:39 +0000

    feat: added code review fix

--- app/api/module/Cli/src/Domain/CommandHandler/LastTmLetter.php
+++ app/api/module/Cli/src/Domain/CommandHandler/LastTmLetter.php
@@ -122,7 +122,7 @@ final class LastTmLetter extends AbstractCommandHandler implements EmailAwareInt
     }
 
     /**
-     * @return array|null
+     * @return int|null
      */
     private function generateDocuments(LicenceEntity $licence, TmlEntity $tml): ?int
     {
