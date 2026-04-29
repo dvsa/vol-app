a1331effc0fcb58d73811ae8f25ed5818c4d948a
William Shelley <william.shelley@dvsa.gov.uk>
2026-04-29 12:06:51 +0100

    fix: add cluster id

--- infra/terraform/modules/service/batch.tf
+++ infra/terraform/modules/service/batch.tf
@@ -152,6 +152,10 @@ locals {
           value = "${var.environment}-aurora-olcsdb-reader"
         },
         {
+          name  = "DBCLUSTER_ID"
+          value = "${var.environment}-aurora-olcsdb-cluster"
+        },
+        {
           name  = "READDB_NAME"
           value = "OLCS_RDS_OLCSDB"
         },
