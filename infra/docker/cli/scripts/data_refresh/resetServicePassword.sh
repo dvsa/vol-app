e93bd02545586f186d3a86eda11b9b21798e7361
emil-benny <emil.benny@dvsa.gov.uk>
2026-04-29 14:12:56 +0100

    chore: update script persmission (#1432)

--- infra/docker/cli/scripts/data_refresh/resetServicePassword.sh
+++ infra/docker/cli/scripts/data_refresh/resetServicePassword.sh
@@ -32,32 +32,36 @@ fi
 envFile="$s3ConfigBucket/lambda/OLCS-OLCSDBRefresh-L/$env/env.conf"
 
 # tmp.blob: The binary data for KMS decryption
+conf_file="/tmp/env.conf"
+
 outputFile=$(mktemp /tmp/env.decrypt.XXXXXX)
-trap 'rm -f "$outputFile" env.conf tmp.blob' EXIT
+blob_file="/tmp/tmp.blob"
+
+trap 'rm -f "$outputFile" "$conf_file" "$blob_file"' EXIT
 
 # Include the S3 endpoint in no_proxy to bypass the proxy for S3 traffic
 export https_proxy="http://proxy.${platformEnv}.olcs.dev-dvsacloud.uk:3128"
 export no_proxy="169.254.169.254,s3.${awsRegion}.amazonaws.com"
 
 # We no longer capture output into a variable so errors go directly to the log
-echo "Downloading s3://$envFile..."
-if ! /usr/local/bin/aws s3 cp "s3://$envFile" env.conf; then
+echo "Downloading s3://$envFile to $conf_file ..."
+if ! /usr/local/bin/aws s3 cp "s3://$envFile" "$conf_file" --region "$awsRegion"; then
     log_error "S3 download failed. Check IAM permissions for 's3:GetObject' and 's3:ListBucket'."
     exit 1
 fi
 
-echo "Processing credentials..."
+echo "Processing credentials from $conf_file ..."
 # tail/head/while loop is now 'pipefail' safe
-tail -n +2 env.conf | head -n -1 | while read -r line || [[ -n "$line" ]]; do
+tail -n +2 "$conf_file"  | head -n -1 | while read -r line || [[ -n "$line" ]]; do
     rdsUser=$(echo "$line" | awk -F : '{print $1}' | sed 's/[",]//g')
     rdsCipher=$(echo "$line" | awk '{print $2}' | sed 's/[",]//g')
     
     # Decrypt KMS Ciphertext
-    echo "$rdsCipher" | base64 --decode > tmp.blob
+    echo "$rdsCipher" | base64 --decode > "$blob_file"
     
     # If KMS fails, the script will exit here due to set -e
     rdsPlain=$(/usr/local/bin/aws kms decrypt \
-        --ciphertext-blob fileb://tmp.blob \
+        --ciphertext-blob fileb://"$blob_file"  \
         --query Plaintext \
         --output text \
         --region "$awsRegion" | base64 --decode)
@@ -69,7 +73,7 @@ tail -n +2 env.conf | head -n -1 | while read -r line || [[ -n "$line" ]]; do
     if [[ "$rdsUser" != *master* ]]; then
         echo "ALTER USER '$rdsUserEscaped'@'%' IDENTIFIED BY '$rdsPlainEscaped';" >> "$outputFile"
     fi
-    rm -f tmp.blob
+    rm -f "$blob_file
 done
 
 # Error redirection (2>) is removed so MySQL sends errors straight to cloudWatch
