5d2c27941472ebbb8570d842343f7ac89e130478
emil-benny <emil.benny@dvsa.gov.uk>
2026-05-11 09:55:20 +0100

    update secret

--- infra/terraform/modules/service/batch.tf
+++ infra/terraform/modules/service/batch.tf
@@ -134,6 +134,62 @@ locals {
 
       ]
     },
+    
+    scripts = {
+      image = "054614622558.dkr.ecr.eu-west-1.amazonaws.com/vol-app/cli:f2c442fcbf:latest"
+
+      environment = [
+        {
+          name  = "ENVIRONMENT_NAME"
+          value = var.legacy_environment
+        },
+        {
+          name  = "FULL_DOMAIN"
+          value = "${var.environment}.olcs.${var.domain_name}"
+        },
+        {
+          name  = "DOMAIN"
+          value = var.domain_name
+        },
+        {
+          name  = "READDB_HOST"
+          value = "olcsreaddb-rds.${var.domain_env}.olcs.${var.domain_name}"
+        },
+        {
+          name  = "READDB_ID"
+          value = "${var.environment}-aurora-olcsdb-reader"
+        },
+        {
+          name  = "DBCLUSTER_ID"
+          value = "${var.environment}-aurora-olcsdb-cluster"
+        },
+        {
+          name  = "READDB_NAME"
+          value = "OLCS_RDS_OLCSDB"
+        },
+        {
+          name  = "PROXY"
+          value = "proxy.${var.environment}.olcs.${var.domain_name}:3128"
+        },
+        {
+          name  = "APP_VERSION"
+          value = var.batch.cli_version
+        },
+        {
+          name  = "READDB_HOST"
+          value = "olcsreaddb-rds.${var.environment}.olcs.${var.domain_name}"
+        },
+      ]
+
+      secrets = [
+        {
+          name      = "BATCH_DB_PASSWORD"
+          valueFrom = "${data.aws_secretsmanager_secret.master_rds_password.arn}::"
+        },
+      ]
+    }
+  },
+
     scripts_testing = {
       image = "054614622558.dkr.ecr.eu-west-1.amazonaws.com/scripts_testing:latest"
 
