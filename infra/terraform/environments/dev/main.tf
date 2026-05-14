0c196eacc1534ffe4bdae3b214e2af8cfb109960
emil-benny <emil.benny@dvsa.gov.uk>
2026-05-14 15:33:37 +0100

    Fix/master rds password dbanon (#1451)
    * fix: adding in logic to pull rds master password direction from environment variable for batch job
    
    * update secret
    
    * Update batch.tf
    
    * Update batch.tf
    
    * chore: update secret
    
    * update secret
    
    * update secret
    
    * update fmt
    
    * update secret
    
    * update secret
    
    * docs: update Terraform docs
    
    * update secret
    
    * Update batch.tf
    
    * update fmt
    
    * update fmt
    
    * update secret
    
    * docs: update Terraform docs
    
    * update secrets
    
    * Update resetServicePassword.sh
    
    ---------
    
    Co-authored-by: laurence.barker@dvsa.gov.uk <laurence.barker@dvsa.gov.uk>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -396,7 +396,7 @@ module "service" {
 
     cli_repository       = data.aws_ecr_repository.this["cli"].repository_url
     liquibase_repository = data.aws_ecr_repository.sservice["liquibase"].repository_url
-    api_secret_file      = data.aws_secretsmanager_secret.this["api"].arn
+    api_secret_file      = data.aws_secretsmanager_secret.this["api"].arn                         
 
     task_iam_role_statements = local.task_iam_role_statements
 
