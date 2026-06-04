ecc46dd3e21846f4c4a8b392fb1aa90b141cfa43
emil-benny <emil.benny@dvsa.gov.uk>
2026-06-04 14:52:48 +0100

    chore: output arn

--- infra/terraform/environments/dev/outputs.tf
+++ infra/terraform/environments/dev/outputs.tf
@@ -17,3 +17,8 @@ output "deployed_cli_image_tag" {
 output "deployed_assets_version" {
   value = null_resource.deployed_versions.triggers["deployed_assets_version"]
 }
+
+output "api_tasks_iam_role_arn" {
+  description = "IAM role ARN for the dev api ECS task role"
+  value       = module.service.api_tasks_iam_role_arn
+}
