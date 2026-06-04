30e33aebea9d0ca999c731e62b69a11649ccff6d
Teja Vaddala <teja.vaddala@dvsa.gov.uk>
2026-06-04 15:45:56 +0100

    feat: tidy up the format

--- app/api/module/Api/src/Entity/Application/Application.php
+++ app/api/module/Api/src/Entity/Application/Application.php
@@ -2135,8 +2135,10 @@ class Application extends AbstractApplication implements ContextProviderInterfac
             'PsvSmallConditions' => $this->psvSmallVhlConfirmation !== null,
             'PsvMainOccupationUndertakings' => $this->isMainOccupationUndertakingsSectionCompleted(),
             'PsvSmallPartWritten' => $this->isWrittenEvidenceSectionCompleted(),
-            'PsvDocumentaryEvidenceSmall' => $this->smallVehicleEvidenceUploaded !== null  && $this->smallVehicleEvidenceUploaded !== self::FINANCIAL_EVIDENCE_UPLOAD_LATER,
-            'PsvDocumentaryEvidenceLarge' => $this->occupationEvidenceUploaded !== null && $this->occupationEvidenceUploaded !== self::FINANCIAL_EVIDENCE_UPLOAD_LATER,
+            'PsvDocumentaryEvidenceSmall' => $this->smallVehicleEvidenceUploaded !== null
+                && $this->smallVehicleEvidenceUploaded !== self::FINANCIAL_EVIDENCE_UPLOAD_LATER,
+            'PsvDocumentaryEvidenceLarge' => $this->occupationEvidenceUploaded !== null
+                && $this->occupationEvidenceUploaded !== self::FINANCIAL_EVIDENCE_UPLOAD_LATER,
             default => throw new \RuntimeException('There is no validation for this section: ' . $applicationSection),
         };
     }
