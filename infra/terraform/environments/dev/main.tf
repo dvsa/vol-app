8f4c2ebeff7acd80d878466fb1ecf0d02e8ba17a
emil-benny <emil.benny@dvsa.gov.uk>
2026-06-09 11:01:22 +0100

    chore: revert

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -100,7 +100,7 @@ locals {
         "s3:ListBucket",
         "s3:DeleteObject"
       ]
-      resource = [
+      resources = [
         "arn:aws:s3:::devapp-shd-pri-olcsci-build-s3",
         "arn:aws:s3:::devapp-shd-pri-olcsci-build-s3/*"
       ]
