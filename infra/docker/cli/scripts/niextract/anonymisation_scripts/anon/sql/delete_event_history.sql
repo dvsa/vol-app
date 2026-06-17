f7c1a9f2816178f56b69a407606937e90e26a84b
laurence.barker@dvsa.gov.uk <laurence.barker@dvsa.gov.uk>
2026-06-17 10:03:46 +0100

    fix: change event history deletion logic to speed this up and stop conflict with other scripts

--- infra/docker/cli/scripts/niextract/anonymisation_scripts/anon/sql/delete_event_history.sql
+++ infra/docker/cli/scripts/niextract/anonymisation_scripts/anon/sql/delete_event_history.sql
@@ -4,8 +4,19 @@
 #
 ################################################################
 
-SELECT CONCAT(now(), ' delete old event_history...') as '';
+SELECT CONCAT(now(), ' Fast optimizing event_history via table swap...') as '';
 
-DELETE FROM event_history WHERE event_datetime < DATE_SUB(NOW(), INTERVAL 6 YEAR);
+-- Create an identical empty structure of the table
+CREATE TABLE event_history_new LIKE event_history;
 
-SELECT CONCAT(now(), ' ...delete old event_history complete.') as '';
+-- Copy only the recent 6 years of data (Bulk INSERT is exponentially faster than DELETE)
+INSERT INTO event_history_new
+SELECT * FROM event_history WHERE event_datetime >= DATE_SUB(NOW(), INTERVAL 6 YEAR);
+
+-- Swap the tables instantly in the engine metadata
+RENAME TABLE event_history TO event_history_old, event_history_new TO event_history;
+
+-- Drop the old massive table to free up AWS cloud storage space
+DROP TABLE event_history_old;
+
+SELECT CONCAT(now(), ' ...event_history optimization complete.') as '';
