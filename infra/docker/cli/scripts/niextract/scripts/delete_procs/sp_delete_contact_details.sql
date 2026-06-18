031fc3499672beb085c6faeb29bfb266f2a8cb87
laurence.barker@dvsa.gov.uk <laurence.barker@dvsa.gov.uk>
2026-06-18 11:17:19 +0100

    chore: optimise sp_delete_delete_contact_details

--- infra/docker/cli/scripts/niextract/scripts/delete_procs/sp_delete_contact_details.sql
+++ infra/docker/cli/scripts/niextract/scripts/delete_procs/sp_delete_contact_details.sql
@@ -1,4 +1,3 @@
-
 DROP PROCEDURE IF EXISTS sp_delete_contact_details;
 DELIMITER $$
 CREATE PROCEDURE sp_delete_contact_details()
@@ -29,49 +28,36 @@ BEGIN
     PRIMARY KEY (`id`) );
   
     INSERT INTO tmpCD (id)
-    select id from contact_details;
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT complainant_contact_details_id FROM complaint);
- 
-    DELETE FROM tmpCD  
-    WHERE id IN (SELECT DISTINCT correspondence_cd_id FROM licence);
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT establishment_cd_id FROM licence);
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT transport_consultant_cd_id FROM licence);
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT contact_details_id FROM opposer);
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT contact_details_id FROM organisation);
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT irfo_contact_details_id FROM organisation);
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT contact_details_id FROM private_hire_licence);
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT requestors_contact_details_id FROM statement);
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT contact_details_id FROM tm_employment);
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT contact_details_id FROM traffic_area);
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT home_cd_id FROM transport_manager);
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT work_cd_id FROM transport_manager);
-
-    DELETE FROM tmpCD
-    WHERE id IN (SELECT DISTINCT contact_details_id FROM workshop);
+    SELECT c.id 
+    FROM contact_details c
+    LEFT JOIN complaint comp               ON c.id = comp.complainant_contact_details_id
+    LEFT JOIN licence l1                   ON c.id = l1.correspondence_cd_id
+    LEFT JOIN licence l2                   ON c.id = l2.establishment_cd_id
+    LEFT JOIN licence l3                   ON c.id = l3.transport_consultant_cd_id
+    LEFT JOIN opposer o                    ON c.id = o.contact_details_id
+    LEFT JOIN organisation org1            ON c.id = org1.contact_details_id
+    LEFT JOIN organisation org2            ON c.id = org2.irfo_contact_details_id
+    LEFT JOIN private_hire_licence phl     ON c.id = phl.contact_details_id
+    LEFT JOIN statement s                  ON c.id = s.requestors_contact_details_id
+    LEFT JOIN tm_employment tme            ON c.id = tme.contact_details_id
+    LEFT JOIN traffic_area ta              ON c.id = ta.contact_details_id
+    LEFT JOIN transport_manager tm1        ON c.id = tm1.home_cd_id
+    LEFT JOIN transport_manager tm2        ON c.id = tm2.work_cd_id
+    LEFT JOIN workshop w                   ON c.id = w.contact_details_id
+    WHERE comp.complainant_contact_details_id IS NULL
+      AND l1.correspondence_cd_id IS NULL
+      AND l2.establishment_cd_id IS NULL
+      AND l3.transport_consultant_cd_id IS NULL
+      AND o.contact_details_id IS NULL
+      AND org1.contact_details_id IS NULL
+      AND org2.irfo_contact_details_id IS NULL
+      AND phl.contact_details_id IS NULL
+      AND s.requestors_contact_details_id IS NULL
+      AND tme.contact_details_id IS NULL
+      AND ta.contact_details_id IS NULL
+      AND tm1.home_cd_id IS NULL
+      AND tm2.work_cd_id IS NULL
+      AND w.contact_details_id IS NULL;
 
     DROP TABLE IF EXISTS `tmpCDbatch`;
     CREATE TEMPORARY TABLE `tmpCDbatch` ( 
@@ -87,6 +73,8 @@ BEGIN
     SET @total:=0;
     SET @rowcount:=10000;
     
+    START TRANSACTION;
+
     WHILE(@rowcount = 10000) DO
 
         INSERT tmpCDbatch
@@ -102,7 +90,7 @@ BEGIN
         SET @total := @total + @rowcount;
     
         DELETE FROM tmpCD
-        WHERE id IN ( SELECT id from tmpCDbatch);
+        LIMIT 10000;
         
 
     
@@ -110,7 +98,12 @@ BEGIN
 
         truncate table tmpCDbatch;
         
+        COMMIT;
+        START TRANSACTION;
+        
     END WHILE;
+
+    COMMIT;
 
     DROP TABLE IF EXISTS `tmpCD`;
     DROP TABLE IF EXISTS `tmpCDbatch`;
@@ -119,7 +112,4 @@ BEGIN
     
 END
 $$
-
-
-  
-  
+DELIMITER ;
