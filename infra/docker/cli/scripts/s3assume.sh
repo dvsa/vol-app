10c893ad88f1f78d455bc9d74147634416790a80
laurence.barker@dvsa.gov.uk <laurence.barker@dvsa.gov.uk>
2026-06-19 10:07:19 +0100

    chore: improve visibility to prevent silent failures

--- infra/docker/cli/scripts/s3assume.sh
+++ infra/docker/cli/scripts/s3assume.sh
@@ -21,7 +21,14 @@ export NO_PROXY=169.254.169.254,169.254.170.2,localhost,127.0.0.1,.s3.eu-west-1.
 region="${AWS_DEFAULT_REGION:-${AWS_REGION:-eu-west-1}}"
 ec2_instance_id="ecs-task-$(date +%s)"
 
-creds=`/usr/local/bin/aws sts assume-role --role-arn $1 --role-session-name $ec2_instance_id --region=$region --external-id $2 | /usr/bin/jq -r '.Credentials'`
+creds=`/usr/local/bin/aws sts assume-role --role-arn $1 --role-session-name $ec2_instance_id --region=$region --external-id $2 2>/tmp/sts_error.txt` || {
+  echo "ERROR: AWS STS Assume-Role failed!"
+  echo "Reason: $(cat /tmp/sts_error.txt)"
+  rm -f /tmp/sts_error.txt
+  exit 7
+}
+rm -f /tmp/sts_error.txt
+
 export AWS_ACCESS_KEY_ID=`echo $creds | /usr/bin/jq -r '.AccessKeyId'`
 export AWS_SECRET_ACCESS_KEY=`echo $creds | /usr/bin/jq -r '.SecretAccessKey'`
 export AWS_SESSION_TOKEN=`echo $creds | /usr/bin/jq -r '.SessionToken'`
