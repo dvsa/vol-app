ccb716227511c03d236fa4c7b59684e715e7f1f3
waheed ahmed <mohammed.ahmed2@dvsa.gov.uk>
2026-06-19 10:45:46 +0100

    fix: revert CanAccessFee.php to original form

--- app/api/module/Api/src/Domain/Validation/Validators/CanAccessFee.php
+++ app/api/module/Api/src/Domain/Validation/Validators/CanAccessFee.php
@@ -2,18 +2,10 @@
 
 namespace Dvsa\Olcs\Api\Domain\Validation\Validators;
 
-use Dvsa\Olcs\Api\Entity\Fee\Fee;
-
 /**
  * Can Access Fee
  */
 class CanAccessFee extends AbstractCanAccessEntity
 {
     protected $repo = 'Fee';
-
-    #[\Override]
-    public function isValid($entityId)
-    {
-        protected $repo = 'Fee';
-    }
 }
