12f0abe028cbf585b3e133daef018a594a73d122
Teja Vaddala <teja.vaddala@dvsa.gov.uk>
2026-06-24 14:13:40 +0100

    feat: converted doctrine-entites-to-attributes

--- app/api/rector.php
+++ app/api/rector.php
@@ -5,8 +5,9 @@ use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
-    ->withPaths([__DIR__ . '/module/Api/src/Entity'])
+    ->withPaths([__DIR__ . '/module/Cli/src/Service/EntityGenerator'])
     ->withPhpVersion(PhpVersion::PHP_83)
     ->withSets([
         DoctrineSetList::DOCTRINE_ORM_219,
+        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
     ]);
