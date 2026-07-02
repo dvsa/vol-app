7c7d63bf40ee6035de619a1195d69c573a36c0d7
waheed ahmed <mohammed.ahmed2@dvsa.gov.uk>
2026-07-02 06:44:33 +0100

    fix: use new validators feebelongstolicence and feebelongstoapplication in validatorhelpers instead of putting logic in CanAccessFeeWithId.php

--- app/api/module/Api/src/Domain/Validation/Handlers/Misc/CanAccessFeeWithId.php
+++ app/api/module/Api/src/Domain/Validation/Handlers/Misc/CanAccessFeeWithId.php
@@ -5,18 +5,15 @@ namespace Dvsa\Olcs\Api\Domain\Validation\Handlers\Misc;
 use Dvsa\Olcs\Api\Domain\Validation\Handlers\AbstractHandler;
 use Dvsa\Olcs\Api\Domain\AuthAwareInterface;
 use Dvsa\Olcs\Api\Domain\AuthAwareTrait;
-use Dvsa\Olcs\Api\Domain\RepositoryManagerAwareInterface;
-use Dvsa\Olcs\Api\Domain\RepositoryManagerAwareTrait;
 
 /**
  * Can Access Fee With Id
  *
  * @author Alex Peshkov <alex.peshkov@valtech.co.uk>
  */
-class CanAccessFeeWithId extends AbstractHandler implements AuthAwareInterface, RepositoryManagerAwareInterface
+class CanAccessFeeWithId extends AbstractHandler implements AuthAwareInterface
 {
     use AuthAwareTrait;
-    use RepositoryManagerAwareTrait;
 
     /**
      * @inheritdoc
@@ -28,14 +25,12 @@ class CanAccessFeeWithId extends AbstractHandler implements AuthAwareInterface, 
             return false;
         }
 
-        $fee = $this->getEntity($dto);
-
         if ($dto->getLicenceId() !== null) {
-            return $fee->getLicence()?->getId() === $dto->getLicenceId();
+            return $this->feeBelongsToLicence($this->getid($dto), $dto->getLicenceId());
         }
 
         if ($dto->getApplicationId() !== null) {
-            return $fee->getApplication()?->getId() === $dto->getApplicationId();
+            return $this->feebelongsToApplication($this->getid($dto), $dto->getApplicationId());
         }
 
         return true;
@@ -44,10 +39,5 @@ class CanAccessFeeWithId extends AbstractHandler implements AuthAwareInterface, 
     protected function getId($dto)
     {
         return $dto->getId();
-    }
-
-    protected function getEntity($dto)
-    {
-        return $this->getRepo('Fee')->fetchById($this->getId($dto));
     }
 }
