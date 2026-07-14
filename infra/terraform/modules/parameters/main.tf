01a613fd10e90528d84ed8353151204f16448c14
William Shelley <william.shelley@dvsa.gov.uk>
2026-07-14 14:42:40 +0100

    fix: update lint version (#1646)

--- infra/terraform/modules/parameters/main.tf
+++ infra/terraform/modules/parameters/main.tf
@@ -1,5 +1,6 @@
 module "application_paramters" {
   source   = "terraform-aws-modules/ssm-parameter/aws"
+  version  = ">= 2.1.1"
   for_each = var.application_parameters
 
   name  = "/applicationparams/${var.environment}/${each.key}"
