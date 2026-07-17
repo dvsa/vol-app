d11ce5751559169d820a6c7226d1cbd2dff896d5
cmarstondvsa <chris.marston@dvsa.gov.uk>
2026-07-17 12:41:07 +0100

    fix: add reg workflows

--- infra/terraform/accounts/nonprod/main.tf
+++ infra/terraform/accounts/nonprod/main.tf
@@ -1,5 +1,5 @@
 locals {
-  environments = ["dev", "int"]
+  environments = ["dev", "int", "reg"]
 }
 
 # Imported as this provider has been created by the `vol-terraform` repository.
@@ -48,6 +48,7 @@ module "account" {
       "dvsa/vol-app:environment:account-nonprod",
       "dvsa/vol-app:environment:dev",
       "dvsa/vol-app:environment:int",
+      "dvsa/vol-app:environment:reg",
     ]
   )
 
