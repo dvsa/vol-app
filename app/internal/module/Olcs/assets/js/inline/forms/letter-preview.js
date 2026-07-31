f3acd1dc1c4210ff5343f93876abb86912ea70b9
Andy Newton <andy.newton@dvsa.gov.uk>
2026-07-31 12:21:44 +0100

    feat: let caseworkers reword a letter's to-dos
    Adds an Editable to-dos group to the preview sidebar and an editor pane per
    to-do, saving through the new instance-scoped command. A to-do shared by several
    of the letter's issues says so, since it renders once and an edit is easy to
    mistake for a narrower change than it is.

--- app/internal/module/Olcs/assets/js/inline/forms/letter-preview.js
+++ app/internal/module/Olcs/assets/js/inline/forms/letter-preview.js
@@ -3,12 +3,13 @@ OLCS.ready(function () {
 
   // Enable/disable "Continue to editor" based on checkbox selection
   $(
-    "input[name='sections[]'], input[name='appendices[]'], input[name='letterSections[]']",
+    "input[name='sections[]'], input[name='appendices[]'], input[name='letterSections[]'], input[name='todos[]']",
   ).on("change", function () {
     var anyChecked =
       $("input[name='sections[]']:checked").length > 0 ||
       $("input[name='appendices[]']:checked").length > 0 ||
-      $("input[name='letterSections[]']:checked").length > 0;
+      $("input[name='letterSections[]']:checked").length > 0 ||
+      $("input[name='todos[]']:checked").length > 0;
     $("#continue-to-editor").prop("disabled", !anyChecked);
     if (anyChecked) {
       $("#sections-error-summary").remove();
@@ -40,6 +41,12 @@ OLCS.ready(function () {
       selectedLetterSections.push($(this).val());
     });
 
+    // Collect selected to-do checkboxes
+    var selectedTodos = [];
+    $("input[name='todos[]']:checked").each(function () {
+      selectedTodos.push($(this).val());
+    });
+
     // Remove any existing error summary
     $("#sections-error-summary").remove();
 
@@ -47,14 +54,15 @@ OLCS.ready(function () {
     if (
       selectedSections.length === 0 &&
       selectedAppendices.length === 0 &&
-      selectedLetterSections.length === 0
+      selectedLetterSections.length === 0 &&
+      selectedTodos.length === 0
     ) {
       var errorHtml =
         '<div id="sections-error-summary" class="govuk-error-summary" data-module="govuk-error-summary" role="alert" tabindex="-1">' +
         '<h2 class="govuk-error-summary__title">There is a problem</h2>' +
         '<div class="govuk-error-summary__body">' +
         '<ul class="govuk-list govuk-error-summary__list">' +
-        "<li>Please select at least one section or appendix to edit</li>" +
+        "<li>Please select at least one item to edit</li>" +
         "</ul></div></div>";
 
       $btn.closest(".small-module").before(errorHtml);
@@ -62,7 +70,7 @@ OLCS.ready(function () {
       return;
     }
 
-    // Build URL with selected sections, appendices and letter instance sections
+    // Build URL with everything that was ticked
     var url = "/letter/edit?id=" + encodeURIComponent(letterInstanceId);
     selectedSections.forEach(function (sectionId) {
       url += "&sections[]=" + encodeURIComponent(sectionId);
@@ -72,6 +80,9 @@ OLCS.ready(function () {
     });
     selectedLetterSections.forEach(function (letterSectionId) {
       url += "&letterSections[]=" + encodeURIComponent(letterSectionId);
+    });
+    selectedTodos.forEach(function (todoId) {
+      url += "&todos[]=" + encodeURIComponent(todoId);
     });
 
     window.location.href = url;
