81698472c5c0b2052ea3b6e547e4a50c5b7438cd
Brandon Thomas-Davies <87308252+BrandonT95@users.noreply.github.com>
2026-08-11 12:22:32 +0100

    fix: fix circular dep

--- app/api/module/Api/config/module.config.php
+++ app/api/module/Api/config/module.config.php
@@ -542,7 +542,7 @@ return [
             ApiSrv\AddressHelper\AddressHelperService::class => ApiSrv\AddressHelper\AddressHelperServiceFactory::class,
 
             Aws\S3\S3Client::class => Dvsa\Olcs\Api\Service\S3\S3ClientFactory::class,
-            EventBridgeClient::class => Dvsa\Olcs\Api\Service\EventBridge\EventBridgeFactory::class,
+            EventBridgeClient::class => Dvsa\Olcs\AwsSdk\Factories\EventBridgeClientFactory::class,
             'default-cache' => \Dvsa\Olcs\Api\Service\Cache\DefaultCacheFactory::class,
             'doctrine-cache' => \Dvsa\Olcs\Api\Service\Cache\DefaultCacheFactory::class,
             'cache.redis.connection'
