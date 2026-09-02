ca357a2660964be69d0a0631741235c8cc6837f9
Brandon Thomas-Davies <87308252+BrandonT95@users.noreply.github.com>
2026-09-02 15:20:24 +0100

    fix: add missing listbucket policy

--- infra/terraform/modules/idp/iam.tf
+++ infra/terraform/modules/idp/iam.tf
@@ -90,6 +90,7 @@ data "aws_iam_policy_document" "classification_sm" {
       "s3:GetObject",
       "s3:GetObjectTagging",
       "s3:PutObjectTagging",
+      "s3:ListBucket",
     ]
     resources = [
       data.aws_s3_bucket.documents.arn,
@@ -337,6 +338,12 @@ data "aws_iam_policy_document" "ai_analysis_sm" {
     resources = ["${aws_s3_bucket.idp_output.arn}/*/analysis/*"]
   }
 
+  statement {
+    sid       = "S3ListOutputBucket"
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.idp_output.arn]
+  }
+
   # Bedrock Converse API to invoke the managed prompt.
   # Scoped to * to cover cross-region inference profile fan-out.
   statement {
@@ -447,7 +454,7 @@ data "aws_iam_policy_document" "analyse_financial_document_sm" {
   # Read S3 object tags to check for pre-existing Classification tag.
   statement {
     sid     = "S3ReadDocumentTags"
-    actions = ["s3:GetObjectTagging"]
+    actions = ["s3:GetObjectTagging", "s3:ListBucket"]
     resources = [
       "arn:aws:s3:::${var.documents_bucket_name}",
       "arn:aws:s3:::${var.documents_bucket_name}/*",
