b221f45756a0307261c9e15518d1dde51af093f8
Ian Lindsay <ian@hemera-business-services.co.uk>
2026-09-24 10:26:36 +0100

    refactor: share knowledge and experience controller logic between selfserve and internal
    Move the identical indexAction into
    Common\Controller\Lva\AbstractKnowledgeExperienceController, following the financial
    evidence section, and add controller and mapper tests.

--- app/internal/module/Olcs/src/Controller/Lva/Application/KnowledgeExperienceController.php
+++ app/internal/module/Olcs/src/Controller/Lva/Application/KnowledgeExperienceController.php
@@ -4,22 +4,20 @@ declare(strict_types=1);
 
 namespace Olcs\Controller\Lva\Application;
 
-use Common\Controller\Lva\AbstractController;
+use Common\Controller\Lva\AbstractKnowledgeExperienceController;
 use Common\Controller\Lva\Adapters\ApplicationKnowledgeExperienceAdapter;
-use Common\Data\Mapper\Lva\KnowledgeExperience;
 use Common\FormService\FormServiceManager;
 use Common\Service\Helper\FileUploadHelperService;
 use Common\Service\Helper\FlashMessengerHelperService;
 use Common\Service\Helper\RestrictionHelperService;
 use Common\Service\Helper\StringHelperService;
 use Common\Service\Script\ScriptFactory;
-use Dvsa\Olcs\Transfer\Command\Application\UpdateKnowledgeExperience;
 use Dvsa\Olcs\Utils\Translation\NiTextTranslation;
 use LmcRbacMvc\Service\AuthorizationService;
 use Olcs\Controller\Interfaces\ApplicationControllerInterface;
 use Olcs\Controller\Lva\Traits\ApplicationControllerTrait;
 
-final class KnowledgeExperienceController extends AbstractController implements
+final class KnowledgeExperienceController extends AbstractKnowledgeExperienceController implements
     ApplicationControllerInterface
 {
     use ApplicationControllerTrait;
@@ -31,87 +29,23 @@ final class KnowledgeExperienceController extends AbstractController implements
     public function __construct(
         NiTextTranslation $niTextTranslationUtil,
         AuthorizationService $authService,
-        protected FlashMessengerHelperService $flashMessengerHelper,
-        protected FormServiceManager $formServiceManager,
-        protected ScriptFactory $scriptFactory,
+        FlashMessengerHelperService $flashMessengerHelper,
+        FormServiceManager $formServiceManager,
+        ScriptFactory $scriptFactory,
         protected RestrictionHelperService $restrictionHelper,
         protected StringHelperService $stringHelper,
-        protected ApplicationKnowledgeExperienceAdapter $lvaAdapter,
-        protected FileUploadHelperService $uploadHelper,
+        ApplicationKnowledgeExperienceAdapter $lvaAdapter,
+        FileUploadHelperService $uploadHelper,
         protected $navigation
     ) {
         parent::__construct(
             $niTextTranslationUtil,
-            $authService
-        );
-    }
-
-    #[\Override]
-    public function indexAction()
-    {
-        $request = $this->getRequest();
-        $id = $this->getIdentifier();
-
-        if ($request->isPost()) {
-            $formData = KnowledgeExperience::mapFromPost(
-                $request->getPost()->getArrayCopy()
-            );
-        } else {
-            $formData = KnowledgeExperience::mapFromResult(
-                $this->lvaAdapter->getData((int) $id)
-            );
-        }
-
-        $form = $this->formServiceManager
-            ->get('lva-application-knowledge_experience')
-            ->getForm($request)
-            ->setData($formData);
-
-        $hasProcessedFiles = $this->processFiles(
-            $form,
-            'evidence->files',
-            function (array $file) use ($id): void {
-                $data = $this->lvaAdapter->getUploadMetaData(
-                    $file,
-                    (int) $id
-                );
-
-                $data['isExternal'] = $this->isExternal();
-
-                $this->uploadFile($file, $data);
-
-                $this->lvaAdapter->getData((int) $id, true);
-            },
-            fn(int $documentId): bool => $this->deleteFile($documentId),
-            fn(): array => $this->lvaAdapter->getDocuments((int) $id),
-            'evidence->uploadedFileCount'
-        );
-
-        if (
-            !$hasProcessedFiles
-            && $request->isPost()
-            && $form->isValid()
-        ) {
-            $data = KnowledgeExperience::mapFromForm($formData);
-            $data['id'] = $id;
-
-            $response = $this->handleCommand(
-                UpdateKnowledgeExperience::create($data)
-            );
-
-            if ($response->isOk()) {
-                return $this->completeSection('knowledge_experience');
-            }
-
-            $this->flashMessengerHelper
-                ->addCurrentErrorMessage('unknown-error');
-        }
-
-        $this->scriptFactory->loadFile('financial-evidence');
-
-        return $this->render(
-            'knowledge_experience',
-            $form
+            $authService,
+            $flashMessengerHelper,
+            $formServiceManager,
+            $scriptFactory,
+            $lvaAdapter,
+            $uploadHelper
         );
     }
 }
--- app/selfserve/module/Application/src/Controller/KnowledgeExperienceController.php
+++ app/selfserve/module/Application/src/Controller/KnowledgeExperienceController.php
@@ -4,21 +4,19 @@ declare(strict_types=1);
 
 namespace Dvsa\Olcs\Application\Controller;
 
-use Common\Controller\Lva\AbstractController;
+use Common\Controller\Lva\AbstractKnowledgeExperienceController;
 use Common\Controller\Lva\Adapters\ApplicationKnowledgeExperienceAdapter;
-use Common\Data\Mapper\Lva\KnowledgeExperience;
 use Common\FormService\FormServiceManager;
 use Common\Service\Helper\FileUploadHelperService;
 use Common\Service\Helper\FlashMessengerHelperService;
+use Common\Service\Helper\RestrictionHelperService;
+use Common\Service\Helper\StringHelperService;
 use Common\Service\Script\ScriptFactory;
-use Dvsa\Olcs\Transfer\Command\Application\UpdateKnowledgeExperience;
-use Olcs\Controller\Lva\Traits\ApplicationControllerTrait;
 use Dvsa\Olcs\Utils\Translation\NiTextTranslation;
 use LmcRbacMvc\Service\AuthorizationService;
-use Common\Service\Helper\StringHelperService;
-use Common\Service\Helper\RestrictionHelperService;
+use Olcs\Controller\Lva\Traits\ApplicationControllerTrait;
 
-final class KnowledgeExperienceController extends AbstractController
+final class KnowledgeExperienceController extends AbstractKnowledgeExperienceController
 {
     use ApplicationControllerTrait;
 
@@ -29,87 +27,22 @@ final class KnowledgeExperienceController extends AbstractController
     public function __construct(
         NiTextTranslation $niTextTranslationUtil,
         AuthorizationService $authService,
-        protected FlashMessengerHelperService $flashMessengerHelper,
-        protected FormServiceManager $formServiceManager,
-        protected ScriptFactory $scriptFactory,
+        FlashMessengerHelperService $flashMessengerHelper,
+        FormServiceManager $formServiceManager,
+        ScriptFactory $scriptFactory,
         protected RestrictionHelperService $restrictionHelper,
         protected StringHelperService $stringHelper,
-        protected ApplicationKnowledgeExperienceAdapter $lvaAdapter,
-        protected FileUploadHelperService $uploadHelper
+        ApplicationKnowledgeExperienceAdapter $lvaAdapter,
+        FileUploadHelperService $uploadHelper
     ) {
         parent::__construct(
             $niTextTranslationUtil,
-            $authService
-        );
-    }
-
-    #[\Override]
-    public function indexAction()
-    {
-        $request = $this->getRequest();
-        $id = $this->getIdentifier();
-
-        if ($request->isPost()) {
-            $formData = KnowledgeExperience::mapFromPost(
-                $request->getPost()->getArrayCopy()
-            );
-        } else {
-            $formData = KnowledgeExperience::mapFromResult(
-                $this->lvaAdapter->getData((int) $id)
-            );
-        }
-
-        $form = $this->formServiceManager
-            ->get('lva-application-knowledge_experience')
-            ->getForm($request)
-            ->setData($formData);
-
-        $hasProcessedFiles = $this->processFiles(
-            $form,
-            'evidence->files',
-            function (array $file) use ($id): void {
-                $data = $this->lvaAdapter->getUploadMetaData(
-                    $file,
-                    (int) $id
-                );
-
-                $data['isExternal'] = $this->isExternal();
-
-                $this->uploadFile($file, $data);
-
-                $this->lvaAdapter->getData((int) $id, true);
-            },
-            fn(int $documentId): bool => $this->deleteFile($documentId),
-            fn(): array => $this->lvaAdapter->getDocuments((int) $id),
-            'evidence->uploadedFileCount'
-        );
-
-        if (
-            !$hasProcessedFiles
-            && $request->isPost()
-            && $form->isValid()
-        ) {
-            $data = KnowledgeExperience::mapFromForm($formData);
-
-            $data['id'] = $id;
-
-            $response = $this->handleCommand(
-                UpdateKnowledgeExperience::create($data)
-            );
-
-            if ($response->isOk()) {
-                return $this->completeSection('knowledge_experience');
-            }
-
-            $this->flashMessengerHelper
-                ->addCurrentErrorMessage('unknown-error');
-        }
-
-        $this->scriptFactory->loadFile('financial-evidence');
-
-        return $this->render(
-            'knowledge_experience',
-            $form
+            $authService,
+            $flashMessengerHelper,
+            $formServiceManager,
+            $scriptFactory,
+            $lvaAdapter,
+            $uploadHelper
         );
     }
 }
